d8a8f4da6074aea3df52633391693b2386da9a5e
Mike Welsh <mwelsh@gmail.com>
2019-05-08 11:54:43 -0700

    Make read_ubits public

--- src/read.rs
+++ src/read.rs
@@ -277,7 +277,7 @@ impl<R: Read> Reader<R> {
         self.bit_index = 0;
     }
 
-    fn read_ubits(&mut self, num_bits: usize) -> Result<u32> {
+    pub fn read_ubits(&mut self, num_bits: usize) -> Result<u32> {
         let mut val = 0u32;
         for _ in 0..num_bits {
             val <<= 1;
@@ -286,7 +286,7 @@ impl<R: Read> Reader<R> {
         Ok(val)
     }
 
-    fn read_sbits(&mut self, num_bits: usize) -> Result<i32> {
+    pub fn read_sbits(&mut self, num_bits: usize) -> Result<i32> {
         if num_bits > 0 {
             self.read_ubits(num_bits)
                 .map(|n| (n as i32) << (32 - num_bits) >> (32 - num_bits))
