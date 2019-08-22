f5fbdbaf14c7c6cd52905ee152b61c145e1da3ae
Gus Caplan <me@gus.host>
2019-08-22 14:24:56 -0500

    various improvements

--- web-extension/rufflify.js
+++ web-extension/rufflify.js
@@ -14,29 +14,54 @@ const error = (...args) => console.error('[RUFFLE FLASH EMULATOR]', ...args);
 
 const Ruffle = init().then(() => Player);
 
-document.querySelectorAll('[type="application/x-shockwave-flash"]')
-  .forEach((object) => {
-    const source = object.getAttribute('data');
-    if (!source) {
-      return;
+function convertObject(object) {
+  const source = object.getAttribute('data');
+  if (!source) {
+    return;
+  }
+  log('Replacing flash content from', source);
+  const canvas = document.createElement('canvas');
+  object.parentElement.insertBefore(canvas, object);
+  object.remove();
+  fetch(new URL(source, window.location))
+    .then((r) => r.arrayBuffer())
+    .then((ab) => {
+      return Ruffle.then((Player) => {
+        const play = () => {
+          log('Running', source);
+          const r = Player.new(canvas, new Uint8Array(ab));
+        };
+        canvas.addEventListener('click', play, { once: true });
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = '#FFAD33';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'black';
+        ctx.fillText('Click to run flash content in Ruffle', 10, 10);
+      });
+    }).catch(error);
+}
+
+function scanForObjects() {
+  document.querySelectorAll('[type="application/x-shockwave-flash"]')
+    .forEach((object) => convertObject(object));
+}
+
+const observer = new MutationObserver((mutations) => {
+  let scan = false;
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length > 0) {
+      scan = true;
     }
-    log('Replacing flash content from', source);
-    fetch(new URL(source, window.location))
-      .then((r) => r.arrayBuffer())
-      .then((ab) => {
-        const canvas = document.createElement('canvas');
-        object.parentElement.appendChild(canvas);
-        object.remove();
-        return Ruffle.then((Player) => {
-          const play = () => {
-            log('Running', source);
-            const r = Player.new(canvas, new Uint8Array(ab));
-          };
-          canvas.addEventListener('click', play);
-          canvas.getContext('2d').fillText('Click to run flash content', 10, 10);
-        });
-      }).catch(error);
   });
+  if (scan) {
+    scanForObjects();
+  }
+});
+
+scanForObjects();
+
+observer.observe(document.body, { attributes: false, childList: true, subtree: true });
+
 `));
 script.type = 'module';
 document.body.appendChild(script);
