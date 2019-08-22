7a6169275d902cac9ef55b475b3d6d7f6062ce93
David Wendt <dcrkid@yahoo.com>
2019-08-22 13:18:51 -0600

    Tell webpack about the extension resource URLs

--- web/extension/js/bootstrap.js
+++ web/extension/js/bootstrap.js
@@ -1,3 +1,8 @@
+// Browser extensions are loaded from a dynamically-generated URL, we have to
+// tell webpack about that.
+
+__webpack_public_path__ = browser.runtime.getURL("dist/0.ruffle.js").replace("0.ruffle.js", "");
+
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
