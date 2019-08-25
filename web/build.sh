9ec4892ae7451d84b65abe727ee008b9a6116708
David Wendt <dcrkid@yahoo.com>
2019-08-25 19:43:12 -0400

    Fix regression caused by the extension file move

--- web/build.sh
+++ web/build.sh
@@ -3,6 +3,6 @@
 #Really hacky way to build everything until I figure out how to get a better/
 #dependency-respecting build system going
 
-rm -r dist
-wasm-pack build --target=no-modules --out-dir=extension/dist ..
+rm -r extension/dist
+wasm-pack build --target=no-modules --out-dir=extension/dist .
 npx webpack
