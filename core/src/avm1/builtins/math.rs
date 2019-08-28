490722d6c5e477594d75a169c450469788102db8
Nathan Adams <dinnerbone@dinnerbone.com>
2019-08-28 21:11:55 +0200

    Use named functions for builtins

--- core/src/avm1/builtins/math.rs
+++ core/src/avm1/builtins/math.rs
@@ -1,24 +1,21 @@
 use crate::avm1::{Object, Value};
 use gc_arena::{GcCell, MutationContext};
 
+fn abs<'gc>(args: &[Value<'gc>]) -> Value<'gc> {
+    let input = args.get(0).unwrap().as_f64().unwrap();
+    Value::Number(input.abs())
+}
+
+fn round<'gc>(args: &[Value<'gc>]) -> Value<'gc> {
+    let input = args.get(0).unwrap().as_f64().unwrap();
+    Value::Number(input.round())
+}
+
 pub fn create<'gc>(gc_context: MutationContext<'gc, '_>) -> Value<'gc> {
     let mut math = Object::new();
 
-    math.set(
-        "abs",
-        Value::NativeFunction(|args: &[Value<'gc>]| {
-            let input = args.get(0).unwrap().as_f64().unwrap();
-            Value::Number(input.abs())
-        }),
-    );
-
-    math.set(
-        "round",
-        Value::NativeFunction(|args: &[Value<'gc>]| {
-            let input = args.get(0).unwrap().as_f64().unwrap();
-            Value::Number(input.round())
-        }),
-    );
+    math.set("abs", Value::NativeFunction(abs));
+    math.set("round", Value::NativeFunction(round));
 
     Value::Object(GcCell::allocate(gc_context, math))
 }
