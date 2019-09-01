4e9fb2676b20df79336b0d9aa6e131ef1fe91e4c
David Wendt <dcrkid@yahoo.com>
2019-09-01 16:40:32 -0600

    Add `getURL` global builtin

--- core/src/avm1/globals.rs
+++ core/src/avm1/globals.rs
@@ -1,12 +1,36 @@
-use crate::avm1::object::Object;
-use gc_arena::MutationContext;
+use crate::avm1::{Object, Value, ActionContext};
+use gc_arena::{MutationContext, GcCell};
 
 mod math;
+
+#[allow(non_snake_case)]
+pub fn getURL<'a, 'gc>(
+    context: &mut ActionContext<'a, 'gc, '_>,
+    _this: GcCell<'gc, Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Value<'gc> {
+    match args.get(0) {
+        Some(url_val) => {
+            let url = url_val.clone().into_string();
+            let window = args.get(1).map(|v| v.clone().into_string());
+            let method = args.get(2).map(|v| v.clone().into_string());
+
+            //TODO: Pull AVM1 locals into key-value storage
+            context.navigator.navigate_to_url(url, window, None);
+        },
+        None => {
+            //TODO: Does AVM1 error out?
+        }
+    }
+
+    Value::Undefined
+}
 
 pub fn create_globals<'gc>(gc_context: MutationContext<'gc, '_>) -> Object<'gc> {
     let mut globals = Object::object(gc_context);
 
     globals.set_object("Math", math::create(gc_context));
+    globals.set_function("getURL", getURL, gc_context);
 
     globals
 }
