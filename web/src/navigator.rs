58ce10ea67f95eb33c7d652b57f9407e0ac67f37
David Wendt <dcrkid@yahoo.com>
2019-09-03 22:15:03 -0400

    Support form submission from Flash.

--- web/src/navigator.rs
+++ web/src/navigator.rs
@@ -1,7 +1,8 @@
 //! Navigator backend for web
 
 use std::collections::HashMap;
-use web_sys::window;
+use web_sys::{window};
+use wasm_bindgen::JsCast;
 use ruffle_core::backend::navigator::{NavigatorBackend, NavigationMethod};
 
 pub struct WebNavigatorBackend {
@@ -17,13 +18,44 @@ impl WebNavigatorBackend {
 }
 
 impl NavigatorBackend for WebNavigatorBackend {
-    fn navigate_to_url(&self, url: String, window_spec: Option<String>, _vars_method: Option<(NavigationMethod, HashMap<String, String>)>) {
+    fn navigate_to_url(&self, url: String, window_spec: Option<String>, vars_method: Option<(NavigationMethod, HashMap<String, String>)>) {
         if let Some(window) = window() {
-            //TODO: Support `vars_method`
             //TODO: Should we return a result for failed opens? Does Flash care?
             #[allow(unused_must_use)]
-            match window_spec {
-                Some(ref window_name) if window_name != "" => { window.open_with_url_and_target(&url, window_name); },
+            match (vars_method, window_spec) {
+                (Some((navmethod, formvars)), window_spec) => {
+                    let document = match window.document() {
+                        Some(document) => document,
+                        None => return
+                    };
+
+                    let form = document.create_element("form").unwrap().dyn_into::<web_sys::HtmlFormElement>().unwrap();
+
+                    form.set_attribute("method", match navmethod {
+                        NavigationMethod::GET => "get",
+                        NavigationMethod::POST => "post"
+                    });
+
+                    form.set_attribute("action", &url);
+
+                    if let Some(target) = window_spec {
+                        form.set_attribute("target", &target);
+                    }
+
+                    for (k, v) in formvars.iter() {
+                        let hidden = document.create_element("hidden").unwrap();
+
+                        hidden.set_attribute("type", "hidden");
+                        hidden.set_attribute("name", k);
+                        hidden.set_attribute("value", v);
+
+                        form.append_child(&hidden);
+                    }
+
+                    document.body().unwrap().append_child(&form);
+                    form.submit();
+                },
+                (_, Some(ref window_name)) if window_name != "" => { window.open_with_url_and_target(&url, window_name); },
                 _ => { window.location().assign(&url); }
             };
         }
