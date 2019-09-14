4250015420d7c3abd4ee5f15cd82a8da236ace36
David Wendt <dcrkid@yahoo.com>
2019-09-13 23:30:38 -0400

    Add query support for desktop and resolve some TODOs

--- desktop/src/navigator.rs
+++ desktop/src/navigator.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use ruffle_core::backend::navigator::{NavigatorBackend, NavigationMethod};
 use webbrowser;
 use log;
+use url::Url;
 
 /// Implementation of `NavigatorBackend` for non-web environments that can call
 /// out to a web browser.
@@ -20,15 +21,37 @@ impl ExternalNavigatorBackend {
 }
 
 impl NavigatorBackend for ExternalNavigatorBackend {
-    fn navigate_to_url(&self, url: String, _window_spec: Option<String>, _vars_method: Option<(NavigationMethod, HashMap<String, String>)>) {
-        //TODO: How does Flash interpret the target on desktop?
-        //TODO: Explicitly reject relative URLs, since `webbrowser` sometimes loads them
-        //TODO: Support `vars_method`
+    fn navigate_to_url(&self, url: String, _window_spec: Option<String>, vars_method: Option<(NavigationMethod, HashMap<String, String>)>) {
         //TODO: Should we return a result for failed opens? Does Flash care?
         
-        match webbrowser::open(&url) {
+        //NOTE: Flash desktop players / projectors ignore the window parameter,
+        //      unless it's a `_layer`, and we shouldn't handle that anyway.
+        let mut parsed_url = match Url::parse(&url) {
+            Ok(parsed_url) => parsed_url,
+            Err(e) => {
+                log::error!("Could not parse URL because of {}, the corrupt URL was: {}", e, url);
+                return;
+            }
+        };
+
+        let modified_url = match vars_method {
+            Some((_, query_pairs)) => {
+                { //lifetime limiter because we don't have NLL yet
+                    let mut modifier = parsed_url.query_pairs_mut();
+
+                    for (k, v) in query_pairs.iter() {
+                        modifier.append_pair(k, v);
+                    }
+                }
+
+                parsed_url.into_string()
+            },
+            None => url
+        };
+        
+        match webbrowser::open(&modified_url) {
             Ok(_output) => {},
-            Err(e) => log::error!("Could not open URL {}: {}", url, e)
+            Err(e) => log::error!("Could not open URL {}: {}", modified_url, e)
         };
     }
 }
