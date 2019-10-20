d964afe72ed3fe2427470855c861e2a2986d07d4
David Wendt <dcrkid@yahoo.com>
2019-10-20 13:14:02 -0600

    Expose the `RufflePlayer` element directly.

--- web/js-src/source-api.js
+++ web/js-src/source-api.js
@@ -1,4 +1,6 @@
 import { polyfill } from "./polyfills";
+import { register_element } from "./register-element";
+import { RufflePlayer } from "./ruffle-player";
 
 /**
  * Represents this particular version of Ruffle.
@@ -28,10 +30,25 @@ export class SourceAPI {
      * Polyfills, once enabled, may not be disabled. However, this function may
      * be called again with a different list to enable further polyfills.
      * 
+     * Do not run polyfills for more than one Ruffle source at a time.
+     * 
      * @param {array} polyfills A list of polyfills. See the `polyfills` module
      * for a list of allowable strings.
      */
     polyfill(polyfills) {
         polyfill(polyfills);
+    }
+    
+    /**
+     * Create a Ruffle player element using this particular version of Ruffle.
+     * 
+     * @returns {RufflePlayer} The player element. This is a DOM element that
+     * may be inserted into the current page as you wish.
+     */
+    create_player() {
+        let player_element_name = register_element("ruffle-player", RufflePlayer);
+        let player = document.createElement(player_element_name);
+
+        return player;
     }
 }
