84cb00b44bf5ae86d8c632c515f7834fdb886856
Mike Welsh <mwelsh@gmail.com>
2019-10-21 02:11:50 -0700

    chore: Fix clippy lint in Value::as_bool

--- core/src/avm1/value.rs
+++ core/src/avm1/value.rs
@@ -144,7 +144,7 @@ impl<'gc> Value<'gc> {
                     num != 0.0
                 }
             }
-            Value::Object(v) => true,
+            Value::Object(_) => true,
             _ => false,
         }
     }
