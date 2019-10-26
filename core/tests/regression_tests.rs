2cdb8d3656feeadb90bc2acf56fa81edca7c3902
Mike Welsh <mwelsh@gmail.com>
2019-10-26 01:42:12 -0700

    tests: execution_order -> execution_order1

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -33,7 +33,7 @@ macro_rules! swf_tests {
 // The test folder is a relative to core/tests/swfs
 // Inside the folder is expected to be "test.swf" and "output.txt" with the correct output.
 swf_tests! {
-    (execution_order, "avm1/execution_order", 3),
+    (execution_order1, "avm1/execution_order1", 3),
     (execution_order2, "avm1/execution_order2", 15),
     (execution_order3, "avm1/execution_order3", 5),
     (single_frame, "avm1/single_frame", 2),
