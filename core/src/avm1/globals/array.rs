ec09b5251b61370e9c16435eeee8e660a4c96956
David Wendt <dcrkid@yahoo.com>
2019-10-26 20:26:36 -0400

    Implement `toString` for `Array`.

--- core/src/avm1/globals/array.rs
+++ core/src/avm1/globals/array.rs
@@ -38,13 +38,53 @@ pub fn constructor<'gc>(
     Ok(Value::Undefined.into())
 }
 
+/// Implements `Array.prototype.toString`
+fn to_string<'gc>(
+    avm: &mut Avm1<'gc>,
+    ac: &mut UpdateContext<'_, 'gc, '_>,
+    this: ObjectCell<'gc>,
+    _: &[Value<'gc>],
+) -> Result<ReturnValue<'gc>, Error> {
+    let length = this
+        .read()
+        .get("length", avm, ac, this)?
+        .resolve(avm, ac)?
+        .as_number() as u64;
+    let mut result = "".to_owned();
+
+    for i in 0..length {
+        let value_str = this
+            .read()
+            .get(&format!("{}", i), avm, ac, this)?
+            .resolve(avm, ac)?
+            .as_string()?
+            .to_owned();
+
+        if i != 0 {
+            result = format!("{},{}", result, value_str);
+        } else {
+            result.push_str(&value_str);
+        }
+    }
+
+    Ok(ReturnValue::Immediate(result.into()))
+}
+
 /// Create `Array.prototype` and fill it with it's built-in methods.
 pub fn create_proto<'gc>(
     gc_context: MutationContext<'gc, '_>,
     obj_proto: ObjectCell<'gc>,
-    _fn_proto: ObjectCell<'gc>,
+    fn_proto: ObjectCell<'gc>,
 ) -> ObjectCell<'gc> {
-    let array_proto = ScriptObject::object(gc_context, Some(obj_proto));
+    let mut array_proto = ScriptObject::object(gc_context, Some(obj_proto));
+
+    array_proto.force_set_function(
+        "toString",
+        to_string,
+        gc_context,
+        EnumSet::empty(),
+        Some(fn_proto),
+    );
 
     GcCell::allocate(gc_context, Box::new(array_proto))
 }
