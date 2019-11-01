ab9af6958e2959ce4ce1529a33ad6426580bae3f
David Wendt <dcrkid@yahoo.com>
2019-11-01 19:02:48 -0400

    Enforce the opt-out entirely within trusted extension space, so that we don't load any untrusted world code at all in that case.

--- web/extension/build/js/lv0.js
+++ web/extension/build/js/lv0.js
@@ -15,20 +15,27 @@
  * 2. The ability to load extension resources such as .wasm files
  */
 async function() {
-    let ext_path = "";
-    if (chrome && chrome.extension && chrome.extension.getURL) {
-        ext_path = chrome.extension.getURL("dist/ruffle.js").replace("dist/ruffle.js", "");
-    } else if (browser && browser.runtime && browser.runtime.getURL) {
-        ext_path = browser.runtime.getURL("dist/ruffle.js").replace("dist/ruffle.js", "");
-    }
-    
-    let ruffle_src_resp = await fetch(ext_path + "dist/ruffle.js");
-    if (ruffle_src_resp.ok) {
-        let ruffle_src = "(function () { var runtime_path = \"" + ext_path + "\";\n" + await ruffle_src_resp.text() + "}())";
-        let scriptelem = document.createElement("script");
-        scriptelem.appendChild(document.createTextNode(ruffle_src));
-        document.head.appendChild(scriptelem);
-    } else {
-        console.error("Critical error loading Ruffle into page")
+    let page_optout = document.getElementsByTagName("html")[0].dataset.ruffleOptout !== undefined;
+
+    if (!page_optout) {
+        let ext_path = "";
+        if (chrome && chrome.extension && chrome.extension.getURL) {
+            ext_path = chrome.extension.getURL("dist/ruffle.js").replace("dist/ruffle.js", "");
+        } else if (browser && browser.runtime && browser.runtime.getURL) {
+            ext_path = browser.runtime.getURL("dist/ruffle.js").replace("dist/ruffle.js", "");
+        }
+        
+        let ruffle_src_resp = await fetch(ext_path + "dist/ruffle.js");
+        if (ruffle_src_resp.ok) {
+            let ruffle_src = "(function () { var runtime_path = \"" +
+                ext_path + "\";\nvar obfuscated_event_prefix = \"" +
+                obfuscated_event_prefix + "\";\n" +
+                await ruffle_src_resp.text() + "}())";
+            let scriptelem = document.createElement("script");
+            scriptelem.appendChild(document.createTextNode(ruffle_src));
+            document.head.appendChild(scriptelem);
+        } else {
+            console.error("Critical error loading Ruffle into page")
+        }
     }
 }());
--- web/extension/js/index.js
+++ web/extension/js/index.js
@@ -1,12 +1,4 @@
 import { PublicAPI } from "../../js-src/public-api";
 import { SourceAPI } from "../../js-src/source-api";
-import { get_config_options } from "../../js-src/config";
 
-let html = document.getElementsByTagName("html")[0];
-let page_options = get_config_options(html);
-
-if (!page_options.optout) {
-    window.RufflePlayer = PublicAPI.negotiate(window.RufflePlayer, "extension", new SourceAPI());
-} else {
-    console.log("WebExtension Ruffle execution prohibited by page");
-}
+window.RufflePlayer = PublicAPI.negotiate(window.RufflePlayer, "extension", new SourceAPI());
