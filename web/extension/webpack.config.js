24bf3a49bb6cd52bbb966ef3db3132437d5434c1
Mike Welsh <mwelsh@gmail.com>
2019-11-25 14:54:29 -0800

    extension: Fix webpack config to build even if web demo hasn't been built. Fix #122

--- web/extension/webpack.config.js
+++ web/extension/webpack.config.js
@@ -1,9 +1,11 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = (env, argv) => {
-  let mode = "development";
+  let mode = "production";
   if (argv && argv.mode) {
     mode = argv.mode;
   }
@@ -11,12 +13,31 @@ module.exports = (env, argv) => {
   console.log(`Building ${mode}...`);
 
   return {
+    externals: /^(ruffle_web)$/i,
+
     entry: path.resolve(__dirname, "js/index.js"),
     output: {
       path: path.resolve(__dirname, "build/dist"),
       filename: "ruffle.js",
     },
     mode: mode,
-    plugins: []
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /..\/pkg\/ruffle/
+      }),
+      new CleanWebpackPlugin({
+        protectWebpackAssets: false,
+        cleanAfterEveryBuildPatterns: ["*.module.wasm"],
+      }),
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, ".."),
+        extraArgs: "--target=no-modules",
+        forceMode: mode,
+      }),
+      new CopyPlugin([
+        { from: "../pkg/ruffle_web.js", to: "ruffle_web.js" },
+        { from: "../pkg/ruffle_web_bg.wasm", to: "ruffle_web_bg.wasm" },
+      ])
+    ]
   }
 };
