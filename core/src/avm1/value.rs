7412745f9e198ae0216d370e7e1d54198f86f511
Mike Welsh <mwelsh@gmail.com>
2019-12-21 22:40:06 -0800

    avm1: Use proper bool conversions for logical ops
    ActionAnd, ActionOr, and ActionNot were incorrectly comparing
    to 0. This only works for SWF<4. Now they all go through the
    Value::as_bool method to handle version specific behavior.
    
    Value::from_bool_v1 was also renamed to Value::from_bool.

--- core/src/avm1/value.rs
+++ core/src/avm1/value.rs
@@ -368,7 +368,10 @@ impl<'gc> Value<'gc> {
         }
     }
 
-    pub fn from_bool_v1(value: bool, swf_version: u8) -> Value<'gc> {
+    /// Converts a bool value into the appropriate value for the platform.
+    /// This should be used when pushing a bool onto the stack.
+    /// This handles SWFv4 pushing a Number, 0 or 1.
+    pub fn from_bool(value: bool, swf_version: u8) -> Value<'gc> {
         // SWF version 4 did not have true bools and will push bools as 0 or 1.
         // e.g. SWF19 p. 72:
         // "If the numbers are equal, true is pushed to the stack for SWF 5 and later. For SWF 4, 1 is pushed to the stack."
