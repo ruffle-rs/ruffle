2133c2cacce654fbb573481eed4c78d5901c4cb0
Nathan Adams <dinnerbone@dinnerbone.com>
2020-01-17 23:17:25 +0100

    avm1: Implement Key constants

--- core/src/avm1/globals/key.rs
+++ core/src/avm1/globals/key.rs
@@ -1,6 +1,6 @@
 use crate::avm1::property::Attribute;
 use crate::avm1::return_value::ReturnValue;
-use crate::avm1::{Avm1, Error, Object, ScriptObject, UpdateContext, Value};
+use crate::avm1::{Avm1, Error, Object, ScriptObject, TObject, UpdateContext, Value};
 
 use crate::events::KeyCode;
 use gc_arena::MutationContext;
@@ -29,6 +29,121 @@ pub fn create_key_object<'gc>(
     fn_proto: Option<Object<'gc>>,
 ) -> Object<'gc> {
     let mut key = ScriptObject::object(gc_context, proto);
+
+    key.define_value(
+        gc_context,
+        "ALT",
+        18.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "BACKSPACE",
+        8.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "CAPSLOCK",
+        20.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "CONTROL",
+        17.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "DELETEKEY",
+        46.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "DOWN",
+        40.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "END",
+        35.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "ENTER",
+        13.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "ESCAPE",
+        27.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "HOME",
+        36.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "INSERT",
+        45.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "LEFT",
+        37.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "PGDN",
+        34.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "PGUP",
+        33.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "RIGHT",
+        39.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "SHIFT",
+        16.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "SPACE",
+        32.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "TAB",
+        9.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
+    key.define_value(
+        gc_context,
+        "UP",
+        38.into(),
+        Attribute::DontEnum | Attribute::DontDelete | Attribute::ReadOnly,
+    );
 
     key.force_set_function(
         "isDown",
