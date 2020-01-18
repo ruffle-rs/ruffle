0446644742a5a45a71d8b206c1edade6c9956d5d
Mike Welsh <mwelsh@gmail.com>
2020-01-17 18:05:26 -0800

    tests: Add target_path test

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -106,6 +106,7 @@ swf_tests! {
     (slash_syntax, "avm1/slash_syntax", 2),
     (strictequals_swf6, "avm1/strictequals_swf6", 1),
     (string_methods, "avm1/string_methods", 1),
+    (target_path, "avm1/target_path", 1),
     (global_is_bare, "avm1/global_is_bare", 1),
     (primitive_type_globals, "avm1/primitive_type_globals", 1),
     (as2_oop, "avm1/as2_oop", 1),
