3558c3afa02b69aa35dfc134014de0a6cd827710
David Wendt <dcrkid@yahoo.com>
2020-03-08 22:29:15 -0400

    Add test of `Function.prototype.call`

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -229,6 +229,7 @@ swf_tests! {
     (context_menu_item, "avm1/context_menu_item", 1),
     (as3_hello_world, "avm2/hello_world", 1),
     (as3_function_call, "avm2/function_call", 1),
+    (as3_function_call_via_call, "avm2/function_call_via_call", 1),
     (as3_constructor_call, "avm2/constructor_call", 1),
     (as3_class_methods, "avm2/class_methods", 1),
     (as3_inheritance, "avm2/inheritance", 1),
