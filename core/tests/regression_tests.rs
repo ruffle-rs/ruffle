6117288fe264409d62e67f028f1c4cc25e32ffd6
David Wendt <dcrkid@yahoo.com>
2020-03-08 23:12:52 -0400

    Add tests for `ifstricteq`, `ifstrictne`, and `strictequals`.

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -255,6 +255,9 @@ swf_tests! {
     (as3_object_value_of, "avm2/object_value_of", 1),
     (as3_function_value_of, "avm2/function_value_of", 1),
     (as3_class_value_of, "avm2/class_value_of", 1),
+    (as3_if_stricteq, "avm2/if_stricteq", 1),
+    (as3_if_strictne, "avm2/if_strictne", 1),
+    (as3_strict_equality, "avm2/strict_equality", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
