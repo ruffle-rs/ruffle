ruffle-rs/ruffle

commit 223edb9bc1d83361170ebed711ea23529bb4ea8b

core: Matrix translation is in twips

web/src/render.rs
@@ -305,8 +305,8 @@ impl WebCanvasRenderBackend {
                 matrix.b.into(),
                 matrix.c.into(),
                 matrix.d.into(),
-                f64::from(matrix.tx) / 20.0,
-                f64::from(matrix.ty) / 20.0,
+                matrix.tx.to_pixels(),
+                matrix.ty.to_pixels(),
             )
             .unwrap();
     }
@@ -774,8 +774,8 @@ fn swf_shape_to_svg(
                             let shift = Matrix {
                                 a: 32768.0 / width,
                                 d: 32768.0 / height,
-                                tx: -16384.0,
-                                ty: -16384.0,
+                                tx: swf::Twips::new(-16384),
+                                ty: swf::Twips::new(-16384),
                                 ..Default::default()
                             };
                             let gradient_matrix = matrix * shift;
@@ -821,8 +821,6 @@ fn swf_shape_to_svg(
                             let shift = Matrix {
                                 a: 32768.0,
                                 d: 32768.0,
-                                tx: 0.0,
-                                ty: 0.0,
                                 ..Default::default()
                             };
                             let gradient_matrix = matrix * shift;
@@ -874,8 +872,6 @@ fn swf_shape_to_svg(
                             let shift = Matrix {
                                 a: 32768.0,
                                 d: 32768.0,
-                                tx: 0.0,
-                                ty: 0.0,
                                 ..Default::default()
                             };
                             let gradient_matrix = matrix * shift;
@@ -1219,8 +1215,8 @@ fn swf_shape_to_canvas_commands(
                         matrix.set_b(a.b);
                         matrix.set_c(a.c);
                         matrix.set_d(a.d);
-                        matrix.set_e(a.tx);
-                        matrix.set_f(a.ty);
+                        matrix.set_e(a.tx.get() as f32);
+                        matrix.set_f(a.ty.get() as f32);
 
                         bitmap_pattern.set_transform(&matrix);
 

desktop/src/render.rs
@@ -710,8 +710,8 @@ impl RenderBackend for GliumRenderBackend {
             [transform.matrix.c, transform.matrix.d, 0.0, 0.0],
             [0.0, 0.0, 1.0, 0.0],
             [
-                transform.matrix.tx / 20.0,
-                transform.matrix.ty / 20.0,
+                transform.matrix.tx.to_pixels() as f32,
+                transform.matrix.ty.to_pixels() as f32,
                 0.0,
                 1.0,
             ],

core/src/avm1/globals/text_field.rs
@@ -85,7 +85,7 @@ pub fn text_width<'gc>(
     {
         let metrics = etext.measure_text(context);
 
-        return Ok(metrics.0.into());
+        return Ok(metrics.0.to_pixels().into());
     }
 
     Ok(Value::Undefined.into())
@@ -103,7 +103,7 @@ pub fn text_height<'gc>(
     {
         let metrics = etext.measure_text(context);
 
-        return Ok(metrics.1.into());
+        return Ok(metrics.1.to_pixels().into());
     }
 
     Ok(Value::Undefined.into())

core/src/display_object.rs
@@ -142,18 +142,18 @@ impl<'gc> DisplayObjectBase<'gc> {
         self.transform.color_transform = *color_transform;
     }
     fn x(&self) -> f64 {
-        f64::from(self.transform.matrix.tx) / Twips::TWIPS_PER_PIXEL
+        self.transform.matrix.tx.to_pixels()
     }
     fn set_x(&mut self, value: f64) {
         self.set_transformed_by_script(true);
-        self.transform.matrix.tx = (value * Twips::TWIPS_PER_PIXEL) as f32
+        self.transform.matrix.tx = Twips::from_pixels(value)
     }
     fn y(&self) -> f64 {
-        f64::from(self.transform.matrix.ty) / Twips::TWIPS_PER_PIXEL
+        self.transform.matrix.ty.to_pixels()
     }
     fn set_y(&mut self, value: f64) {
         self.set_transformed_by_script(true);
-        self.transform.matrix.ty = (value * Twips::TWIPS_PER_PIXEL) as f32
+        self.transform.matrix.ty = Twips::from_pixels(value)
     }
 
     /// Caches the scale and rotation factors for this display object, if necessary.

core/src/display_object/edit_text.rs
@@ -226,9 +226,9 @@ impl<'gc> EditText<'gc> {
         transform.color_transform.a_mult = f32::from(color.a) / 255.0;
 
         if let Some(layout) = &static_data.text.layout {
-            transform.matrix.tx += layout.left_margin.get() as f32;
-            transform.matrix.tx += layout.indent.get() as f32;
-            transform.matrix.ty -= layout.leading.get() as f32;
+            transform.matrix.tx += layout.left_margin;
+            transform.matrix.tx += layout.indent;
+            transform.matrix.ty -= layout.leading;
         }
 
         transform
@@ -239,31 +239,31 @@ impl<'gc> EditText<'gc> {
     ///
     /// This function takes the current font size and the transform to adjust,
     /// and returns the adjusted transform.
-    pub fn newline(self, height: f32, mut transform: Transform) -> Transform {
+    pub fn newline(self, height: Twips, mut transform: Transform) -> Transform {
         let edit_text = self.0.read();
         let static_data = &edit_text.static_data;
 
-        transform.matrix.tx = 0.0;
-        transform.matrix.ty += height * Twips::TWIPS_PER_PIXEL as f32;
+        transform.matrix.tx = Twips::new(0);
+        transform.matrix.ty += height;
         if let Some(layout) = &static_data.text.layout {
-            transform.matrix.tx += layout.left_margin.get() as f32;
-            transform.matrix.tx += layout.indent.get() as f32;
-            transform.matrix.ty += layout.leading.get() as f32;
+            transform.matrix.tx += layout.left_margin;
+            transform.matrix.tx += layout.indent;
+            transform.matrix.ty += layout.leading;
         }
 
         transform
     }
 
-    pub fn line_width(self) -> f32 {
+    pub fn line_width(self) -> Twips {
         let edit_text = self.0.read();
         let static_data = &edit_text.static_data;
 
-        let mut base_width = self.width() as f32;
+        let mut base_width = Twips::from_pixels(self.width());
 
         if let Some(layout) = &static_data.text.layout {
-            base_width -= layout.left_margin.to_pixels() as f32;
-            base_width -= layout.indent.to_pixels() as f32;
-            base_width -= layout.right_margin.to_pixels() as f32;
+            base_width -= layout.left_margin;
+            base_width -= layout.indent;
+            base_width -= layout.right_margin;
         }
 
         base_width
@@ -311,8 +311,7 @@ impl<'gc> EditText<'gc> {
                 let height = static_data
                     .text
                     .height
-                    .map(|v| v.to_pixels() as f32)
-                    .unwrap_or_else(|| font.scale());
+                    .unwrap_or_else(|| Twips::from_pixels(font.scale().into()));
 
                 for natural_line in edit_text.text.split('\n') {
                     if break_base != 0 {
@@ -364,14 +363,14 @@ impl<'gc> EditText<'gc> {
     /// Measure the width and height of the `EditText`'s current text load.
     ///
     /// The returned tuple should be interpreted as width, then height.
-    pub fn measure_text(self, context: &mut UpdateContext<'_, 'gc, '_>) -> (f32, f32) {
+    pub fn measure_text(self, context: &mut UpdateContext<'_, 'gc, '_>) -> (Twips, Twips) {
         let breakpoints = self.line_breaks_cached(context.gc_context, context.library);
 
         let edit_text = self.0.read();
         let static_data = &edit_text.static_data;
         let font_id = static_data.text.font_id.unwrap_or(0);
 
-        let mut size: (f32, f32) = (0.0, 0.0);
+        let mut size: (Twips, Twips) = Default::default();
 
         if let Some(font) = context
             .library
@@ -399,15 +398,14 @@ impl<'gc> EditText<'gc> {
             let height = static_data
                 .text
                 .height
-                .map(|v| v.to_pixels() as f32)
-                .unwrap_or_else(|| font.scale());
+                .unwrap_or_else(|| Twips::from_pixels(font.scale().into()));
 
             for chunk in chunks {
                 let chunk_size = font.measure(chunk, height);
 
                 size.0 = size.0.max(chunk_size.0);
                 if let Some(layout) = &static_data.text.layout {
-                    size.1 += layout.leading.to_pixels() as f32;
+                    size.1 += layout.leading;
                 }
                 size.1 += chunk_size.1;
             }
@@ -490,8 +488,7 @@ impl<'gc> TDisplayObject<'gc> for EditText<'gc> {
             let height = static_data
                 .text
                 .height
-                .map(|v| v.to_pixels() as f32)
-                .unwrap_or_else(|| font.scale());
+                .unwrap_or_else(|| Twips::from_pixels(font.scale().into()));
 
             let breakpoints = edit_text
                 .cached_break_points

core/src/display_object/text.rs
@@ -71,25 +71,25 @@ impl<'gc> TDisplayObject<'gc> for Text<'gc> {
             a: 0,
         };
         let mut font_id = 0;
-        let mut height = 0.0;
+        let mut height = Twips::new(0);
         let mut transform: Transform = Default::default();
         for block in &tf.static_data.text_blocks {
             if let Some(x) = block.x_offset {
-                transform.matrix.tx = x.get() as f32;
+                transform.matrix.tx = x;
             }
             if let Some(y) = block.y_offset {
-                transform.matrix.ty = y.get() as f32;
+                transform.matrix.ty = y;
             }
             color = block.color.as_ref().unwrap_or(&color).clone();
             font_id = block.font_id.unwrap_or(font_id);
-            height = block.height.map(|h| h.get() as f32).unwrap_or(height);
+            height = block.height.unwrap_or(height);
             if let Some(font) = context
                 .library
                 .library_for_movie(self.movie().unwrap())
                 .unwrap()
                 .get_font(font_id)
             {
-                let scale = height / font.scale();
+                let scale = (height.get() as f32) / font.scale();
                 transform.matrix.a = scale;
                 transform.matrix.d = scale;
                 transform.color_transform.r_mult = f32::from(color.r) / 255.0;
@@ -103,7 +103,7 @@ impl<'gc> TDisplayObject<'gc> for Text<'gc> {
                             .renderer
                             .render_shape(glyph.shape, context.transform_stack.transform());
                         context.transform_stack.pop();
-                        transform.matrix.tx += c.advance as f32;
+                        transform.matrix.tx += Twips::new(c.advance);
                     }
                 }
             }

core/src/font.rs
@@ -132,13 +132,13 @@ impl<'gc> Font<'gc> {
         self,
         text: &str,
         mut transform: Transform,
-        height: f32,
+        height: Twips,
         mut glyph_func: FGlyph,
     ) where
         FGlyph: FnMut(&Transform, &Glyph),
     {
-        transform.matrix.ty += height * Twips::TWIPS_PER_PIXEL as f32;
-        let scale = (height * Twips::TWIPS_PER_PIXEL as f32) / self.scale();
+        transform.matrix.ty += height;
+        let scale = height.get() as f32 / self.scale();
 
         transform.matrix.a = scale;
         transform.matrix.d = scale;
@@ -148,26 +148,24 @@ impl<'gc> Font<'gc> {
             if let Some(glyph) = self.get_glyph_for_char(c) {
                 glyph_func(&transform, &glyph);
                 // Step horizontally.
-                let mut advance = f32::from(glyph.advance);
+                let mut advance = Twips::new(glyph.advance);
                 if has_kerning_info {
-                    advance += self
-                        .get_kerning_offset(c, chars.peek().cloned().unwrap_or('\0'))
-                        .get() as f32;
+                    advance += self.get_kerning_offset(c, chars.peek().cloned().unwrap_or('\0'));
                 }
-                transform.matrix.tx += advance * scale;
+                transform.matrix.tx += Twips::new((advance.get() as f32 * scale) as i32);
             }
         }
     }
 
     /// Measure a particular string's metrics (width and height).
-    pub fn measure(self, text: &str, height: f32) -> (f32, f32) {
-        let mut size = (0.0, 0.0);
+    pub fn measure(self, text: &str, height: Twips) -> (Twips, Twips) {
+        let mut size = (Twips::new(0), Twips::new(0));
 
         self.evaluate(text, Default::default(), height, |transform, _glyph| {
-            let tx = transform.matrix.tx / Twips::TWIPS_PER_PIXEL as f32;
-            let ty = transform.matrix.ty / Twips::TWIPS_PER_PIXEL as f32;
-            size.0 = f32::max(size.0, tx);
-            size.1 = f32::max(size.1, ty);
+            let tx = transform.matrix.tx;
+            let ty = transform.matrix.ty;
+            size.0 = std::cmp::max(size.0, tx);
+            size.1 = std::cmp::max(size.1, ty);
         });
 
         size
@@ -178,7 +176,7 @@ impl<'gc> Font<'gc> {
     ///
     /// This function assumes only `" "` is valid whitespace to split words on,
     /// and will not attempt to break words that are longer than `width`.
-    pub fn split_wrapped_lines(self, text: &str, height: f32, width: f32) -> Vec<usize> {
+    pub fn split_wrapped_lines(self, text: &str, height: Twips, width: Twips) -> Vec<usize> {
         let mut result = vec![];
         let mut current_width = width;
         let mut current_word = &text[0..0];

commit 26914d7b62b56653a6cac57c540058bc94a2639c

web: Fix off-center gradient rendering
Regression caused by 223edb9. Write twips value to SVG matrix.
Addresses some incorrect rendering in #437.

web/src/render.rs
@@ -791,8 +791,8 @@ fn swf_shape_to_svg(
                                         gradient_matrix.b,
                                         gradient_matrix.c,
                                         gradient_matrix.d,
-                                        gradient_matrix.tx,
-                                        gradient_matrix.ty
+                                        gradient_matrix.tx.get(),
+                                        gradient_matrix.ty.get()
                                     ),
                                 );
                             for record in &gradient.records {
@@ -839,8 +839,8 @@ fn swf_shape_to_svg(
                                         gradient_matrix.b,
                                         gradient_matrix.c,
                                         gradient_matrix.d,
-                                        gradient_matrix.tx,
-                                        gradient_matrix.ty
+                                        gradient_matrix.tx.get(),
+                                        gradient_matrix.ty.get()
                                     ),
                                 );
                             for record in &gradient.records {
@@ -891,8 +891,8 @@ fn swf_shape_to_svg(
                                         gradient_matrix.b,
                                         gradient_matrix.c,
                                         gradient_matrix.d,
-                                        gradient_matrix.tx,
-                                        gradient_matrix.ty
+                                        gradient_matrix.tx.get(),
+                                        gradient_matrix.ty.get()
                                     ),
                                 );
                             for record in &gradient.records {