2f3da48335b1decebbd6c9331ed732b00ffc45fe
Mike Welsh <mwelsh@gmail.com>
2020-03-20 18:27:59 -0700

    web: Activate polyfill if embed src ends in .swf
    Fixes part of #457. Even if the MIME type/class IDs are missing
    on the embed/object tags, browsers will still attempt to play
    the file as Flash if it ends in a .swf extension.

--- web/js-src/ruffle-embed.js
+++ web/js-src/ruffle-embed.js
@@ -1,5 +1,5 @@
-import {FLASH_MIMETYPE, FUTURESPLASH_MIMETYPE, RufflePlayer} from "./ruffle-player.js";
-import {register_element} from "./register-element";
+import { FLASH_MIMETYPE, FUTURESPLASH_MIMETYPE, is_swf_filename, RufflePlayer } from "./ruffle-player.js";
+import { register_element } from "./register-element";
 
 export default class RuffleEmbed extends RufflePlayer {
     constructor(...args) {
@@ -34,7 +34,13 @@ export default class RuffleEmbed extends RufflePlayer {
     }
 
     static is_interdictable(elem) {
-        return elem.type === FLASH_MIMETYPE || elem.type === FUTURESPLASH_MIMETYPE;
+        if (elem.type === FLASH_MIMETYPE || elem.type === FUTURESPLASH_MIMETYPE) {
+            return true;
+        } else if (elem.type === undefined || elem.type === "") {
+            return is_swf_filename(elem.src);
+        }
+
+        return false;
     }
 
     static from_native_embed_element(elem) {
