774aa59210429d1f6050ab742d4fa3f1b68f2c2c
Mike Welsh <mwelsh@gmail.com>
2020-03-25 00:54:21 -0700

    web: Default public_path to currentScript URL
    Also automatically add trailing slash on public_path if necessary.

--- web/js-src/public-path.js
+++ web/js-src/public-path.js
@@ -7,25 +7,36 @@
  * 
  * 1. The public path can be specified on a per-source basis using the
  * RufflePlayer config, for example:
- * `window.RufflePlayer.config.public_paths.local = "/dist";`
+ * `window.RufflePlayer.config.public_paths.local = "/dist/";`
  * 2. A global public path can be specified for all sources, also in config.
- * `window.RufflePlayer.config.public_path = "/dist";`
+ * `window.RufflePlayer.config.public_path = "/dist/";`
  * 3. If there is absolutely no configuration that yields a public path then we
- * return "", which will configure resources to be loaded relative to the
- * current page. This is liable to be useless outside of contrived scenarios.
+ * return the parent path of where this script is hosted, which should be
+ * the correct default in most cases.
  * 
  * @param {object} config The `window.RufflePlayer.config` object.
  * @param {string} source_name The name of the source.
- * @returns {string} A 
+ * @returns {string} The public path for the given source.
  */
 export function public_path(config, source_name) {
+    let public_path = "";
     if (config !== undefined && config.public_paths !== undefined && config.public_paths[source_name] !== undefined) {
-        return config.public_paths[source_name];
-    }
-
-    if (config !== undefined && config.public_path !== undefined) {
-        return config.public_path;
+        public_path = config.public_paths[source_name];
+    } else if (config !== undefined && config.public_path !== undefined) {
+        public_path = config.public_path;
+    } else if (document.currentScript !== undefined) {
+        // Default to the directory where this script resides.
+        try {
+            public_path = new URL(".", document.currentScript.src).href;
+        } catch (e) {
+            console.warn("Unable to get currentScript URL");
+        }
     }
     
-    return "";
+    // Webpack expects the paths to end with a /.
+    if (public_path !== "" && !public_path.endsWith("/")) {
+        public_path += "/";
+    }
+
+    return public_path;
 }
