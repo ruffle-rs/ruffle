e1ec0d571430a2402dc95ca109cebce8d02353bb
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2020-05-21 12:30:21 -0400

    exporter: first time for everything. Correct bug made when using Githug suggestions for the first time.

--- exporter/src/main.rs
+++ exporter/src/main.rs
@@ -73,7 +73,7 @@ fn take_screenshot(
             ));
         }
         player.lock().unwrap().run_frame();
-        i >= skipframes {
+        if i >= skipframes {
             player.lock().unwrap().render();
             let mut player = player.lock().unwrap();
             let renderer = player
