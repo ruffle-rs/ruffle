15476618b11ccf750182852c66610d59a97d8269
JustinCB <JustinCB@opmbx.org>
2020-05-28 17:30:12 -0400

    Complete settings localization, localize extension description, & add spanish translation

--- web/packages/extension/js/settings.js
+++ web/packages/extension/js/settings.js
@@ -51,13 +51,15 @@ function set_sync_storage(key) {
 get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
     var play_flash_message = get_i18n_string("settings_ruffle_enable");
     var ignore_optout_message = get_i18n_string("settings_page_ignore_optout");
+    var title_text = get_i18n_string("settings_page");
     var play_flash_label = document.getElementById("enablelabel");
     var ignore_optout_label = document.getElementById("ignorelabel");
     var play_flash_checkbox = document.getElementById("enable");
     var ignore_optout_checkbox = document.getElementById("ignoreoptout");
     var save_button = document.getElementById("save");
-    console.log(play_flash_message);
-    console.log(ignore_optout_message);
+    var title = document.getElementById("title");
+    title.innerHTML = title_text;
+    document.title = title_text;
     play_flash_label.innerHTML = play_flash_message + "<br />";
     ignore_optout_label.innerHTML = ignore_optout_message + "<br />";
     if (data.ruffle_enable == "on") {
