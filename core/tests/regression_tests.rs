3d094ed68950a73073297f039bb4f1f2314567a5
David Wendt <dcrkid@yahoo.com>
2020-06-04 19:31:28 -0400

    Add margins test.
    This test is also approx'd to 1px due to the same issue where heights are off by one.

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -212,6 +212,7 @@ swf_tests_approx! {
     (stage_object_properties_swf6, "avm1/stage_object_properties_swf6", 4, 0.051),
     (movieclip_getbounds, "avm1/movieclip_getbounds", 1, 0.051),
     (edittext_letter_spacing, "avm1/edittext_letter_spacing", 1, 1.0),
+    (edittext_margins, "avm1/edittext_margins", 1, 1.0),
 }
 
 /// Wrapper around string slice that makes debug output `{:?}` to print string same way as `{}`.
