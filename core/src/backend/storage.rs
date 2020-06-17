62834fd69070fdbf3010f27a60d31763429fb482
CUB3D <callumthom11@gmail.com>
2020-06-18 00:23:33 +0100

    chore: Remove unnecessary comment

--- core/src/backend/storage.rs
+++ core/src/backend/storage.rs
@@ -40,28 +40,3 @@ impl StorageBackend for MemoryStorageBackend {
         self.map.remove(name);
     }
 }
-
-// impl SharedObjectBackend for MemoryBackend {
-// }
-//
-// // In the desktop player
-// struct DiskBackend {}
-//
-// impl SharedObjectBackend for DiskBackend {
-//     pub fn get_obj_json(name: String) -> String {
-//         // read from file
-//     }
-//     pub fn set_obj_json(name: String, value: String) {
-//         // write to file
-//         //TODO: if slow maybe keep and internal cache, also consider async io for fast flushing
-//     }
-//
-//     pub fn clear() {
-//         // Delete the file, modifying the avm objects from any of these will be done in shared_object
-//     }
-// }
-//
-// // for web player
-// struct LocalStorageBackend {}
-//
-// //TODO: check the issue about this, need to prefix with url of site somehow to avoid collisions
