6eb41035cf49c0d3496ecb85deb67235f0a0b296
David Wendt <dcrkid@yahoo.com>
2020-06-26 00:09:42 -0400

    Add & test ECMA-262 ToInt32 and `convert_i` opcode.
    The ECMA-262 documentation is awfully overwrought for something that boils down to "chop off the non-whole part, wrap to 32 bits, then reinterpret as signed". Bitwise operations are *hell* to describe mathematically, and such descriptions are even harder to understand.

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -278,6 +278,7 @@ swf_tests! {
     (as3_boolean_negation, "avm2/boolean_negation", 1),
     (as3_convert_boolean, "avm2/convert_boolean", 1),
     (as3_convert_number, "avm2/convert_number", 1),
+    (as3_convert_integer, "avm2/convert_integer", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
