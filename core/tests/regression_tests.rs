d1732dd3d695da2fe1f1c07c59d01cf4fa231208
Nathan Adams <dinnerbone@dinnerbone.com>
2020-07-02 00:24:18 +0200

    avm1: Returning from with{} should return from the parent activation

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -189,6 +189,7 @@ swf_tests! {
     (loadvariablesnum, "avm1/loadvariablesnum", 3),
     (loadvariables_method, "avm1/loadvariables_method", 3),
     (xml_load, "avm1/xml_load", 1),
+    (with_return, "avm1/with_return", 1),
     (cross_movie_root, "avm1/cross_movie_root", 5),
     (roots_and_levels, "avm1/roots_and_levels", 1),
     (swf6_case_insensitive, "avm1/swf6_case_insensitive", 1),
