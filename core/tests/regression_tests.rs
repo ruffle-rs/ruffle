2cbbe32412a9ac78fb15ea413081542e3fd80bd2
Mike Welsh <mwelsh@gmail.com>
2020-07-02 15:16:11 -0700

    tests: Add test for GetVariable/SetVariable on _x, etc.

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -218,6 +218,7 @@ swf_tests! {
     (color_transform, "avm1/color_transform", 1),
     (with, "avm1/with", 1),
     (prototype_properties, "avm1/prototype_properties", 1),
+    (stage_object_properties_get_var, "avm1/stage_object_properties_get_var", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
