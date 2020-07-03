b6e05519cd9330590724ec2c448a11e7350dfb0d
David Wendt <dcrkid@yahoo.com>
2020-07-02 21:19:31 -0400

    Remove `Avm2ClassEntry`. It is no longer used.

--- core/src/avm2/class.rs
+++ core/src/avm2/class.rs
@@ -6,61 +6,7 @@ use crate::avm2::r#trait::{Trait, TraitKind};
 use crate::avm2::script::TranslationUnit;
 use crate::avm2::Error;
 use gc_arena::{Collect, GcCell, MutationContext};
-use std::rc::Rc;
-use swf::avm2::types::{AbcFile, Class as AbcClass, Index, Instance as AbcInstance};
-
-/// Represents a reference to an AVM2 class.
-///
-/// For some reason, this comes in two parts, one for static properties (called
-/// the "class") and one for dynamic properties (called the "instance", even
-/// though it really defines what ES3/AS2 would call a prototype)
-#[derive(Collect, Clone, Debug)]
-#[collect(require_static)]
-pub struct Avm2ClassEntry {
-    /// The ABC file this function was defined in.
-    pub abc: Rc<AbcFile>,
-
-    /// The ABC class (used to define static properties).
-    ///
-    /// This is also the index of the ABC instance, which holds instance
-    /// properties.
-    pub abc_class: u32,
-}
-
-impl Avm2ClassEntry {
-    /// Construct an `Avm2MethodEntry` from an `AbcFile` and method index.
-    ///
-    /// This function returns `None` if the given class index does not resolve
-    /// to a valid ABC class, or a valid ABC instance. As mentioned in the type
-    /// documentation, ABC classes and instances are intended to be paired.
-    pub fn from_class_index(abc: Rc<AbcFile>, abc_class: Index<AbcClass>) -> Option<Self> {
-        if abc.classes.get(abc_class.0 as usize).is_some()
-            && abc.instances.get(abc_class.0 as usize).is_some()
-        {
-            return Some(Self {
-                abc,
-                abc_class: abc_class.0,
-            });
-        }
-
-        None
-    }
-
-    /// Get the underlying ABC file.
-    pub fn abc(&self) -> Rc<AbcFile> {
-        self.abc.clone()
-    }
-
-    /// Get a reference to the ABC class entry this refers to.
-    pub fn class(&self) -> &AbcClass {
-        self.abc.classes.get(self.abc_class as usize).unwrap()
-    }
-
-    /// Get a reference to the ABC class instance entry this refers to.
-    pub fn instance(&self) -> &AbcInstance {
-        self.abc.instances.get(self.abc_class as usize).unwrap()
-    }
-}
+use swf::avm2::types::{Class as AbcClass, Instance as AbcInstance};
 
 /// A loaded ABC Class which can be used to construct objects with.
 #[derive(Clone, Debug, Collect)]
