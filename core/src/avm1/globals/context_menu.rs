f3aa6f7f015b176134e144e0f2f4786d219180e1
CUB3D <callumthom11@gmail.com>
2020-07-03 01:59:48 +0100

    core: Add stub of ContextMenuItem

--- core/src/avm1/globals/context_menu.rs
+++ core/src/avm1/globals/context_menu.rs
@@ -1,60 +1,81 @@
 use gc_arena::MutationContext;
-use crate::avm1::{Value, ScriptObject, Avm1};
-use crate::avm1::function::{FunctionObject, Executable};
+use crate::avm1::{Value, ScriptObject};
 use crate::avm1::Object;
 use crate::context::UpdateContext;
-use crate::avm1::return_value::ReturnValue;
 use crate::avm1::error::Error;
 use enumset::EnumSet;
+use crate::avm1::activation::Activation;
+use crate::avm1::object::TObject;
+
+//TODO: in future this will want to be a custom object, as it has a callback function in constructor arg
+//TODO: note: callback should be called when menu is opened but before it is displayed
+//TODO: check for hidden props
+//TODO: there shuold be a menu prop somewhere (as2:444)
 
 pub fn constructor<'gc>(
-    _avm: &mut Avm1<'gc>,
-    _context: &mut UpdateContext<'_, 'gc, '_>,
-    _this: Object<'gc>,
+    activation: &mut Activation<'_, 'gc>,
+    context: &mut UpdateContext<'_, 'gc, '_>,
+    this: Object<'gc>,
     _args: &[Value<'gc>],
-) -> Result<ReturnValue<'gc>, Error> {
+) -> Result<Value<'gc>, Error<'gc>> {
+    let obj_proto = activation.avm().prototypes.object;
+    let built_in_items = obj_proto.new(activation, context, obj_proto, &[])?;
+    let _ = crate::avm1::globals::object::constructor(activation, context, built_in_items, &[]);
+    //TODO Check that this lines up with FP
+    built_in_items.set("zoom", true.into(), activation, context)?;
+    built_in_items.set("quality", true.into(), activation, context)?;
+    built_in_items.set("play", true.into(), activation, context)?;
+    built_in_items.set("loop", true.into(), activation, context)?;
+    built_in_items.set("rewind", true.into(), activation, context)?;
+    built_in_items.set("forward_back", true.into(), activation, context)?;
+    built_in_items.set("print", true.into(), activation, context)?;
+
+    //TODO: check if these are on the proto or not
+    //TODO: are they virt?
+    this.set("builtInItems", built_in_items.into(), activation, context)?;
+
+    let array_proto = activation.avm().prototypes.array;
+    let custom_items = array_proto.new(activation, context, array_proto, &[])?;
+    let _ = crate::avm1::globals::array::constructor(activation, context, custom_items, &[]);
+
+    this.set("customItems", custom_items.into(), activation, context)?;
+
     Ok(Value::Undefined.into())
 }
 
-pub fn create_context_menu_object<'gc>(
-    gc_context: MutationContext<'gc, '_>,
-    context_menu_proto: Option<Object<'gc>>,
-    fn_proto: Option<Object<'gc>>,
-) -> Object<'gc> {
-    FunctionObject::function(
-        gc_context,
-        Executable::Native(constructor),
-        fn_proto,
-        context_menu_proto,
-    )
-}
-
 pub fn copy<'gc>(
-    _avm: &mut Avm1<'gc>,
+    _activation: &mut Activation<'_, 'gc>,
     _context: &mut UpdateContext<'_, 'gc, '_>,
     _this: Object<'gc>,
     _args: &[Value<'gc>],
-) -> Result<ReturnValue<'gc>, Error> {
+) -> Result<Value<'gc>, Error<'gc>> {
     log::warn!("ContextMenu.copy() not implemented");
     Ok(Value::Undefined.into())
 }
 
 pub fn hide_builtin_items<'gc>(
-    _avm: &mut Avm1<'gc>,
-    _context: &mut UpdateContext<'_, 'gc, '_>,
-    _this: Object<'gc>,
+    activation: &mut Activation<'_, 'gc>,
+    context: &mut UpdateContext<'_, 'gc, '_>,
+    this: Object<'gc>,
     _args: &[Value<'gc>],
-) -> Result<ReturnValue<'gc>, Error> {
-    log::warn!("ContextMenu.hideBuiltInItems() not implemented");
+) -> Result<Value<'gc>, Error<'gc>> {
+    let built_in_items = this.get("builtInItems", activation, context)?.coerce_to_object(activation, context);
+    built_in_items.set("zoom", false.into(), activation, context)?;
+    built_in_items.set("quality", false.into(), activation, context)?;
+    built_in_items.set("play", false.into(), activation, context)?;
+    built_in_items.set("loop", false.into(), activation, context)?;
+    built_in_items.set("rewind", false.into(), activation, context)?;
+    built_in_items.set("forward_back", false.into(), activation, context)?;
+    built_in_items.set("print", false.into(), activation, context)?;
     Ok(Value::Undefined.into())
 }
 
 pub fn on_select<'gc>(
-    _avm: &mut Avm1<'gc>,
+    _activation: &mut Activation<'_, 'gc>,
     _context: &mut UpdateContext<'_, 'gc, '_>,
     _this: Object<'gc>,
     _args: &[Value<'gc>],
-) -> Result<ReturnValue<'gc>, Error> {
+) -> Result<Value<'gc>, Error<'gc>> {
     log::warn!("ContextMenu.onSelect() not implemented");
     Ok(Value::Undefined.into())
 }
