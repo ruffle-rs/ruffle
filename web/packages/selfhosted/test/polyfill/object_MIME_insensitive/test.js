4f755eafff5401318c88f55a8b119770abda3701
JustinCB <JustinCB@opmbx.org>
2020-07-14 10:15:55 -0400

    web: Add tests for new behaviour

---
+++ web/packages/selfhosted/test/polyfill/object_MIME_insensitive/test.js
@@ -0,0 +1,21 @@
+const { open_test, inject_ruffle_and_wait } = require("../utils");
+const { expect, use } = require("chai");
+const chaiHtml = require("chai-html");
+const fs = require("fs");
+
+use(chaiHtml);
+
+describe("Object with case-insensitive MIME type", () => {
+    it("loads the test", () => {
+        open_test(browser, __dirname);
+    });
+
+    it("Polyfills", () => {
+        inject_ruffle_and_wait(browser);
+        browser.$("<ruffle-object />").waitForExist();
+
+        const actual = browser.$("#test-container").getHTML(false);
+        const expected = fs.readFileSync(`${__dirname}/expected.html`, "utf8");
+        expect(actual).html.to.equal(expected);
+    });
+});
