39b881915a4257c38dbd192e0c40dd88d5e4aa60
David Wendt <dcrkid@yahoo.com>
2020-08-15 18:00:30 -0400

    Implement `addFrameScript`.

--- core/src/avm2/globals/flash/display/movieclip.rs
+++ core/src/avm2/globals/flash/display/movieclip.rs
@@ -4,9 +4,11 @@ use crate::avm2::activation::Activation;
 use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
-use crate::avm2::object::Object;
+use crate::avm2::object::{Object, TObject};
+use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
+use crate::display_object::TDisplayObject;
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `flash.display.MovieClip`'s instance constructor.
@@ -27,13 +29,44 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `addFrameScript`, an undocumented method of `MovieClip` used to
+/// specify what methods of a clip's class run on which frames.
+pub fn add_frame_script<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(mc) = this
+        .and_then(|o| o.as_display_object())
+        .and_then(|dobj| dobj.as_movie_clip())
+    {
+        for (frame_id, callable) in args.chunks_exact(2).map(|s| (s[0].clone(), s[1].clone())) {
+            let frame_id = frame_id.coerce_to_u32(activation)? as u16;
+            let callable = callable.coerce_to_object(activation)?;
+
+            mc.register_frame_script(frame_id, callable, &mut activation.context);
+        }
+    } else {
+        log::error!("Attempted to add frame scripts to non-MovieClip this!");
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `MovieClip`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
-    Class::new(
+    let class = Class::new(
         QName::new(Namespace::package("flash.display"), "MovieClip"),
         Some(QName::new(Namespace::package("flash.display"), "Sprite").into()),
         Method::from_builtin(instance_init),
         Method::from_builtin(class_init),
         mc,
-    )
+    );
+
+    class.write(mc).define_instance_trait(Trait::from_method(
+        QName::new(Namespace::package(""), "addFrameScript"),
+        Method::from_builtin(add_frame_script),
+    ));
+
+    class
 }
