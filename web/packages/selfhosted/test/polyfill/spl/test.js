402f8608bdda8b706e08e908237c60c4e919effd
JustinCB <JustinCB@opmbx.org>
2020-08-19 16:02:12 -0400

    web(test): Add test for spl

---
+++ web/packages/selfhosted/test/polyfill/spl/test.js
@@ -0,0 +1,19 @@
+const { inject_ruffle_and_wait, open_test } = require("../utils");
+const { expect, use } = require("chai");
+const chaiHtml = require("chai-html");
+const fs = require("fs");
+
+use(chaiHtml);
+
+describe("SPL", () => {
+    it("loads the test", () => {
+        open_test(browser, __dirname);
+    });
+
+    it("polyfills with ruffle", () => {
+        inject_ruffle_and_wait(browser);
+        const actual = browser.$("#test-container").getHTML(false);
+        const expected = fs.readFileSync(`${__dirname}/expected.html`, "utf8");
+        expect(actual).html.to.equal(expected);
+    });
+});
