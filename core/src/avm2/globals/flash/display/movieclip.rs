7ca34452a1dd643b1c487b79d18147c63623c3a0
David Wendt <dcrkid@yahoo.com>
2020-08-18 22:36:38 -0400

    avm2: Implement `currentFrameLabel` and `currentFrame`.

--- core/src/avm2/globals/flash/display/movieclip.rs
+++ core/src/avm2/globals/flash/display/movieclip.rs
@@ -5,6 +5,7 @@ use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{Object, TObject};
+use crate::avm2::string::AvmString;
 use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
@@ -64,10 +65,56 @@ pub fn current_frame<'gc>(
         .and_then(|dobj| dobj.as_movie_clip())
     {
         if let Some((_scene, scene_basis)) = mc.current_scene() {
-            return Ok((mc.current_frame() - scene_basis + 1).into());
+            return Ok(((mc.current_frame() + 1) - scene_basis).into());
         } else {
             return Ok(mc.current_frame().into());
         }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `currentFrameLabel`.
+pub fn current_frame_label<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(mc) = this
+        .and_then(|o| o.as_display_object())
+        .and_then(|dobj| dobj.as_movie_clip())
+    {
+        return Ok(mc
+            .current_label()
+            .and_then(|(label, start_frame)| {
+                if start_frame < mc.current_frame() {
+                    None
+                } else {
+                    Some(AvmString::new(activation.context.gc_context, label).into())
+                }
+            })
+            .unwrap_or(Value::Null));
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `currentLabel`.
+pub fn current_label<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(mc) = this
+        .and_then(|o| o.as_display_object())
+        .and_then(|dobj| dobj.as_movie_clip())
+    {
+        return Ok(mc
+            .current_label()
+            .map(|(label, _start_frame)| {
+                AvmString::new(activation.context.gc_context, label).into()
+            })
+            .unwrap_or(Value::Null));
     }
 
     Ok(Value::Undefined)
@@ -93,6 +140,16 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
     write.define_instance_trait(Trait::from_getter(
         QName::new(Namespace::package(""), "currentFrame"),
         Method::from_builtin(current_frame),
+    ));
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::package(""), "currentFrameLabel"),
+        Method::from_builtin(current_frame_label),
+    ));
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::package(""), "currentLabel"),
+        Method::from_builtin(current_label),
     ));
 
     class
