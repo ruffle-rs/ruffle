8be23b2b13d2fac4255364a20d89b08bab5f74fe
Mike Welsh <mwelsh@gmail.com>
2020-08-20 14:49:33 -0700

    tests: Add test for targetPath action

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -297,6 +297,7 @@ swf_tests! {
     (conflicting_instance_names, "avm1/conflicting_instance_names", 6),
     (button_children, "avm1/button_children", 1),
     (transform, "avm1/transform", 1),
+    (target_path, "avm1/target_path", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
