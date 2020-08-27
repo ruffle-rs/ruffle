bd35ebb7932820835524d1ec508416fb28974bf8
David Wendt <dcrkid@yahoo.com>
2020-08-26 22:29:59 -0400

    avm2: Impl `Array` constructor

--- core/src/avm2/globals/array.rs
+++ core/src/avm2/globals/array.rs
@@ -4,18 +4,37 @@ use crate::avm2::activation::Activation;
 use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
-use crate::avm2::object::Object;
+use crate::avm2::object::{Object, TObject};
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `Array`'s instance initializer.
 pub fn instance_init<'gc>(
-    _activation: &mut Activation<'_, 'gc, '_>,
-    _this: Option<Object<'gc>>,
-    _args: &[Value<'gc>],
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error> {
-    Err("Array constructor is a stub.".into())
+    if let Some(this) = this {
+        if let Some(mut array) = this.as_array_storage_mut(activation.context.gc_context) {
+            if args.len() == 1 {
+                if let Some(expected_len) = args
+                    .get(0)
+                    .and_then(|v| v.as_number(activation.context.gc_context).ok())
+                {
+                    array.set_length(expected_len as usize);
+
+                    return Ok(Value::Undefined);
+                }
+            }
+
+            for (i, arg) in args.iter().enumerate() {
+                array.set(i, arg.clone());
+            }
+        }
+    }
+
+    Ok(Value::Undefined)
 }
 
 /// Implements `Array`'s class initializer.
