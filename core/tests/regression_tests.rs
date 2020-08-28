2da3c0d3191158d21f245a0bf4015c283cf930c4
Mike Welsh <mwelsh@gmail.com>
2020-08-27 17:41:03 -0700

    tests: Add hitTest shapeflag test

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -121,6 +121,7 @@ swf_tests! {
     (object_prototypes, "avm1/object_prototypes", 1),
     (movieclip_prototype_extension, "avm1/movieclip_prototype_extension", 1),
     (movieclip_hittest, "avm1/movieclip_hittest", 1),
+    (movieclip_hittest_shapeflag, "avm1/movieclip_hittest_shapeflag", 10),
     #[ignore] (textfield_text, "avm1/textfield_text", 1),
     (recursive_prototypes, "avm1/recursive_prototypes", 2),
     (stage_object_children, "avm1/stage_object_children", 2),
