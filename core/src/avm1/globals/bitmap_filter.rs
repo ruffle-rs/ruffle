5daa878cc0992d1a16ac2fbd5bb420749c7fe92e
CUB3D <callumthom11@gmail.com>
2020-09-22 23:07:47 +0100

    core: Fix edge cases and implement rest of bevel filter

--- core/src/avm1/globals/bitmap_filter.rs
+++ core/src/avm1/globals/bitmap_filter.rs
@@ -2,7 +2,7 @@
 
 use crate::avm1::activation::Activation;
 use crate::avm1::error::Error;
-use crate::avm1::{Object, ScriptObject, Value};
+use crate::avm1::{Object, ScriptObject, TObject, Value};
 use enumset::EnumSet;
 use gc_arena::MutationContext;
 
@@ -15,10 +15,57 @@ pub fn constructor<'gc>(
 }
 
 pub fn clone<'gc>(
-    _activation: &mut Activation<'_, 'gc, '_>,
-    _this: Object<'gc>,
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Object<'gc>,
     _args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
+    if let Some(this) = this.as_blur_filter_object() {
+        let proto = activation.context.avm1.prototypes.blur_filter_constructor;
+
+        let blur_x = this.get("blurX", activation)?;
+        let blur_y = this.get("blurY", activation)?;
+        let quality = this.get("quality", activation)?;
+
+        let cloned = proto.construct(activation, &[blur_x, blur_y, quality])?;
+        return Ok(cloned.into());
+    }
+
+    if let Some(this) = this.as_bevel_filter_object() {
+        let proto = activation.context.avm1.prototypes.bevel_filter_constructor;
+
+        let distance = this.get("distance", activation)?;
+        let angle = this.get("angle", activation)?;
+        let highlight_color = this.get("highlightColor", activation)?;
+        let highlight_alpha = this.get("highlightAlpha", activation)?;
+        let shadow_color = this.get("shadowColor", activation)?;
+        let shadow_alpha = this.get("shadowAlpha", activation)?;
+        let blur_x = this.get("blurX", activation)?;
+        let blur_y = this.get("blurY", activation)?;
+        let strength = this.get("strength", activation)?;
+        let quality = this.get("quality", activation)?;
+        let type_ = this.get("type", activation)?;
+        let knockout = this.get("knockout", activation)?;
+
+        let cloned = proto.construct(
+            activation,
+            &[
+                distance,
+                angle,
+                highlight_color,
+                highlight_alpha,
+                shadow_color,
+                shadow_alpha,
+                blur_x,
+                blur_y,
+                strength,
+                quality,
+                type_,
+                knockout,
+            ],
+        )?;
+        return Ok(cloned.into());
+    }
+
     Ok(Value::Undefined)
 }
 
