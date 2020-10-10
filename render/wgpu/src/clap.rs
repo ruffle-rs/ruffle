89385e84df7a33256e93b97c46329fbcd7bce1b4
Nathan Adams <dinnerbone@dinnerbone.com>
2020-10-10 22:16:32 +0200

    render: Move wgpu options out from desktop and into wgpu render crate

---
+++ render/wgpu/src/clap.rs
@@ -0,0 +1,39 @@
+use clap::Clap;
+
+#[derive(Copy, Clone, Clap, PartialEq, Debug)]
+pub enum GraphicsBackend {
+    Default,
+    Vulkan,
+    Metal,
+    Dx12,
+    Dx11,
+}
+
+impl From<GraphicsBackend> for wgpu::BackendBit {
+    fn from(backend: GraphicsBackend) -> Self {
+        match backend {
+            GraphicsBackend::Default => wgpu::BackendBit::PRIMARY,
+            GraphicsBackend::Vulkan => wgpu::BackendBit::VULKAN,
+            GraphicsBackend::Metal => wgpu::BackendBit::METAL,
+            GraphicsBackend::Dx12 => wgpu::BackendBit::DX12,
+            GraphicsBackend::Dx11 => wgpu::BackendBit::DX11,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Clap, PartialEq, Debug)]
+pub enum PowerPreference {
+    Default = 0,
+    Low = 1,
+    High = 2,
+}
+
+impl From<PowerPreference> for wgpu::PowerPreference {
+    fn from(preference: PowerPreference) -> Self {
+        match preference {
+            PowerPreference::Default => wgpu::PowerPreference::Default,
+            PowerPreference::Low => wgpu::PowerPreference::LowPower,
+            PowerPreference::High => wgpu::PowerPreference::HighPerformance,
+        }
+    }
+}
