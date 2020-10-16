c47490b5199e53ce751da700be19d2acbe441cec
JustinCB <JustinCB@opmbx.org>
2020-10-16 00:15:58 -0400

    web: Re-try getting obfuscated_event_prefix to prevent errors

--- web/packages/extension/js/index.js
+++ web/packages/extension/js/index.js
@@ -5,22 +5,29 @@ window.RufflePlayer = PublicAPI.negotiate(
     "extension",
     new SourceAPI("extension")
 );
+function make_event_handler() {
+    if (window.obfuscated_event_prefix) {
+        document.addEventListener(
+            obfuscated_event_prefix + "_request",
+            function (e) {
+                let body = JSON.parse(e.detail);
+                let response = {};
 
-if (obfuscated_event_prefix) {
-    document.addEventListener(
-        obfuscated_event_prefix + "_request",
-        function (e) {
-            let body = JSON.parse(e.detail);
-            let response = {};
+                if (body.action === "get_page_options") {
+                    //response.page_options = page_options;
+                }
 
-            if (body.action === "get_page_options") {
-                //response.page_options = page_options;
+                let event = new CustomEvent(
+                    obfuscated_event_prefix + "_response",
+                    {
+                        detail: JSON.stringify(response),
+                    }
+                );
+                document.dispatchEvent(event);
             }
-
-            let event = new CustomEvent(obfuscated_event_prefix + "_response", {
-                detail: JSON.stringify(response),
-            });
-            document.dispatchEvent(event);
-        }
-    );
+        );
+    } else {
+        window.setTimeout(make_event_handler, 500);
+    }
 }
+make_event_handler();
