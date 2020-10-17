990052db77601f792f2867554a5f49c2c793bcfd
Mike Welsh <mwelsh@gmail.com>
2020-10-17 13:21:40 -0700

    chore: Bump to webpack 5

--- web/packages/demo/webpack.config.js
+++ web/packages/demo/webpack.config.js
@@ -19,6 +19,9 @@ module.exports = (env, argv) => {
             filename: "index.js",
         },
         mode: mode,
+        experiments: {
+            syncWebAssembly: true,
+        },
         plugins: [
             new CleanWebpackPlugin(),
             new CopyWebpackPlugin({
