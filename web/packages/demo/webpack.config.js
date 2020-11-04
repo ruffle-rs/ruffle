dab79107b2948f2312a7eb8b5d7cd6342b551467
Nathan Adams <dinnerbone@dinnerbone.com>
2020-11-04 23:27:55 +0100

    web: Include git details into the build

--- web/packages/demo/webpack.config.js
+++ web/packages/demo/webpack.config.js
@@ -2,6 +2,7 @@
 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 const path = require("path");
 
 module.exports = (env, argv) => {
@@ -9,6 +10,16 @@ module.exports = (env, argv) => {
     if (argv && argv.mode) {
         mode = argv.mode;
     }
+
+    const commitHash = require("child_process")
+        .execSync("git rev-parse --short HEAD")
+        .toString();
+
+    const commitDate = require("child_process")
+        .execSync("git log -1 --date=short --pretty=format:%cd")
+        .toString();
+
+    const channel = process.env.CFG_RELEASE_CHANNEL || "nightly".toLowerCase();
 
     console.log(`Building ${mode}...`);
 
@@ -25,6 +36,11 @@ module.exports = (env, argv) => {
         },
         plugins: [
             new CleanWebpackPlugin(),
+            new webpack.DefinePlugin({
+                __COMMIT_HASH__: JSON.stringify(commitHash),
+                __COMMIT_DATE__: JSON.stringify(commitDate),
+                __CHANNEL__: JSON.stringify(channel),
+            }),
             new CopyWebpackPlugin({
                 patterns: [
                     {
