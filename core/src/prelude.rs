c97ff2b02464c0493b71dff6873eb8969a7e8890
David Wendt <dcrkid@yahoo.com>
2020-11-10 19:34:46 -0500

    core: Implement remove-by-range functionality

--- core/src/prelude.rs
+++ core/src/prelude.rs
@@ -7,6 +7,7 @@ pub use crate::{
     impl_display_object, impl_display_object_container, impl_display_object_sansbounds,
 };
 pub use log::{error, info, trace, warn};
+pub use std::ops::Range;
 pub use swf::Matrix;
 pub use swf::{CharacterId, Color, Twips};
 
