879fa534174bfa32ff1241c5b27ef6a4f50747a5
Nathan Adams <dinnerbone@dinnerbone.com>
2020-11-17 00:01:25 +0100

    web: Ignore ruffle-imports, it's not an API

--- web/packages/core/src/ruffle-imports.ts
+++ web/packages/core/src/ruffle-imports.ts
@@ -1,16 +1,19 @@
 /**
- * Functions improted from JS into Ruffle.
+ * Functions imported from JS into Ruffle.
+ *
+ * @ignore
  */
 
 /**
  * Copies data into the given audio channel.
  * This is necessary because Safari does not support `AudioBuffer.copyToChannel`.
+ * @internal
  */
 export function copyToAudioBuffer(
     audio_buffer: AudioBuffer,
     left_data: ArrayLike<number>,
     right_data: ArrayLike<number>
-) {
+): void {
     if (left_data) {
         const dst_buffer = audio_buffer.getChannelData(0);
         dst_buffer.set(left_data);
