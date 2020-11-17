c20edf9e3964ae6970bcbf6f65c899727949abaa
Nathan Adams <dinnerbone@dinnerbone.com>
2020-11-17 22:59:26 +0100

    web: Fix documentation and warnings inside plugin-polyfill

--- web/packages/core/src/load-ruffle.ts
+++ web/packages/core/src/load-ruffle.ts
@@ -1,8 +1,10 @@
-/* global runtime_path */
+/* eslint @typescript-eslint/no-explicit-any: "off" */
 
 /**
  * Conditional ruffle loader
  */
+
+import { Ruffle } from "../pkg/ruffle_web";
 
 /**
  * Load ruffle from an automatically-detected location.
@@ -11,7 +13,7 @@
  * You should not use it directly; this module will memoize the resource
  * download.
  */
-async function fetch_ruffle() {
+async function fetch_ruffle(): Promise<{ new (...args: any[]): Ruffle }> {
     try {
         //If runtime_path is defined then we are executing inside the extension
         //closure. In that case, we configure our local Webpack instance
@@ -30,14 +32,14 @@ async function fetch_ruffle() {
     return ruffle_module.Ruffle;
 }
 
-let last_loaded_ruffle: any = null;
+let last_loaded_ruffle: Promise<{ new (...args: any[]): Ruffle }> | null = null;
 
 /**
  * Obtain an instance of `Ruffle`.
  *
  * This function returns a promise which yields `Ruffle` asynchronously.
  */
-export function load_ruffle() {
+export function load_ruffle(): Promise<{ new (...args: any[]): Ruffle }> {
     if (last_loaded_ruffle == null) {
         last_loaded_ruffle = fetch_ruffle();
     }
