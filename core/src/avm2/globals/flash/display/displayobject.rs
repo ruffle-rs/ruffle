bd00bea744ca340ed60f4f871f7cdbcea121bb87
David Wendt <dcrkid@yahoo.com>
2020-11-25 21:56:39 -0500

    avm2: Expose `alpha`, `height`, `scaleY`, and `scaleX` to script code.

--- core/src/avm2/globals/flash/display/displayobject.rs
+++ core/src/avm2/globals/flash/display/displayobject.rs
@@ -4,9 +4,12 @@ use crate::avm2::activation::Activation;
 use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
-use crate::avm2::object::Object;
+use crate::avm2::object::{Object, TObject};
+use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
+use crate::display_object::TDisplayObject;
+use crate::types::Percent;
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `flash.display.DisplayObject`'s instance constructor.
@@ -27,13 +30,178 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `alpha`'s getter.
+pub fn alpha<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        return Ok(dobj.alpha().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `alpha`'s setter.
+pub fn set_alpha<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        let new_alpha = args
+            .get(0)
+            .cloned()
+            .unwrap_or(Value::Undefined)
+            .coerce_to_number(activation)?;
+        dobj.set_alpha(activation.context.gc_context, new_alpha);
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `height`'s getter.
+pub fn height<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        return Ok(dobj.height().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `height`'s setter.
+pub fn set_height<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        let new_alpha = args
+            .get(0)
+            .cloned()
+            .unwrap_or(Value::Undefined)
+            .coerce_to_number(activation)?;
+        dobj.set_height(activation.context.gc_context, new_alpha);
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `scaleX`'s getter.
+pub fn scale_y<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        return Ok(Value::Number(
+            dobj.scale_y(activation.context.gc_context).into_unit(),
+        ));
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `scaleX`'s setter.
+pub fn set_scale_y<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        let new_scale = args
+            .get(0)
+            .cloned()
+            .unwrap_or(Value::Undefined)
+            .coerce_to_number(activation)?;
+        dobj.set_scale_y(activation.context.gc_context, Percent::from_unit(new_scale));
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `scaleX`'s getter.
+pub fn scale_x<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        return Ok(Value::Number(
+            dobj.scale_x(activation.context.gc_context).into_unit(),
+        ));
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `scaleX`'s setter.
+pub fn set_scale_x<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(dobj) = this.and_then(|this| this.as_display_object()) {
+        let new_scale = args
+            .get(0)
+            .cloned()
+            .unwrap_or(Value::Undefined)
+            .coerce_to_number(activation)?;
+        dobj.set_scale_x(activation.context.gc_context, Percent::from_unit(new_scale));
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `DisplayObject`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
-    Class::new(
+    let class = Class::new(
         QName::new(Namespace::package("flash.display"), "DisplayObject"),
         Some(QName::new(Namespace::package("flash.events"), "EventDispatcher").into()),
         Method::from_builtin(instance_init),
         Method::from_builtin(class_init),
         mc,
-    )
+    );
+
+    let mut write = class.write(mc);
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::package(""), "alpha"),
+        Method::from_builtin(alpha),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::package(""), "alpha"),
+        Method::from_builtin(set_alpha),
+    ));
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::package(""), "height"),
+        Method::from_builtin(height),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::package(""), "height"),
+        Method::from_builtin(set_height),
+    ));
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::package(""), "scaleY"),
+        Method::from_builtin(scale_y),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::package(""), "scaleY"),
+        Method::from_builtin(set_scale_y),
+    ));
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::package(""), "scaleX"),
+        Method::from_builtin(scale_x),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::package(""), "scaleX"),
+        Method::from_builtin(set_scale_x),
+    ));
+
+    class
 }
