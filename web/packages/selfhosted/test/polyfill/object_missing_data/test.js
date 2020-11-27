7898776e0c34c7774e1aecf12887caa86b3ff174
Mike Welsh <mwelsh@gmail.com>
2020-11-26 16:56:37 -0800

    tests: Add polyfill tests for missing movie

---
+++ web/packages/selfhosted/test/polyfill/object_missing_data/test.js
@@ -0,0 +1,19 @@
+const { open_test, inject_ruffle_and_wait } = require("../utils");
+const { expect, use } = require("chai");
+const chaiHtml = require("chai-html");
+const fs = require("fs");
+
+use(chaiHtml);
+
+describe("Object without data attribute", () => {
+    it("loads the test", () => {
+        open_test(browser, __dirname);
+    });
+
+    it("doesn't polyfill with ruffle", () => {
+        inject_ruffle_and_wait(browser);
+        const actual = browser.$("#test-container").getHTML(false);
+        const expected = fs.readFileSync(`${__dirname}/expected.html`, "utf8");
+        expect(actual).html.to.equal(expected);
+    });
+});
