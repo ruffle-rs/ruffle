afacf2a7ef5c1c6d16b001266a2aeb9a8b704297
David Wendt <dcrkid@yahoo.com>
2020-12-07 21:54:32 -0500

    avm2: Implement `Event.bubbles`, `cancelable`, and `type`.

--- core/src/avm2/globals/flash/events/event.rs
+++ core/src/avm2/globals/flash/events/event.rs
@@ -6,6 +6,7 @@ use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{EventObject, Object, TObject};
 use crate::avm2::scope::Scope;
+use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use gc_arena::{GcCell, MutationContext};
@@ -49,15 +50,71 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `bubbles` property's getter
+pub fn bubbles<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(evt) = this.unwrap().as_event() {
+        return Ok(evt.is_bubbling().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `cancelable` property's getter
+pub fn cancelable<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(evt) = this.unwrap().as_event() {
+        return Ok(evt.is_cancelable().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `type` property's getter
+pub fn get_type<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(evt) = this.unwrap().as_event() {
+        return Ok(evt.event_type().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `Event`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
-    Class::new(
+    let class = Class::new(
         QName::new(Namespace::package("flash.events"), "Event"),
         Some(QName::new(Namespace::public_namespace(), "Object").into()),
         Method::from_builtin(instance_init),
         Method::from_builtin(class_init),
         mc,
-    )
+    );
+
+    let mut write = class.write(mc);
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public_namespace(), "bubbles"),
+        Method::from_builtin(bubbles),
+    ));
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public_namespace(), "cancelable"),
+        Method::from_builtin(cancelable),
+    ));
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public_namespace(), "type"),
+        Method::from_builtin(get_type),
+    ));
+
+    class
 }
 
 /// Object deriver for `Event`
