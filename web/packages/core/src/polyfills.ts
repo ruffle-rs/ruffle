c03f7ef08dda7049925ec2eb30e90f266b06caf5
Toad06 <gd.productions@laposte.net>
2020-12-09 11:15:54 +0100

    web: Avoid double polyfilling object-inside-object

--- web/packages/core/src/polyfills.ts
+++ web/packages/core/src/polyfills.ts
@@ -35,26 +35,27 @@ let embeds: HTMLCollectionOf<HTMLElement>;
 function replaceFlashInstances(): void {
     try {
         // Create live collections to track embed tags.
-        objects = objects || document.getElementsByTagName("object");
-        embeds = embeds || document.getElementsByTagName("embed");
+        objects = objects ?? document.getElementsByTagName("object");
+        embeds = embeds ?? document.getElementsByTagName("embed");
 
         // Replace <object> first, because <object> often wraps <embed>.
-        for (const elem of Array.from(objects)) {
-            if (RuffleObject.isInterdictable(elem)) {
+        for (const obj of Array.from(objects)) {
+            const elem = RuffleObject.isInterdictable(obj);
+            if (elem) {
                 const ruffleObject = RuffleObject.fromNativeObjectElement(elem);
                 elem.replaceWith(ruffleObject);
             }
         }
-        for (const elem of Array.from(embeds)) {
-            if (RuffleEmbed.isInterdictable(elem)) {
-                const ruffleObject = RuffleEmbed.fromNativeEmbedElement(elem);
-                elem.replaceWith(ruffleObject);
+        for (const emb of Array.from(embeds)) {
+            const elem = RuffleEmbed.isInterdictable(emb);
+            if (elem) {
+                const ruffleEmbed = RuffleEmbed.fromNativeEmbedElement(elem);
+                elem.replaceWith(ruffleEmbed);
             }
         }
     } catch (err) {
         console.error(
-            "Serious error encountered when polyfilling native Flash elements: " +
-                err
+            `Serious error encountered when polyfilling native Flash elements: ${err}`
         );
     }
 }
