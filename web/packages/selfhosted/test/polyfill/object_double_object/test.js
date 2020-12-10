c5acdae944d6dd20023ff03b3ccef2a4773dcd7c
Toad06 <gd.productions@laposte.net>
2020-12-10 10:24:36 +0100

    tests: Add polyfill test for object inside object

---
+++ web/packages/selfhosted/test/polyfill/object_double_object/test.js
@@ -0,0 +1,30 @@
+const {
+    inject_ruffle_and_wait,
+    open_test,
+    play_and_monitor,
+} = require("../utils");
+const { expect, use } = require("chai");
+const chaiHtml = require("chai-html");
+const fs = require("fs");
+
+use(chaiHtml);
+
+describe("Object with another object tag", () => {
+    it("loads the test", () => {
+        open_test(browser, __dirname);
+    });
+
+    it("polyfills only the first tag with ruffle", () => {
+        inject_ruffle_and_wait(browser);
+        const actual = browser.$("#test-container").getHTML(false);
+        const expected = fs.readFileSync(`${__dirname}/expected.html`, "utf8");
+        expect(actual).html.to.equal(expected);
+    });
+
+    it("Plays a movie", () => {
+        play_and_monitor(
+            browser,
+            browser.$("#test-container").$("<ruffle-object />")
+        );
+    });
+});
