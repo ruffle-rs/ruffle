b9aed2c7bb344219b48988c3ef107b4deb1822cb
Toad06 <gd.productions@laposte.net>
2020-12-14 10:58:47 +0100

    web: Rename from snake_case to camelCase

--- web/packages/extension/js/lv0.js
+++ web/packages/extension/js/lv0.js
@@ -26,18 +26,18 @@ const {
 } = require("./util.js");
 
 get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
-    let page_optout = document.documentElement.hasAttribute(
+    let pageOptout = document.documentElement.hasAttribute(
         "data-ruffle-optout"
     );
     try {
         if (
-            !page_optout &&
+            !pageOptout &&
             window.top &&
             window.top.document &&
             window.top.document.documentElement
         ) {
             /* In case the opting out page uses iframes */
-            page_optout = window.top.document.documentElement.hasAttribute(
+            pageOptout = window.top.document.documentElement.hasAttribute(
                 "data-ruffle-optout"
             );
         }
@@ -45,28 +45,28 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
         console.log("Unable to check top-level optout: " + e.message);
     }
 
-    let should_load_untrusted_world = !(page_optout || window.RufflePlayer);
-    let obfuscated_event_prefix =
+    let shouldLoadUntrustedWorld = !(pageOptout || window.RufflePlayer);
+    let obfuscatedEventPrefix =
         "rufEvent" + Math.floor(Math.random() * 100000000000);
-    let next_response_promise = null;
-    let next_response_promise_resolve = null;
+    let nextResponsePromise = null;
+    let nextResponsePromiseResolve = null;
 
     if (data) {
-        should_load_untrusted_world =
+        shouldLoadUntrustedWorld =
             data.ruffle_enable &&
-            !((page_optout && !data.ignore_optout) || window.RufflePlayer);
+            !((pageOptout && !data.ignore_optout) || window.RufflePlayer);
     } else {
         console.log("Couldn't read settings.");
     }
 
     document.addEventListener(
-        obfuscated_event_prefix + "_response",
+        obfuscatedEventPrefix + "_response",
         function (e) {
-            if (next_response_promise_resolve !== null) {
-                next_response_promise_resolve(e);
+            if (nextResponsePromiseResolve !== null) {
+                nextResponsePromiseResolve(e);
 
-                next_response_promise = null;
-                next_response_promise_resolve = null;
+                nextResponsePromise = null;
+                nextResponsePromiseResolve = null;
             }
         }
     );
@@ -75,14 +75,14 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
      * Returns a promise which resolves the next time we receive our custom
      * event response.
      */
-    function next_response() {
-        if (next_response_promise == null) {
-            next_response_promise = new Promise(function (resolve) {
-                next_response_promise_resolve = resolve;
+    function nextResponse() {
+        if (nextResponsePromise == null) {
+            nextResponsePromise = new Promise(function (resolve) {
+                nextResponsePromiseResolve = resolve;
             });
         }
 
-        return next_response_promise;
+        return nextResponsePromise;
     }
 
     /**
@@ -95,29 +95,29 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
      * @param request The request data from the background/popup page
      * @returns Response data to reply to the sender with.
      */
-    async function marshal_message_into_untrusted_world(request) {
-        let req_event = new CustomEvent(obfuscated_event_prefix + "_request", {
+    async function marshalMessageIntoUntrustedWorld(request) {
+        let reqEvent = new CustomEvent(obfuscatedEventPrefix + "_request", {
             detail: JSON.stringify(request),
         });
-        let resp_event_handler = next_response();
+        let respEventHandler = nextResponse();
 
-        document.dispatchEvent(req_event);
+        document.dispatchEvent(reqEvent);
 
-        let resp_event = await resp_event_handler;
-        return JSON.parse(resp_event.detail);
+        let respEvent = await respEventHandler;
+        return JSON.parse(respEvent.detail);
     }
 
-    set_message_listener(function (request, sender, response_callback) {
-        if (should_load_untrusted_world) {
-            let response_promise = marshal_message_into_untrusted_world(
+    set_message_listener(function (request, sender, responseCallback) {
+        if (shouldLoadUntrustedWorld) {
+            let responsePromise = marshalMessageIntoUntrustedWorld(
                 request
             );
-            response_promise
+            responsePromise
                 .then(function (response) {
-                    response_callback({
+                    responseCallback({
                         loaded: true,
                         tab_settings: data,
-                        optout: page_optout,
+                        optout: pageOptout,
                         untrusted_response: response,
                     });
                 })
@@ -131,46 +131,46 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
 
             return true;
         } else {
-            response_callback({
+            responseCallback({
                 loaded: false,
                 tab_settings: data,
-                optout: page_optout,
+                optout: pageOptout,
             });
 
             return false;
         }
     });
 
-    const ext_path = get_extension_url();
+    const extPath = get_extension_url();
 
-    if (should_load_untrusted_world) {
+    if (shouldLoadUntrustedWorld) {
         // We must run the plugin polyfill before any flash detection scripts.
         // Unfortunately, this might still be too late for some websites when using Chrome (issue #969).
-        let polyfill_script = document.createElement("script");
-        polyfill_script.innerHTML =
+        let polyfillScript = document.createElement("script");
+        polyfillScript.innerHTML =
             '(function(){class RuffleMimeType{constructor(a,b,c){this.type=a,this.description=b,this.suffixes=c}}class RuffleMimeTypeArray{constructor(a){this.__mimetypes=[],this.__named_mimetypes={};for(let b of a)this.install(b)}install(a){let b=this.__mimetypes.length;this.__mimetypes.push(a),this.__named_mimetypes[a.type]=a,this[a.type]=a,this[b]=a}item(a){return this.__mimetypes[a]}namedItem(a){return this.__named_mimetypes[a]}get length(){return this.__mimetypes.length}}class RufflePlugin extends RuffleMimeTypeArray{constructor(a,b,c,d){super(d),this.name=a,this.description=b,this.filename=c}install(a){a.enabledPlugin||(a.enabledPlugin=this),super.install(a)}}class RufflePluginArray{constructor(a){this.__plugins=[],this.__named_plugins={};for(let b of a)this.install(b)}install(a){let b=this.__plugins.length;this.__plugins.push(a),this.__named_plugins[a.name]=a,this[a.name]=a,this[b]=a}item(a){return this.__plugins[a]}namedItem(a){return this.__named_plugins[a]}get length(){return this.__plugins.length}}const FLASH_PLUGIN=new RufflePlugin("Shockwave Flash","Shockwave Flash 32.0 r0","ruffle.js",[new RuffleMimeType("application/futuresplash","Shockwave Flash","spl"),new RuffleMimeType("application/x-shockwave-flash","Shockwave Flash","swf"),new RuffleMimeType("application/x-shockwave-flash2-preview","Shockwave Flash","swf"),new RuffleMimeType("application/vnd.adobe.flash-movie","Shockwave Flash","swf")]);function install_plugin(a){navigator.plugins.install||Object.defineProperty(navigator,"plugins",{value:new RufflePluginArray(navigator.plugins),writable:!1}),navigator.plugins.install(a),0<a.length&&!navigator.mimeTypes.install&&Object.defineProperty(navigator,"mimeTypes",{value:new RuffleMimeTypeArray(navigator.mimeTypes),writable:!1});for(var b=0;b<a.length;b+=1)navigator.mimeTypes.install(a[b])}install_plugin(FLASH_PLUGIN);})();';
         (document.head || document.documentElement).appendChild(
-            polyfill_script
+            polyfillScript
         );
 
         // Load Ruffle script asynchronously. By doing so, we can inject extra variables and isolate them from the global scope.
         (async function () {
-            let ruffle_src_resp = await fetch(ext_path + "dist/ruffle.js");
-            if (ruffle_src_resp.ok) {
-                let ruffle_source =
+            let ruffleSrcResp = await fetch(extPath + "dist/ruffle.js");
+            if (ruffleSrcResp.ok) {
+                let ruffleSource =
                     '(function () { var ruffleRuntimePath = "' +
-                    ext_path +
-                    '";\nvar obfuscated_event_prefix = "' +
-                    obfuscated_event_prefix +
+                    extPath +
+                    '";\nvar obfuscatedEventPrefix = "' +
+                    obfuscatedEventPrefix +
                     '";\n' +
-                    (await ruffle_src_resp.text()) +
+                    (await ruffleSrcResp.text()) +
                     "}())";
-                let ruffle_script = document.createElement("script");
-                ruffle_script.appendChild(
-                    document.createTextNode(ruffle_source)
+                let ruffleScript = document.createElement("script");
+                ruffleScript.appendChild(
+                    document.createTextNode(ruffleSource)
                 );
                 (document.head || document.documentElement).appendChild(
-                    ruffle_script
+                    ruffleScript
                 );
             } else {
                 console.error("Critical error loading Ruffle into page");
