25d5f48d185ec8768426e397a31321b3fcef6302
Toad06 <gd.productions@laposte.net>
2020-12-14 11:29:20 +0100

    web: Run eslint

--- web/packages/extension/js/lv0.js
+++ web/packages/extension/js/lv0.js
@@ -109,9 +109,7 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
 
     set_message_listener(function (request, sender, responseCallback) {
         if (shouldLoadUntrustedWorld) {
-            let responsePromise = marshalMessageIntoUntrustedWorld(
-                request
-            );
+            let responsePromise = marshalMessageIntoUntrustedWorld(request);
             responsePromise
                 .then(function (response) {
                     responseCallback({
@@ -149,9 +147,7 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
         let polyfillScript = document.createElement("script");
         polyfillScript.innerHTML =
             '(function(){class RuffleMimeType{constructor(a,b,c){this.type=a,this.description=b,this.suffixes=c}}class RuffleMimeTypeArray{constructor(a){this.__mimetypes=[],this.__named_mimetypes={};for(let b of a)this.install(b)}install(a){let b=this.__mimetypes.length;this.__mimetypes.push(a),this.__named_mimetypes[a.type]=a,this[a.type]=a,this[b]=a}item(a){return this.__mimetypes[a]}namedItem(a){return this.__named_mimetypes[a]}get length(){return this.__mimetypes.length}}class RufflePlugin extends RuffleMimeTypeArray{constructor(a,b,c,d){super(d),this.name=a,this.description=b,this.filename=c}install(a){a.enabledPlugin||(a.enabledPlugin=this),super.install(a)}}class RufflePluginArray{constructor(a){this.__plugins=[],this.__named_plugins={};for(let b of a)this.install(b)}install(a){let b=this.__plugins.length;this.__plugins.push(a),this.__named_plugins[a.name]=a,this[a.name]=a,this[b]=a}item(a){return this.__plugins[a]}namedItem(a){return this.__named_plugins[a]}get length(){return this.__plugins.length}}const FLASH_PLUGIN=new RufflePlugin("Shockwave Flash","Shockwave Flash 32.0 r0","ruffle.js",[new RuffleMimeType("application/futuresplash","Shockwave Flash","spl"),new RuffleMimeType("application/x-shockwave-flash","Shockwave Flash","swf"),new RuffleMimeType("application/x-shockwave-flash2-preview","Shockwave Flash","swf"),new RuffleMimeType("application/vnd.adobe.flash-movie","Shockwave Flash","swf")]);function install_plugin(a){navigator.plugins.install||Object.defineProperty(navigator,"plugins",{value:new RufflePluginArray(navigator.plugins),writable:!1}),navigator.plugins.install(a),0<a.length&&!navigator.mimeTypes.install&&Object.defineProperty(navigator,"mimeTypes",{value:new RuffleMimeTypeArray(navigator.mimeTypes),writable:!1});for(var b=0;b<a.length;b+=1)navigator.mimeTypes.install(a[b])}install_plugin(FLASH_PLUGIN);})();';
-        (document.head || document.documentElement).appendChild(
-            polyfillScript
-        );
+        (document.head || document.documentElement).appendChild(polyfillScript);
 
         // Load Ruffle script asynchronously. By doing so, we can inject extra variables and isolate them from the global scope.
         (async function () {
@@ -166,9 +162,7 @@ get_sync_storage(["ruffle_enable", "ignore_optout"], function (data) {
                     (await ruffleSrcResp.text()) +
                     "}())";
                 let ruffleScript = document.createElement("script");
-                ruffleScript.appendChild(
-                    document.createTextNode(ruffleSource)
-                );
+                ruffleScript.appendChild(document.createTextNode(ruffleSource));
                 (document.head || document.documentElement).appendChild(
                     ruffleScript
                 );
