2faf5259ca4884993ae30ca1fd9b730b1c2d6100
David Wendt <dcrkid@yahoo.com>
2020-12-17 18:26:47 -0500

    avm2: Add a representation for event dispatch lists.
    For various reasons, we store the dispatch list for an object on a separate property of an `EventDispatcher` rather than dictating that all children of `EventDispatcher` use a specific object type. This is because `EventDispatcher` is a very general class with lots of object representations it needs to cover. So instead, we introduce a new object representation for a *property* and store it in a Ruffle private namespace that is as isolated from user code as alternate object representations are.

--- core/src/avm2/events.rs
+++ core/src/avm2/events.rs
@@ -1,8 +1,10 @@
 //! Core event structure
 
-use crate::avm2::object::Object;
+use crate::avm2::object::{Object, TObject};
 use crate::avm2::string::AvmString;
 use gc_arena::Collect;
+use std::collections::{BTreeMap, HashMap};
+use std::hash::{Hash, Hasher};
 
 /// Which phase of event dispatch is currently occurring.
 #[derive(Copy, Clone, Collect, Debug, PartialEq, Eq)]
@@ -159,5 +161,147 @@ impl<'gc> Event<'gc> {
 
     pub fn current_target(&self) -> Option<Object<'gc>> {
         self.current_target
+    }
+}
+
+/// A set of handlers organized by event type, priority, and order added.
+#[derive(Clone, Collect, Debug)]
+#[collect(no_drop)]
+pub struct DispatchList<'gc>(HashMap<AvmString<'gc>, BTreeMap<i32, Vec<EventHandler<'gc>>>>);
+
+impl<'gc> DispatchList<'gc> {
+    /// Construct a new dispatch list.
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    /// Get all of the event handlers for a given event type, if such a type
+    /// exists.
+    fn get_event(
+        &self,
+        event: impl Into<AvmString<'gc>>,
+    ) -> Option<&BTreeMap<i32, Vec<EventHandler<'gc>>>> {
+        self.0.get(&event.into())
+    }
+
+    /// Get all of the event handlers for a given event type, for mutation.
+    ///
+    /// If the event type does not exist, it will be added to the dispatch
+    /// list.
+    fn get_event_mut(
+        &mut self,
+        event: impl Into<AvmString<'gc>>,
+    ) -> &mut BTreeMap<i32, Vec<EventHandler<'gc>>> {
+        self.0.entry(event.into()).or_insert_with(BTreeMap::new)
+    }
+
+    /// Get a single priority level of event handlers for a given event type,
+    /// for mutation.
+    fn get_event_priority_mut(
+        &mut self,
+        event: impl Into<AvmString<'gc>>,
+        priority: i32,
+    ) -> &mut Vec<EventHandler<'gc>> {
+        self.0
+            .entry(event.into())
+            .or_insert_with(BTreeMap::new)
+            .entry(priority)
+            .or_insert_with(Vec::new)
+    }
+
+    /// Add an event handler to this dispatch list.
+    ///
+    /// This enforces the invariant that an `EventHandler` must not appear at
+    /// more than one priority (since we can't enforce that with clever-er data
+    /// structure selection). If an event handler already exists, it will not
+    /// be added again, and this function will silently fail.
+    pub fn add_event_listener(
+        &mut self,
+        event: impl Into<AvmString<'gc>> + Clone,
+        priority: i32,
+        handler: Object<'gc>,
+        use_capture: bool,
+    ) {
+        let new_handler = (handler, use_capture).into();
+
+        if let Some(event_sheaf) = self.get_event(event.clone()) {
+            for (_other_prio, other_set) in event_sheaf.iter() {
+                if other_set.contains(&new_handler) {
+                    return;
+                }
+            }
+        }
+
+        self.get_event_priority_mut(event, priority)
+            .push(new_handler);
+    }
+
+    /// Remove an event handler from this dispatch list.
+    ///
+    /// Any listener that has the same handler and capture-phase flag will be
+    /// removed from any priority in the list.
+    pub fn remove_event_listener(
+        &mut self,
+        event: impl Into<AvmString<'gc>>,
+        handler: Object<'gc>,
+        use_capture: bool,
+    ) {
+        let old_handler: EventHandler<'gc> = (handler, use_capture).into();
+
+        for (_prio, set) in self.get_event_mut(event).iter_mut() {
+            if let Some(pos) = set.iter().position(|h| *h == old_handler) {
+                set.remove(pos);
+            }
+        }
+    }
+
+    /// Determine if there are any event listeners in this dispatch list.
+    pub fn has_event_listener(&self, event: impl Into<AvmString<'gc>>) -> bool {
+        if let Some(event_sheaf) = self.get_event(event) {
+            for (_prio, set) in event_sheaf.iter() {
+                if !set.is_empty() {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+}
+
+/// A single instance of an event handler.
+#[derive(Clone, Collect, Debug)]
+#[collect(no_drop)]
+struct EventHandler<'gc> {
+    /// The event handler to call.
+    handler: Object<'gc>,
+
+    /// Indicates if this handler should only be called for capturing events
+    /// (when `true`), or if it should only be called for bubbling and
+    /// at-target events (when `false`).
+    use_capture: bool,
+}
+
+impl<'gc> From<(Object<'gc>, bool)> for EventHandler<'gc> {
+    fn from(parts: (Object<'gc>, bool)) -> Self {
+        Self {
+            handler: parts.0,
+            use_capture: parts.1,
+        }
+    }
+}
+
+impl<'gc> PartialEq for EventHandler<'gc> {
+    fn eq(&self, rhs: &Self) -> bool {
+        self.use_capture == rhs.use_capture && Object::ptr_eq(self.handler, rhs.handler)
+    }
+}
+
+impl<'gc> Eq for EventHandler<'gc> {}
+
+impl<'gc> Hash for EventHandler<'gc> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.use_capture.hash(state);
+        self.handler.as_ptr().hash(state);
     }
 }
