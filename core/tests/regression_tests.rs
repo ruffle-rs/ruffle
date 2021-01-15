094563e28f59da3757e3360ee05ca94481195664
David Wendt <dcrkid@yahoo.com>
2021-01-14 22:26:15 -0500

    tests: Add a (currently failing) test for the execution order of various events in the presence of programmed display tree manipulations.

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -523,6 +523,7 @@ swf_tests! {
     (as3_string_char_code_at, "avm2/string_char_code_at", 1),
     (as3_typeof, "avm2/typeof", 1),
     (use_hand_cursor, "avm1/use_hand_cursor", 1),
+    (as3_movieclip_displayevents, "avm2/movieclip_displayevents", 9),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
