a94c9c4c905dd32827baa74bb00f8545760ac67c
Toad06 <gd.productions@laposte.net>
2021-01-18 17:10:36 +0100

    web: Display appropriate errors when Ruffle fails to initialize

--- web/packages/core/src/load-ruffle.ts
+++ web/packages/core/src/load-ruffle.ts
@@ -22,6 +22,8 @@ async function fetchRuffle(): Promise<{ new (...args: any[]): Ruffle }> {
     // libraries, if needed.
     setPolyfillsOnLoad();
 
+    let isExtension = true;
+
     try {
         // If ruffleRuntimePath is defined then we are executing inside the extension
         // closure. In that case, we configure our local Webpack instance.
@@ -32,18 +34,25 @@ async function fetchRuffle(): Promise<{ new (...args: any[]): Ruffle }> {
         if (!(e instanceof ReferenceError)) {
             throw e;
         }
+        isExtension = false;
     }
 
     // We currently assume that if we are not executing inside the extension,
     // then we can use webpack to get Ruffle.
 
-    // wasm files are set to use file-loader,
-    // so this package will resolve to the URL of the wasm file.
-    const ruffleWasm = await import(
-        /* webpackMode: "eager" */
-        "../pkg/ruffle_web_bg.wasm"
-    );
-    await init(ruffleWasm.default);
+    try {
+        // wasm files are set to use file-loader,
+        // so this package will resolve to the URL of the wasm file.
+        const ruffleWasm = await import(
+            /* webpackMode: "eager" */
+            "../pkg/ruffle_web_bg.wasm"
+        );
+        await init(ruffleWasm.default);
+    } catch (e) {
+        e.ruffleIsExtension = isExtension;
+        throw e;
+    }
+
     return Ruffle;
 }
 
