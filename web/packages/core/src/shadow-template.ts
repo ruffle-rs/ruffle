f9d981bc20bbaa062688e8021c9b117398692757
relrelb <relrelbachar@gmail.com>
2021-01-23 18:02:13 +0200

    web: Rename "right click menu" to "context menu"

--- web/packages/core/src/shadow-template.ts
+++ web/packages/core/src/shadow-template.ts
@@ -156,7 +156,8 @@ ruffleShadowTemplate.innerHTML = `
             background-color: rgba(255, 255, 255, 0.3);
         }
 
-        #right_click_menu {
+        #context-menu {
+            display: none;
             color: #FFAD33;
             background-color: #37528c;
             border-radius: 5px;
@@ -169,38 +170,38 @@ ruffleShadowTemplate.innerHTML = `
             margin: 0;
         }
 
-        #right_click_menu .menu_item {
+        #context-menu .menu_item {
             padding: 5px 10px;
         }
 
-        #right_click_menu .menu_separator {
+        #context-menu .menu_separator {
             padding: 5px 5px;
         }
 
-        #right_click_menu .active {
+        #context-menu .active {
             cursor: pointer;
             color: #FFAD33;
         }
 
-        #right_click_menu .disabled {
+        #context-menu .disabled {
             cursor: default;
             color: #94672F;
         }
 
-        #right_click_menu .active:hover {
+        #context-menu .active:hover {
             background-color: #184778;
         }
 
-        #right_click_menu hr {
+        #context-menu hr {
             color: #FFAD33;
         }
 
-        #right_click_menu > :first-child {
+        #context-menu > :first-child {
             border-top-right-radius: 5px;
             border-top-left-radius: 5px;
         }
 
-        #right_click_menu > :last-child {
+        #context-menu > :last-child {
             border-bottom-right-radius: 5px;
             border-bottom-left-radius: 5px;
         }
@@ -212,5 +213,5 @@ ruffleShadowTemplate.innerHTML = `
         <div id="unmute_overlay"><div class="background"></div><div class="icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" viewBox="0 0 512 584" style="width:100%;height:100%;scale:0.8;"><path fill="#FFF" stroke="#FFF" d="m457.941 256 47.029-47.029c9.372-9.373 9.372-24.568 0-33.941-9.373-9.373-24.568-9.373-33.941 0l-47.029 47.029-47.029-47.029c-9.373-9.373-24.568-9.373-33.941 0-9.372 9.373-9.372 24.568 0 33.941l47.029 47.029-47.029 47.029c-9.372 9.373-9.372 24.568 0 33.941 4.686 4.687 10.827 7.03 16.97 7.03s12.284-2.343 16.971-7.029l47.029-47.03 47.029 47.029c4.687 4.687 10.828 7.03 16.971 7.03s12.284-2.343 16.971-7.029c9.372-9.373 9.372-24.568 0-33.941z"/><path fill="#FFF" stroke="#FFF" d="m99 160h-55c-24.301 0-44 19.699-44 44v104c0 24.301 19.699 44 44 44h55c2.761 0 5-2.239 5-5v-182c0-2.761-2.239-5-5-5z"/><path fill="#FFF" stroke="#FFF" d="m280 56h-24c-5.269 0-10.392 1.734-14.578 4.935l-103.459 79.116c-1.237.946-1.963 2.414-1.963 3.972v223.955c0 1.557.726 3.026 1.963 3.972l103.459 79.115c4.186 3.201 9.309 4.936 14.579 4.936h23.999c13.255 0 24-10.745 24-24v-352.001c0-13.255-10.745-24-24-24z"/><text x="256" y="560" text-anchor="middle" style="font-size:60px;fill:#FFF;stroke:#FFF;">Click to unmute</text></svg></div></div>
     </div>
 
-    <ul id="right_click_menu" style="display: none"></ul>
+    <ul id="context-menu"></ul>
 `;
