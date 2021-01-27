3c0ba65c579f474a22888c81a551be871d9af17c
TÖRÖK Attila <torokati44@gmail.com>
2021-01-28 00:51:04 +0100

    video/vp6: Add the external VP6 decoder

--- core/src/backend/video/software.rs
+++ core/src/backend/video/software.rs
@@ -1,14 +1,18 @@
 //! Pure software video decoding backend.
 
-use crate::backend::render::{BitmapInfo, RenderBackend};
+use crate::backend::render::{BitmapHandle, BitmapInfo, RenderBackend};
 use crate::backend::video::{
     EncodedFrame, Error, FrameDependency, VideoBackend, VideoStreamHandle,
 };
 use generational_arena::Arena;
 use swf::{VideoCodec, VideoDeblocking};
+use vp6_dec_rs::VP6State;
 
 /// A single preloaded video stream.
-pub enum VideoStream {}
+pub enum VideoStream {
+    /// A VP6 video stream.
+    Vp6(VP6State, Option<BitmapHandle>),
+}
 
 /// Software video backend that proxies to CPU-only codec implementations that
 /// ship with Ruffle.
@@ -38,7 +42,10 @@ impl VideoBackend for SoftwareVideoBackend {
         codec: VideoCodec,
         _filter: VideoDeblocking,
     ) -> Result<VideoStreamHandle, Error> {
-        Err(format!("Unsupported video codec type {:?}", codec).into())
+        match codec {
+            VideoCodec::Vp6 => Ok(self.streams.insert(VideoStream::Vp6(VP6State::new(), None))),
+            _ => Err(format!("Unsupported video codec type {:?}", codec).into()),
+        }
     }
 
     fn preload_video_stream_frame(
@@ -46,25 +53,48 @@ impl VideoBackend for SoftwareVideoBackend {
         stream: VideoStreamHandle,
         _encoded_frame: EncodedFrame<'_>,
     ) -> Result<FrameDependency, Error> {
-        let _stream = self
+        let stream = self
             .streams
             .get_mut(stream)
             .ok_or("Unregistered video stream")?;
 
-        unreachable!()
+        match stream {
+            VideoStream::Vp6(_state, _last_bitmap) => {
+                // TODO actually parse the frame header and report correctly
+                Ok(FrameDependency::None)
+            }
+        }
     }
 
     fn decode_video_stream_frame(
         &mut self,
         stream: VideoStreamHandle,
-        _encoded_frame: EncodedFrame<'_>,
-        _renderer: &mut dyn RenderBackend,
+        encoded_frame: EncodedFrame<'_>,
+        renderer: &mut dyn RenderBackend,
     ) -> Result<BitmapInfo, Error> {
-        let _stream = self
+        let stream = self
             .streams
             .get_mut(stream)
             .ok_or("Unregistered video stream")?;
 
-        unreachable!()
+        match stream {
+            VideoStream::Vp6(state, last_bitmap) => {
+                let (rgba, (width, height)) = state.decode(encoded_frame.data);
+
+                let handle = if let Some(lb) = last_bitmap {
+                    renderer.update_texture(*lb, width as u32, height as u32, rgba)?
+                } else {
+                    renderer.register_bitmap_raw(width as u32, height as u32, rgba)?
+                };
+
+                *last_bitmap = Some(handle);
+
+                Ok(BitmapInfo {
+                    handle,
+                    width: width as u16,
+                    height: height as u16,
+                })
+            }
+        }
     }
 }
