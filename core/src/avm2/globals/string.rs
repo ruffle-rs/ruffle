4a87d707f078e1dd56e3534c8897aba396a353c1
paq <89paku@gmail.com>
2021-02-06 18:48:28 +0900

    avm2: Implement String constructor

--- core/src/avm2/globals/string.rs
+++ core/src/avm2/globals/string.rs
@@ -4,18 +4,30 @@ use crate::avm2::activation::Activation;
 use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
-use crate::avm2::object::Object;
+use crate::avm2::object::{Object, TObject};
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `String`'s instance initializer.
 pub fn instance_init<'gc>(
-    _activation: &mut Activation<'_, 'gc, '_>,
-    _this: Option<Object<'gc>>,
-    _args: &[Value<'gc>],
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error> {
-    Err("String constructor is a stub.".into())
+    if let Some(this) = this {
+        activation.super_init(this, &[])?;
+
+        if let Some(mut value) = this.as_primitive_mut(activation.context.gc_context) {
+            *value = args
+                .get(0)
+                .unwrap_or(&Value::String("".into()))
+                .coerce_to_string(activation)?
+                .into();
+        }
+    }
+
+    Ok(Value::Undefined)
 }
 
 /// Implements `String`'s class initializer.
---
+++ core/tests/swfs/avm2/string_constr/Test.as
@@ -0,0 +1,35 @@
+﻿package {
+	public class Test {}
+}
+
+trace("//new String();");
+trace(new String());
+
+trace("//new String(undefined);");
+trace(new String(undefined));
+trace("//new String(null);");
+trace(new String(null));
+
+trace("//new String(false);");
+trace(new String(false));
+trace("//new String(true);");
+trace(new String(true));
+
+trace("//new String(0);");
+trace(new String(0));
+trace("//new String(123);");
+trace(new String(123));
+trace("//new String(-1.23);");
+trace(new String(-1.23));
+
+trace("//new String(\"\");");
+trace(new String(""));
+trace("//new String(\"abc012aáâ!?*你好こんにちはمَرحَبًا\");");
+trace(new String("abc012aáâ!?*你好こんにちはمَرحَبًا"));
+
+trace("//new String(new Object());");
+trace(new String(new Object()));
+trace("//function f():void {}");
+trace("//new String(f);");
+function f():void {}
+trace(new String(f));
