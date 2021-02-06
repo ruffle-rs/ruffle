b93904c1d07d5dbca4ff8bb14be6b631d002de17
paq <89paku@gmail.com>
2021-02-06 18:55:28 +0900

    avm2: Implement String.length

--- core/src/avm2/globals/string.rs
+++ core/src/avm2/globals/string.rs
@@ -1,12 +1,12 @@
 //! `String` impl
 
-use crate::avm2::activation::Activation;
-use crate::avm2::class::Class;
+use crate::avm2::class::{Class, ClassAttributes};
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{Object, TObject};
 use crate::avm2::value::Value;
 use crate::avm2::Error;
+use crate::avm2::{activation::Activation, traits::Trait};
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `String`'s instance initializer.
@@ -39,13 +39,38 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `length` property's getter
+fn length<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Value::String(s) = this.value_of(activation.context.gc_context)? {
+            return Ok(s.encode_utf16().count().into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `String`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
-    Class::new(
+    let class = Class::new(
         QName::new(Namespace::public(), "String"),
         Some(QName::new(Namespace::public(), "Object").into()),
         Method::from_builtin(instance_init),
         Method::from_builtin(class_init),
         mc,
-    )
+    );
+
+    let mut write = class.write(mc);
+    write.set_attributes(ClassAttributes::FINAL | ClassAttributes::SEALED);
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public(), "length"),
+        Method::from_builtin(length),
+    ));
+
+    class
 }
