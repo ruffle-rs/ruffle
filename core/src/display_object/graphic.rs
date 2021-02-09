c78ed29e15991b23f0fc1d889946d9f906cd3e31
David Wendt <dcrkid@yahoo.com>
2021-02-08 22:40:22 -0500

    avm2: Stub `flash.display.Shape`.

--- core/src/display_object/graphic.rs
+++ core/src/display_object/graphic.rs
@@ -1,9 +1,16 @@
+use crate::avm1::Object as Avm1Object;
+use crate::avm2::{
+    Activation as Avm2Activation, Error as Avm2Error, Namespace as Avm2Namespace,
+    Object as Avm2Object, QName as Avm2QName, StageObject as Avm2StageObject,
+    TObject as Avm2TObject,
+};
 use crate::backend::render::ShapeHandle;
 use crate::context::{RenderContext, UpdateContext};
 use crate::display_object::{DisplayObjectBase, TDisplayObject};
 use crate::prelude::*;
 use crate::tag_utils::SwfMovie;
 use crate::types::{Degrees, Percent};
+use crate::vminterface::{AvmType, Instantiator};
 use gc_arena::{Collect, GcCell};
 use std::sync::Arc;
 
@@ -16,6 +23,7 @@ pub struct Graphic<'gc>(GcCell<'gc, GraphicData<'gc>>);
 pub struct GraphicData<'gc> {
     base: DisplayObjectBase<'gc>,
     static_data: gc_arena::Gc<'gc, GraphicStatic>,
+    avm2_object: Option<Avm2Object<'gc>>,
 }
 
 impl<'gc> Graphic<'gc> {
@@ -38,6 +46,7 @@ impl<'gc> Graphic<'gc> {
             GraphicData {
                 base: Default::default(),
                 static_data: gc_arena::Gc::allocate(context.gc_context, static_data),
+                avm2_object: None,
             },
         ))
     }
@@ -95,6 +104,57 @@ impl<'gc> TDisplayObject<'gc> for Graphic<'gc> {
         } else {
             false
         }
+    }
+
+    fn post_instantiation(
+        &self,
+        context: &mut UpdateContext<'_, 'gc, '_>,
+        display_object: DisplayObject<'gc>,
+        _init_object: Option<Avm1Object<'gc>>,
+        _instantiated_by: Instantiator,
+        run_frame: bool,
+    ) {
+        if self.vm_type(context) == AvmType::Avm2 {
+            let mut allocator = || {
+                let mut activation = Avm2Activation::from_nothing(context.reborrow());
+                let mut proto = activation.context.avm2.prototypes().shape;
+                let constr = proto
+                    .get_property(
+                        proto,
+                        &Avm2QName::new(Avm2Namespace::public(), "constructor"),
+                        &mut activation,
+                    )?
+                    .coerce_to_object(&mut activation)?;
+
+                let object = Avm2StageObject::for_display_object(
+                    activation.context.gc_context,
+                    display_object,
+                    proto,
+                )
+                .into();
+                constr.call(Some(object), &[], &mut activation, Some(proto))?;
+
+                Ok(object)
+            };
+            let result: Result<Avm2Object<'gc>, Avm2Error> = allocator();
+
+            match result {
+                Ok(object) => self.0.write(context.gc_context).avm2_object = Some(object),
+                Err(e) => log::error!("Got {} when constructing AVM2 side of display object", e),
+            }
+        }
+
+        if run_frame {
+            self.run_frame(context);
+        }
+    }
+
+    fn object2(&self) -> Avm2Value<'gc> {
+        self.0
+            .read()
+            .avm2_object
+            .map(Avm2Value::from)
+            .unwrap_or(Avm2Value::Undefined)
     }
 }
 
