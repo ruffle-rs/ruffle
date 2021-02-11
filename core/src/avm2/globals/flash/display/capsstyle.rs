cf4ae264c4b28b63d1796e85ad010a318c8fcb68
David Wendt <dcrkid@yahoo.com>
2021-02-11 17:37:18 -0500

    avm2: Implement `CapsStyle`

---
+++ core/src/avm2/globals/flash/display/capsstyle.rs
@@ -0,0 +1,66 @@
+//! `flash.display.CapsStyle` builtin/prototype
+
+use crate::avm2::activation::Activation;
+use crate::avm2::class::{Class, ClassAttributes};
+use crate::avm2::method::Method;
+use crate::avm2::names::{Namespace, QName};
+use crate::avm2::object::Object;
+use crate::avm2::traits::Trait;
+use crate::avm2::value::Value;
+use crate::avm2::Error;
+use gc_arena::{GcCell, MutationContext};
+
+/// Implements `flash.display.CapsStyle`'s instance constructor.
+pub fn instance_init<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        activation.super_init(this, &[])?;
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `flash.display.CapsStyle`'s class constructor.
+pub fn class_init<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    _this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    Ok(Value::Undefined)
+}
+
+/// Construct `CapsStyle`'s class.
+pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
+    let class = Class::new(
+        QName::new(Namespace::package("flash.display"), "CapsStyle"),
+        Some(QName::new(Namespace::public(), "Object").into()),
+        Method::from_builtin(instance_init),
+        Method::from_builtin(class_init),
+        mc,
+    );
+
+    let mut write = class.write(mc);
+
+    write.set_attributes(ClassAttributes::SEALED);
+
+    write.define_class_trait(Trait::from_const(
+        QName::new(Namespace::public(), "NONE"),
+        QName::new(Namespace::public(), "String").into(),
+        Some("none".into()),
+    ));
+    write.define_class_trait(Trait::from_const(
+        QName::new(Namespace::public(), "ROUND"),
+        QName::new(Namespace::public(), "String").into(),
+        Some("round".into()),
+    ));
+    write.define_class_trait(Trait::from_const(
+        QName::new(Namespace::public(), "SQUARE"),
+        QName::new(Namespace::public(), "String").into(),
+        Some("square".into()),
+    ));
+
+    class
+}
