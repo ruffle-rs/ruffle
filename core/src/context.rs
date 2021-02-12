b484b5484b5e781daf573b3b6c41fd95d022974e
Aaron Hill <aa1ronham@gmail.com>
2021-02-12 11:22:22 -0500

    Derive Collect for QueuedActions

--- core/src/context.rs
+++ core/src/context.rs
@@ -297,6 +297,8 @@ impl<'a, 'gc, 'gc_context> UpdateContext<'a, 'gc, 'gc_context> {
 }
 
 /// A queued ActionScript call.
+#[derive(Collect)]
+#[collect(no_drop)]
 pub struct QueuedActions<'gc> {
     /// The movie clip this ActionScript is running on.
     pub clip: DisplayObject<'gc>,
@@ -306,14 +308,6 @@ pub struct QueuedActions<'gc> {
 
     /// Whether this is an unload action, which can still run if the clip is removed.
     pub is_unload: bool,
-}
-
-unsafe impl<'gc> Collect for QueuedActions<'gc> {
-    #[inline]
-    fn trace(&self, cc: gc_arena::CollectionContext) {
-        self.clip.trace(cc);
-        self.action_type.trace(cc);
-    }
 }
 
 /// Action and gotos need to be queued up to execute at the end of the frame.
