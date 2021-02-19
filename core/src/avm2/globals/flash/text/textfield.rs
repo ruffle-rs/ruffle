e35f66fafe370f60da3231b7dd930a973e42ff48
David Wendt <dcrkid@yahoo.com>
2021-02-18 21:32:44 -0500

    avm2: Impl `TextField.autoSize`

--- core/src/avm2/globals/flash/text/textfield.rs
+++ core/src/avm2/globals/flash/text/textfield.rs
@@ -5,9 +5,10 @@ use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{Object, TObject};
+use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
-use crate::display_object::EditText;
+use crate::display_object::{AutoSizeMode, EditText, TDisplayObject};
 use crate::tag_utils::SwfMovie;
 use crate::vminterface::AvmType;
 use gc_arena::{GcCell, MutationContext};
@@ -52,13 +53,74 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+pub fn autosize<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this
+        .and_then(|this| this.as_display_object())
+        .and_then(|this| this.as_edit_text())
+    {
+        return Ok(match this.autosize() {
+            AutoSizeMode::None => "none".into(),
+            AutoSizeMode::Left => "left".into(),
+            AutoSizeMode::Center => "center".into(),
+            AutoSizeMode::Right => "right".into(),
+        });
+    }
+
+    Ok(Value::Undefined)
+}
+
+pub fn set_autosize<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this
+        .and_then(|this| this.as_display_object())
+        .and_then(|this| this.as_edit_text())
+    {
+        let value = args
+            .get(0)
+            .cloned()
+            .unwrap_or(Value::Undefined)
+            .coerce_to_string(activation)?;
+        this.set_autosize(
+            match &*value {
+                "left" => AutoSizeMode::Left,
+                "center" => AutoSizeMode::Center,
+                "right" => AutoSizeMode::Right,
+                _ => AutoSizeMode::None,
+            },
+            &mut activation.context,
+        );
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `TextField`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
-    Class::new(
+    let class = Class::new(
         QName::new(Namespace::package("flash.text"), "TextField"),
         Some(QName::new(Namespace::package("flash.display"), "InteractiveObject").into()),
         Method::from_builtin(instance_init),
         Method::from_builtin(class_init),
         mc,
-    )
+    );
+
+    let mut write = class.write(mc);
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public(), "autoSize"),
+        Method::from_builtin(autosize),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::public(), "autoSize"),
+        Method::from_builtin(set_autosize),
+    ));
+
+    class
 }
