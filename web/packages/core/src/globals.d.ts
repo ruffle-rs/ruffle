c7d037dccefaa08100e7c262cf0edf18552f71ac
relrelb <relrelbachar@gmail.com>
2021-02-19 18:28:37 +0200

    web: Remove stale globals definitions
    They were removed in 3e084963c6484a4af0d2debc5c4f7c3afa207770.

--- web/packages/core/src/globals.d.ts
+++ web/packages/core/src/globals.d.ts
@@ -2,8 +2,6 @@
 
 declare let __webpack_public_path__: string;
 declare let ruffleRuntimePath: string;
-declare let __CHANNEL__: string;
-declare let __COMMIT_DATE__: string;
 
 interface Error {
     ruffleIsExtension?: boolean;
