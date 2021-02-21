a80590dd425bed621b9956ae633c86b1c8105ee2
relrelb <relrelbachar@gmail.com>
2021-02-21 23:52:02 +0200

    web: Fix panic message alignment

--- web/packages/core/src/shadow-template.ts
+++ web/packages/core/src/shadow-template.ts
@@ -82,6 +82,9 @@ ruffleShadowTemplate.innerHTML = `
             /* Inverted colors from play button! */
             background: linear-gradient(180deg, rgba(253,58,64,1) 0%, rgba(253,161,56,1) 100%);
             color: white;
+            display: flex;
+            flex-flow: column;
+            justify-content: space-around;
         }
 
         #panic a {
@@ -89,46 +92,27 @@ ruffleShadowTemplate.innerHTML = `
             font-weight: bold;
         }
 
-        #panic > div {
-            position: absolute;
-        }
-
         #panic-title {
-            width: inherit;
-            top: 30px;
-            font-size: 42px;
+            font-size: xxx-large;
             font-weight: bold;
         }
 
-        #panic-body {
-            top: 100px;
-            bottom: 80px;
-            left: 50px;
-            right: 50px;
+        #panic-body.details {
+            flex: 0.9;
+            margin: 0 10px;
         }
 
         #panic-body textarea {
             width: 100%;
             height: 100%;
-        }
-
-        #panic-footer {
-            bottom: 30px;
-            width: inherit;
+            resize: none;
         }
 
         #panic ul {
-            margin-top: 35px;
             padding: 0;
-            width: inherit;
             display: flex;
             list-style-type: none;
-            justify-content: center;
-            align-items: center;
-        }
-
-        #panic li {
-            padding: 10px 50px;
+            justify-content: space-evenly;
         }
 
         #message_overlay {
