2b9f9958f89f5297fee67dffbde86a20c22af781
David Wendt <dcrkid@yahoo.com>
2021-02-25 19:13:23 -0500

    avm2: Impl `TextField.displayAsPassword`

--- core/src/avm2/globals/flash/text/textfield.rs
+++ core/src/avm2/globals/flash/text/textfield.rs
@@ -244,6 +244,42 @@ pub fn set_default_text_format<'gc>(
     Ok(Value::Undefined)
 }
 
+pub fn display_as_password<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this
+        .and_then(|this| this.as_display_object())
+        .and_then(|this| this.as_edit_text())
+    {
+        return Ok(this.is_password().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
+pub fn set_display_as_password<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this
+        .and_then(|this| this.as_display_object())
+        .and_then(|this| this.as_edit_text())
+    {
+        let is_password = args
+            .get(0)
+            .cloned()
+            .unwrap_or(Value::Undefined)
+            .coerce_to_boolean();
+
+        this.set_password(is_password, &mut activation.context);
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `TextField`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
@@ -295,6 +331,14 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
     write.define_instance_trait(Trait::from_setter(
         QName::new(Namespace::public(), "defaultTextFormat"),
         Method::from_builtin(set_default_text_format),
+    ));
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public(), "displayAsPassword"),
+        Method::from_builtin(display_as_password),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::public(), "displayAsPassword"),
+        Method::from_builtin(set_display_as_password),
     ));
 
     class
