56cece3336b215882572b1fcf038cb4892cbaf23
relrelb <relrelbachar@gmail.com>
2021-03-09 22:33:20 +0200

    web: Use publicPath() instead of ruffleRuntimePath
    Similarly to how selfhosted currently does.

--- web/packages/extension/js/index.js
+++ web/packages/extension/js/index.js
@@ -1,10 +1,11 @@
-import { PublicAPI, SourceAPI } from "ruffle-core";
+import { PublicAPI, SourceAPI, publicPath } from "ruffle-core";
 
 window.RufflePlayer = PublicAPI.negotiate(
     window.RufflePlayer,
     "extension",
     new SourceAPI("extension")
 );
+__webpack_public_path__ = publicPath(window.RufflePlayer.config, "extension");
 
 if (obfuscatedEventPrefix) {
     document.addEventListener(obfuscatedEventPrefix + "_request", function (e) {
