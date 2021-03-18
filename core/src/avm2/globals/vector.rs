888fcc85ca68b8eb6a34f7266bbfa5b9f27d2695
David Wendt <dcrkid@yahoo.com>
2021-03-17 21:53:03 -0400

    avm2: Impl `Vector.length`

--- core/src/avm2/globals/vector.rs
+++ core/src/avm2/globals/vector.rs
@@ -3,7 +3,7 @@
 use crate::avm2::activation::Activation;
 use crate::avm2::class::{Class, ClassAttributes};
 use crate::avm2::globals::NS_VECTOR;
-use crate::avm2::method::Method;
+use crate::avm2::method::{Method, NativeMethodImpl};
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{vector_allocator, Object, TObject};
 use crate::avm2::value::Value;
@@ -48,6 +48,42 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// `Vector.length` getter
+pub fn length<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(vector) = this.as_vector_storage() {
+            return Ok(vector.length().into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// `Vector.length` setter
+pub fn set_length<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(mut vector) = this.as_vector_storage_mut(activation.context.gc_context) {
+            let new_length = args
+                .get(0)
+                .cloned()
+                .unwrap_or(Value::Unsigned(0))
+                .coerce_to_u32(activation)? as usize;
+
+            vector.resize(new_length)?;
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `Sprite`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
@@ -62,6 +98,13 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
 
     write.set_attributes(ClassAttributes::GENERIC);
     write.set_instance_allocator(vector_allocator);
+
+    const PUBLIC_INSTANCE_PROPERTIES: &[(
+        &str,
+        Option<NativeMethodImpl>,
+        Option<NativeMethodImpl>,
+    )] = &[("length", Some(length), Some(set_length))];
+    write.define_public_builtin_instance_properties(mc, PUBLIC_INSTANCE_PROPERTIES);
 
     class
 }
