737e35bf4619a0e027cb9642d33c1cd39f457fa0
David Wendt <dcrkid@yahoo.com>
2021-03-19 22:01:44 -0400

    avm2: Add Loader Info class & object type

---
+++ core/src/avm2/object/loaderinfo_object.rs
@@ -0,0 +1,136 @@
+//! Loader-info object
+
+use crate::avm2::activation::Activation;
+use crate::avm2::class::Class;
+use crate::avm2::names::{Namespace, QName};
+use crate::avm2::object::script_object::{ScriptObjectClass, ScriptObjectData};
+use crate::avm2::object::{Object, ObjectPtr, TObject};
+use crate::avm2::scope::Scope;
+use crate::avm2::string::AvmString;
+use crate::avm2::traits::Trait;
+use crate::avm2::value::Value;
+use crate::avm2::Error;
+use crate::tag_utils::SwfMovie;
+use crate::{impl_avm2_custom_object, impl_avm2_custom_object_properties};
+use gc_arena::{Collect, GcCell, MutationContext};
+use std::sync::Arc;
+
+/// Represents a thing which can be loaded by a loader.
+#[derive(Collect, Debug, Clone)]
+#[collect(no_drop)]
+pub enum LoaderStream {
+    /// A loaded SWF movie.
+    SWF(Arc<SwfMovie>),
+}
+
+/// An Object which represents a loadable object, such as a SWF movie or image
+/// resource.
+#[derive(Collect, Debug, Clone, Copy)]
+#[collect(no_drop)]
+pub struct LoaderInfoObject<'gc>(GcCell<'gc, LoaderInfoObjectData<'gc>>);
+
+#[derive(Collect, Debug, Clone)]
+#[collect(no_drop)]
+pub struct LoaderInfoObjectData<'gc> {
+    /// All normal script data.
+    base: ScriptObjectData<'gc>,
+
+    /// The loaded stream that this gets it's info from.
+    loaded_stream: Option<LoaderStream>,
+}
+
+impl<'gc> LoaderInfoObject<'gc> {
+    /// Box a movie into a loader info object.
+    pub fn from_movie(
+        movie: Arc<SwfMovie>,
+        base_proto: Object<'gc>,
+        mc: MutationContext<'gc, '_>,
+    ) -> Result<Object<'gc>, Error> {
+        let base = ScriptObjectData::base_new(Some(base_proto), ScriptObjectClass::NoClass);
+        let loaded_stream = Some(LoaderStream::SWF(movie));
+
+        Ok(LoaderInfoObject(GcCell::allocate(
+            mc,
+            LoaderInfoObjectData {
+                base,
+                loaded_stream,
+            },
+        ))
+        .into())
+    }
+
+    /// Construct a loader-info subclass.
+    pub fn derive(
+        base_proto: Object<'gc>,
+        mc: MutationContext<'gc, '_>,
+        class: GcCell<'gc, Class<'gc>>,
+        scope: Option<GcCell<'gc, Scope<'gc>>>,
+    ) -> Result<Object<'gc>, Error> {
+        let base = ScriptObjectData::base_new(
+            Some(base_proto),
+            ScriptObjectClass::InstancePrototype(class, scope),
+        );
+
+        Ok(LoaderInfoObject(GcCell::allocate(
+            mc,
+            LoaderInfoObjectData {
+                base,
+                loaded_stream: None,
+            },
+        ))
+        .into())
+    }
+}
+
+impl<'gc> TObject<'gc> for LoaderInfoObject<'gc> {
+    impl_avm2_custom_object!(base);
+    impl_avm2_custom_object_properties!(base);
+
+    fn value_of(&self, mc: MutationContext<'gc, '_>) -> Result<Value<'gc>, Error> {
+        if let Some(class) = self.as_proto_class() {
+            Ok(AvmString::new(mc, format!("[object {}]", class.read().name().local_name())).into())
+        } else {
+            Ok("[object Object]".into())
+        }
+    }
+
+    fn construct(
+        &self,
+        activation: &mut Activation<'_, 'gc, '_>,
+        _args: &[Value<'gc>],
+    ) -> Result<Object<'gc>, Error> {
+        let this: Object<'gc> = Object::LoaderInfoObject(*self);
+        let base = ScriptObjectData::base_new(Some(this), ScriptObjectClass::NoClass);
+
+        Ok(LoaderInfoObject(GcCell::allocate(
+            activation.context.gc_context,
+            LoaderInfoObjectData {
+                base,
+                loaded_stream: None,
+            },
+        ))
+        .into())
+    }
+
+    fn derive(
+        &self,
+        activation: &mut Activation<'_, 'gc, '_>,
+        class: GcCell<'gc, Class<'gc>>,
+        scope: Option<GcCell<'gc, Scope<'gc>>>,
+    ) -> Result<Object<'gc>, Error> {
+        let this: Object<'gc> = Object::LoaderInfoObject(*self);
+        let base = ScriptObjectData::base_new(
+            Some(this),
+            ScriptObjectClass::InstancePrototype(class, scope),
+        );
+
+        Ok(LoaderInfoObject(GcCell::allocate(
+            activation.context.gc_context,
+            LoaderInfoObjectData {
+                base,
+                loaded_stream: None,
+            },
+        ))
+        .into())
+    }
+}
