5a69357b5bfbcd9ecc2c1d88df9b88817ee563df
desuwa <desuwane@gmail.com>
2021-04-15 20:47:18 +0200

    tests: Add tests for Player::run_actions at timer callbacks.

--- core/tests/regression_tests.rs
+++ core/tests/regression_tests.rs
@@ -554,6 +554,7 @@ swf_tests! {
     (nan_scale, "avm1/nan_scale", 1),
     (as3_nan_scale, "avm2/nan_scale", 1),
     (as3_documentclass, "avm2/documentclass", 1),
+    (timer_run_actions, "avm1/timer_run_actions", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
