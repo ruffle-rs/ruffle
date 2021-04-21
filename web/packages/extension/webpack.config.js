a7d24715aefb5338822a4cead4a67c6cc8412712
relrelb <relrelbachar@gmail.com>
2021-04-21 22:26:02 +0300

    extension: Convert manifest to JSON5
    JSON5 is a more flexible superset of JSON. It supports comments,
    trailing commas and more nice features that JSON doesn't support.

--- web/packages/extension/webpack.config.js
+++ web/packages/extension/webpack.config.js
@@ -1,7 +1,41 @@
 /* eslint-env node */
 
 const path = require("path");
+const json5 = require("json5");
 const CopyPlugin = require("copy-webpack-plugin");
+
+function transformManifest(content, env) {
+    const manifest = json5.parse(content);
+
+    const packageVersion = process.env.npm_package_version;
+
+    const versionChannel = process.env.CFG_RELEASE_CHANNEL || "nightly";
+
+    const buildDate = new Date().toISOString().substring(0, 10);
+
+    // The extension marketplaces require the version to monotonically increase,
+    // so append the build date onto the end of the manifest version.
+    const version = process.env.BUILD_ID
+        ? `${packageVersion}.${process.env.BUILD_ID}`
+        : packageVersion;
+
+    const version_name =
+        versionChannel === "nightly"
+            ? `${packageVersion} nightly ${buildDate}`
+            : packageVersion;
+
+    Object.assign(manifest, { version, version_name });
+    if (env.firefox) {
+        const id = process.env.FIREFOX_EXTENSION_ID || "ruffle@ruffle.rs";
+        Object.assign(manifest, {
+            browser_specific_settings: {
+                gecko: { id },
+            },
+        });
+    }
+
+    return JSON.stringify(manifest);
+}
 
 module.exports = (env, argv) => {
     let mode = "production";
@@ -38,42 +72,9 @@ module.exports = (env, argv) => {
             new CopyPlugin({
                 patterns: [
                     {
-                        from: "manifest.json",
-                        to: "..",
-                        transform(content) {
-                            const manifest = JSON.parse(content.toString());
-
-                            const packageVersion =
-                                process.env.npm_package_version;
-                            const versionChannel =
-                                process.env.CFG_RELEASE_CHANNEL || "nightly";
-                            const buildDate = new Date()
-                                .toISOString()
-                                .substring(0, 10);
-                            // The extension marketplaces require the version to monotonically increase,
-                            // so append the build date onto the end of the manifest version.
-                            const version = process.env.BUILD_ID
-                                ? `${packageVersion}.${process.env.BUILD_ID}`
-                                : packageVersion;
-                            const version_name =
-                                versionChannel === "nightly"
-                                    ? `${packageVersion} nightly ${buildDate}`
-                                    : packageVersion;
-
-                            Object.assign(manifest, { version, version_name });
-                            if (env.firefox) {
-                                const id =
-                                    process.env.FIREFOX_EXTENSION_ID ||
-                                    "ruffle@ruffle.rs";
-
-                                Object.assign(manifest, {
-                                    browser_specific_settings: {
-                                        gecko: { id },
-                                    },
-                                });
-                            }
-                            return JSON.stringify(manifest);
-                        },
+                        from: "manifest.json5",
+                        to: "../manifest.json",
+                        transform: (content) => transformManifest(content, env),
                     },
                     { from: "LICENSE*" },
                     { from: "README.md" },
