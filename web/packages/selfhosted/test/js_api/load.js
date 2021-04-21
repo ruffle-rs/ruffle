0c5951da52e624ec605ddae62fc4c1ab5e93d2aa
Mike Welsh <mwelsh@gmail.com>
2021-04-21 16:33:00 -0700

    tests: Add tests for Ruffle JS API
    Add some basic scaffolding for testing the JS API. Adds a test for
    `RufflePlayer.load` and `RufflePlayer.metadata`.

---
+++ web/packages/selfhosted/test/js_api/load.js
@@ -0,0 +1,17 @@
+const { js_api_before, play_and_monitor } = require("../polyfill/utils");
+const { use } = require("chai");
+const chaiHtml = require("chai-html");
+
+use(chaiHtml);
+
+describe("RufflePlayer.load", () => {
+    js_api_before();
+
+    it("loads and plays a URL", () => {
+        const player = browser.$("<ruffle-player>");
+        browser.execute((player) => {
+            player.load("/test_assets/example.swf");
+        }, player);
+        play_and_monitor(browser, player);
+    });
+});
