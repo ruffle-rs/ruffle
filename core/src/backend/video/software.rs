5d01a27d7893f22f1faf8e75a6e35f9b79ae2774
TÖRÖK Attila <torokati44@gmail.com>
2021-04-30 02:13:30 +0200

    video/vp6: Crop the unwanted encoded pixels on the right/bottom manually

--- core/src/backend/video/software.rs
+++ core/src/backend/video/software.rs
@@ -11,7 +11,7 @@ use vp6_dec_rs::Vp6State;
 /// A single preloaded video stream.
 pub enum VideoStream {
     /// A VP6 video stream, with or without alpha channel.
-    Vp6(Vp6State, Option<BitmapHandle>),
+    Vp6(Vp6State, (u16, u16), Option<BitmapHandle>),
 }
 
 /// Software video backend that proxies to CPU-only codec implementations that
@@ -38,17 +38,21 @@ impl VideoBackend for SoftwareVideoBackend {
     fn register_video_stream(
         &mut self,
         _num_frames: u32,
-        _size: (u16, u16),
+        size: (u16, u16),
         codec: VideoCodec,
         _filter: VideoDeblocking,
     ) -> Result<VideoStreamHandle, Error> {
         match codec {
-            VideoCodec::Vp6 => Ok(self
-                .streams
-                .insert(VideoStream::Vp6(Vp6State::new(false), None))),
-            VideoCodec::Vp6WithAlpha => Ok(self
-                .streams
-                .insert(VideoStream::Vp6(Vp6State::new(true), None))),
+            VideoCodec::Vp6 => {
+                Ok(self
+                    .streams
+                    .insert(VideoStream::Vp6(Vp6State::new(false), size, None)))
+            }
+            VideoCodec::Vp6WithAlpha => {
+                Ok(self
+                    .streams
+                    .insert(VideoStream::Vp6(Vp6State::new(true), size, None)))
+            }
             _ => Err(format!("Unsupported video codec type {:?}", codec).into()),
         }
     }
@@ -64,7 +68,7 @@ impl VideoBackend for SoftwareVideoBackend {
             .ok_or("Unregistered video stream")?;
 
         match stream {
-            VideoStream::Vp6(_state, _last_bitmap) => {
+            VideoStream::Vp6(_state, _bounds, _last_bitmap) => {
                 // Luckily the very first bit of the encoded frames is exactly
                 // this flag, so we don't have to bother asking any "proper"
                 // decoder or parser.
@@ -92,8 +96,40 @@ impl VideoBackend for SoftwareVideoBackend {
             .ok_or("Unregistered video stream")?;
 
         match stream {
-            VideoStream::Vp6(state, last_bitmap) => {
-                let (rgba, (width, height)) = state.decode(encoded_frame.data);
+            VideoStream::Vp6(state, bounds, last_bitmap) => {
+                let (mut rgba, (mut width, mut height)) = state.decode(encoded_frame.data);
+
+                if width < bounds.0 as usize || height < bounds.1 as usize {
+                    log::warn!("A VP6 video frame is smaller than the bounds of the stream it belongs in. This is not supported.");
+                    // Flash Player just produces a black image in this case!
+                }
+
+                if width > bounds.0 as usize {
+                    // Removing the unwanted pixels on the right edge (most commonly: unused pieces of macroblocks)
+                    // by squishing all the rows tightly next to each other.
+                    // Even though the vp6 decoder in FFmpeg could do this trimming on its own (accepts parameters
+                    // for it in the extradata field), the swscale colorspace conversion would still have to be done
+                    // into a frame that has a stride which is a multiple of 32 or 64 bytes (for performance reasons;
+                    // otherwise it leaves the right edge blank) and that is often greater than the actual width*4,
+                    // so there will be gaps between the rows in these cases.
+                    // And Bitmap at the moment does not allow these gaps, so we need to remove them.
+                    // Also dropping any unwanted rows on the bottom while we're at it.
+                    let new_width = bounds.0 as usize;
+                    let new_height = usize::min(height, bounds.1 as usize);
+                    // no need to move the first row, nor any rows on the bottom that will end up being cropped entirely
+                    for row in 1..new_height {
+                        rgba.copy_within(
+                            row * width * 4..(row * width + new_width) * 4,
+                            row * new_width * 4,
+                        );
+                    }
+                    width = new_width;
+                    height = new_height;
+                }
+
+                // Cropping the unwanted rows on the bottom, also dropping any unused space at the end left by the squish above
+                height = usize::min(height, bounds.1 as usize);
+                rgba.truncate(width * height * 4);
 
                 let handle = if let Some(lb) = last_bitmap {
                     renderer.update_texture(*lb, width as u32, height as u32, rgba)?
