3b431333720f09c75a25b7fb9c9eb999c3327c11
Mike Welsh <mwelsh@gmail.com>
2021-05-09 12:35:53 -0700

    desktop: Append .sol extension to SharedObject files
    Also, prefix the shared object name with # if it contains a
    slash, (e.g. `#mygame/foo`). This matches Flash's directory
    structure and makes it easier to transfer saved data to Ruffle.

--- desktop/src/storage.rs
+++ desktop/src/storage.rs
@@ -1,52 +1,72 @@
 use ruffle_core::backend::storage::StorageBackend;
 use std::fs;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::Write;
+use std::path::PathBuf;
 
 pub struct DiskStorageBackend {
     base_path: PathBuf,
+    shared_objects_path: PathBuf,
 }
 
 impl DiskStorageBackend {
     pub fn new() -> Self {
-        let base_path = dirs::data_local_dir().unwrap().join(Path::new("ruffle"));
+        let base_path = dirs::data_local_dir().unwrap().join("ruffle");
+        let shared_objects_path = base_path.join("SharedObjects");
 
         // Create a base dir if one doesn't exist yet
-        if !base_path.exists() {
+        if !shared_objects_path.exists() {
             log::info!("Creating storage dir");
             if let Err(r) = fs::create_dir_all(&base_path) {
                 log::warn!("Unable to create storage dir {}", r);
             }
         }
 
-        DiskStorageBackend { base_path }
+        DiskStorageBackend {
+            base_path,
+            shared_objects_path,
+        }
+    }
+
+    fn get_shared_object_path(&self, name: &str) -> PathBuf {
+        self.shared_objects_path.join(format!("{}.sol", name))
+    }
+
+    fn get_back_compat_shared_object_path(&self, name: &str) -> PathBuf {
+        // Backwards compatibility with pre-05/09/2021:
+        // Search for data in old location, without .sol extension and # prefix.
+        // Remove this code eventually.
+        self.base_path.join(name.replacen("/#", "/", 1))
     }
 }
 
 impl StorageBackend for DiskStorageBackend {
     fn get(&self, name: &str) -> Option<Vec<u8>> {
-        let full_path = self.base_path.join(Path::new(name));
-
-        match File::open(full_path) {
-            Ok(mut file) => {
-                let mut buffer = Vec::new();
-                if let Err(r) = file.read_to_end(&mut buffer) {
-                    log::warn!("Unable to read file content {:?}", r);
-                    None
-                } else {
-                    Some(buffer)
+        let path = self.get_shared_object_path(name);
+        match std::fs::read(path) {
+            Ok(data) => Some(data),
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+                // Backwards compatibility with pre-05/09/2021:
+                // Search for data in old location, without .sol extension.
+                // Remove this eventually.
+                let path = self.get_back_compat_shared_object_path(name);
+                match std::fs::read(path) {
+                    Ok(data) => Some(data),
+                    Err(e) => {
+                        log::warn!("Unable to read file {:?}", e);
+                        None
+                    }
                 }
             }
-            Err(r) => {
-                log::warn!("Unable to open file {:?}", r);
+            Err(e) => {
+                log::warn!("Unable to read file {:?}", e);
                 None
             }
         }
     }
 
     fn put(&mut self, name: &str, value: &[u8]) -> bool {
-        let full_path = self.base_path.join(Path::new(name));
+        let full_path = self.get_shared_object_path(name);
         if let Some(parent_dir) = full_path.parent() {
             if !parent_dir.exists() {
                 if let Err(r) = fs::create_dir_all(&parent_dir) {
@@ -73,7 +93,7 @@ impl StorageBackend for DiskStorageBackend {
     }
 
     fn remove_key(&mut self, name: &str) {
-        let full_path = self.base_path.join(Path::new(name));
+        let full_path = self.get_shared_object_path(name);
         let _ = fs::remove_file(full_path);
     }
 }
