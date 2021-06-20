1a132eb22d73c8c31d31a3cfdbbf5fc1febbda51
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2021-06-20 20:44:17 +0200

    avm1: Don't attempt to serialize MovieClips

--- core/src/avm1/globals/shared_object.rs
+++ core/src/avm1/globals/shared_object.rs
@@ -64,45 +64,45 @@ fn serialize_value<'gc>(
         Value::Number(f) => Some(AmfValue::Number(f)),
         Value::String(s) => Some(AmfValue::String(s.to_string())),
         Value::Object(o) => {
-            // Don't attempt to serialize functions
             let function = activation.context.avm1.prototypes.function;
             let array = activation.context.avm1.prototypes.array;
             let xml = activation.context.avm1.prototypes.xml_node;
             let date = activation.context.avm1.prototypes.date;
 
-            if !o
-                .is_instance_of(activation, o, function)
+            // TODO: Find a more general rule for which object types should be skipped,
+            // and which turned into Undefined
+            if o.is_instance_of(activation, o, function)
                 .unwrap_or_default()
             {
-                if o.is_instance_of(activation, o, array).unwrap_or_default() {
-                    let mut values = Vec::new();
-                    recursive_serialize(activation, o, &mut values);
-
-                    // TODO: What happens if an exception is thrown here?
-                    let length = o.length(activation).unwrap();
-                    Some(AmfValue::ECMAArray(vec![], values, length as u32))
-                } else if o.is_instance_of(activation, o, xml).unwrap_or_default() {
-                    o.as_xml_node().and_then(|xml_node| {
-                        xml_node
-                            .into_string(&mut |_| true)
-                            .map(|xml_string| AmfValue::XML(xml_string, true))
-                            .ok()
-                    })
-                } else if o.is_instance_of(activation, o, date).unwrap_or_default() {
-                    o.as_date_object()
-                        .and_then(|date_obj| {
-                            date_obj
-                                .date_time()
-                                .map(|date_time| date_time.timestamp_millis())
-                        })
-                        .map(|millis| AmfValue::Date(millis as f64, None))
-                } else {
-                    let mut object_body = Vec::new();
-                    recursive_serialize(activation, o, &mut object_body);
-                    Some(AmfValue::Object(object_body, None))
-                }
-            } else {
                 None
+            } else if o.as_display_object().is_some() {
+                Some(AmfValue::Undefined)
+            } else if o.is_instance_of(activation, o, array).unwrap_or_default() {
+                let mut values = Vec::new();
+                recursive_serialize(activation, o, &mut values);
+
+                // TODO: What happens if an exception is thrown here?
+                let length = o.length(activation).unwrap();
+                Some(AmfValue::ECMAArray(vec![], values, length as u32))
+            } else if o.is_instance_of(activation, o, xml).unwrap_or_default() {
+                o.as_xml_node().and_then(|xml_node| {
+                    xml_node
+                        .into_string(&mut |_| true)
+                        .map(|xml_string| AmfValue::XML(xml_string, true))
+                        .ok()
+                })
+            } else if o.is_instance_of(activation, o, date).unwrap_or_default() {
+                o.as_date_object()
+                    .and_then(|date_obj| {
+                        date_obj
+                            .date_time()
+                            .map(|date_time| date_time.timestamp_millis())
+                    })
+                    .map(|millis| AmfValue::Date(millis as f64, None))
+            } else {
+                let mut object_body = Vec::new();
+                recursive_serialize(activation, o, &mut object_body);
+                Some(AmfValue::Object(object_body, None))
             }
         }
     }
