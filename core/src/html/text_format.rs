87e9dda2508d32bbc0dd8791d7412026f617686a
relrelb <relrelbachar@gmail.com>
2021-06-22 19:32:07 +0300

    chore: Use from_le_bytes in more places

--- core/src/html/text_format.rs
+++ core/src/html/text_format.rs
@@ -574,7 +574,7 @@ impl TextFormat {
             "color",
             self.color
                 .clone()
-                .map(|v| (((v.r as u32) << 16) + ((v.g as u32) << 8) + v.b as u32).into())
+                .map(|v| v.to_rgb().into())
                 .unwrap_or(Avm1Value::Null),
             activation,
         )?;
@@ -729,7 +729,7 @@ impl TextFormat {
             &Avm2QName::new(Avm2Namespace::public(), "color"),
             self.color
                 .clone()
-                .map(|v| (((v.r as u32) << 16) + ((v.g as u32) << 8) + v.b as u32).into())
+                .map(|v| v.to_rgb().into())
                 .unwrap_or(Avm2Value::Null),
             activation,
         )?;
