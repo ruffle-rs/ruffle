d9b5b1a0af749771cb7770bc87e649bee4109ba0
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2021-07-03 17:28:10 +0200

    desktop: Add DX11 to default graphics backends

--- render/wgpu/src/clap.rs
+++ render/wgpu/src/clap.rs
@@ -12,7 +12,7 @@ pub enum GraphicsBackend {
 impl From<GraphicsBackend> for wgpu::BackendBit {
     fn from(backend: GraphicsBackend) -> Self {
         match backend {
-            GraphicsBackend::Default => wgpu::BackendBit::PRIMARY,
+            GraphicsBackend::Default => wgpu::BackendBit::PRIMARY | wgpu::BackendBit::DX11,
             GraphicsBackend::Vulkan => wgpu::BackendBit::VULKAN,
             GraphicsBackend::Metal => wgpu::BackendBit::METAL,
             GraphicsBackend::Dx12 => wgpu::BackendBit::DX12,
