939d467897410f2aef8299e85750b2299a7d7ea7
Callum Thomson <callumthom11@gmail.com>
2021-07-09 00:39:07 +0000

    avm1: Correct ToInteger
    * Coerce objects as well.
    * Wrap numbers around i32.

--- core/src/avm1/activation.rs
+++ core/src/avm1/activation.rs
@@ -2194,14 +2194,8 @@ impl<'a, 'gc, 'gc_context> Activation<'a, 'gc, 'gc_context> {
     }
 
     fn action_to_integer(&mut self) -> Result<FrameControl<'gc>, Error<'gc>> {
-        let val = self.context.avm1.pop();
-
-        let value = match val {
-            Value::Undefined | Value::Null | Value::Object(_) => 0.0,
-            _ => val.coerce_to_f64(self)?,
-        };
-
-        self.context.avm1.push(value.trunc());
+        let val = self.context.avm1.pop().coerce_to_i32(self)?;
+        self.context.avm1.push(val);
         Ok(FrameControl::Continue)
     }
 
