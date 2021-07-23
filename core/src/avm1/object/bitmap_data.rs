ruffle-rs/ruffle

commit d2eb46d3be8dcb884847f4ecf0e3f25f9fe075bc

chore: Reserve capacity for vectors
This avoids unnecessary re-allocations and thus should improve
performance in both memory and time terms.

core/src/avm1/object/bitmap_data.rs
@@ -206,13 +206,10 @@ impl BitmapData {
     }
 
     pub fn pixels_rgba(&self) -> Vec<u8> {
-        let mut output = Vec::new();
-
-        for p in &self.pixels {
-            output.extend_from_slice(&[p.red(), p.green(), p.blue(), p.alpha()])
-        }
-
-        output
+        self.pixels
+            .iter()
+            .flat_map(|p| [p.red(), p.green(), p.blue(), p.alpha()])
+            .collect()
     }
 
     pub fn width(&self) -> u32 {

core/src/avm1/function.rs
@@ -12,10 +12,7 @@ use crate::tag_utils::SwfSlice;
 use gc_arena::{Collect, CollectionContext, Gc, GcCell, MutationContext};
 use std::borrow::Cow;
 use std::fmt;
-use swf::{
-    avm1::types::{FunctionFlags, FunctionParam},
-    SwfStr,
-};
+use swf::{avm1::types::FunctionFlags, SwfStr};
 
 /// Represents a function defined in Ruffle's code.
 ///
@@ -144,24 +141,24 @@ impl<'gc> Avm1Function<'gc> {
             )
         };
 
-        let mut owned_params = Vec::new();
-        for FunctionParam {
-            name: s,
-            register_index: r,
-        } in &swf_function.params
-        {
-            owned_params.push((
-                *r,
-                (*s).to_string_lossy(SwfStr::encoding_for_version(swf_version)),
-            ))
-        }
+        let params = swf_function
+            .params
+            .iter()
+            .map(|p| {
+                (
+                    p.register_index,
+                    p.name
+                        .to_string_lossy(SwfStr::encoding_for_version(swf_version)),
+                )
+            })
+            .collect();
 
         Avm1Function {
             swf_version,
             data: actions,
             name,
             register_count: swf_function.register_count,
-            params: owned_params,
+            params,
             scope,
             constant_pool,
             base_clip,

core/src/avm2/class.rs
@@ -215,8 +215,8 @@ impl<'gc> Class<'gc> {
             None
         };
 
-        let mut interfaces = Vec::new();
-        for interface_name in abc_instance.interfaces.iter() {
+        let mut interfaces = Vec::with_capacity(abc_instance.interfaces.len());
+        for interface_name in &abc_instance.interfaces {
             interfaces.push(Multiname::from_abc_multiname_static(
                 unit,
                 interface_name.clone(),
@@ -302,9 +302,9 @@ impl<'gc> Class<'gc> {
         body: &AbcMethodBody,
     ) -> Result<GcCell<'gc, Self>, Error> {
         let name = translation_unit.pool_string(method.name.as_u30(), mc)?;
-        let mut traits = Vec::new();
 
-        for trait_entry in body.traits.iter() {
+        let mut traits = Vec::with_capacity(body.traits.len());
+        for trait_entry in &body.traits {
             traits.push(Trait::from_abc_trait(
                 translation_unit,
                 trait_entry,

core/src/avm2/globals/array.rs
@@ -764,8 +764,8 @@ pub fn splice<'gc>(
                     .as_array_storage()
                     .map(|a| a.iter().collect::<Vec<Option<Value<'gc>>>>())
                     .unwrap();
-                let mut resolved = Vec::new();
 
+                let mut resolved = Vec::with_capacity(contents.len());
                 for (i, v) in contents.iter().enumerate() {
                     resolved.push(resolve_array_hole(activation, this, i, v.clone())?);
                 }
@@ -936,15 +936,18 @@ fn sort_postprocess<'gc>(
             );
         } else {
             if let Some(mut old_array) = this.as_array_storage_mut(activation.context.gc_context) {
-                let mut new_vec = Vec::new();
-
-                for (src, v) in values.iter() {
-                    if old_array.get(*src).is_none() && !matches!(v, Value::Undefined) {
-                        new_vec.push(Some(v.clone()));
-                    } else {
-                        new_vec.push(old_array.get(*src).clone());
-                    }
-                }
+                let new_vec = values
+                    .iter()
+                    .map(|(src, v)| {
+                        if let Some(old_value) = old_array.get(*src) {
+                            Some(old_value)
+                        } else if !matches!(v, Value::Undefined) {
+                            Some(v.clone())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
 
                 let mut new_array = ArrayStorage::from_storage(new_vec);
 
@@ -968,13 +971,7 @@ fn extract_array_values<'gc>(
     let object = value.coerce_to_object(activation).ok();
     let holey_vec = if let Some(object) = object {
         if let Some(field_array) = object.as_array_storage() {
-            let mut array = Vec::new();
-
-            for v in field_array.iter() {
-                array.push(v);
-            }
-
-            array
+            field_array.clone()
         } else {
             return Ok(None);
         }
@@ -982,7 +979,7 @@ fn extract_array_values<'gc>(
         return Ok(None);
     };
 
-    let mut unholey_vec = Vec::new();
+    let mut unholey_vec = Vec::with_capacity(holey_vec.length());
     for (i, v) in holey_vec.iter().enumerate() {
         unholey_vec.push(resolve_array_hole(
             activation,
@@ -1104,12 +1101,12 @@ fn extract_maybe_array_strings<'gc>(
     activation: &mut Activation<'_, 'gc, '_>,
     value: Value<'gc>,
 ) -> Result<Vec<AvmString<'gc>>, Error> {
-    let mut out = Vec::new();
+    let values = extract_maybe_array_values(activation, value)?;
 
-    for value in extract_maybe_array_values(activation, value)? {
+    let mut out = Vec::with_capacity(values.len());
+    for value in values {
         out.push(value.coerce_to_string(activation)?);
     }
-
     Ok(out)
 }
 
@@ -1123,14 +1120,14 @@ fn extract_maybe_array_sort_options<'gc>(
     activation: &mut Activation<'_, 'gc, '_>,
     value: Value<'gc>,
 ) -> Result<Vec<SortOptions>, Error> {
-    let mut out = Vec::new();
+    let values = extract_maybe_array_values(activation, value)?;
 
-    for value in extract_maybe_array_values(activation, value)? {
+    let mut out = Vec::with_capacity(values.len());
+    for value in values {
         out.push(SortOptions::from_bits_truncate(
             value.coerce_to_u32(activation)? as u8,
         ));
     }
-
     Ok(out)
 }
 

core/src/avm2/globals/flash/display/movieclip.rs
@@ -163,10 +163,10 @@ fn labels_for_scene<'gc>(
         start: scene_start,
         length: scene_length,
     } = scene;
-    let mut frame_labels = Vec::new();
     let frame_label_proto = activation.context.avm2.prototypes().framelabel;
-
-    for (name, frame) in mc.labels_in_range(*scene_start, scene_start + scene_length) {
+    let labels = mc.labels_in_range(*scene_start, scene_start + scene_length);
+    let mut frame_labels = Vec::with_capacity(labels.len());
+    for (name, frame) in labels {
         let name: Value<'gc> = AvmString::new(activation.context.gc_context, name).into();
         let local_frame = frame - scene_start + 1;
         let args = [name, local_frame.into()];
@@ -252,9 +252,7 @@ pub fn scenes<'gc>(
         .and_then(|o| o.as_display_object())
         .and_then(|dobj| dobj.as_movie_clip())
     {
-        let mut scene_objects = Vec::new();
         let mut mc_scenes = mc.scenes();
-
         if mc.scenes().is_empty() {
             mc_scenes.push(Scene {
                 name: "".to_string(),
@@ -263,6 +261,7 @@ pub fn scenes<'gc>(
             });
         }
 
+        let mut scene_objects = Vec::with_capacity(mc_scenes.len());
         for scene in mc_scenes {
             let (scene_name, scene_length, scene_labels) =
                 labels_for_scene(activation, mc, &scene)?;

core/src/avm2/globals/flash/events/event.rs
@@ -162,14 +162,16 @@ pub fn format_to_string<'gc>(
     this: Option<Object<'gc>>,
     args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error> {
+    use std::fmt::Write;
+
     if let Some(mut this) = this {
         let class_name = args
             .get(0)
             .cloned()
             .unwrap_or(Value::Undefined)
             .coerce_to_string(activation)?;
-        let mut stringified_params = Vec::new();
 
+        let mut stringified_params = String::new();
         if let Some(params) = args.get(1..) {
             for param_name in params {
                 let param_name = QName::dynamic_name(match param_name {
@@ -180,13 +182,19 @@ pub fn format_to_string<'gc>(
                 let param_value = this
                     .get_property(this, &param_name, activation)?
                     .coerce_to_debug_string(activation)?;
-                stringified_params.push(format!(" {}={}", param_name.local_name(), param_value));
+                write!(
+                    stringified_params,
+                    " {}={}",
+                    param_name.local_name(),
+                    param_value
+                )
+                .unwrap();
             }
         }
 
         return Ok(AvmString::new(
             activation.context.gc_context,
-            format!("[{}{}]", class_name, stringified_params.join("")),
+            format!("[{}{}]", class_name, stringified_params),
         )
         .into());
     }

core/src/avm2/globals/function.rs
@@ -77,7 +77,7 @@ fn apply<'gc>(
             .map(|a| a.iter().collect())
             .ok_or_else(|| Error::from("Second parameter of apply must be an array"))?;
 
-        let mut resolved_args = Vec::new();
+        let mut resolved_args = Vec::with_capacity(arg_storage.len());
         for (i, v) in arg_storage.iter().enumerate() {
             resolved_args.push(resolve_array_hole(activation, arg_array, i, v.clone())?);
         }

core/src/avm2/object/function_object.rs
@@ -124,8 +124,8 @@ impl<'gc> FunctionObject<'gc> {
         mut class_proto: Object<'gc>,
         scope: Option<GcCell<'gc, Scope<'gc>>>,
     ) -> Result<(Object<'gc>, Object<'gc>), Error> {
-        let mut interfaces = Vec::new();
         let interface_names = class.read().interfaces().to_vec();
+        let mut interfaces = Vec::with_capacity(interface_names.len());
         for interface_name in interface_names {
             let interface = if let Some(scope) = scope {
                 scope

commit db3bef4239d3eb87fa3f6b3aba58b400aca4d216

avm1: Fix performance regression in `BitmapData::pixels_rgba`
It was regressed in #4822 when changed to use `flat_map`. But
currently Rust emits suboptimal code for such case. For now use
`Vec::with_capacity` manually to avoid unnecessary re-allocations.

core/src/avm1/object/bitmap_data.rs
@@ -206,10 +206,21 @@ impl BitmapData {
     }
 
     pub fn pixels_rgba(&self) -> Vec<u8> {
-        self.pixels
-            .iter()
-            .flat_map(|p| [p.red(), p.green(), p.blue(), p.alpha()])
-            .collect()
+        // TODO: This could have been implemented as follows:
+        //
+        // self.pixels
+        //     .iter()
+        //     .flat_map(|p| [p.red(), p.green(), p.blue(), p.alpha()])
+        //     .collect()
+        //
+        // But currently Rust emits suboptimal code in that case. For now we use
+        // `Vec::with_capacity` manually to avoid unnecessary re-allocations.
+
+        let mut output = Vec::with_capacity(self.pixels.len() * 4);
+        for p in &self.pixels {
+            output.extend_from_slice(&[p.red(), p.green(), p.blue(), p.alpha()])
+        }
+        output
     }
 
     pub fn width(&self) -> u32 {