2c2643b0e0f78b93dfbdfa075c23d9856970eb45
TÖRÖK Attila <torokati44@gmail.com>
2021-08-07 02:41:31 +0200

    core,avm1: Extract ColorTransformParams from ColorTransformData

--- core/src/avm1/object/color_transform_object.rs
+++ core/src/avm1/object/color_transform_object.rs
@@ -1,8 +1,8 @@
 use crate::add_field_accessors;
 use crate::avm1::{Object, ScriptObject, TObject};
+use crate::bitmap::color_transform_params::ColorTransformParams;
 use crate::impl_custom_object;
 use gc_arena::{Collect, GcCell, MutationContext};
-
 use std::fmt;
 
 /// A ColorTransform
@@ -15,29 +15,21 @@ pub struct ColorTransformObject<'gc>(GcCell<'gc, ColorTransformData<'gc>>);
 pub struct ColorTransformData<'gc> {
     /// The underlying script object.
     base: ScriptObject<'gc>,
-
-    red_multiplier: f64,
-    green_multiplier: f64,
-    blue_multiplier: f64,
-    alpha_multiplier: f64,
-    red_offset: f64,
-    green_offset: f64,
-    blue_offset: f64,
-    alpha_offset: f64,
+    params: ColorTransformParams,
 }
 
 impl fmt::Debug for ColorTransformObject<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let this = self.0.read();
         f.debug_struct("ColorTransform")
-            .field("redMultiplier", &this.red_multiplier)
-            .field("greenMultiplier", &this.green_multiplier)
-            .field("blueMultiplier", &this.blue_multiplier)
-            .field("alphaMultiplier", &this.alpha_multiplier)
-            .field("redOffset", &this.red_offset)
-            .field("greenOffset", &this.green_offset)
-            .field("blueOffset", &this.blue_offset)
-            .field("alphaOffset", &this.alpha_offset)
+            .field("redMultiplier", &this.params.red_multiplier)
+            .field("greenMultiplier", &this.params.green_multiplier)
+            .field("blueMultiplier", &this.params.blue_multiplier)
+            .field("alphaMultiplier", &this.params.alpha_multiplier)
+            .field("redOffset", &this.params.red_offset)
+            .field("greenOffset", &this.params.green_offset)
+            .field("blueOffset", &this.params.blue_offset)
+            .field("alphaOffset", &this.params.alpha_offset)
             .finish()
     }
 }
@@ -51,42 +43,49 @@ impl<'gc> ColorTransformObject<'gc> {
             gc_context,
             ColorTransformData {
                 base: ScriptObject::object(gc_context, proto),
-                red_multiplier: 0.0,
-                green_multiplier: 0.0,
-                blue_multiplier: 0.0,
-                alpha_multiplier: 0.0,
-                red_offset: 0.0,
-                green_offset: 0.0,
-                blue_offset: 0.0,
-                alpha_offset: 0.0,
+                params: ColorTransformParams {
+                    red_multiplier: 0.0,
+                    green_multiplier: 0.0,
+                    blue_multiplier: 0.0,
+                    alpha_multiplier: 0.0,
+                    red_offset: 0.0,
+                    green_offset: 0.0,
+                    blue_offset: 0.0,
+                    alpha_offset: 0.0,
+                },
             },
         ))
     }
 
     add_field_accessors!(
-        [set_red_multiplier, get_red_multiplier, red_multiplier, f64],
+        [
+            set_red_multiplier,
+            get_red_multiplier,
+            params.red_multiplier,
+            f64
+        ],
         [
             set_green_multiplier,
             get_green_multiplier,
-            green_multiplier,
+            params.green_multiplier,
             f64
         ],
         [
             set_blue_multiplier,
             get_blue_multiplier,
-            blue_multiplier,
+            params.blue_multiplier,
             f64
         ],
         [
             set_alpha_multiplier,
             get_alpha_multiplier,
-            alpha_multiplier,
+            params.alpha_multiplier,
             f64
         ],
-        [set_red_offset, get_red_offset, red_offset, f64],
-        [set_green_offset, get_green_offset, green_offset, f64],
-        [set_blue_offset, get_blue_offset, blue_offset, f64],
-        [set_alpha_offset, get_alpha_offset, alpha_offset, f64],
+        [set_red_offset, get_red_offset, params.red_offset, f64],
+        [set_green_offset, get_green_offset, params.green_offset, f64],
+        [set_blue_offset, get_blue_offset, params.blue_offset, f64],
+        [set_alpha_offset, get_alpha_offset, params.alpha_offset, f64],
     );
 }
 
