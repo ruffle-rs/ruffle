4a770e1bbafb244e758e462efaa98c689c09b0fb
relrelb <relrelbachar@gmail.com>
2021-08-13 12:35:23 +0300

    audio: Move ADPCM's tables out of `AdpcmDecoder`

--- core/src/backend/audio/decoders/adpcm.rs
+++ core/src/backend/audio/decoders/adpcm.rs
@@ -2,6 +2,74 @@ use super::{Decoder, SeekableDecoder};
 use bitstream_io::{BigEndian, BitRead, BitReader};
 use std::io::{Cursor, Read};
 
+const INDEX_TABLE: [&[i16]; 4] = [
+    &[-1, 2],
+    &[-1, -1, 2, 4],
+    &[-1, -1, -1, -1, 2, 4, 6, 8],
+    &[-1, -1, -1, -1, -1, -1, -1, -1, 1, 2, 4, 6, 8, 10, 13, 16],
+];
+
+const STEP_TABLE: [u16; 89] = [
+    7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60, 66,
+    73, 80, 88, 97, 107, 118, 130, 143, 157, 173, 190, 209, 230, 253, 279, 307, 337, 371, 408, 449,
+    494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282, 1411, 1552, 1707, 1878, 2066, 2272,
+    2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358, 5894, 6484, 7132, 7845, 8630, 9493,
+    10442, 11487, 12635, 13899, 15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767,
+];
+
+const SAMPLE_DELTA_CALCULATOR: [fn(u16, i32) -> u16; 4] = [
+    // 2 bits
+    |step: u16, magnitude: i32| {
+        let mut delta = step >> 1;
+        if magnitude & 1 != 0 {
+            delta += step;
+        };
+        delta
+    },
+    // 3 bits
+    |step: u16, magnitude: i32| {
+        let mut delta = step >> 2;
+        if magnitude & 1 != 0 {
+            delta += step >> 1;
+        }
+        if magnitude & 2 != 0 {
+            delta += step;
+        }
+        delta
+    },
+    // 4 bits
+    |step: u16, magnitude: i32| {
+        let mut delta = step >> 3;
+        if magnitude & 1 != 0 {
+            delta += step >> 2;
+        }
+        if magnitude & 2 != 0 {
+            delta += step >> 1;
+        }
+        if magnitude & 4 != 0 {
+            delta += step;
+        }
+        delta
+    },
+    // 5 bits
+    |step: u16, magnitude: i32| {
+        let mut delta = step >> 4;
+        if magnitude & 1 != 0 {
+            delta += step >> 3;
+        }
+        if magnitude & 2 != 0 {
+            delta += step >> 2;
+        }
+        if magnitude & 4 != 0 {
+            delta += step >> 1;
+        }
+        if magnitude & 8 != 0 {
+            delta += step;
+        }
+        delta
+    },
+];
+
 #[derive(Clone, Default)]
 struct Channel {
     sample: i16,
@@ -18,75 +86,6 @@ pub struct AdpcmDecoder<R: Read> {
 }
 
 impl<R: Read> AdpcmDecoder<R> {
-    const INDEX_TABLE: [&'static [i16]; 4] = [
-        &[-1, 2],
-        &[-1, -1, 2, 4],
-        &[-1, -1, -1, -1, 2, 4, 6, 8],
-        &[-1, -1, -1, -1, -1, -1, -1, -1, 1, 2, 4, 6, 8, 10, 13, 16],
-    ];
-
-    const STEP_TABLE: [u16; 89] = [
-        7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60,
-        66, 73, 80, 88, 97, 107, 118, 130, 143, 157, 173, 190, 209, 230, 253, 279, 307, 337, 371,
-        408, 449, 494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282, 1411, 1552, 1707, 1878,
-        2066, 2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358, 5894, 6484, 7132, 7845,
-        8630, 9493, 10442, 11487, 12635, 13899, 15289, 16818, 18500, 20350, 22385, 24623, 27086,
-        29794, 32767,
-    ];
-
-    const SAMPLE_DELTA_CALCULATOR: [fn(u16, i32) -> u16; 4] = [
-        // 2 bits
-        |step: u16, magnitude: i32| {
-            let mut delta = step >> 1;
-            if magnitude & 1 != 0 {
-                delta += step;
-            };
-            delta
-        },
-        // 3 bits
-        |step: u16, magnitude: i32| {
-            let mut delta = step >> 2;
-            if magnitude & 1 != 0 {
-                delta += step >> 1;
-            }
-            if magnitude & 2 != 0 {
-                delta += step;
-            }
-            delta
-        },
-        // 4 bits
-        |step: u16, magnitude: i32| {
-            let mut delta = step >> 3;
-            if magnitude & 1 != 0 {
-                delta += step >> 2;
-            }
-            if magnitude & 2 != 0 {
-                delta += step >> 1;
-            }
-            if magnitude & 4 != 0 {
-                delta += step;
-            }
-            delta
-        },
-        // 5 bits
-        |step: u16, magnitude: i32| {
-            let mut delta = step >> 4;
-            if magnitude & 1 != 0 {
-                delta += step >> 3;
-            }
-            if magnitude & 2 != 0 {
-                delta += step >> 2;
-            }
-            if magnitude & 4 != 0 {
-                delta += step >> 1;
-            }
-            if magnitude & 8 != 0 {
-                delta += step;
-            }
-            delta
-        },
-    ];
-
     pub fn new(inner: R, is_stereo: bool, sample_rate: u16) -> Self {
         let mut reader = BitReader::new(inner);
         let bits_per_sample = reader.read::<u8>(2).unwrap_or_else(|e| {
@@ -103,7 +102,7 @@ impl<R: Read> AdpcmDecoder<R> {
             bits_per_sample,
             sample_num: 0,
             channels: vec![Default::default(); num_channels],
-            decoder: Self::SAMPLE_DELTA_CALCULATOR[bits_per_sample - 2],
+            decoder: SAMPLE_DELTA_CALCULATOR[bits_per_sample - 2],
         }
     }
 }
@@ -124,7 +123,7 @@ impl<R: Read> Iterator for AdpcmDecoder<R> {
 
         for channel in &mut self.channels {
             let data = self.inner.read::<u32>(self.bits_per_sample as u32).ok()? as i32;
-            let step = Self::STEP_TABLE[channel.step_index as usize];
+            let step = STEP_TABLE[channel.step_index as usize];
 
             // (data + 0.5) * step / 2^(bits_per_sample - 2)
             // `data` is sign-magnitude, NOT two's complement.
@@ -139,8 +138,8 @@ impl<R: Read> Iterator for AdpcmDecoder<R> {
                 (channel.sample as i32 + delta as i32).min(i16::MAX.into())
             } as i16;
 
-            channel.step_index += Self::INDEX_TABLE[self.bits_per_sample - 2][magnitude as usize];
-            channel.step_index = channel.step_index.clamp(0, Self::STEP_TABLE.len() as i16 - 1);
+            channel.step_index += INDEX_TABLE[self.bits_per_sample - 2][magnitude as usize];
+            channel.step_index = channel.step_index.clamp(0, STEP_TABLE.len() as i16 - 1);
         }
 
         let left = self.channels[0].sample;
