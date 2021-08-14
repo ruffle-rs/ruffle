edbb94e7b01736238e2f497187949104a9bd12b0
David Wendt <dcrkid@yahoo.com>
2021-08-13 23:36:25 -0400

    tests: Add a test for AS3 `SoundTransform` properties

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -654,6 +654,7 @@ swf_tests! {
     (as3_vector_legacy, "avm2/vector_legacy", 1),
     (as3_sound_valueof, "avm2/sound_valueof", 1),
     (as3_sound_embeddedprops, "avm2/sound_embeddedprops", 1),
+    (as3_soundtransform, "avm2/soundtransform", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
