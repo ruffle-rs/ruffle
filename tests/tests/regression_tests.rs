efe36e79c04556b2adb1b3c70bc431d839d9e7b0
David Wendt <dcrkid@yahoo.com>
2021-08-18 22:04:36 -0400

    avm2: Implement `SoundChannel.soundTransform`

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -660,6 +660,7 @@ swf_tests! {
     (as3_soundmixer_soundtransform, "avm2/soundmixer_soundtransform", 49),
     (as3_sound_play, "avm2/sound_play", 1),
     #[ignore] (as3_soundchannel_position, "avm2/soundchannel_position", 75),
+    (as3_soundchannel_soundtransform, "avm2/soundchannel_soundtransform", 49),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
