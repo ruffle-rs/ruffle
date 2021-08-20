b98c3e0c855eec98a380737c48966bc5f2e11dcf
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2021-08-21 00:48:59 +0200

    chore: fmt

--- core/src/avm2/globals/flash/events/mouseevent.rs
+++ core/src/avm2/globals/flash/events/mouseevent.rs
@@ -42,9 +42,7 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
 
     write.set_attributes(ClassAttributes::SEALED);
 
-    const CONSTANTS: &[(&str, &str)] = &[
-        ("CLICK", "click"),
-    ];
+    const CONSTANTS: &[(&str, &str)] = &[("CLICK", "click")];
     write.define_public_constant_string_class_traits(CONSTANTS);
 
     class
