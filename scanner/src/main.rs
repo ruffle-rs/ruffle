707c6a8fb9e745fa8bd5b3fe1a61696ab8c83616
David Wendt <dcrkid@yahoo.com>
2021-08-23 21:07:18 -0400

    scanner: Run the first frame of every movie we scan.
    This currently fails to scan the entire Flashpoint `localflash` directory because Rust considers stack overflow to be an immediate program termination, no ifs, ands, or buts about it.

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,8 +1,19 @@
 use clap::Clap;
 use indicatif::{ProgressBar, ProgressStyle};
+use log::{Level, LevelFilter, Log, Metadata, Record};
 use path_slash::PathExt;
 use rayon::prelude::*;
+use ruffle_core::backend::audio::NullAudioBackend;
+use ruffle_core::backend::locale::NullLocaleBackend;
+use ruffle_core::backend::log::LogBackend;
+use ruffle_core::backend::navigator::{NullExecutor, NullNavigatorBackend};
+use ruffle_core::backend::render::NullRenderer;
+use ruffle_core::backend::storage::MemoryStorageBackend;
+use ruffle_core::backend::ui::NullUiBackend;
+use ruffle_core::backend::video::NullVideoBackend;
 use ruffle_core::swf::{decompress_swf, parse_swf};
+use ruffle_core::tag_utils::SwfMovie;
+use ruffle_core::Player;
 use swf::{FileAttributes, Tag};
 
 use serde::Serialize;
@@ -10,6 +21,11 @@ use std::path::{Path, PathBuf};
 
 use std::panic::catch_unwind;
 use walkdir::{DirEntry, WalkDir};
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::Arc;
+use std::time::Duration;
 
 #[derive(Serialize, Debug)]
 enum AvmType {
@@ -23,6 +39,8 @@ enum Progress {
     Read,
     Decompressed,
     Parsed,
+    Executed,
+    Completed,
 }
 
 #[derive(Serialize, Debug)]
@@ -70,7 +88,86 @@ fn find_files(root: &Path, ignore: &[String]) -> Vec<DirEntry> {
     results
 }
 
+/// Log backend that specifically discards AVM trace output
+struct ScanLogBackend();
+
+impl ScanLogBackend {
+    pub fn new() -> Self {
+        Self()
+    }
+}
+
+impl LogBackend for ScanLogBackend {
+    fn avm_trace(&self, message: &str) {}
+}
+
+thread_local! {
+    /// Thread local log buffer.
+    pub static LOCAL_LOGGER: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
+}
+
+/// `log` backend (not to be confused with Ruffle's notion of a log backend)
+/// that only logs errors to a thread-local area.
+struct ThreadLocalScanLogger();
+
+static GLOBAL_LOGGER: ThreadLocalScanLogger = ThreadLocalScanLogger();
+
+impl ThreadLocalScanLogger {
+    fn init() {
+        log::set_logger(&GLOBAL_LOGGER)
+            .map(|()| log::set_max_level(LevelFilter::Info))
+            .unwrap();
+    }
+}
+
+impl Log for ThreadLocalScanLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() == Level::Error
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            LOCAL_LOGGER.with(|log_buffer| {
+                log_buffer.borrow_mut().push(format!("{}", record.args()));
+            })
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+fn execute_swf(file: DirEntry) {
+    let base_path = Path::new(file.path()).parent().unwrap();
+    let (mut executor, channel) = NullExecutor::new();
+    let movie = SwfMovie::from_path(file.path(), None).unwrap();
+    let frame_time = 1000.0 / movie.frame_rate().to_f64();
+    let player = Player::new(
+        Box::new(NullRenderer::new()),
+        Box::new(NullAudioBackend::new()),
+        Box::new(NullNavigatorBackend::with_base_path(base_path, channel)),
+        Box::new(MemoryStorageBackend::default()),
+        Box::new(NullLocaleBackend::new()),
+        Box::new(NullVideoBackend::new()),
+        Box::new(ScanLogBackend::new()),
+        Box::new(NullUiBackend::new()),
+    )
+    .unwrap();
+
+    player.lock().unwrap().set_root_movie(Arc::new(movie));
+    player
+        .lock()
+        .unwrap()
+        .set_max_execution_duration(Duration::from_secs(300));
+    player.lock().unwrap().run_frame();
+    player.lock().unwrap().update_timers(frame_time);
+    //executor.poll_all().unwrap();
+}
+
 fn scan_file(file: DirEntry, name: String) -> FileResults {
+    LOCAL_LOGGER.with(|log_buffer| {
+        log_buffer.borrow_mut().truncate(0);
+    });
+
     let mut progress = Progress::Nothing;
 
     let data = match std::fs::read(file.path()) {
@@ -146,6 +243,42 @@ fn scan_file(file: DirEntry, name: String) -> FileResults {
 
     progress = Progress::Parsed;
 
+    //Run one frame of the movie in Ruffle.
+    if let Err(e) = catch_unwind(|| execute_swf(file)) {
+        match e.downcast::<String>() {
+            Ok(e) => {
+                return FileResults {
+                    progress,
+                    name,
+                    error: Some(format!("PANIC: {}", e.to_string())),
+                    vm_type: None,
+                }
+            }
+            Err(_) => {
+                return FileResults {
+                    progress,
+                    name,
+                    error: Some("PANIC".to_string()),
+                    vm_type: None,
+                }
+            }
+        }
+    }
+
+    progress = Progress::Executed;
+
+    let errors = LOCAL_LOGGER.with(|log_buffer| log_buffer.borrow_mut().join("\n"));
+    if !errors.is_empty() {
+        return FileResults {
+            progress,
+            name,
+            error: Some(errors),
+            vm_type,
+        };
+    }
+
+    progress = Progress::Completed;
+
     FileResults {
         progress,
         name,
@@ -155,7 +288,7 @@ fn scan_file(file: DirEntry, name: String) -> FileResults {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    env_logger::init();
+    ThreadLocalScanLogger::init();
 
     let opt = Opt::parse();
     let to_scan = find_files(&opt.input_path, &opt.ignore);
