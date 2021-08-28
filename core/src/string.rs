8663088feef4e19e7bfc24fb1c88db68d086271c
Moulins <arthur.heuillard@orange.fr>
2021-08-28 16:40:14 +0200

    core: Introduce `WStr` and `WStrMut` types, for representing Flash strings
    These types represent an UCS2 string (UTF-16 with unpaired surrogates).
    
    The string is stored either as a sequence of u8s (Bytes) or u16s (Wide);
    the type of string is tracked by setting the high bit of the length of
    its fat pointer.

--- core/src/string.rs
+++ core/src/string.rs
@@ -1,5 +1,27 @@
+//! Provides UCS2 string types for usage in AVM1 and AVM2.
+//!
+//! Internally, these types are represeted by a sequence of 1-byte or 2-bytes (wide) code units,
+//! that may contains null bytes or unpaired surrogates.
+//!
+//! To match Flash behavior, the string length is limited to 2³¹-1 code units;
+//! any attempt to create a longer string will panic.
+
+#[macro_use]
+mod common;
+
 mod avm;
+mod ops;
+mod raw;
+mod slice;
 mod tables;
 pub mod utils;
 
+/// The maximum string length, equals to 2³¹-1.
+pub const MAX_STRING_LEN: usize = raw::MAX_STRING_LEN;
+
 pub use avm::AvmString;
+pub use common::{BorrowWStr, BorrowWStrMut, Units};
+pub use ops::Iter;
+pub use slice::{WStr, WStrMut};
+
+use common::panic_on_invalid_length;
