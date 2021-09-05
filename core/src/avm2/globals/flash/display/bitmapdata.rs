335aec5be0e103e1ce478cd1589b5a73bab766c7
David Wendt <dcrkid@yahoo.com>
2021-09-05 17:32:14 -0400

    avm2: Implement `BitmapData`'s constructor.
    This includes support for both embedded bitmap data (resolved via the SymbolClass mechanism) as well as empty bitmaps configured via arguments.

--- core/src/avm2/globals/flash/display/bitmapdata.rs
+++ core/src/avm2/globals/flash/display/bitmapdata.rs
@@ -4,19 +4,109 @@ use crate::avm2::activation::Activation;
 use crate::avm2::class::{Class, ClassAttributes};
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
-use crate::avm2::object::{bitmapdata_allocator, Object};
+use crate::avm2::object::{bitmapdata_allocator, Object, TObject};
 use crate::avm2::value::Value;
 use crate::avm2::Error;
+use crate::bitmap::bitmap_data::BitmapData;
+use crate::bitmap::is_size_valid;
+use crate::character::Character;
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `flash.display.BitmapData`'s instance constructor.
 pub fn instance_init<'gc>(
     activation: &mut Activation<'_, 'gc, '_>,
     this: Option<Object<'gc>>,
-    _args: &[Value<'gc>],
+    args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error> {
     if let Some(this) = this {
         activation.super_init(this, &[])?;
+
+        let name = this
+            .as_class_object()
+            .and_then(|t| t.as_class())
+            .map(|c| c.read().name().clone());
+        let character = this
+            .as_class_object()
+            .and_then(|t| {
+                activation
+                    .context
+                    .library
+                    .avm2_class_registry()
+                    .class_symbol(t)
+            })
+            .and_then(|(movie, chara_id)| {
+                activation
+                    .context
+                    .library
+                    .library_for_movie_mut(movie)
+                    .character_by_id(chara_id)
+                    .cloned()
+            });
+
+        let new_bitmap_data =
+            GcCell::allocate(activation.context.gc_context, BitmapData::default());
+
+        if let Some(Character::Bitmap(bd)) = character {
+            let bitmap_handle = bd.bitmap_handle();
+
+            if let Some(bitmap_pixels) =
+                activation.context.renderer.get_bitmap_pixels(bitmap_handle)
+            {
+                let bitmap_pixels: Vec<i32> = bitmap_pixels.data.into();
+                new_bitmap_data
+                    .write(activation.context.gc_context)
+                    .set_pixels(
+                        bd.width().into(),
+                        bd.height().into(),
+                        true,
+                        bitmap_pixels.into_iter().map(|p| p.into()).collect(),
+                    );
+            } else {
+                log::warn!(
+                    "Could not read bitmap data associated with class {:?}",
+                    name
+                );
+            }
+        } else {
+            if character.is_some() {
+                //TODO: Determine if mismatched symbols will still work as a
+                //regular BitmapData subclass, or if this should throw
+                log::warn!(
+                    "BitmapData subclass {:?} is associated with a non-bitmap symbol",
+                    name
+                );
+            }
+
+            let width = args
+                .get(0)
+                .unwrap_or(&Value::Undefined)
+                .coerce_to_i32(activation)? as u32;
+            let height = args
+                .get(1)
+                .unwrap_or(&Value::Undefined)
+                .coerce_to_i32(activation)? as u32;
+            let transparency = args
+                .get(2)
+                .unwrap_or(&Value::Bool(true))
+                .coerce_to_boolean();
+            let fill_color = args
+                .get(3)
+                .unwrap_or(&Value::Unsigned(0xFFFFFFFF))
+                .coerce_to_u32(activation)?;
+
+            if !is_size_valid(activation.context.swf.version(), width, height) {
+                return Err("Bitmap size is not valid".into());
+            }
+
+            new_bitmap_data
+                .write(activation.context.gc_context)
+                .init_pixels(width, height, transparency, fill_color as i32);
+        }
+
+        new_bitmap_data
+            .write(activation.context.gc_context)
+            .init_object2(this);
+        this.init_bitmap_data(activation.context.gc_context, new_bitmap_data);
     }
 
     Ok(Value::Undefined)
@@ -34,7 +124,7 @@ pub fn class_init<'gc>(
 /// Construct `BitmapData`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
-        QName::new(Namespace::package("flash.media"), "BitmapData"),
+        QName::new(Namespace::package("flash.display"), "BitmapData"),
         Some(QName::new(Namespace::package(""), "Object").into()),
         Method::from_builtin(instance_init, "<BitmapData instance initializer>", mc),
         Method::from_builtin(class_init, "<BitmapData class initializer>", mc),
