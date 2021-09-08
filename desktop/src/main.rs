4b07c7347f93a07932059a316497d2f38bd75f2d
relrelb <relrelbachar@gmail.com>
2021-09-08 22:22:25 +0300

    desktop: Refactor clap settings
    * Fix #5252 by passing `multiple_occurrences = true`.
    * Remove `case_insensitive = true`.
    * Reformat comments a bit.

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -55,36 +55,30 @@ use winit::window::{Fullscreen, Icon, Window, WindowBuilder};
     version = include_str!(concat!(env!("OUT_DIR"), "/version-info.txt")),
 )]
 struct Opt {
-    /// Path to a flash movie (swf) to play
+    /// Path to a Flash movie (SWF) to play.
     #[clap(name = "FILE", parse(from_os_str))]
     input_path: Option<PathBuf>,
 
     /// A "flashvars" parameter to provide to the movie.
-    /// This can be repeated multiple times, for example -Pkey=value -Pfoo=bar
-    #[clap(short = 'P', number_of_values = 1)]
+    /// This can be repeated multiple times, for example -Pkey=value -Pfoo=bar.
+    #[clap(short = 'P', number_of_values = 1, multiple_occurrences = true)]
     parameters: Vec<String>,
 
     /// Type of graphics backend to use. Not all options may be supported by your current system.
     /// Default will attempt to pick the most supported graphics backend.
-    #[clap(
-        long,
-        short,
-        case_insensitive = true,
-        default_value = "default",
-        arg_enum
-    )]
+    #[clap(long, short, default_value = "default", arg_enum)]
     graphics: GraphicsBackend,
 
     /// Power preference for the graphics device used. High power usage tends to prefer dedicated GPUs,
     /// whereas a low power usage tends prefer integrated GPUs.
-    #[clap(long, short, case_insensitive = true, default_value = "high", arg_enum)]
+    #[clap(long, short, default_value = "high", arg_enum)]
     power: PowerPreference,
 
-    /// Width of window in pixels
+    /// Width of window in pixels.
     #[clap(long, display_order = 1)]
     width: Option<f64>,
 
-    /// Height of window in pixels
+    /// Height of window in pixels.
     #[clap(long, display_order = 2)]
     height: Option<f64>,
 
@@ -93,11 +87,11 @@ struct Opt {
     #[cfg(feature = "render_trace")]
     trace_path: Option<PathBuf>,
 
-    /// (Optional) Proxy to use when loading movies via URL
-    #[clap(long, case_insensitive = true)]
+    /// Proxy to use when loading movies via URL.
+    #[clap(long)]
     proxy: Option<Url>,
 
-    /// (Optional) Replace all embedded http URLs with https
+    /// Replace all embedded HTTP URLs with HTTPS.
     #[clap(long, takes_value = false)]
     upgrade_to_https: bool,
 
