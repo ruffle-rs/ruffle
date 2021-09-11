2abea5aebaa314a98f097b6e5a3fb13f72b1fbbe
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-09-11 23:48:30 +0100

    web: don't panic if failing to create an audio buffer

--- web/src/audio.rs
+++ web/src/audio.rs
@@ -681,7 +681,7 @@ impl WebAudioBackend {
                 num_sample_frames,
                 f32::from(sample_rate),
             )
-            .unwrap();
+            .map_err(|e| format!("Failed to create AudioBuffer: {:?}", e))?;
 
         copy_to_audio_buffer(
             &audio_buffer,
