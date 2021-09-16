fa3c5e610b4682e2e170715e2af19441089f9760
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-09-16 16:22:29 +0100

    chore: cargo fmt

--- core/src/avm2/activation.rs
+++ core/src/avm2/activation.rs
@@ -1757,7 +1757,7 @@ impl<'a, 'gc, 'gc_context> Activation<'a, 'gc, 'gc_context> {
 
     fn op_coerce_o(&mut self) -> Result<FrameControl<'gc>, Error> {
         let value = self.context.avm2.pop();
-        
+
         let coerced = match value {
             Value::Undefined | Value::Null => Value::Null,
             _ => value.coerce_to_object(self)?.into(),
