1c83d7112d32b87257f01aaf10b53efdfc76624b
David Wendt <dcrkid@yahoo.com>
2021-09-17 00:40:46 -0400

    avm2: `Video` is not a subclass of `flash.media.DisplayObject` (which doesn't exist)

--- core/src/avm2/globals/flash/media/video.rs
+++ core/src/avm2/globals/flash/media/video.rs
@@ -35,7 +35,7 @@ pub fn class_init<'gc>(
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
         QName::new(Namespace::package("flash.media"), "Video"),
-        Some(QName::new(Namespace::package("flash.media"), "DisplayObject").into()),
+        Some(QName::new(Namespace::package("flash.display"), "DisplayObject").into()),
         Method::from_builtin(instance_init, "<Video instance initializer>", mc),
         Method::from_builtin(class_init, "<Video class initializer>", mc),
         mc,
