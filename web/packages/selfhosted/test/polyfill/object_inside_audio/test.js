7b0561e4248537cd540cb2b0dce8182c8b0d9610
Toad06 <gd.productions@laposte.net>
2021-10-17 18:53:32 +0200

    tests: Don't polyfill files meant to be used as fallback content

---
+++ web/packages/selfhosted/test/polyfill/object_inside_audio/test.js
@@ -0,0 +1,19 @@
+const { inject_ruffle_and_wait, open_test } = require("../../utils");
+const { expect, use } = require("chai");
+const chaiHtml = require("chai-html");
+const fs = require("fs");
+
+use(chaiHtml);
+
+describe("Object inside audio node", () => {
+    it("loads the test", async () => {
+        await open_test(browser, __dirname);
+    });
+
+    it("doesn't polyfill with ruffle", async () => {
+        await inject_ruffle_and_wait(browser);
+        const actual = await browser.$("#test-container").getHTML(false);
+        const expected = fs.readFileSync(`${__dirname}/expected.html`, "utf8");
+        expect(actual).html.to.equal(expected);
+    });
+});
