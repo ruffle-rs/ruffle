aa01a682e1c108281fd59e23b555574b85c4d82c
David Wendt <dcrkid@yahoo.com>
2021-11-15 20:10:52 -0500

    avm2: Implement `uint`'s various formatting methods, too.

--- core/src/avm2/globals/uint.rs
+++ core/src/avm2/globals/uint.rs
@@ -2,11 +2,12 @@
 
 use crate::avm2::activation::Activation;
 use crate::avm2::class::Class;
-use crate::avm2::method::{Method, ParamConfig};
+use crate::avm2::globals::number::{print_with_precision, print_with_radix};
+use crate::avm2::method::{Method, NativeMethodImpl, ParamConfig};
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{primitive_allocator, Object, TObject};
 use crate::avm2::value::Value;
-use crate::avm2::Error;
+use crate::avm2::{AvmString, Error};
 use gc_arena::{GcCell, MutationContext};
 
 /// Implements `uint`'s instance initializer.
@@ -53,6 +54,136 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `uint.toExponential`
+pub fn to_exponential<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(this) = this.as_primitive() {
+            let number = this.coerce_to_u32(activation)?;
+            let digits = args
+                .get(0)
+                .cloned()
+                .unwrap_or(Value::Unsigned(0))
+                .coerce_to_u32(activation)? as usize;
+
+            if digits > 20 {
+                return Err("toExponential can only print with 0 through 20 digits.".into());
+            }
+
+            return Ok(AvmString::new_utf8(
+                activation.context.gc_context,
+                format!("{0:.1$e}", number, digits)
+                    .replace("e", "e+")
+                    .replace("e+-", "e-")
+                    .replace("e+0", ""),
+            )
+            .into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `uint.toFixed`
+pub fn to_fixed<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(this) = this.as_primitive() {
+            let number = this.coerce_to_u32(activation)?;
+            let digits = args
+                .get(0)
+                .cloned()
+                .unwrap_or(Value::Unsigned(0))
+                .coerce_to_u32(activation)? as usize;
+
+            if digits > 20 {
+                return Err("toFixed can only print with 0 through 20 digits.".into());
+            }
+
+            return Ok(AvmString::new_utf8(
+                activation.context.gc_context,
+                format!("{0:.1$}", number as f64, digits),
+            )
+            .into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `uint.toPrecision`
+pub fn to_precision<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(this) = this.as_primitive() {
+            let number = this.coerce_to_u32(activation)?;
+            let wanted_digits = args
+                .get(0)
+                .cloned()
+                .unwrap_or(Value::Unsigned(0))
+                .coerce_to_u32(activation)? as usize;
+
+            if wanted_digits < 1 || wanted_digits > 21 {
+                return Err("toPrecision can only print with 1 through 21 digits.".into());
+            }
+
+            return Ok(print_with_precision(activation, number as f64, wanted_digits)?.into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `uint.toString`
+pub fn to_string<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(this) = this.as_primitive() {
+            let number = this.coerce_to_u32(activation)?;
+            let radix = args
+                .get(0)
+                .cloned()
+                .unwrap_or(Value::Unsigned(10))
+                .coerce_to_u32(activation)? as usize;
+
+            if radix < 2 || radix > 36 {
+                return Err("toString can only print in bases 2 thru 36.".into());
+            }
+
+            return Ok(print_with_radix(activation, number as f64, radix)?.into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `uint.valueOf`
+pub fn value_of<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(this) = this.as_primitive() {
+            return Ok(this.clone());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `uint`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
@@ -78,6 +209,15 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
 
     const CLASS_CONSTANTS: &[(&str, u32)] = &[("MAX_VALUE", u32::MAX), ("MIN_VALUE", u32::MIN)];
     write.define_public_constant_uint_class_traits(CLASS_CONSTANTS);
+
+    const AS3_INSTANCE_METHODS: &[(&str, NativeMethodImpl)] = &[
+        ("toExponential", to_exponential),
+        ("toFixed", to_fixed),
+        ("toPrecision", to_precision),
+        ("toString", to_string),
+        ("valueOf", value_of),
+    ];
+    write.define_as3_builtin_instance_methods(mc, AS3_INSTANCE_METHODS);
 
     class
 }
