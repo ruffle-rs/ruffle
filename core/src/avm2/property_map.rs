c99231b610d04354c7e73ba024329ca713affffb
David Wendt <dcrkid@yahoo.com>
2021-11-23 23:29:06 -0500

    avm2: Index the property map by local name and bucket all namespaces together into a single list.

--- core/src/avm2/property_map.rs
+++ core/src/avm2/property_map.rs
@@ -1,7 +1,103 @@
 //! Property map
 
-use crate::avm2::names::QName;
+use crate::avm2::names::{Namespace, QName};
+use crate::avm2::AvmString;
+use gc_arena::Collect;
 use std::collections::HashMap;
+use std::mem::swap;
 
 /// Type which represents named properties on an object.
-pub type PropertyMap<'gc, V> = HashMap<QName<'gc>, V>;
+///
+/// This type exposes interfaces akin to `HashMap<QName<'gc>, V>`, and is
+/// intended to serve as a drop-in replacement optimized for objects where few
+/// properties have overlapping local names. However, we have made slight
+/// changes to the API in the following cases:
+///
+///  * Iterators return tuples of namespace, local-name, and value; rather than
+///    a qualified name and value pair.
+///  * Only `HashMap` methods and traits that we need are implemented.
+///
+/// The internal structure of the `PropertyMap` technically allows storage of
+/// multiple values per `QName`. It's implementation enforces the invariant
+/// that each `QName` only have one associated `V`.
+#[derive(Clone, Collect, Debug)]
+#[collect(no_drop)]
+pub struct PropertyMap<'gc, V>(HashMap<AvmString<'gc>, Vec<(Namespace<'gc>, V)>>);
+
+impl<'gc, V> Default for PropertyMap<'gc, V> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'gc, V> PropertyMap<'gc, V> {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    pub fn get(&self, name: &QName<'gc>) -> Option<&V> {
+        self.0
+            .get(&name.local_name())
+            .iter()
+            .filter_map(|v| {
+                v.iter()
+                    .filter(|(n, _)| n == name.namespace())
+                    .map(|(_, v)| v)
+                    .next()
+            })
+            .next()
+    }
+
+    pub fn get_mut(&mut self, name: &QName<'gc>) -> Option<&mut V> {
+        if let Some(bucket) = self.0.get_mut(&name.local_name()) {
+            if let Some((_, old_value)) = bucket.iter_mut().find(|(n, _)| n == name.namespace()) {
+                return Some(old_value);
+            }
+        }
+
+        None
+    }
+
+    pub fn contains_key(&self, name: &QName<'gc>) -> bool {
+        self.0
+            .get(&name.local_name())
+            .iter()
+            .any(|v| v.iter().any(|(n, _)| n == name.namespace()))
+    }
+
+    pub fn insert(&mut self, name: QName<'gc>, mut value: V) -> Option<V> {
+        let bucket = self.0.entry(name.local_name()).or_default();
+
+        if let Some((_, old_value)) = bucket.iter_mut().find(|(n, _)| n == name.namespace()) {
+            swap(old_value, &mut value);
+
+            Some(value)
+        } else {
+            bucket.push((name.namespace().clone(), value));
+
+            None
+        }
+    }
+
+    pub fn remove(&mut self, name: &QName<'gc>) -> Option<V> {
+        let bucket = self.0.get_mut(&name.local_name());
+
+        if let Some(bucket) = bucket {
+            let position = bucket
+                .iter_mut()
+                .enumerate()
+                .find(|(_, (n, _))| n == name.namespace());
+            if let Some((position, _)) = position {
+                return Some(bucket.remove(position).1);
+            }
+        }
+
+        None
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&Namespace<'gc>, &AvmString<'gc>, &V)> {
+        self.0
+            .iter()
+            .flat_map(|(s, b)| b.iter().map(move |(n, v)| (n, s, v)))
+    }
+}
