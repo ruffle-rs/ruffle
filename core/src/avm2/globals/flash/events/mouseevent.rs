33d2bdf666e09963d51865eab9dac52fa7bb2ff5
David Wendt <dcrkid@yahoo.com>
2021-12-01 17:32:50 -0500

    avm2: Impl `MouseEvent.buttonDown`

--- core/src/avm2/globals/flash/events/mouseevent.rs
+++ core/src/avm2/globals/flash/events/mouseevent.rs
@@ -272,6 +272,46 @@ pub fn set_shift_key<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `buttonDown`'s getter.
+pub fn button_down<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(evt) = this.as_event() {
+            if let EventData::MouseEvent { button_down, .. } = evt.event_data() {
+                return Ok(Value::Bool(*button_down));
+            }
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// Implements `buttonDown`'s setter.
+pub fn set_button_down<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(mut evt) = this.as_event_mut(activation.context.gc_context) {
+            if let EventData::MouseEvent { button_down, .. } = evt.event_data_mut() {
+                let value = args
+                    .get(0)
+                    .cloned()
+                    .unwrap_or(Value::Undefined)
+                    .coerce_to_boolean();
+
+                *button_down = value;
+            }
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `MouseEvent`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
@@ -319,6 +359,7 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
         ("controlKey", Some(control_key), Some(set_control_key)),
         ("ctrlKey", Some(control_key), Some(set_control_key)),
         ("shiftKey", Some(shift_key), Some(set_shift_key)),
+        ("buttonDown", Some(button_down), Some(set_button_down)),
     ];
     write.define_public_builtin_instance_properties(mc, PUBLIC_INSTANCE_PROPERTIES);
 
