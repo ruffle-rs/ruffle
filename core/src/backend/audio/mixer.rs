37a6b2b1e3e41fbcadaf7a57d5dac24fc283aaa5
Mike Welsh <mwelsh@gmail.com>
2022-01-03 14:35:23 -0800

    audio: Handle gaps in MP3 SoundStreamBlock tags (fix #3817)
    An MP3 "stream" sound can sometimes have frames without a
    SoundStreamBlock tag, despite the SWF spec saying there should
    at least be a tag with 0 samples on each frame. Ruffle would
    stop the sound in this case, but the Flash Player may or may not
    stop thje sound in the audio depending on the number of "empty"
    frames. This could cause the audio to stutter as it continuously
    stopped and restarted.
    
    Handle this by keeping track of how many samples we've encountered
    in MP3 blocks, and deducting the amount of samples consumed by each
    timeline frame. Stop the sound if we run out of samples, as opposed
    to when we hit a frame without a SoundStreamBlock.
    
    Fixes #3817.

--- core/src/backend/audio/mixer.rs
+++ core/src/backend/audio/mixer.rs
@@ -290,11 +290,11 @@ impl AudioMixer {
     /// Creates a `Stream` that decodes and resamples a timeline "stream" sound.
     fn make_stream_from_swf_slice<'a>(
         &self,
-        format: &swf::SoundFormat,
+        stream_info: &swf::SoundStreamHead,
         data_stream: SwfSlice,
     ) -> Result<Box<dyn 'a + Stream>, Error> {
         // Instantiate a decoder for the compression that the sound data uses.
-        let clip_stream_decoder = decoders::make_stream_decoder(format, data_stream)?;
+        let clip_stream_decoder = decoders::make_stream_decoder(stream_info, data_stream)?;
 
         // Convert the `Decoder` to a `Stream`, and resample it to the output sample rate.
         let stream = DecoderStream::new(clip_stream_decoder);
@@ -377,12 +377,10 @@ impl AudioMixer {
         clip_data: SwfSlice,
         stream_info: &swf::SoundStreamHead,
     ) -> Result<SoundInstanceHandle, Error> {
-        let format = &stream_info.stream_format;
-
         // The audio data for stream sounds is distributed among the frames of a
         // movie clip. The stream tag reader will parse through the SWF and
         // feed the decoder audio data on the fly.
-        let stream = self.make_stream_from_swf_slice(format, clip_data)?;
+        let stream = self.make_stream_from_swf_slice(stream_info, clip_data)?;
 
         let mut sound_instances = self.sound_instances.lock().unwrap();
         let handle = sound_instances.insert(SoundInstance {
