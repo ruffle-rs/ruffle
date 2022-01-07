38549fdbdf5b7ba8e5169af9fefdc53e98c76215
relrelb <relrelbachar@gmail.com>
2022-01-07 16:22:59 +0200

    web: Publish NPM package
    Publish nightly releases as part of the CI, so Ruffle can be easily
    integrated into a website using a CDN (such as jsDelivr). It would
    also be easier for websites to keep up-to-date, as NPM auto-updates
    the `latest` tag to refer to the latest release.
    
    A dedicated `package.json` is used for the published NPM package,
    because `ruffle-selfhosted` depends on the private `ruffle-core`
    package, which shouldn't be published.
    
    Also, the `version` field must monotonically increase. So Webpack
    auto-fills it to be `0.1.0-nightly.YYYY.MM.DD`. This format satisfies
    a couple of needs:
    * Newer nightly releases should take precedence over older ones.
    * Stable releases (e.g. `1.0.0`) should take precedence over nightly
    ones.

--- web/packages/selfhosted/webpack.config.js
+++ web/packages/selfhosted/webpack.config.js
@@ -1,7 +1,30 @@
 /* eslint-env node */
 
 const path = require("path");
+const json5 = require("json5");
 const CopyPlugin = require("copy-webpack-plugin");
+
+function transformPackage(content) {
+    const package = json5.parse(content);
+
+    const packageVersion = process.env.npm_package_version;
+
+    const versionChannel = process.env.CFG_RELEASE_CHANNEL || "nightly";
+
+    const buildDate = new Date()
+        .toISOString()
+        .substring(0, 10)
+        .replace(/-/g, ".");
+
+    // The npm registry requires the version to monotonically increase,
+    // so append the build date onto the end of the package version.
+    package.version =
+        versionChannel !== "stable"
+            ? `${packageVersion}-${versionChannel}.${buildDate}`
+            : packageVersion;
+
+    return JSON.stringify(package);
+}
 
 module.exports = (_env, _argv) => {
     const mode = process.env.NODE_ENV || "production";
@@ -24,7 +47,15 @@ module.exports = (_env, _argv) => {
         devtool: "source-map",
         plugins: [
             new CopyPlugin({
-                patterns: [{ from: "LICENSE*" }, { from: "README.md" }],
+                patterns: [
+                    {
+                        from: "npm-package.json5",
+                        to: "package.json",
+                        transform: transformPackage,
+                    },
+                    { from: "LICENSE*" },
+                    { from: "README.md" },
+                ],
             }),
         ],
     };
