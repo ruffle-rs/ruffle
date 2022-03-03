59e9c11775f60171b5b0e621c43c26cad9e7c35b
Aaron Hill <aa1ronham@gmail.com>
2022-03-02 23:34:10 -0500

    Add `define_indirect_properties` helper macro for property/slot pairs
    In both `FrameLabel` and `Scene`, we define multiple
    'public property / private slot' pairs.
    The public property has a getter which delegates to the private
    property. There is no setter for the property, ensuring that
    the private slot can only be modified from within Ruffle itself.
    
    This PR adds a macro `define_indirect_properties` to abstract over
    this pattern. Currently, it only supports the read-only property
    pattern - however, it could be extended in the future to generate
    a setter that invokes a caller-provided callback function.
    
    This needs to be a macro (rather than a method) so that we can
    generate a function with the property name hard-coded into it.
    Using a closure that references an upvar will not work, since
    `Method::from_builtin` requires a function pointer.

--- core/src/avm2/globals/flash/display/framelabel.rs
+++ core/src/avm2/globals/flash/display/framelabel.rs
@@ -1,9 +1,9 @@
 //! `flash.display.FrameLabel` impl
 
 use crate::avm2::activation::Activation;
-use crate::avm2::class::Class;
+use crate::avm2::class::{define_indirect_properties, Class};
 use crate::avm2::globals::NS_RUFFLE_INTERNAL;
-use crate::avm2::method::{Method, NativeMethodImpl};
+use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{Object, TObject};
 use crate::avm2::value::Value;
@@ -53,39 +53,6 @@ pub fn class_init<'gc>(
 ) -> Result<Value<'gc>, Error> {
     Ok(Value::Undefined)
 }
-
-/// Implements `FrameLabel.name`.
-pub fn name<'gc>(
-    activation: &mut Activation<'_, 'gc, '_>,
-    this: Option<Object<'gc>>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error> {
-    if let Some(this) = this {
-        return this.get_property(
-            &QName::new(Namespace::Private(NS_RUFFLE_INTERNAL.into()), "name").into(),
-            activation,
-        );
-    }
-
-    Ok(Value::Undefined)
-}
-
-/// Implements `FrameLabel.frame`.
-pub fn frame<'gc>(
-    activation: &mut Activation<'_, 'gc, '_>,
-    this: Option<Object<'gc>>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error> {
-    if let Some(this) = this {
-        return this.get_property(
-            &QName::new(Namespace::Private(NS_RUFFLE_INTERNAL.into()), "frame").into(),
-            activation,
-        );
-    }
-
-    Ok(Value::Undefined)
-}
-
 /// Construct `FrameLabel`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
     let class = Class::new(
@@ -98,18 +65,6 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
 
     let mut write = class.write(mc);
 
-    const PUBLIC_INSTANCE_PROPERTIES: &[(
-        &str,
-        Option<NativeMethodImpl>,
-        Option<NativeMethodImpl>,
-    )] = &[("name", Some(name), None), ("frame", Some(frame), None)];
-    write.define_public_builtin_instance_properties(mc, PUBLIC_INSTANCE_PROPERTIES);
-
-    const PRIVATE_INSTANCE_SLOTS: &[(&str, &str, &str, &str)] = &[
-        (NS_RUFFLE_INTERNAL, "name", "", "String"),
-        (NS_RUFFLE_INTERNAL, "frame", "", "int"),
-    ];
-    write.define_private_slot_instance_traits(PRIVATE_INSTANCE_SLOTS);
-
+    define_indirect_properties!(write, mc, [("name", "", "String"), ("frame", "", "int")]);
     class
 }
