872101fe51bd8c011fea60ebb3ff49a3993c998c
TÖRÖK Attila <torokati44@gmail.com>
2022-03-25 01:25:53 +0100

    cleanup

--- android/app/app/src/main/java/com/example/ruffle2/MainActivity.java
+++ android/app/app/src/main/java/com/example/ruffle2/MainActivity.java
@@ -1,5 +1,4 @@
-package com.example.ruffle2;
-
+package rs.ruffle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -8,16 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.NativeActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     static {
-        System.loadLibrary("wgpu_android");
+        // load the native activity
+        System.loadLibrary("ruffle_android");
     }
 
     @Override
@@ -27,32 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityResultLauncher launcher = registerForActivityResult(new ActivityResultContracts.GetContent(),
                 uri -> {
-                    /*try */{
-                        Intent intent = new Intent(MainActivity.this, NativeActivity.class);
-
-                        /*
-                        InputStream stream = getContentResolver().openInputStream(uri);
-                        // assuming the whole contents will be available at once
-                        int size = stream.available();
-                        byte[] bytes = new byte[size];
-                        stream.read(bytes);
-
-                        intent.putExtra("SWF_BYTES", bytes);
-                        */
-
-                        intent.putExtra("SWF_URI", uri);
-
-                        startActivity(intent);
-                    }/* catch (IOException e) {
-                        Log.e("rfl", "IO Error when loading SWF");
-                    }*/
-                }
-                );
+                    Intent intent = new Intent(MainActivity.this, NativeActivity.class);
+                    intent.putExtra("SWF_URI", uri);
+                    startActivity(intent);
+                });
 
         View button = findViewById(R.id.button);
 
         button.setOnClickListener((event) -> {
-            launcher.launch("*/*" /* "application/x-shockwave-flash" */ );
+            // should really be: "application/x-shockwave-flash"
+            launcher.launch("*/*");
         });
 
     }
--- android/native/src/lib.rs
+++ android/native/src/lib.rs
@@ -28,10 +28,6 @@ use ruffle_core::{
 use ruffle_render_wgpu::WgpuRenderBackend;
 use std::time::Instant;
 
-
-//const SWF : &'static [u8] = include_bytes!("z0r-de_6249.swf");
-//const SWF : &'static [u8] = include_bytes!("z0r-de_420.swf");
-
 async fn run(event_loop: EventLoop<()>, window: Window) {
 
     let mut time = Instant::now();
@@ -39,13 +35,9 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
     let mut playerbox : Option<Arc<Mutex<Player>>> = None;
 
-
     log::info!("running eventloop");
 
     event_loop.run(move |event, _, control_flow| {
-        //let _ = (&instance, &adapter, &shader, &pipeline_layout);
-
-        //log::info!("RUFFLE EVENT: {:#?}", event);
 
         *control_flow = ControlFlow::Poll;
         match event {
@@ -54,17 +46,14 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 ..
             } => {
                 log::info!("resized");
-                /*sc_desc.width = size.width;
-                sc_desc.height = size.height;*/
-                //swap_chain = Some(device.create_swap_chain(&surface.as_ref().unwrap(), &sc_desc));
             }
             Event::Resumed =>
             {
+                println!("resume");
                 log::info!("RUFFLE RESUMED");
 
                 if playerbox.is_none() {
 
-                    println!("resume");
                     let size = window.inner_size();
 
                     let renderer = Box::new(WgpuRenderBackend::for_window(
@@ -81,18 +70,13 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     let audio = Box::new(CpalAudioBackend::new().unwrap());
                     let navigator = Box::new(NullNavigatorBackend::new());
                     let storage = Box::new(MemoryStorageBackend::default());
-                    //let renderer = Box::new(NullRenderer::new());
                     let video = Box::new(SoftwareVideoBackend::new());
                     let ui = Box::new(NullUiBackend::new());
-
-
 
                     playerbox = Some(Player::new(renderer, audio, navigator, storage, video, log, ui).unwrap());
 
                     let mut player  = playerbox.as_ref().unwrap();
-
                     let mut player_lock = player.lock().unwrap();
-
 
                     match get_swf_bytes() {
                         Ok(bytes) => {
@@ -109,31 +93,18 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         }
 
                     }
-
-
                 }
-                /*sc_desc.width = size.width;
-                sc_desc.height = size.height;
-                surface = Some(unsafe { instance.create_surface(&window) });*/
-                //swap_chain = Some(device.create_swap_chain(&surface.as_ref().unwrap(), &sc_desc));
-                //println!("surface: {:?}", surface);
             },
             Event::Suspended =>
             {
                 println!("suspend");
-                //swap_chain.take();
-                //surface.take();
             },
             Event::MainEventsCleared => {
-
-
                 let new_time = Instant::now();
                 let dt = new_time.duration_since(time).as_micros();
-                //log::info!("EVENTS CLEARED DT: {}", dt);
+
                 if dt > 0 {
                     time = new_time;
-
-
                     if playerbox.is_some() {
                         let mut player = playerbox.as_ref().unwrap();
 
@@ -147,8 +118,6 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                             log::info!("REQUESTED REDRAW");
                         }
                     }
-                    //window.request_redraw();
-
                 }
 
             }
@@ -156,7 +125,8 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             // Render
             Event::RedrawRequested(_) => {
                 log::info!("REDRAWING");
-                // Don't render when minimized to avoid potential swap chain errors in `wgpu`.
+                // TODO: Don't render when minimized to avoid potential swap chain errors in `wgpu`.
+                // TODO: also disable when suspended!
 
                 if playerbox.is_some() {
                     let mut player = playerbox.as_ref().unwrap();
@@ -165,16 +135,12 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     player_lock.render();
                     log::info!("RUFFLE RENDERED");
                 }
-                //
-                log::info!("REDRAW DONE");
             }
-
 
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
             } => *control_flow = ControlFlow::Exit,
-
 
             _ => {}
         }
@@ -190,195 +156,42 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
 
 fn get_swf_bytes() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-
-
-    log::info!("-- GETTING SWF CONTENTS --");
-
-
     // Create a VM for executing Java calls
     let ctx = ndk_context::android_context();
     let vm = unsafe { jni::JavaVM::from_raw(ctx.vm().cast()) }?;
     let env = vm.attach_current_thread()?;
 
+    // The following is basically equivalent to this Java code:
+    /*
+    Intent intent = getIntent();
+    Bundle extras = intent.getExtras();
+    Uri uri = extras.get("SWF_URI");
 
+    ContentResolver resolver = getContentResolver();
+    InputStream inputStream = resolver.openInputStream(uri);
+
+    int available = inputStream.available();
+    byte[] bytes = new byte[available];
+    // assuming the whole contents will be available at once
+    int _num_bytes_read = inputStream.read(bytes);
+    */
 
     let intent = env.call_method(ctx.context() as jni::sys::jobject, "getIntent", "()Landroid/content/Intent;", &[])?;
-
-
-
-    log::info!("GOT INTENT");
-
-
     let extras = env.call_method(intent.l()?, "getExtras", "()Landroid/os/Bundle;", &[])?;
-
-
-    log::info!("GOT EXTRAS");
-
     let uri = env.call_method(extras.l()?, "get", "(Ljava/lang/String;)Ljava/lang/Object;", &[jni::objects::JValue::Object(env.new_string("SWF_URI")?.into())])?;
 
-    log::info!("GOT URI");
-
     let resolver = env.call_method(ctx.context() as jni::sys::jobject, "getContentResolver", "()Landroid/content/ContentResolver;", &[])?;
-
-    log::info!("GOT RESOLVER");
-
     let stream = env.call_method(resolver.l()?, "openInputStream", "(Landroid/net/Uri;)Ljava/io/InputStream;", &[jni::objects::JValue::Object(uri.l()?)])?;
 
-    log::info!("GOT STREAM");
-
     let available = env.call_method(stream.l()?, "available", "()I", &[])?;
-
-    log::info!("GOT AVAILABLE: {:}", available.i()?);
-
     let bytes = env.new_byte_array(available.i()?)?;
+    let _num_bytes_read = env.call_method(stream.l()?, "read", "([B)I", &[jni::objects::JValue::Object(jni::objects::JObject::from(bytes))])?;
 
-    log::info!("MADE ARRAY");
-
-    let num_bytes_read = env.call_method(stream.l()?, "read", "([B)I", &[jni::objects::JValue::Object(jni::objects::JObject::from(bytes))])?;
-
-    log::info!("READ BYTES: {:}", num_bytes_read.i()?);
-
-
-
+    // And finally getting the bytes into a Vec
     let elements = env.get_byte_array_elements(bytes as jbyteArray, ReleaseMode::NoCopyBack)?;
-
-    log::info!("GOT ELEMENTS");
-
     unsafe {
         Ok(Vec::from_raw_parts(elements.as_ptr() as *mut u8, elements.size()? as usize, elements.size()? as usize))
     }
-
-
-
-/*****************************************
-    let intent = env.call_method(ctx.context() as jni::sys::jobject, "getIntent", "()Landroid/content/Intent;", &[])?;
-
-
-
-    log::info!("GOT INTENT");
-
-
-    let extras = env.call_method(intent.l()?, "getExtras", "()Landroid/os/Bundle;", &[])?;
-
-
-    log::info!("GOT EXTRAS");
-
-    let bytearray = env.call_method(extras.l()?, "getByteArray", "(Ljava/lang/String;)[B", &[jni::objects::JValue::Object(env.new_string("SWF_BYTES")?.into())])?;
-
-    log::info!("GOT FIELD");
-
-    let elements = env.get_byte_array_elements(bytearray.l()?.into_inner() as jbyteArray, ReleaseMode::NoCopyBack)?;
-
-    log::info!("GOT ELEMENTS");
-
-    unsafe {
-        Ok(Vec::from_raw_parts(elements.as_ptr() as *mut u8, elements.size()? as usize, elements.size()? as usize))
-    }
-
-********************************/
-
-
-
-
-
-
-/*************************
-
-
-    let class_intent = env.find_class("android/content/Intent")?;
-    let ACTION_GET_CONTENT = env.get_static_field(class_intent, "ACTION_GET_CONTENT", "Ljava/lang/String;")?;
-    let intent = env.new_object(class_intent, "(Ljava/lang/String;)V", &[ACTION_GET_CONTENT])?;
-
-
-    log::info!("INTENT CREATED");
-    env.call_method(intent, "setType", "(Ljava/lang/String;)Landroid/content/Intent;", &[jni::objects::JValue::Object(env.new_string("application/x-shockwave-flash")?.into())])?;
-    log::info!("TYPE SET");
-
-
-    //intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-    let activity_class = env.call_method(ctx.context() as jni::sys::jobject, "getClass", "()Ljava/lang/Class;", &[])?.l()?;
-
-    let activity_class_name = env.call_method(activity_class, "getName", "()Ljava/lang/String;", &[])?.l()?;
-
-    log::info!("activity class name: {:#?}", activity_class_name);
-    let str = env.get_string_utf_chars(activity_class_name.into())?;
-    let mut str2 = str;
-    unsafe {
-    while *str2 != 0 {
-        str2 = str2.add(1);
-    }
-    let sl = std::slice::from_raw_parts(str, str2.offset_from(str) as usize);
-    let s = String::from_utf8(sl.to_vec()).unwrap();
-    log::info!("REAL CLASS NAME: {:#?}", s);
-    }
-    env.release_string_utf_chars(activity_class_name.into(), str);
-
-
-    // here's hoping our Context is an Activity
-    env.call_method(ctx.context() as jni::sys::jobject, "startActivityForResult", "(Landroid/content/Intent;I)V", &[jni::objects::JValue::Object(intent), jni::objects::JValue::Int(0)])?;
-
-
-*************************/
-
-
-/*
-
-    let audio_manager = env
-        .call_method(
-            ctx.context().cast(),
-            "getSystemService",
-            // JNI type signature needs to be derived from the Java API
-            // (ArgTys)ResultTy
-            "(Ljava/lang/String;)Ljava/lang/Object;",
-            &[audio_service],
-        )?
-        .l()?;
-
-    // Enumerate output devices
-    let devices = env.call_method(
-        audio_manager,
-        "getDevices",
-        "(I)[Landroid/media/AudioDeviceInfo;",
-        &[GET_DEVICES_OUTPUTS.into()],
-    )?;
-
-    log::info!("-- Output Audio Devices --");
-
-    let device_array = devices.l()?.into_inner();
-    let len = env.get_array_length(device_array)?;
-    for i in 0..len {
-        let device = env.get_object_array_element(device_array, i)?;
-
-        // Collect device information
-        // See https://developer.android.com/reference/android/media/AudioDeviceInfo
-        let product_name: String = {
-            let name =
-                env.call_method(device, "getProductName", "()Ljava/lang/CharSequence;", &[])?;
-            let name = env.call_method(name.l()?, "toString", "()Ljava/lang/String;", &[])?;
-            env.get_string(name.l()?.into())?.into()
-        };
-        let id = env.call_method(device, "getId", "()I", &[])?.i()?;
-        let ty = env.call_method(device, "getType", "()I", &[])?.i()?;
-
-        let sample_rates = {
-            let sample_array = env
-                .call_method(device, "getSampleRates", "()[I", &[])?
-                .l()?
-                .into_inner();
-            let len = env.get_array_length(sample_array)?;
-
-            let mut sample_rates = vec![0; len as usize];
-            env.get_int_array_region(sample_array, 0, &mut sample_rates)?;
-            sample_rates
-        };
-
-        log::info!("Device {}: Id {}, Type {}", product_name, id, ty);
-        log::info!("sample rates: {:#?}", sample_rates);
-    }
-
-    Ok(())
-    */
 }
 
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on", logger(level = "info", tag = "ruffle")))]
