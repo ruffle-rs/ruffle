225c4a80ca9fb2d4bc70c37a9cedda538f115c8c
Mike Welsh <mwelsh@gmail.com>
2022-04-25 13:38:16 -0700

    ci: Bump mocha timeout length
    Bump mocha's timeout from 60 secs to 120 secs to attempt to improve
    the reliability of our Windows node tests.

--- web/packages/selfhosted/wdio.conf.js
+++ web/packages/selfhosted/wdio.conf.js
@@ -160,7 +160,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: "bdd",
-        timeout: 60000,
+        timeout: 120000,
     },
     //
     // =====
