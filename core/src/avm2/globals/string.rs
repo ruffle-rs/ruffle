e39cfa8027a4d8b9a28b63a28e7761746a221969
Aaron Hill <aa1ronham@gmail.com>
2022-05-11 17:44:49 -0400

    Define `String.fromCharCode` as both a public and AS3 method
    This is relied on by SteamBirds
    (https://www.kongregate.com/games/weasello/steambirds)

--- core/src/avm2/globals/string.rs
+++ core/src/avm2/globals/string.rs
@@ -564,7 +564,9 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
     write.define_as3_builtin_instance_methods(mc, AS3_INSTANCE_METHODS);
 
     const AS3_CLASS_METHODS: &[(&str, NativeMethodImpl)] = &[("fromCharCode", from_char_code)];
+    const PUBLIC_CLASS_METHODS: &[(&str, NativeMethodImpl)] = &[("fromCharCode", from_char_code)];
     write.define_as3_builtin_class_methods(mc, AS3_CLASS_METHODS);
+    write.define_public_builtin_class_methods(mc, PUBLIC_CLASS_METHODS);
 
     class
 }
