39d3fa78e0c5b0937ee347de5613b7f220763380
relrelb <relrelbachar@gmail.com>
2022-05-14 08:49:07 +0300

    desktop: Fetch root movie asynchronously
    Use `Player::fetch_root_movie` for loading the root movie, as an
    early preparation for chunked loading.

--- desktop/src/custom_event.rs
+++ desktop/src/custom_event.rs
@@ -4,4 +4,7 @@
 pub enum RuffleEvent {
     /// Indicates that one or more tasks are ready to poll on our executor.
     TaskPoll,
+
+    /// Indicates that an asynchronous SWF metadata load has been completed.
+    OnMetadata(ruffle_core::swf::HeaderExt),
 }
