6114f14fe0673502b814bfb9e24b90eaacfa1acb
Robert Setter <r.setter@comcast.net>
2022-05-29 07:08:46 -0500

    Updating the verbiage for the AS3 warnings to try and reduce the number of support questions about it. (#7106)
    * Updating AS3 warning verbiage.
    
    * Updating AS3 verbiage
    
    * Increase root font size
    
    * Refining font size.
    
    * correcting font size in .message

--- web/packages/core/src/shadow-template.ts
+++ web/packages/core/src/shadow-template.ts
@@ -138,6 +138,7 @@ ruffleShadowTemplate.innerHTML = `
             max-height: 100%;
             max-width: 100%;
             padding: 5%;
+	    font-size: 20px;
         }
 
         #message_overlay p {
