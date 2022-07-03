1963e47392bece835b9ceb65974d74bcea1faa0b
Mike Welsh <mwelsh@gmail.com>
2022-07-02 22:24:17 -0700

    avm1: Increment register when preloading super (fix #7338)
    The register index was not being increment when preloading the
    `super` register which would cause issues when multiple registers
    were preloaded.
    
    Fixes #7338.

--- core/src/avm1/function.rs
+++ core/src/avm1/function.rs
@@ -236,6 +236,7 @@ impl<'gc> Avm1Function<'gc> {
         if preload {
             // The register is set to undefined if both flags are set.
             frame.set_local_register(*preload_r, zuper.unwrap_or(Value::Undefined));
+            *preload_r += 1;
         } else if let Some(zuper) = zuper {
             frame.force_define_local("super".into(), zuper);
         }
