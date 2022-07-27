75becaf3ba195a4ebc3bb97b888879ee9e7b27ba
Daniel Jacobs <danielhunterjacobs@gmail.com>
2022-07-27 17:02:17 -0400

    demo: Add metadata to status bar on the page

--- web/packages/demo/www/index.js
+++ web/packages/demo/www/index.js
@@ -29,9 +29,63 @@ const defaultConfig = {
     logLevel: "warn",
 };
 
+const swfToFlashVersion = {
+    1: "FP1",
+    2: "FP2",
+    3: "FP3",
+    4: "FP4",
+    5: "FP5",
+    6: "FP6",
+    7: "FP7",
+    8: "FP8",
+    9: "FP9.0",
+    10: "FP10.0 or FP10.1",
+    11: "FP10.2",
+    12: "FP10.3",
+    13: "FP11.0",
+    14: "FP11.1",
+    15: "FP11.2",
+    16: "FP11.3",
+    17: "FP11.4",
+    18: "FP11.5",
+    19: "FP11.6",
+    20: "FP11.7",
+    21: "FP11.8",
+    22: "FP11.9",
+    23: "FP12",
+    24: "FP13",
+    25: "FP14",
+    26: "FP15",
+    27: "FP16",
+    28: "FP17",
+    29: "FP18",
+    30: "FP19",
+    31: "FP20",
+    32: "FP21",
+    33: "FP22",
+    34: "FP23",
+    35: "FP24",
+    36: "FP25",
+    37: "FP26",
+    38: "FP27",
+    39: "FP28",
+    40: "FP29",
+    41: "FP30",
+    42: "FP31",
+    43: "FP32",
+}
+
 function unload() {
     if (player) {
         player.remove();
+        document.getElementById("flashversion").textContent = "loading";
+        document.getElementById("asversion").textContent = "loading";
+        document.getElementById("frameRate").textContent = "loading";
+        document.getElementById("filesize").textContent = "loading";
+        document.getElementById("width").textContent = "loading";
+        document.getElementById("height").textContent = "loading";
+        document.getElementById("numFrames").textContent = "loading";
+        document.getElementById("backgroundColor").textContent = "loading";
     }
 }
 
@@ -41,6 +95,33 @@ function load(options) {
     player.id = "player";
     main.append(player);
     player.load(options);
+    player.addEventListener("loadedmetadata", function() {
+        if (this.metadata) {
+            if (this.metadata.swfVersion) {
+                document.getElementById("flashversion").textContent = swfToFlashVersion[this.metadata.swfVersion];
+            }
+            if (this.metadata.isActionScript3) {
+                document.getElementById("asversion").textContent = "yes";
+            } else {
+                document.getElementById("asversion").textContent = "no";
+            }
+            if (this.metadata.frameRate) {
+                document.getElementById("frameRate").textContent = `${this.metadata.frameRate} FPS`;
+            }
+            if (this.metadata.width) {
+                document.getElementById("width").textContent = `${this.metadata.width}px`;
+            }
+            if (this.metadata.height) {
+                document.getElementById("height").textContent = `${this.metadata.height}px`;
+            }
+            if (this.metadata.numFrames) {
+                document.getElementById("numFrames").textContent = this.metadata.numFrames;
+            }
+            if (this.metadata.backgroundColor) {
+                document.getElementById("backgroundColor").textContent = this.metadata.backgroundColor;
+            }
+        }
+    });
 }
 
 function showSample(swfData) {
@@ -67,12 +148,27 @@ async function loadFile(file) {
     hideSample();
     const data = await new Response(file).arrayBuffer();
     load({ data, ...defaultConfig });
+    if (file.size) {
+        document.getElementById("filesize").textContent = `${Math.round(file.size/1024)}Kb`;
+    }
+}
+
+function getFileSize(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("HEAD", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            document.getElementById("filesize").textContent = `${Math.round(parseInt(xhr.getResponseHeader("Content-Length"))/1024)}Kb`;
+        }
+    };
+    xhr.send();
 }
 
 function loadSample() {
     const swfData = sampleFileInput[sampleFileInput.selectedIndex].swfData;
     localFileName.textContent = "No file selected.";
     if (swfData) {
+        getFileSize(swfData.location);
         showSample(swfData);
         const config = swfData.config || defaultConfig;
         load({ url: swfData.location, ...config });
