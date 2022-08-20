bfd84fae0ae360e7250a4860f8951cbbfb69bec2
= <dinnerbone@dinnerbone.com>
2022-08-21 01:23:20 +0200

    core: Made SwfSlice::to_subslice return an empty slice in case of errors

--- core/src/binary_data.rs
+++ core/src/binary_data.rs
@@ -5,6 +5,6 @@ pub type BinaryData = SwfSlice;
 
 impl BinaryData {
     pub fn from_swf_tag(movie: Arc<SwfMovie>, tag: &swf::DefineBinaryData) -> Self {
-        SwfSlice::from(movie).to_subslice(tag.data).unwrap()
+        SwfSlice::from(movie).to_subslice(tag.data)
     }
 }
