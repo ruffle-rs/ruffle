7c839ef8aea25551b8b61f3a907c708b9420eb76
= <dinnerbone@dinnerbone.com>
2022-08-26 00:30:07 +0200

    video: Add software video crate, moved VideoDecoder to it

--- core/src/backend/video/software/mod.rs
+++ core/src/backend/video/software/mod.rs
@@ -5,8 +5,9 @@ use ruffle_render::backend::RenderBackend;
 use ruffle_render::bitmap::{Bitmap, BitmapFormat, BitmapHandle, BitmapInfo};
 use ruffle_video::backend::VideoBackend;
 use ruffle_video::error::Error;
-use ruffle_video::frame::{DecodedFrame, EncodedFrame, FrameDependency};
+use ruffle_video::frame::{EncodedFrame, FrameDependency};
 use ruffle_video::VideoStreamHandle;
+use ruffle_video_software::decoder::VideoDecoder;
 use swf::{VideoCodec, VideoDeblocking};
 
 pub mod decoders;
@@ -124,34 +125,4 @@ impl VideoStream {
             bitmap: None,
         }
     }
-}
-
-/// Trait for video decoders.
-/// This should be implemented for each video codec.
-trait VideoDecoder {
-    /// Preload a frame.
-    ///
-    /// No decoding is intended to happen at this point in time. Instead, the
-    /// video data should be inspected to determine inter-frame dependencies
-    /// between this and any previous frames in the stream.
-    ///
-    /// Frames should be preloaded in the order that they are recieved.
-    ///
-    /// Any dependencies listed here are inherent to the video bitstream. The
-    /// containing video stream is also permitted to introduce additional
-    /// interframe dependencies.
-    fn preload_frame(&mut self, encoded_frame: EncodedFrame<'_>) -> Result<FrameDependency, Error>;
-
-    /// Decode a frame of a given video stream.
-    ///
-    /// This function is provided the external index of the frame, the codec
-    /// used to decode the data, and what codec to decode it with. The codec
-    /// provided here must match the one used to register the video stream.
-    ///
-    /// Frames may be decoded in any order that does not violate the frame
-    /// dependencies declared by the output of `preload_video_stream_frame`.
-    ///
-    /// The decoded frame should be returned. An `Error` can be returned if
-    /// a drawable bitmap can not be produced.
-    fn decode_frame(&mut self, encoded_frame: EncodedFrame<'_>) -> Result<DecodedFrame, Error>;
 }
