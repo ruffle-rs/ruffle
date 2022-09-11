2174c55d76f36987998253ae137b3009a387e0d8
David Wendt <dcrkid@yahoo.com>
2022-09-11 17:16:16 -0400

    tests: Add a test for Flex-style image embeds.

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -192,6 +192,7 @@ swf_tests! {
     (as3_bitmap_constr, "avm2/bitmap_constr", 1),
     (as3_bitmapdata_copypixels, "avm2/bitmapdata_copypixels", 2, img = true),
     #[ignore] (as3_bitmap_properties, "avm2/bitmap_properties", 1),
+    (as3_bitmap_subclass, "avm2/bitmap_subclass", 1),
     (as3_bitmap_timeline, "avm2/bitmap_timeline", 1),
     (as3_bitmapdata_constr, "avm2/bitmapdata_constr", 1),
     (as3_bitmapdata_dispose, "avm2/bitmapdata_dispose", 1),
