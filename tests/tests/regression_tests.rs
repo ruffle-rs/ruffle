d0a0f3f408aa9facea17b897527a8ce5ddcd5479
EmperorBale <hswhsw490@gmail.com>
2022-09-18 20:03:29 -0700

    tests: Add test for ByteArray.writeObject

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -211,6 +211,7 @@ swf_tests! {
     (as3_boolean_tostring, "avm2/boolean_tostring", 1),
     (as3_bytearray_readobject_amf0, "avm2/bytearray_readobject_amf0", 1),
     (as3_bytearray_readobject_amf3, "avm2/bytearray_readobject_amf3", 1),
+    (as3_bytearray_writeobject, "avm2/bytearray_writeobject", 1),
     (as3_bytearray, "avm2/bytearray", 1),
     (as3_class_call, "avm2/class_call", 1),
     (as3_class_cast_call, "avm2/class_cast_call", 1),
