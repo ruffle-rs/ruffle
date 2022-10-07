ruffle-rs/ruffle

commit 85cf383c84de574b4517ff250cb9e7904e77f128

core: Allow callers of `decode_tags` to flag when they would like decoding to stop

core/src/backend/audio/decoders.rs
@@ -16,7 +16,7 @@ pub use mp3::symphonia::{mp3_metadata, Mp3Decoder};
 pub use nellymoser::NellymoserDecoder;
 pub use pcm::PcmDecoder;
 
-use crate::tag_utils::SwfSlice;
+use crate::tag_utils::{ControlFlow, SwfSlice};
 use std::io::{Cursor, Read};
 use swf::{AudioCompression, SoundFormat, TagCode};
 use thiserror::Error;
@@ -319,18 +319,17 @@ impl Iterator for StreamTagReader {
                             audio_block = &audio_block[4..];
                         }
                         *audio_data = swf_data.to_subslice(audio_block);
-                        Ok(())
+                        Ok(ControlFlow::Exit)
                     }
                     TagCode::ShowFrame if compression == AudioCompression::Mp3 => {
                         self.mp3_samples_buffered -= i32::from(self.mp3_samples_per_block);
-                        Ok(())
+                        Ok(ControlFlow::Continue)
                     }
-                    _ => Ok(()),
+                    _ => Ok(ControlFlow::Continue),
                 };
 
             let mut reader = self.swf_data.read_from(self.pos as u64);
-            let _ =
-                crate::tag_utils::decode_tags(&mut reader, tag_callback, TagCode::ShowFrame, None);
+            let _ = crate::tag_utils::decode_tags(&mut reader, tag_callback);
             self.pos = reader.get_ref().as_ptr() as usize - swf_data.as_ref().as_ptr() as usize;
 
             // If we hit a SoundStreamBlock within this frame, return it. Otherwise, the stream should end.

core/src/avm2/globals.rs
@@ -14,7 +14,7 @@ use crate::avm2::Multiname;
 use crate::avm2::Namespace;
 use crate::avm2::QName;
 use crate::string::AvmString;
-use crate::tag_utils::{self, SwfMovie, SwfSlice, SwfStream};
+use crate::tag_utils::{self, ControlFlow, SwfMovie, SwfSlice, SwfStream};
 use gc_arena::{Collect, GcCell, MutationContext};
 use std::sync::Arc;
 use swf::TagCode;
@@ -680,10 +680,10 @@ fn load_playerglobal<'gc>(
                 tag_code
             )
         }
-        Ok(())
+        Ok(ControlFlow::Continue)
     };
 
-    let _ = tag_utils::decode_tags(&mut reader, tag_callback, TagCode::End, None);
+    let _ = tag_utils::decode_tags(&mut reader, tag_callback);
     macro_rules! avm2_system_classes_playerglobal {
         ($activation:expr, $script:expr, [$(($package:expr, $class_name:expr, $field:ident)),* $(,)?]) => {
             $(

core/src/tag_utils.rs
@@ -24,7 +24,16 @@ pub enum Error {
     InvalidSwfUrl,
 }
 
-pub type DecodeResult = Result<(), Error>;
+/// Whether or not to end tag decoding.
+pub enum ControlFlow {
+    /// Stop decoding after this tag.
+    Exit,
+
+    /// Continue decoding the next tag.
+    Continue,
+}
+
+pub type DecodeResult = Result<ControlFlow, Error>;
 pub type SwfStream<'a> = swf::read::Reader<'a>;
 
 /// An open, fully parsed SWF movie ready to play back, either in a Player or a
@@ -350,34 +359,19 @@ impl SwfSlice {
 ///
 /// Decoding will terminate when the following conditions occur:
 ///
-///  * After the given `stop_tag` is encountered and passed to the callback
+///  * The `tag_callback` calls for the decoding to finish.
 ///  * The decoder encounters a tag longer than the underlying SWF slice
-///  * The decoder decodes more than `chunk_limit` tags (if provided), in which
-///    case this function also returns `false`
 ///  * The SWF stream is otherwise corrupt or unreadable (indicated as an error
 ///    result)
 ///
 /// Decoding will also log tags longer than the SWF slice, error messages
 /// yielded from the tag callback, and unknown tags. It will *only* return an
-/// error message if the SWF tag itself could not be parsed. Otherwise, it
-/// returns `true` if decoding progressed to the stop tag or EOF, or `false` if
-/// the chunk limit was reached.
-pub fn decode_tags<'a, F>(
-    reader: &mut SwfStream<'a>,
-    mut tag_callback: F,
-    stop_tag: TagCode,
-    mut chunk_limit: Option<usize>,
-) -> Result<bool, Error>
+/// error message if the SWF tag itself could not be parsed.
+pub fn decode_tags<'a, F>(reader: &mut SwfStream<'a>, mut tag_callback: F) -> Result<(), Error>
 where
-    F: for<'b> FnMut(&'b mut SwfStream<'a>, TagCode, usize) -> DecodeResult,
+    F: for<'b> FnMut(&'b mut SwfStream<'a>, TagCode, usize) -> Result<ControlFlow, Error>,
 {
     loop {
-        if let Some(chunk_limit) = chunk_limit {
-            if chunk_limit < 1 {
-                return Ok(false);
-            }
-        }
-
         let (tag_code, tag_len) = reader.read_tag_code_and_length()?;
         if tag_len > reader.get_ref().len() {
             log::error!("Unexpected EOF when reading tag");
@@ -391,24 +385,22 @@ where
             *reader.get_mut() = tag_slice;
             let result = tag_callback(reader, tag, tag_len);
 
-            if let Err(e) = result {
-                log::error!("Error running definition tag: {:?}, got {}", tag, e);
-            }
-
-            if stop_tag == tag {
-                *reader.get_mut() = end_slice;
-                break;
+            match result {
+                Err(e) => {
+                    log::error!("Error running definition tag: {:?}, got {}", tag, e)
+                }
+                Ok(ControlFlow::Exit) => {
+                    *reader.get_mut() = end_slice;
+                    break;
+                }
+                Ok(ControlFlow::Continue) => {}
             }
         } else {
             log::warn!("Unknown tag code: {:?}", tag_code);
         }
 
         *reader.get_mut() = end_slice;
-
-        if let Some(ref mut chunk_limit) = chunk_limit {
-            *chunk_limit -= 1;
-        }
     }
 
-    Ok(true)
+    Ok(())
 }

commit 5a38219198b93bc6beb56b2dee36d4cabd6004f0

core: Don't desync on frames with no audio blocks.
Fixes regression caused by me in 85cf383c84de574b4517ff250cb9e7904e77f128

core/src/backend/audio/decoders.rs
@@ -319,12 +319,13 @@ impl Iterator for StreamTagReader {
                             audio_block = &audio_block[4..];
                         }
                         *audio_data = swf_data.to_subslice(audio_block);
-                        Ok(ControlFlow::Exit)
+                        Ok(ControlFlow::Continue)
                     }
                     TagCode::ShowFrame if compression == AudioCompression::Mp3 => {
                         self.mp3_samples_buffered -= i32::from(self.mp3_samples_per_block);
-                        Ok(ControlFlow::Continue)
+                        Ok(ControlFlow::Exit)
                     }
+                    TagCode::ShowFrame => Ok(ControlFlow::Exit),
                     _ => Ok(ControlFlow::Continue),
                 };
 