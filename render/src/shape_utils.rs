f5a587ce6108b6a42572e1aec7fe0bdb71def11c
Nathan Adams <dinnerbone@dinnerbone.com>
2023-01-09 16:00:39 +0100

    render: Better error message when points is empty

--- render/src/shape_utils.rs
+++ render/src/shape_utils.rs
@@ -195,7 +195,7 @@ impl PathSegment {
     fn to_draw_commands(&self) -> impl '_ + Iterator<Item = DrawCommand> {
         assert!(!self.is_empty());
         let mut i = self.points.iter();
-        let first = i.next().expect("Points isn't empty");
+        let first = i.next().expect("Points should not be empty");
         std::iter::once(DrawCommand::MoveTo {
             x: first.x,
             y: first.y,
