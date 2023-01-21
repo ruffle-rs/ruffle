b4bf13d5841d1c8b51cb892e2574352e164b90a7
David Wendt <dcrkid@yahoo.com>
2023-01-20 20:22:29 -0500

    chore: JS-side lints

--- web/packages/core/tools/set_version.js
+++ web/packages/core/tools/set_version.js
@@ -1,6 +1,6 @@
 const replace = require("replace-in-file");
 const childProcess = require("child_process");
-const fs = require('fs');
+const fs = require("fs");
 
 let version_number = process.env.npm_package_version;
 let version_channel = process.env.CFG_RELEASE_CHANNEL || "nightly";
@@ -19,23 +19,26 @@ let version_name =
         ? `nightly ${build_date.substr(0, 10)}`
         : process.env.npm_package_version;
 
+let version_seal = {};
+
 if (process.env.ENABLE_VERSION_SEAL === "true") {
-    if (fs.existsSync("version_seal.json")) { //Using the version seal stored previously.
+    if (fs.existsSync("version_seal.json")) {
+        //Using the version seal stored previously.
         version_seal = JSON.parse(fs.readFileSync("version_seal.json"));
 
-        version_number = version_seal["version_number"];
-        version_name = version_seal["version_name"];
-        version_channel = version_seal["version_channel"];
-        build_date = version_seal["build_date"];
-        commitHash = version_seal["commitHash"];
+        version_number = version_seal.version_number;
+        version_name = version_seal.version_name;
+        version_channel = version_seal.version_channel;
+        build_date = version_seal.build_date;
+        commitHash = version_seal.commitHash;
     } else {
         version_seal = {
-            "version_number": version_number,
-            "version_name": version_name,
-            "version_channel": version_channel,
-            "build_date": build_date,
-            "commitHash": commitHash
-        }
+            version_number: version_number,
+            version_name: version_name,
+            version_channel: version_channel,
+            build_date: build_date,
+            commitHash: commitHash,
+        };
 
         fs.writeFileSync("version_seal.json", JSON.stringify(version_seal));
     }
