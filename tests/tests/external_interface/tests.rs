1da5a4b98c4224503baadcdfac4b22d3f7440c27
Nathan Adams <dinnerbone@dinnerbone.com>
2023-01-27 19:42:41 +0100

    tests: Move external interface tests to their own file

---
+++ tests/tests/external_interface/tests.rs
@@ -0,0 +1,106 @@
+use crate::external_interface::ExternalInterfaceTestProvider;
+use crate::set_logger;
+use crate::util::runner::test_swf_with_hooks;
+use anyhow::Result;
+use ruffle_core::external::Value as ExternalValue;
+use std::collections::BTreeMap;
+use std::path::Path;
+
+pub fn external_interface_avm1() -> Result<()> {
+    set_logger();
+    test_swf_with_hooks(
+        Path::new("tests/swfs/avm1/external_interface/test.swf"),
+        1,
+        Path::new("tests/swfs/avm1/external_interface/input.json"),
+        Path::new("tests/swfs/avm1/external_interface/output.txt"),
+        |player| {
+            player
+                .lock()
+                .unwrap()
+                .add_external_interface(Box::new(ExternalInterfaceTestProvider::new()));
+            Ok(())
+        },
+        |player| {
+            let mut player_locked = player.lock().unwrap();
+
+            let parroted =
+                player_locked.call_internal_interface("parrot", vec!["Hello World!".into()]);
+            player_locked.log_backend().avm_trace(&format!(
+                "After calling `parrot` with a string: {parroted:?}",
+            ));
+
+            let mut nested = BTreeMap::new();
+            nested.insert(
+                "list".to_string(),
+                vec![
+                    "string".into(),
+                    100.into(),
+                    false.into(),
+                    ExternalValue::Object(BTreeMap::new()),
+                ]
+                .into(),
+            );
+
+            let mut root = BTreeMap::new();
+            root.insert("number".to_string(), (-500.1).into());
+            root.insert("string".to_string(), "A string!".into());
+            root.insert("true".to_string(), true.into());
+            root.insert("false".to_string(), false.into());
+            root.insert("null".to_string(), ExternalValue::Null);
+            root.insert("nested".to_string(), nested.into());
+            let result = player_locked
+                .call_internal_interface("callWith", vec!["trace".into(), root.into()]);
+            player_locked.log_backend().avm_trace(&format!(
+                "After calling `callWith` with a complex payload: {result:?}",
+            ));
+            Ok(())
+        },
+        false,
+        false,
+    )
+}
+
+pub fn external_interface_avm2() -> Result<()> {
+    set_logger();
+    test_swf_with_hooks(
+        Path::new("tests/swfs/avm2/external_interface/test.swf"),
+        1,
+        Path::new("tests/swfs/avm2/external_interface/input.json"),
+        Path::new("tests/swfs/avm2/external_interface/output.txt"),
+        |player| {
+            player
+                .lock()
+                .unwrap()
+                .add_external_interface(Box::new(ExternalInterfaceTestProvider::new()));
+            Ok(())
+        },
+        |player| {
+            let mut player_locked = player.lock().unwrap();
+
+            let parroted =
+                player_locked.call_internal_interface("parrot", vec!["Hello World!".into()]);
+            player_locked.log_backend().avm_trace(&format!(
+                "After calling `parrot` with a string: {parroted:?}",
+            ));
+
+            player_locked.call_internal_interface("freestanding", vec!["Hello World!".into()]);
+
+            let root: ExternalValue = vec![
+                "string".into(),
+                100.into(),
+                ExternalValue::Null,
+                false.into(),
+            ]
+            .into();
+
+            let result =
+                player_locked.call_internal_interface("callWith", vec!["trace".into(), root]);
+            player_locked.log_backend().avm_trace(&format!(
+                "After calling `callWith` with a complex payload: {result:?}",
+            ));
+            Ok(())
+        },
+        false,
+        false,
+    )
+}
