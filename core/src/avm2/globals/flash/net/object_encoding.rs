6e37757c9c9e8865500393bbc3c8c4c4282b3606
Nathan Adams <dinnerbone@dinnerbone.com>
2023-01-31 19:31:38 +0100

    avm2: Replaced stubs in ObjectEncoding with new format

--- core/src/avm2/globals/flash/net/object_encoding.rs
+++ core/src/avm2/globals/flash/net/object_encoding.rs
@@ -2,21 +2,30 @@ use crate::avm2::activation::Activation;
 use crate::avm2::object::Object;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
+use crate::{avm2_stub_getter, avm2_stub_setter};
 
 pub fn get_dynamic_property_writer<'gc>(
-    _activation: &mut Activation<'_, 'gc>,
+    activation: &mut Activation<'_, 'gc>,
     _this: Option<Object<'gc>>,
     _args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
-    tracing::warn!("ObjectEncoding.dynamicPropertyWriter: Not yet implemented");
+    avm2_stub_getter!(
+        activation,
+        "flash.net.ObjectEncoding",
+        "dynamicPropertyWriter"
+    );
     Ok(Value::Undefined)
 }
 
 pub fn set_dynamic_property_writer<'gc>(
-    _activation: &mut Activation<'_, 'gc>,
+    activation: &mut Activation<'_, 'gc>,
     _this: Option<Object<'gc>>,
     _args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
-    tracing::warn!("ObjectEncoding.dynamicPropertyWriter: Not yet implemented");
+    avm2_stub_setter!(
+        activation,
+        "flash.net.ObjectEncoding",
+        "dynamicPropertyWriter"
+    );
     Ok(Value::Undefined)
 }
