22dd3ab99870a2b3c2cf624db5bfea3d47f42f04
Nathan Adams <dinnerbone@dinnerbone.com>
2023-01-31 17:45:49 +0100

    core: Add feature known_stubs to retrieve all known stubs

--- core/src/avm1.rs
+++ core/src/avm1.rs
@@ -63,3 +63,31 @@ macro_rules! avm_error {
         }
     )
 }
+
+#[macro_export]
+macro_rules! avm1_stub {
+    ($activation: ident, $class: literal, $method: literal) => {
+        #[cfg_attr(
+            feature = "known_stubs",
+            linkme::distributed_slice($crate::stub::KNOWN_STUBS)
+        )]
+        static STUB: $crate::stub::Stub = $crate::stub::Stub::Avm1Method {
+            class: $class,
+            method: $method,
+            specifics: None,
+        };
+        $activation.context.stub_tracker.encounter(&STUB);
+    };
+    ($activation: ident, $class: literal, $method: literal, $specifics: literal) => {
+        #[cfg_attr(
+            feature = "known_stubs",
+            linkme::distributed_slice($crate::stub::KNOWN_STUBS)
+        )]
+        static STUB: $crate::stub::Stub = $crate::stub::Stub::Avm1Method {
+            class: $class,
+            method: $method,
+            specifics: Some(Cow::Borrowed($specifics)),
+        };
+        $activation.context.stub_tracker.encounter(&STUB);
+    };
+}
