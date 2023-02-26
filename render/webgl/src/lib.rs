dc9ec40d311e92daef5cc0b00cba4fa6c0ab5e88
relrelb <relrelbachar@gmail.com>
2023-02-26 13:09:20 +0200

    chore: Allow `clippy::bool_to_int_with_if`
    Seems like Clippy no longer complains about it.

--- render/webgl/src/lib.rs
+++ render/webgl/src/lib.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::bool_to_int_with_if)]
 #![deny(clippy::unwrap_used)]
 // This is a new lint with false positives, see https://github.com/rust-lang/rust-clippy/issues/10318
 #![allow(clippy::extra_unused_type_parameters)]
