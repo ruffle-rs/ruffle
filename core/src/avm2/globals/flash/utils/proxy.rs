3eee8c5c503ce62fe29bc4247b3eed306856fe72
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2023-03-06 18:30:07 +0100

    avm2: Implement proxy.isAttribute()

--- core/src/avm2/globals/flash/utils/proxy.rs
+++ core/src/avm2/globals/flash/utils/proxy.rs
@@ -4,14 +4,14 @@ use crate::avm2::value::Value;
 use crate::avm2::Error;
 
 pub use crate::avm2::object::proxy_allocator;
-use crate::avm2_stub_method;
 
 pub fn is_attribute<'gc>(
-    activation: &mut Activation<'_, 'gc>,
+    _activation: &mut Activation<'_, 'gc>,
     _this: Option<Object<'gc>>,
-    _args: &[Value<'gc>],
+    args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
-    // yes, this is supposed to be implemented
-    avm2_stub_method!(activation, "flash.utils.Proxy", "isAttribute");
+    if let Some(Value::Object(Object::QNameObject(qname_object))) = args.get(0) {
+        return Ok(qname_object.name().is_attribute().into());
+    }
     Ok(false.into())
 }
