4707a6b0e92b2df8eb0dbd6afb7d58b7b680dafa
Jay Sackwild <jayrlsw@users.noreply.github.com>
2023-03-15 11:01:33 +0000

    core: Enable click events on non-selectable text fields (#10005)

--- core/src/display_object/edit_text.rs
+++ core/src/display_object/edit_text.rs
@@ -407,6 +407,10 @@ impl<'gc> EditText<'gc> {
         !self.0.read().flags.contains(EditTextFlag::READ_ONLY)
     }
 
+    pub fn was_static(self) -> bool {
+        self.0.read().flags.contains(EditTextFlag::WAS_STATIC)
+    }
+
     pub fn set_editable(self, is_editable: bool, context: &mut UpdateContext<'_, 'gc>) {
         self.0
             .write(context.gc_context)
@@ -1853,17 +1857,24 @@ impl<'gc> TInteractiveObject<'gc> for EditText<'gc> {
         // The text is hovered if the mouse is over any child nodes.
         if self.visible()
             && self.mouse_enabled()
-            && self.is_selectable()
             && self.hit_test_shape(context, point, HitTestOptions::MOUSE_PICK)
         {
-            Avm2MousePick::Hit((*self).into())
+            if self.was_static() {
+                Avm2MousePick::PropagateToParent
+            } else {
+                Avm2MousePick::Hit((*self).into())
+            }
         } else {
             Avm2MousePick::Miss
         }
     }
 
     fn mouse_cursor(self, _context: &mut UpdateContext<'_, 'gc>) -> MouseCursor {
-        MouseCursor::IBeam
+        if self.is_selectable() {
+            MouseCursor::IBeam
+        } else {
+            MouseCursor::Arrow
+        }
     }
 }
 
