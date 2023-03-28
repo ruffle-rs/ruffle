5deab5fae0fb0c559638ba1e305809c9a3fca4b3
Mike Welsh <mwelsh@gmail.com>
2023-03-27 17:09:24 -0700

    desktop: Show menubar by holding Esc

--- desktop/src/gui.rs
+++ desktop/src/gui.rs
@@ -1,6 +1,7 @@
 use crate::custom_event::RuffleEvent;
 use egui::*;
 use std::rc::Rc;
+use std::time::{Duration, Instant};
 use winit::event_loop::{EventLoop, EventLoopProxy};
 use winit::window::Window;
 
@@ -119,74 +120,183 @@ impl GuiController {
         command_buffers.push(encoder.finish());
         command_buffers
     }
+
+    pub fn set_ui_visible(&mut self, value: bool) {
+        self.gui.set_ui_visible(value);
+    }
+
+    pub fn show_context_menu(&mut self, menu: Vec<ruffle_core::ContextMenuItem>) {
+        self.gui.show_context_menu(menu);
+    }
+
+    pub fn is_context_menu_visible(&mut self) -> bool {
+        self.gui.is_context_menu_visible()
+    }
 }
 
 /// The main controller for the Ruffle GUI.
 pub struct RuffleGui {
     event_loop: EventLoopProxy<RuffleEvent>,
+    esc_start_time: Option<Instant>,
+    is_esc_down: bool,
+    is_ui_visible: bool,
+    context_menu: Vec<ruffle_core::ContextMenuItem>,
 }
 
 impl RuffleGui {
     fn new(event_loop: EventLoopProxy<RuffleEvent>) -> Self {
-        Self { event_loop }
+        Self {
+            event_loop,
+            esc_start_time: None,
+            is_esc_down: false,
+            is_ui_visible: false,
+            context_menu: vec![],
+        }
     }
-}
 
-impl RuffleGui {
     /// Renders all of the main Ruffle UI, including the main menu and context menus.
     fn update(&mut self, egui_ctx: &egui::Context) {
-        egui::TopBottomPanel::top("menu_bar").show(egui_ctx, |ui| {
-            self.main_menu_bar(ui);
+        egui_ctx.input_mut(|input| {
+            // Listen for Esc press to toggle GUI.
+            let mut esc_this_frame = false;
+            if input.key_down(Key::Escape) {
+                if !self.is_esc_down {
+                    esc_this_frame = true;
+                }
+                self.is_esc_down = true;
+            } else {
+                self.is_esc_down = false;
+            }
+
+            if self.is_ui_visible {
+                if esc_this_frame {
+                    self.set_ui_visible(false);
+                }
+            } else if self.is_esc_down {
+                // Require holding Esc to show the UI to avoid interfering with games that use Esc.
+                if esc_this_frame {
+                    self.esc_start_time = Some(Instant::now());
+                }
+
+                if let Some(esc_start_time) = self.esc_start_time {
+                    let esc_duration = Instant::now().duration_since(esc_start_time);
+                    const HOLD_ESCAPE_TIME: f32 = 0.5;
+                    if esc_duration >= Duration::from_secs_f32(HOLD_ESCAPE_TIME) {
+                        self.set_ui_visible(true);
+                    }
+                }
+            }
         });
+
+        if self.is_ui_visible {
+            self.main_menu_bar(egui_ctx);
+        }
+
+        if !self.context_menu.is_empty() {
+            self.context_menu(egui_ctx);
+        }
+    }
+
+    pub fn set_ui_visible(&mut self, value: bool) {
+        self.is_ui_visible = value;
+        self.esc_start_time = None;
+    }
+
+    pub fn show_context_menu(&mut self, menu: Vec<ruffle_core::ContextMenuItem>) {
+        self.context_menu = menu;
+    }
+
+    pub fn is_context_menu_visible(&self) -> bool {
+        !self.context_menu.is_empty()
     }
 
     /// Renders the main menu bar at the top of the window.
-    fn main_menu_bar(&mut self, ui: &mut egui::Ui) {
-        // TODO(mike): Make some MenuItem struct with shortcut info to handle this more cleanly.
-        if ui.ctx().input_mut(|input| {
-            input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::O))
-        }) {
-            self.open_file(ui);
-        }
-        if ui.ctx().input_mut(|input| {
-            input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::Q))
-        }) {
-            self.request_exit(ui);
-        }
+    fn main_menu_bar(&mut self, egui_ctx: &egui::Context) {
+        egui::TopBottomPanel::top("menu_bar").show(egui_ctx, |ui| {
+            // TODO(mike): Make some MenuItem struct with shortcut info to handle this more cleanly.
+            if ui.ctx().input_mut(|input| {
+                input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::O))
+            }) {
+                self.open_file(ui);
+            }
+            if ui.ctx().input_mut(|input| {
+                input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::Q))
+            }) {
+                self.request_exit(ui);
+            }
 
-        menu::bar(ui, |ui| {
-            menu::menu_button(ui, "File", |ui| {
-                let mut shortcut;
-                shortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::O);
+            menu::bar(ui, |ui| {
+                menu::menu_button(ui, "File", |ui| {
+                    let mut shortcut;
+                    shortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::O);
 
-                if Button::new("Open...")
-                    .shortcut_text(ui.ctx().format_shortcut(&shortcut))
-                    .ui(ui)
-                    .clicked()
-                {
-                    self.open_file(ui);
-                }
-                ui.separator();
+                    if Button::new("Open...")
+                        .shortcut_text(ui.ctx().format_shortcut(&shortcut))
+                        .ui(ui)
+                        .clicked()
+                    {
+                        self.open_file(ui);
+                    }
+                    ui.separator();
 
-                shortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::Q);
-                if Button::new("Exit")
-                    .shortcut_text(ui.ctx().format_shortcut(&shortcut))
-                    .ui(ui)
-                    .clicked()
-                {
-                    self.request_exit(ui);
+                    shortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::Q);
+                    if Button::new("Exit")
+                        .shortcut_text(ui.ctx().format_shortcut(&shortcut))
+                        .ui(ui)
+                        .clicked()
+                    {
+                        self.request_exit(ui);
+                    }
+                });
+                menu::menu_button(ui, "Help", |ui| {
+                    if ui.button("Discord").clicked() {
+                        self.launch_discord(ui);
+                    }
+                    ui.separator();
+                    if ui.button("About Ruffle...").clicked() {
+                        self.show_about_screen(ui);
+                    }
+                })
+            });
+        });
+    }
+
+    /// Renders the right-click context menu.
+    fn context_menu(&mut self, egui_ctx: &egui::Context) {
+        let mut item_clicked = false;
+        let mut menu_visible = false;
+        // TODO: What is the proper way in egui to spawn a random context menu?
+        egui::CentralPanel::default()
+            .frame(Frame::none())
+            .show(egui_ctx, |_| {})
+            .response
+            .context_menu(|ui| {
+                menu_visible = true;
+                for (i, item) in self.context_menu.iter().enumerate() {
+                    if i != 0 && item.separator_before {
+                        ui.separator();
+                    }
+                    let clicked = if item.checked {
+                        Checkbox::new(&mut true, &item.caption).ui(ui).clicked()
+                    } else {
+                        Button::new(&item.caption).ui(ui).clicked()
+                    };
+                    if clicked {
+                        let _ = self
+                            .event_loop
+                            .send_event(RuffleEvent::ContextMenuItemClicked(i));
+                        item_clicked = true;
+                    }
                 }
             });
-            menu::menu_button(ui, "Help", |ui| {
-                if ui.button("Discord").clicked() {
-                    self.launch_discord(ui);
-                }
-                ui.separator();
-                if ui.button("About Ruffle...").clicked() {
-                    self.show_about_screen(ui);
-                }
-            })
-        });
+
+        if item_clicked
+            || !menu_visible
+            || egui_ctx.input_mut(|input| input.consume_key(Modifiers::NONE, Key::Escape))
+        {
+            // Hide menu.
+            self.context_menu.clear();
+        }
     }
 
     fn open_file(&mut self, ui: &mut egui::Ui) {
