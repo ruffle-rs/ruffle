34766c6f4369d8d9f527b03edde2ab881c423b31
David Wendt <dcrkid@yahoo.com>
2023-04-12 19:41:21 -0400

    flv: All relevant output data types need to be public.

--- flv/src/lib.rs
+++ flv/src/lib.rs
@@ -8,4 +8,7 @@ mod reader;
 
 pub use header::Header;
 pub use reader::FlvReader;
-pub use tag::Tag;
+pub use script::{ScriptData, Value, Variable};
+pub use sound::{AudioData, AudioDataType, SoundFormat, SoundRate, SoundSize, SoundType};
+pub use tag::{Tag, TagData};
+pub use video::{CodecId, CommandFrame, FrameType, VideoData, VideoPacket};
