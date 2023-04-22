9c2e77c88f633f8a3c3bc879282d987e8b5019eb
Nathan Adams <dinnerbone@dinnerbone.com>
2023-04-22 12:30:27 +0200

    desktop: Use system preferred language

--- desktop/src/ui.rs
+++ desktop/src/ui.rs
@@ -5,6 +5,7 @@ use ruffle_core::backend::ui::{
     FullscreenError, LanguageIdentifier, MouseCursor, UiBackend, US_ENGLISH,
 };
 use std::rc::Rc;
+use sys_locale::get_locale;
 use tracing::error;
 use winit::window::{Fullscreen, Window};
 
@@ -12,14 +13,20 @@ pub struct DesktopUiBackend {
     window: Rc<Window>,
     cursor_visible: bool,
     clipboard: Clipboard,
+    language: LanguageIdentifier,
 }
 
 impl DesktopUiBackend {
     pub fn new(window: Rc<Window>) -> Result<Self, Error> {
+        let preferred_language = get_locale();
+        let language = preferred_language
+            .and_then(|l| l.parse().ok())
+            .unwrap_or_else(|| US_ENGLISH.clone());
         Ok(Self {
             window,
             cursor_visible: true,
             clipboard: Clipboard::new().context("Couldn't get platform clipboard")?,
+            language,
         })
     }
 }
@@ -101,6 +108,6 @@ impl UiBackend for DesktopUiBackend {
     fn open_virtual_keyboard(&self) {}
 
     fn language(&self) -> &LanguageIdentifier {
-        &US_ENGLISH
+        &self.language
     }
 }
