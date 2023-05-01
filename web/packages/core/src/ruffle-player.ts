ruffle-rs/ruffle

commit 3bf78d43f92eb9cf9a019528386cb73e33467dab

fmt

web/packages/core/src/ruffle-player.ts
@@ -1898,8 +1898,8 @@ export class RufflePlayer extends HTMLElement {
                     this.container.querySelector("#panic-body")
                 );
                 panicBody.classList.add("details");
-                
-                let panicText = document.createElement("textarea");
+
+                const panicText = document.createElement("textarea");
                 panicText.value = errorText;
                 panicBody.appendChild(panicText);
                 return false;

commit 02abc4754339d631415c39205bb61e0ecc07f24c

web: Fix panic error details display
Fix regression caused by #10819

web/packages/core/src/ruffle-player.ts
@@ -1901,7 +1901,7 @@ export class RufflePlayer extends HTMLElement {
 
                 const panicText = document.createElement("textarea");
                 panicText.value = errorText;
-                panicBody.appendChild(panicText);
+                panicBody.replaceChildren(panicText);
                 return false;
             };
         }