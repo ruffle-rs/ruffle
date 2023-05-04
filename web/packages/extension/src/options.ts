b53c1a393689bcc429c0f8c24110b55bc70a322b
Toad06 <gd.productions@laposte.net>
2023-05-04 16:58:37 +0200

    extension: Localize more strings

--- web/packages/extension/src/options.ts
+++ web/packages/extension/src/options.ts
@@ -3,5 +3,7 @@ import { bindOptions } from "./common";
 
 window.addEventListener("DOMContentLoaded", () => {
     document.title = utils.i18n.getMessage("settings_page");
+    document.getElementById("advanced_options")!.textContent =
+        utils.i18n.getMessage("settings_advanced_options");
     bindOptions();
 });
