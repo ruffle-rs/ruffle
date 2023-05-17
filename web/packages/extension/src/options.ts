0ff4f26db9030db0df851d4b290a9d6702565ea0
Toad06 <gd.productions@laposte.net>
2023-05-18 00:14:48 +0200

    extension: Add a button to reset settings

--- web/packages/extension/src/options.ts
+++ web/packages/extension/src/options.ts
@@ -1,9 +1,21 @@
 import * as utils from "./utils";
-import { bindOptions } from "./common";
+import { bindOptions, resetOptions } from "./common";
 
 window.addEventListener("DOMContentLoaded", () => {
     document.title = utils.i18n.getMessage("settings_page");
-    document.getElementById("advanced_options")!.textContent =
-        utils.i18n.getMessage("settings_advanced_options");
+    {
+        const ao = document.getElementById("advanced_options")!;
+        ao.textContent = utils.i18n.getMessage("settings_advanced_options");
+    }
+    {
+        const rs = document.getElementById("reset_settings")!;
+        rs.textContent = utils.i18n.getMessage("settings_reset");
+        rs.addEventListener("click", async () => {
+            if (confirm(utils.i18n.getMessage("settings_reset_confirm"))) {
+                await resetOptions();
+                window.location.reload();
+            }
+        });
+    }
     bindOptions();
 });
