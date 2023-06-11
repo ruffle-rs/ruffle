dfe77ddffea0e582743f78440dae294985d19ba9
Aaron Hill <aa1ronham@gmail.com>
2023-06-10 19:25:03 -0500

    render: Add 'renderdoc' feature/module for manual frame capture
    This feature is disabled by default. When enabled, you can use
    `ruffle_render::renderdoc::begin_frame_capture` and
    `ruffle_render::renderdoc::end_frame_capture` to manually trigger
    a RenderDoc frame capture (if Ruffle wasn't launched by RenderDoc,
    this logs an error).
    
    This is very useful when debugging Stage3D/PixelBender bugs, as you
    can produce a capture containing only the relevant graphics calls.

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -6,6 +6,9 @@ pub mod error;
 pub mod filters;
 pub mod matrix;
 pub mod pixel_bender;
+// The `renderdoc` crate doesn't compile on apple platforms
+#[cfg(all(feature = "renderdoc", not(target_vendor = "apple")))]
+pub mod renderdoc;
 pub mod shader_source;
 pub mod shape_utils;
 pub mod transform;
