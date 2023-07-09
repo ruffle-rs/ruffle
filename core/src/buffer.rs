c03aa31b21bb6d809f8294d336164bb2e6896503
David Wendt <dcrkid@yahoo.com>
2023-07-08 21:09:53 -0400

    core: Add a `Substream` type for referencing multiple slices of a `Buffer`.
    A `Substream` can be read in one of two ways:
    
     * Converting it to an iterator that yields individual chunks of the substream in `Slice` form.
     * Converting it to a `SubstreamCursor`, which implements `Read` for bytewise read access.
    
    Chunk-based reading is intended for code that needs to know about chunk boundaries. Self-terminating chunk formats should be read with the cursor type.

--- core/src/buffer.rs
+++ core/src/buffer.rs
@@ -1,7 +1,10 @@
 //! Shared-ownership buffer types
 
 use gc_arena::Collect;
+use std::cmp::min;
 use std::fmt::Debug;
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, Read, Result as IoResult};
+use std::iter::Iterator;
 use std::ops::{Bound, Deref, RangeBounds};
 use std::sync::{Arc, RwLock, RwLockReadGuard};
 
@@ -88,8 +91,8 @@ impl Buffer {
         if start <= s.len() && end <= s.len() && start <= end {
             Some(Slice {
                 buf: Self(self.0.clone()),
-                start: start,
-                end: end,
+                start,
+                end,
             })
         } else {
             None
@@ -105,9 +108,21 @@ impl Buffer {
     }
 }
 
+impl Default for Buffer {
+    fn default() -> Self {
+        Buffer::new()
+    }
+}
+
 impl From<Vec<u8>> for Buffer {
     fn from(val: Vec<u8>) -> Self {
         Self(Arc::new(RwLock::new(val)))
+    }
+}
+
+impl PartialEq for Buffer {
+    fn eq(&self, other: &Buffer) -> bool {
+        Arc::ptr_eq(&self.0, &other.0)
     }
 }
 
@@ -242,6 +257,167 @@ impl Slice {
     }
 }
 
+/// A list of multiple slices of the same buffer.
+///
+/// `Substream` represents a substream of the underlying buffer. Slices can be
+/// appended to the chunks list
+#[derive(Clone, Debug)]
+pub struct Substream {
+    buf: Option<Buffer>,
+    chunks: Vec<(usize, usize)>,
+}
+
+impl Substream {
+    pub fn new() -> Self {
+        Self {
+            buf: None,
+            chunks: vec![],
+        }
+    }
+
+    pub fn append(&mut self, slice: Slice) -> Result<(), ()> {
+        if self.buf.is_none() {
+            self.buf = Some(slice.buf);
+            self.chunks.push((slice.start, slice.end));
+
+            Ok(())
+        } else if self.buf == Some(slice.buf) {
+            self.chunks.push((slice.start, slice.end));
+
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn num_chunks(&self) -> usize {
+        self.chunks.len()
+    }
+
+    pub fn as_cursor(&self) -> SubstreamCursor {
+        SubstreamCursor {
+            substream: self.clone(),
+            chunk_pos: 0,
+            bytes_pos: 0,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        let mut tally = 0;
+        for (chunk_start, chunk_end) in &self.chunks {
+            tally += chunk_end - chunk_start;
+        }
+
+        tally
+    }
+
+    pub fn iter_chunks(&self) -> SubstreamChunksIter {
+        SubstreamChunksIter {
+            substream: self.clone(),
+            next_buf: 0,
+        }
+    }
+}
+
+impl From<Buffer> for Substream {
+    fn from(buf: Buffer) -> Self {
+        Self {
+            buf: Some(buf),
+            chunks: vec![],
+        }
+    }
+}
+
+impl From<Slice> for Substream {
+    fn from(slice: Slice) -> Self {
+        Self {
+            buf: Some(slice.buf),
+            chunks: vec![(slice.start, slice.end)],
+        }
+    }
+}
+
+/// A readable cursor into a buffer substream.
+///
+/// Reads from the cursor (via `Read` etc) will be filled as if the substream
+/// referred to in the chunks list is a single contiguous stream of bytes with
+/// no other data in between. Code using a `SubstreamCursor` can thus work with
+/// both multiplexed and unmultiplexed data in the same way.
+pub struct SubstreamCursor {
+    substream: Substream,
+    chunk_pos: usize,
+    bytes_pos: usize,
+}
+
+impl Read for SubstreamCursor {
+    fn read(&mut self, data: &mut [u8]) -> IoResult<usize> {
+        let mut out_count = 0;
+        if self.substream.buf.is_none() {
+            return Ok(out_count);
+        }
+
+        let buf_owned = self.substream.buf.clone().expect("buffer");
+        let buf = buf_owned.0.read().map_err(|_| {
+            IoError::new(
+                IoErrorKind::Other,
+                "the underlying substream is locked by a panicked process",
+            )
+        })?;
+
+        while out_count < data.len() {
+            let cur_chunk = self.substream.chunks.get(self.chunk_pos);
+            if cur_chunk.is_none() {
+                return Ok(out_count);
+            }
+
+            let cur_chunk = cur_chunk.expect("cur_chunk should never be None");
+
+            let chunk_len = cur_chunk.1 - cur_chunk.0;
+            let copy_count = min(data.len() - out_count, chunk_len - self.bytes_pos);
+
+            data[out_count..out_count + copy_count].copy_from_slice(
+                buf.get(cur_chunk.0..cur_chunk.0 + copy_count)
+                    .expect("Slice offsets are always valid"),
+            );
+            self.bytes_pos += copy_count;
+            if self.bytes_pos < chunk_len {
+                break;
+            }
+
+            out_count += copy_count;
+
+            self.chunk_pos += 1;
+            self.bytes_pos = 0;
+        }
+
+        Ok(out_count)
+    }
+}
+
+/// Iterator for substream chunks
+pub struct SubstreamChunksIter {
+    substream: Substream,
+    next_buf: usize,
+}
+
+impl Iterator for SubstreamChunksIter {
+    type Item = Slice;
+
+    fn next(&mut self) -> Option<Slice> {
+        if let Some((start, end)) = self.substream.chunks.get(self.next_buf) {
+            if let Some(buf) = &self.substream.buf {
+                return Some(Slice {
+                    buf: buf.clone(),
+                    start: *start,
+                    end: *end,
+                });
+            }
+        }
+
+        None
+    }
+}
+
 #[derive(Debug)]
 pub struct SliceRef<'a> {
     guard: RwLockReadGuard<'a, Vec<u8>>,
@@ -267,7 +443,8 @@ impl PartialEq for SliceRef<'_> {
 
 #[cfg(test)]
 mod test {
-    use crate::buffer::Buffer;
+    use crate::buffer::{Buffer, Substream};
+    use std::io::Read;
 
     #[test]
     fn buf_slice() {
@@ -287,5 +464,42 @@ mod test {
         buf.append(&mut vec![6, 7, 8, 9]);
 
         assert_eq!(&*slice.data(), &[2, 3]);
+    }
+
+    #[test]
+    fn substream_cursor_read_inside() {
+        let buf = Buffer::from(vec![
+            38, 26, 99, 1, 1, 1, 1, 38, 12, 14, 1, 1, 93, 86, 1, 88,
+        ]);
+        let mut substream = Substream::new();
+
+        substream.append(buf.get(3..7).unwrap()).unwrap();
+        substream.append(buf.get(10..12).unwrap()).unwrap();
+        substream.append(buf.get(14..15).unwrap()).unwrap();
+
+        let mut cursor = substream.as_cursor();
+        let mut data = vec![0; 7];
+        let result = cursor.read(&mut data);
+        assert_eq!(result.unwrap(), 7);
+        assert_eq!(data, vec![1; 7]);
+    }
+
+    #[test]
+    fn substream_cursor_read_outside() {
+        let buf = Buffer::from(vec![
+            38, 26, 99, 1, 1, 1, 1, 38, 12, 14, 1, 1, 93, 86, 1, 88,
+        ]);
+        let mut substream = Substream::new();
+
+        substream.append(buf.get(0..3).unwrap()).unwrap();
+        substream.append(buf.get(7..10).unwrap()).unwrap();
+        substream.append(buf.get(12..14).unwrap()).unwrap();
+        substream.append(buf.get(15..).unwrap()).unwrap();
+
+        let mut cursor = substream.as_cursor();
+        let mut data = vec![0; 7];
+        let result = cursor.read(&mut data);
+        assert_eq!(result.unwrap(), 7);
+        assert_eq!(data, vec![38, 26, 99, 38, 12, 14, 93, 86, 88]);
     }
 }
