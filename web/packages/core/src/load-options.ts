1c7d10596cbc71c8e1bb9f2a394cc7852a0a2c43
Nathan Adams <dinnerbone@dinnerbone.com>
2023-07-22 19:48:42 +0200

    web: Readd warnOnUnsupportedContent for backwards compatibility, but mark it deprecated and unused

--- web/packages/core/src/load-options.ts
+++ web/packages/core/src/load-options.ts
@@ -338,6 +338,19 @@ export interface BaseLoadOptions {
     favorFlash?: boolean;
 
     /**
+     * This is no longer used and does not affect anything.
+     * It is only kept for backwards compatibility.
+     *
+     * Previously:
+     * "Whether or not to display an overlay with a warning when
+     * loading a movie with unsupported content."
+     *
+     * @default true
+     * @deprecated
+     */
+    warnOnUnsupportedContent?: boolean;
+
+    /**
      * Console logging level.
      *
      * @default LogLevel.Error
