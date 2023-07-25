38b4357f598bb99659428b77413fb5887dc29d0a
Lord-McSweeney <84632019+Lord-McSweeney@users.noreply.github.com>
2023-07-24 23:00:32 -0700

    avm1: Implement Button.filters

--- core/src/avm1/globals/button.rs
+++ core/src/avm1/globals/button.rs
@@ -2,7 +2,9 @@
 
 use crate::avm1::activation::Activation;
 use crate::avm1::error::Error;
+use crate::avm1::globals::bitmap_filter;
 use crate::avm1::property_decl::{define_properties_on, Declaration};
+use crate::avm1::ArrayObject;
 use crate::avm1::{globals, Object, ScriptObject, TObject, Value};
 use crate::context::GcContext;
 use crate::display_object::{Avm1Button, TDisplayObject};
@@ -40,6 +42,7 @@ const PROTO_DECLS: &[Declaration] = declare_properties! {
     "useHandCursor" => bool(true);
     "getDepth" => method(globals::get_depth; DONT_DELETE | READ_ONLY | VERSION_6);
     "blendMode" => property(button_getter!(blend_mode), button_setter!(set_blend_mode); DONT_DELETE | VERSION_8);
+    "filters" => property(button_getter!(filters), button_setter!(set_filters); DONT_DELETE | DONT_ENUM | VERSION_8);
 };
 
 pub fn create_proto<'gc>(
@@ -80,5 +83,39 @@ fn set_blend_mode<'gc>(
     } else {
         tracing::error!("Unknown blend mode {value:?}");
     }
+    Ok(())
+}
+
+fn filters<'gc>(
+    this: Avm1Button<'gc>,
+    activation: &mut Activation<'_, 'gc>,
+) -> Result<Value<'gc>, Error<'gc>> {
+    Ok(ArrayObject::new(
+        activation.context.gc_context,
+        activation.context.avm1.prototypes().array,
+        this.filters()
+            .into_iter()
+            .map(|filter| bitmap_filter::filter_to_avm1(activation, filter)),
+    )
+    .into())
+}
+
+fn set_filters<'gc>(
+    this: Avm1Button<'gc>,
+    activation: &mut Activation<'_, 'gc>,
+    value: Value<'gc>,
+) -> Result<(), Error<'gc>> {
+    let mut filters = vec![];
+    if let Value::Object(value) = value {
+        for index in value.get_keys(activation, false).into_iter().rev() {
+            let filter_object = value.get(index, activation)?.coerce_to_object(activation);
+            if let Some(filter) =
+                bitmap_filter::avm1_to_filter(filter_object, &mut activation.context)
+            {
+                filters.push(filter);
+            }
+        }
+    }
+    this.set_filters(activation.context.gc_context, filters);
     Ok(())
 }
