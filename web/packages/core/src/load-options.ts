c170c8561b8f69411354aff35cbd3c5e775b0db8
sleepycatcoding <131554884+sleepycatcoding@users.noreply.github.com>
2023-09-12 12:09:36 +0300

    web: rudimentary documentation for JS API

--- web/packages/core/src/load-options.ts
+++ web/packages/core/src/load-options.ts
@@ -248,10 +248,22 @@ export const enum NetworkingAccessMode {
     None = "none",
 }
 
+/**
+ * Represents a host, port and proxyUrl. Used when a SWF file tries to use a Socket.
+ */
 export interface SocketProxy {
+    /**
+     * Host used by the SWF.
+     */
     host: string;
+    /**
+     * Port used by the SWF.
+     */
     port: number;
 
+    /**
+     * The proxy URL to use when SWF file tries to connect to the specified host and port.
+     */
     proxyUrl: string;
 }
 
@@ -536,6 +548,18 @@ export interface BaseLoadOptions {
      */
     openInNewTab?: ((swf: URL) => void) | null;
 
+    /**
+     * An array of SocketProxy objects.
+     *
+     * When a SWF tries to establish a Socket connection, Ruffle will search for
+     * a matching SocketProxy object in this array and use it to establish a WebSocket connection,
+     * through which all communication is tunneled through.
+     *
+     * When none are found, Ruffle will fail the connection gracefully.
+     * When multiple matching SocketProxy objects exist, the first one is used.
+     *
+     * @default []
+     */
     socketProxy?: Array<SocketProxy>;
 }
 
