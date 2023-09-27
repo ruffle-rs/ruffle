d9c0a3f1fff0e12934446d7bb7c7611fc6dfa697
Leonardo Oliveira Reis <leonardooliveirareis@glados.meuintelbras.local>
2023-09-27 01:10:59 -0300

    used clippy suggestions

--- core/src/player.rs
+++ core/src/player.rs
@@ -1380,22 +1380,22 @@ impl Player {
                 // and fires "drag" events. Other objects are ignored.
                 if context.input.is_mouse_down() {
                     context.mouse_over_object = new_over_object;
-                    cur_over_object.map(|old_hovered_object| {
+                    if let Some(old_hovered_object) = cur_over_object {
                         events.push((
                             old_hovered_object,
                             ClipEvent::DragOut {
                                 to: new_over_object,
                             },
                         ));
-                    });
-                    new_over_object.map(|new_hovered_object| {
+                    }
+                    if let Some(new_hovered_object) = new_over_object {
                         events.push((
                             new_hovered_object,
                             ClipEvent::DragOver {
                                 from: cur_over_object,
                             },
                         ));
-                    });
+                    }
                 } else {
                     // The mouse button is up, so fire rollover states for the object we are hovering over.
                     // Rolled out of the previous object.
