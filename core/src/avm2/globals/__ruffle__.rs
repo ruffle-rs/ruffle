7a3e7fafa8a4bf98d85795bec9d8c15964086412
Lord-McSweeney <Lord-McSweeney@github.com>
2023-10-13 09:17:08 -0700

    avm2: Add back log_warn

--- core/src/avm2/globals/__ruffle__.rs
+++ core/src/avm2/globals/__ruffle__.rs
@@ -2,6 +2,7 @@ use crate::avm2::activation::Activation;
 use crate::avm2::error::Error;
 use crate::avm2::object::Object;
 use crate::avm2::value::Value;
+use crate::string::WStr;
 use crate::stub::Stub;
 use std::borrow::Cow;
 
@@ -116,6 +117,30 @@ pub fn stub_constructor<'gc>(
                 });
         }
         _ => tracing::warn!("(__ruffle__.stub_constructor called with wrong args)"),
+    }
+
+    Ok(Value::Undefined)
+}
+
+pub fn log_warn<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    _this: Object<'gc>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error<'gc>> {
+    match args {
+        [] => tracing::warn!("(__ruffle__.log_warn called with no arg)"),
+        [arg] => {
+            let msg = arg.coerce_to_string(activation)?;
+            tracing::warn!("{}", &msg.to_utf8_lossy());
+        }
+        args => {
+            let strings = args
+                .iter()
+                .map(|a| a.coerce_to_string(activation))
+                .collect::<Result<Vec<_>, _>>()?;
+            let msg = crate::string::join(&strings, &WStr::from_units(b" "));
+            tracing::warn!("{}", &msg.to_utf8_lossy());
+        }
     }
 
     Ok(Value::Undefined)
