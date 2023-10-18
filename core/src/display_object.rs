c4f24843f77b80edcfabf3dd79d4e453b77a03d0
Aaron Hill <aa1ronham@gmail.com>
2023-10-17 23:54:02 -0400

    avm2: Don't stop root movie clip, even if it doesn't extend `MovieClip`
    Normally, Flash Player will ignores frames for a movie clip with
    a symbol class that doesn't extend `MovieClip` (e.g. it extends
    `Sprite`). However, the root movie appears to have frame run
    unconditionally, even if it only extends `Sprite`. This can
    be observed by adding a Graphics child in the second frame only -
    the child will flicker in and out as the player switches between
    frames, but only for the root movie clip.
    
    Seedling relies on this behavior - it has `DoAbc2` tags in the second
    frame, and has a main `Preloader` class that extends `Sprite`.

--- core/src/display_object.rs
+++ core/src/display_object.rs
@@ -1908,7 +1908,8 @@ pub trait TDisplayObject<'gc>:
             // It's possible to have a DefineSprite tag with multiple frames, but have
             // the corresponding `SymbolClass` *not* extend `MovieClip` (e.g. extending `Sprite` directly.)
             // When this occurs, Flash Player will run the first frame, and immediately stop.
-            if !obj.is_of_type(movieclip_class, context) {
+            // However, Flash Player runs frames for the root movie clip, even if it doesn't extend `MovieClip`.
+            if !obj.is_of_type(movieclip_class, context) && !movie.is_root() {
                 movie.stop(context);
             }
         }
---
+++ tests/tests/swfs/avm2/sprite_with_frames/MainClass.as
@@ -0,0 +1,17 @@
+﻿package  {
+	
+	import flash.display.Sprite;
+	
+	
+	public class MainClass extends Sprite {
+		
+		
+		public function MainClass() {
+			var child = new MyChild();
+			child.x = 100;
+			child.y = 50;
+			this.addChild(child);
+		}
+	}
+	
+}
