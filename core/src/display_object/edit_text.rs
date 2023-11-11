3cc991507651a7b61b9dbec2c7df6eb9919291ff
Lord-McSweeney <Lord-McSweeney@github.com>
2023-11-10 21:03:19 -0800

    avm2: TLF EditTexts should not be selectable

--- core/src/display_object/edit_text.rs
+++ core/src/display_object/edit_text.rs
@@ -391,6 +391,7 @@ impl<'gc> EditText<'gc> {
     ) -> Self {
         let text = Self::new(context, swf_movie, x, y, width, height);
         text.set_is_tlf(context.gc_context, true);
+        text.set_selectable(false, context);
 
         text
     }
