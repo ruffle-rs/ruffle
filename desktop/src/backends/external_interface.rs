48102a20a29bd10f9849c367079bb1d5f3912fa0
Tom Schuster <evilpies@gmail.com>
2023-11-14 19:56:44 +0100

    desktop: Add window.location.href spoofing to the fake ExternalInterface

--- desktop/src/backends/external_interface.rs
+++ desktop/src/backends/external_interface.rs
@@ -1,10 +1,30 @@
-use ruffle_core::external::{ExternalInterfaceMethod, ExternalInterfaceProvider};
+use ruffle_core::context::UpdateContext;
+use ruffle_core::external::{
+    ExternalInterfaceMethod, ExternalInterfaceProvider, Value as ExternalValue,
+};
+use url::Url;
 
-#[derive(Default)]
-pub struct DesktopExternalInterfaceProvider;
+pub struct DesktopExternalInterfaceProvider {
+    pub spoof_url: Option<Url>,
+}
+
+struct FakeWindowLocationHrefToString(Url);
+
+impl ExternalInterfaceMethod for FakeWindowLocationHrefToString {
+    fn call(&self, _context: &mut UpdateContext<'_, '_>, _args: &[ExternalValue]) -> ExternalValue {
+        ExternalValue::String(self.0.to_string())
+    }
+}
 
 impl ExternalInterfaceProvider for DesktopExternalInterfaceProvider {
-    fn get_method(&self, _name: &str) -> Option<Box<dyn ExternalInterfaceMethod>> {
+    fn get_method(&self, name: &str) -> Option<Box<dyn ExternalInterfaceMethod>> {
+        if let Some(ref url) = self.spoof_url {
+            if name == "window.location.href.toString" {
+                return Some(Box::new(FakeWindowLocationHrefToString(url.clone())));
+            }
+        }
+
+        tracing::warn!("Trying to call unknown ExternalInterface method: {name}");
         None
     }
 
