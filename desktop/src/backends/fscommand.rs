d3b9d8a4516a985a78d8a081a764aacabd8e3b6d
Lord-McSweeney <Lord-McSweeney@github.com>
2023-11-25 14:40:38 -0800

    dekstop: Support `quit` and `fullscreen` fscommands

---
+++ desktop/src/backends/fscommand.rs
@@ -0,0 +1,33 @@
+use crate::custom_event::RuffleEvent;
+
+use ruffle_core::external::FsCommandProvider;
+use std::rc::Rc;
+use winit::event_loop::EventLoopProxy;
+use winit::window::{Fullscreen, Window};
+
+pub struct DesktopFSCommandProvider {
+    pub event_loop: EventLoopProxy<RuffleEvent>,
+    pub window: Rc<Window>,
+}
+
+impl FsCommandProvider for DesktopFSCommandProvider {
+    fn on_fs_command(&self, command: &str, args: &str) -> bool {
+        match command {
+            "quit" => {
+                let _ = self.event_loop.send_event(RuffleEvent::ExitRequested);
+            }
+            "fullscreen" => {
+                match args {
+                    "true" => self
+                        .window
+                        .set_fullscreen(Some(Fullscreen::Borderless(None))),
+                    "false" => self.window.set_fullscreen(None),
+                    _ => {}
+                };
+            }
+            _ => return false,
+        };
+
+        true
+    }
+}
