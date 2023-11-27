f7af8ca3987bfecda52b9aca410187da91cf6b14
David Wendt <dcrkid@yahoo.com>
2023-11-26 20:39:48 -0500

    avm1: Implement strict array conversion for AVM1, too.

--- core/src/avm1/flv.rs
+++ core/src/avm1/flv.rs
@@ -1,4 +1,4 @@
-use crate::avm1::{Activation, ScriptObject, TObject as _, Value as Avm1Value};
+use crate::avm1::{Activation, ArrayObject, ScriptObject, TObject as _, Value as Avm1Value};
 use crate::string::AvmString;
 use flv_rs::{Value as FlvValue, Variable as FlvVariable};
 
@@ -24,6 +24,18 @@ fn avm1_object_from_flv_variables<'gc>(
     info_object.into()
 }
 
+fn avm1_array_from_flv_values<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    values: Vec<FlvValue>,
+) -> Avm1Value<'gc> {
+    ArrayObject::new(
+        activation.context.gc_context,
+        activation.context.avm1.prototypes().array,
+        values.iter().map(|v| v.clone().to_avm1_value(activation)),
+    )
+    .into()
+}
+
 pub trait FlvValueAvm1Ext<'gc> {
     fn to_avm1_value(self, activation: &mut Activation<'_, 'gc>) -> Avm1Value<'gc>;
 }
@@ -32,6 +44,7 @@ impl<'gc> FlvValueAvm1Ext<'gc> for FlvValue<'_> {
     fn to_avm1_value(self, activation: &mut Activation<'_, 'gc>) -> Avm1Value<'gc> {
         match self {
             FlvValue::EcmaArray(values) => avm1_object_from_flv_variables(activation, values),
+            FlvValue::StrictArray(values) => avm1_array_from_flv_values(activation, values),
             FlvValue::String(string_data) | FlvValue::LongString(string_data) => {
                 AvmString::new_utf8_bytes(activation.context.gc_context, string_data).into()
             }
