ruffle-rs/ruffle

commit 205741782495274bce600434ee79284ee79f5f06

avm2: Implement `Error.prototype.toString`
And remove `ErrorObject::to_string` implementation. This fixes a few avmplus tests.

core/src/avm2/globals/Error.as
@@ -20,5 +20,10 @@ package {
 		public native function getStackTrace():String;
 
 		public static const length:int = 1;
+
+		prototype.toString = function():String {
+			var self:Error = this;
+			return self.message.length == 0 ? self.name : self.name + ": " + self.message;
+		};
 	}
 }

tests/tests/swfs/from_avmplus/ecma3/ErrorObject/e15_11_2_1/test.toml
@@ -1,2 +1,1 @@
 num_ticks = 1
-known_failure = true

tests/tests/swfs/from_avmplus/ecma3/ObjectObjects/class_003/test.toml
@@ -1,2 +1,1 @@
 num_ticks = 1
-known_failure = true

tests/tests/swfs/from_avmplus/ecma3/ObjectObjects/regress_72773_rt/test.toml
@@ -1,2 +1,1 @@
 num_ticks = 1
-known_failure = true

core/src/avm2/object/error_object.rs
@@ -4,7 +4,6 @@ use crate::avm2::activation::Activation;
 use crate::avm2::call_stack::CallStack;
 use crate::avm2::object::script_object::ScriptObjectData;
 use crate::avm2::object::{ClassObject, Object, ObjectPtr, TObject};
-use crate::avm2::string::AvmString;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use crate::string::WString;
@@ -137,10 +136,6 @@ impl<'gc> TObject<'gc> for ErrorObject<'gc> {
 
     fn value_of(&self, _mc: &Mutation<'gc>) -> Result<Value<'gc>, Error<'gc>> {
         Ok(Value::Object(Object::from(*self)))
-    }
-
-    fn to_string(&self, activation: &mut Activation<'_, 'gc>) -> Result<Value<'gc>, Error<'gc>> {
-        Ok(AvmString::new(activation.context.gc_context, self.display()?).into())
     }
 
     fn as_error_object(&self) -> Option<ErrorObject<'gc>> {

commit 87f7bc85a4a14fd3519f48b2bbd541622e74c9b9

avm2: Fix `Error.toString` regression
This was introduced by #14897, and to make sure this won't happen again I added a test.

Fixes #14901.

core/src/avm2/globals/Error.as
@@ -23,7 +23,7 @@ package {
 
 		prototype.toString = function():String {
 			var self:Error = this;
-			return self.message.length == 0 ? self.name : self.name + ": " + self.message;
+			return self.message !== "" ? self.name + ": " + self.message : self.name;
 		};
 	}
 }

tests/tests/swfs/avm2/issue_14901/Test.as
@@ -1,0 +1,8 @@
+package {
+import flash.display.Sprite;
+
+public class Test extends Sprite {}
+}
+
+var e = new Error(null);
+trace(e.toString());

tests/tests/swfs/avm2/issue_14901/test.toml
@@ -1,0 +1,1 @@
+num_frames = 1