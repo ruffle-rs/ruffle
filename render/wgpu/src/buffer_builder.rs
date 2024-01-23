2a96d10d2e4ae8f2fc2d2af2508d35099773c51e
Nathan Adams <dinnerbone@dinnerbone.com>
2024-01-23 20:07:55 +0100

    wgpu: Add limits to BufferBuilder (but currently not respected for Meshes)

--- render/wgpu/src/buffer_builder.rs
+++ render/wgpu/src/buffer_builder.rs
@@ -5,27 +5,61 @@ use wgpu::util::DeviceExt;
 pub struct BufferBuilder {
     inner: Vec<u8>,
     align_mask: usize,
+    limit: u64,
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct BufferFull;
+
 impl BufferBuilder {
-    pub fn new(alignment: usize) -> Self {
+    pub fn new_for_vertices(limits: &wgpu::Limits) -> Self {
         Self {
             inner: Vec::new(),
-            align_mask: if alignment > 0 { alignment - 1 } else { 0 },
+            align_mask: 0,
+            limit: limits.max_buffer_size,
         }
     }
 
-    pub fn add<T: NoUninit + AnyBitPattern>(&mut self, value: &[T]) -> Range<wgpu::BufferAddress> {
-        if !self.inner.is_empty() && self.align_mask > 0 {
-            // Pad the internal buffer to match alignment requirements
-            // Pad on creation so that we don't wastefully pad the end of the buffer
-            let length = (self.inner.len() + self.align_mask) & !self.align_mask;
-            self.inner.resize(length, 0);
+    pub fn new_for_uniform(limits: &wgpu::Limits) -> Self {
+        Self {
+            inner: Vec::new(),
+            align_mask: if limits.min_uniform_buffer_offset_alignment > 0 {
+                (limits.min_uniform_buffer_offset_alignment - 1) as usize
+            } else {
+                0
+            },
+            limit: limits
+                .max_buffer_size
+                .min(limits.max_uniform_buffer_binding_size as u64),
+        }
+    }
+
+    pub fn add<T: NoUninit + AnyBitPattern>(
+        &mut self,
+        value: &[T],
+    ) -> Result<Range<wgpu::BufferAddress>, BufferFull> {
+        let start_pos = if !self.inner.is_empty() {
+            if self.align_mask > 0 {
+                // Pad the internal buffer to match alignment requirements
+                // Pad on creation so that we don't wastefully pad the end of the buffer
+                (self.inner.len() + self.align_mask) & !self.align_mask
+            } else {
+                self.inner.len()
+            }
+        } else {
+            0
+        };
+
+        if (start_pos + value.len()) as u64 > self.limit {
+            return Err(BufferFull);
         }
 
-        let address = self.inner.len() as wgpu::BufferAddress;
+        if start_pos > 0 && self.align_mask > 0 {
+            self.inner.resize(start_pos, 0);
+        }
+
         self.inner.extend_from_slice(bytemuck::cast_slice(value));
-        address..(self.inner.len() as wgpu::BufferAddress)
+        Ok((start_pos as wgpu::BufferAddress)..(self.inner.len() as wgpu::BufferAddress))
     }
 
     pub fn finish(
