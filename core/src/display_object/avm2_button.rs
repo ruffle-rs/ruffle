ruffle-rs/ruffle

commit 5f83b45692083f05612ce83c096c343c6bc40c1f

avm2: Add `Avm2Button::all_state_children` to centralize button child ordering

core/src/display_object/avm2_button.rs
@@ -283,21 +283,35 @@ impl<'gc> Avm2Button<'gc> {
     pub fn set_state(self, context: &mut UpdateContext<'_, 'gc>, state: ButtonState) {
         self.invalidate_cached_bitmap(context.gc());
 
-        if let Some(state) = self.0.up_state.get() {
-            state.set_parent(context, None);
-        }
-        if let Some(state) = self.0.over_state.get() {
-            state.set_parent(context, None);
-        }
-        if let Some(state) = self.0.down_state.get() {
-            state.set_parent(context, None);
-        }
-        if let Some(state) = self.0.hit_area.get() {
+        for state in self.all_state_children(false) {
             state.set_parent(context, None);
         }
         if let Some(state) = self.get_state_child(state.into()) {
             state.set_parent(context, Some(self.into()));
         }
+    }
+
+    /// Get all the display objects representing button states.
+    fn all_state_children(
+        &self,
+        weird_order: bool,
+    ) -> impl Iterator<Item = DisplayObject<'gc>> + '_ {
+        let children = if weird_order {
+            [
+                &self.0.up_state,
+                &self.0.over_state,
+                &self.0.down_state,
+                &self.0.hit_area,
+            ]
+        } else {
+            [
+                &self.0.hit_area,
+                &self.0.up_state,
+                &self.0.down_state,
+                &self.0.over_state,
+            ]
+        };
+        children.into_iter().filter_map(Lock::get)
     }
 
     /// Get the display object that represents a particular button state.
@@ -436,38 +450,14 @@ impl<'gc> TDisplayObject<'gc> for Avm2Button<'gc> {
     }
 
     fn enter_frame(&self, context: &mut UpdateContext<'_, 'gc>) {
-        if let Some(hit_area) = self.0.hit_area.get() {
-            hit_area.enter_frame(context);
-        }
-
-        if let Some(up_state) = self.0.up_state.get() {
-            up_state.enter_frame(context);
-        }
-
-        if let Some(down_state) = self.0.down_state.get() {
-            down_state.enter_frame(context);
-        }
-
-        if let Some(over_state) = self.0.over_state.get() {
-            over_state.enter_frame(context);
+        for state in self.all_state_children(false) {
+            state.enter_frame(context);
         }
     }
 
     fn construct_frame(&self, context: &mut UpdateContext<'_, 'gc>) {
-        if let Some(hit_area) = self.0.hit_area.get() {
-            hit_area.construct_frame(context);
-        }
-
-        if let Some(up_state) = self.0.up_state.get() {
-            up_state.construct_frame(context);
-        }
-
-        if let Some(down_state) = self.0.down_state.get() {
-            down_state.construct_frame(context);
-        }
-
-        if let Some(over_state) = self.0.over_state.get() {
-            over_state.construct_frame(context);
+        for state in self.all_state_children(false) {
+            state.construct_frame(context);
         }
 
         let needs_avm2_construction = self.0.object.get().is_none();
@@ -579,32 +569,8 @@ impl<'gc> TDisplayObject<'gc> for Avm2Button<'gc> {
     }
 
     fn run_frame_scripts(self, context: &mut UpdateContext<'_, 'gc>) {
-        if self.0.weird_framescript_order.take() {
-            if let Some(up_state) = self.0.up_state.get() {
-                up_state.run_frame_scripts(context);
-            }
-            if let Some(over_state) = self.0.over_state.get() {
-                over_state.run_frame_scripts(context);
-            }
-            if let Some(down_state) = self.0.down_state.get() {
-                down_state.run_frame_scripts(context);
-            }
-            if let Some(hit_area) = self.0.hit_area.get() {
-                hit_area.run_frame_scripts(context);
-            }
-        } else {
-            if let Some(hit_area) = self.0.hit_area.get() {
-                hit_area.run_frame_scripts(context);
-            }
-            if let Some(up_state) = self.0.up_state.get() {
-                up_state.run_frame_scripts(context);
-            }
-            if let Some(down_state) = self.0.down_state.get() {
-                down_state.run_frame_scripts(context);
-            }
-            if let Some(over_state) = self.0.over_state.get() {
-                over_state.run_frame_scripts(context);
-            }
+        for state in self.all_state_children(self.0.weird_framescript_order.take()) {
+            state.run_frame_scripts(context);
         }
     }
 

commit 727a9a8334dd927f069276f85957d2831d03c029

avm2: Don't forget to set the state in `Avm2Button::set_state`
Fixes #14922. Also removes a redundant line. These regressed in #14820.

core/src/display_object/avm2_button.rs
@@ -282,6 +282,7 @@ impl<'gc> Avm2Button<'gc> {
     /// Change the rendered state of the button.
     pub fn set_state(self, context: &mut UpdateContext<'_, 'gc>, state: ButtonState) {
         self.invalidate_cached_bitmap(context.gc());
+        self.0.state.set(state);
 
         for state in self.all_state_children(false) {
             state.set_parent(context, None);
@@ -505,7 +506,6 @@ impl<'gc> TDisplayObject<'gc> for Avm2Button<'gc> {
             unlock!(write, Avm2ButtonData, down_state).set(Some(down_state));
             unlock!(write, Avm2ButtonData, hit_area).set(Some(hit_area));
             write.skip_current_frame.set(true);
-            write.needs_frame_construction.set(false);
 
             let mut fire_state_events = |should_fire, state: DisplayObject<'gc>| {
                 if should_fire {