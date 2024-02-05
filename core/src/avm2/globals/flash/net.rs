5028f666083c91202307d1d262117fae23a0e8ca
svitkin <svitkin32@gmail.com>
2024-02-04 18:28:18 -0800

    avm2: parse data for flash.net.navigateToURL (closes #13898)

--- core/src/avm2/globals/flash/net.rs
+++ core/src/avm2/globals/flash/net.rs
@@ -3,6 +3,8 @@
 use crate::avm2::error::type_error;
 use crate::avm2::object::TObject;
 use crate::avm2::{Activation, Error, Object, Value};
+use crate::backend::navigator::NavigationMethod;
+use indexmap::IndexMap;
 
 pub mod file_reference;
 pub mod local_connection;
@@ -14,6 +16,29 @@ pub mod shared_object;
 pub mod socket;
 pub mod url_loader;
 pub mod xml_socket;
+
+fn object_to_index_map<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    obj: &Object<'gc>,
+) -> Result<IndexMap<String, String>, Error<'gc>> {
+    let mut map = IndexMap::new();
+    let mut last_index = obj.get_next_enumerant(0, activation)?;
+    while let Some(index) = last_index {
+        let name = obj
+            .get_enumerant_name(index, activation)?
+            .coerce_to_string(activation)?;
+        let value = obj
+            .get_public_property(name, activation)?
+            .coerce_to_string(activation)?
+            .to_utf8_lossy()
+            .to_string();
+
+        let name = name.to_utf8_lossy().to_string();
+        map.insert(name, value);
+        last_index = obj.get_next_enumerant(index, activation)?;
+    }
+    Ok(map)
+}
 
 /// Implements `flash.net.navigateToURL`
 pub fn navigate_to_url<'gc>(
@@ -39,10 +64,20 @@ pub fn navigate_to_url<'gc>(
         )?)),
         url => {
             let url = url.coerce_to_string(activation)?;
+            let method = request
+                .get_public_property("method", activation)?
+                .coerce_to_string(activation)?;
+            let method =
+                NavigationMethod::from_method_str(&method).unwrap();
+            let data = request
+                .get_public_property("data", activation)?
+                .coerce_to_object(activation)?;
+            // If data is byte array this will not work
+            let data = object_to_index_map(activation, &data).unwrap_or_default();
             activation.context.navigator.navigate_to_url(
                 &url.to_utf8_lossy(),
                 &target.to_utf8_lossy(),
-                None,
+                Some((method, data)),
             );
             Ok(Value::Undefined)
         }
