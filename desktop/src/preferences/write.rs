0b7b9eb0a1bae080fdd3082a8efd3087e3223ba1
sleepycatcoding <131554884+sleepycatcoding@users.noreply.github.com>
2024-03-27 20:57:41 +0200

    desktop: add `bookmarks.toml` file for storing bookmark data
    This is in a separate file to not create clutter in the main preferences.toml file.

--- desktop/src/preferences/write.rs
+++ desktop/src/preferences/write.rs
@@ -1,7 +1,7 @@
 use crate::log::FilenamePattern;
-use crate::preferences::PreferencesAndDocument;
+use crate::preferences::{Bookmark, BookmarksAndDocument, PreferencesAndDocument};
 use ruffle_render_wgpu::clap::{GraphicsBackend, PowerPreference};
-use toml_edit::value;
+use toml_edit::{array, value, Table};
 use unic_langid::LanguageIdentifier;
 
 pub struct PreferencesWriter<'a>(&'a mut PreferencesAndDocument);
@@ -48,6 +48,57 @@ impl<'a> PreferencesWriter<'a> {
     pub fn set_log_filename_pattern(&mut self, pattern: FilenamePattern) {
         self.0.toml_document["log"]["filename_pattern"] = value(pattern.as_str());
         self.0.values.log.filename_pattern = pattern;
+    }
+}
+
+pub struct BookmarksWriter<'a>(&'a mut BookmarksAndDocument);
+
+impl<'a> BookmarksWriter<'a> {
+    pub(super) fn new(bookmarks: &'a mut BookmarksAndDocument) -> Self {
+        Self(bookmarks)
+    }
+
+    pub fn add(&mut self, bookmark: Bookmark) {
+        // TODO: if more fields are added, this should use URL matching (e.g. other properties are ignored)
+        if !self.0.values.contains(&bookmark) {
+            if let Some(array) = self.0.toml_document["bookmark"]
+                .or_insert(array())
+                .as_array_of_tables_mut()
+            {
+                // TODO: If we add a BookmarkWriter use this here instead rather than duplicating the table write code.
+                let mut table = Table::new();
+                table["url"] = value(bookmark.url.to_string());
+                array.push(table);
+                self.0.values.push(bookmark);
+            } else {
+                // TODO: There is definitely a better way to handle this, then just logging a warning.
+                tracing::warn!("bookmark is not an array of tables, bookmarks will NOT be saved.");
+            }
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) {
+        // We need to get the URL to find the bookmark in the TOML file, since index may not correspond to
+        // the same table entry (i.e. invalid tables and such that we want to keep intact for compatibility purposes)
+        let bookmark = self.0.values.remove(index);
+
+        // Remove the bookmark from the TOML file.
+        if let Some(array) = self.0.toml_document["bookmark"]
+            .or_insert(array())
+            .as_array_of_tables_mut()
+        {
+            let bookmark_url = bookmark.url.to_string();
+            array.retain(|x| {
+                if let Some(url) = x.get("url").and_then(|x| x.as_str()) {
+                    return url != bookmark_url;
+                }
+
+                true
+            });
+        } else {
+            // TODO: We should add a way to return an error from write methods.
+            tracing::warn!("bookmark is not an array of tables, bookmarks will NOT be saved.");
+        }
     }
 }
 
@@ -186,6 +237,47 @@ mod tests {
                 |writer| writer.set_log_filename_pattern(FilenamePattern::SingleFile),
                 "[log]\nfilename_pattern = \"single_file\"\n",
             );
+        }
+    }
+
+    #[allow(clippy::unwrap_used)]
+    mod bookmarks {
+        use super::*;
+        use crate::preferences::read::read_bookmarks;
+        use std::str::FromStr;
+
+        define_serialization_test_helpers!(read_bookmarks, BookmarksAndDocument, BookmarksWriter);
+
+        #[test]
+        fn add_bookmark() {
+            test(
+                "",
+                |writer| {
+                    writer.add(Bookmark {
+                        url: url::Url::from_str("file:///home/user/example.swf").unwrap(),
+                    })
+                },
+                "[[bookmark]]\nurl = \"file:///home/user/example.swf\"\n",
+            );
+            test("[[bookmark]]\nurl = \"file:///home/user/example.swf\"\n", |writer| writer.add(Bookmark {
+            url: url::Url::from_str("file:///home/user/another_file.swf").unwrap()
+        }), "[[bookmark]]\nurl = \"file:///home/user/example.swf\"\n\n[[bookmark]]\nurl = \"file:///home/user/another_file.swf\"\n");
+        }
+
+        #[test]
+        fn remove_bookmark() {
+            test(
+            "[[bookmark]]\nurl = \"file://home/user/example.swf\"\n\n[[bookmark]]\nurl = \"https://ruffle.rs/logo-anim.swf\"\n\n[[bookmark]]\nurl = \"file:///another_file.swf\"\n",
+            |writer| {
+                writer.remove(1);
+            },
+            "[[bookmark]]\nurl = \"file://home/user/example.swf\"\n\n[[bookmark]]\nurl = \"file:///another_file.swf\"\n",
+        );
+
+            // Test that we leave invalid bookmark tables intact when removing a bookmark.
+            test("[[bookmark]]\nurl = \"file://home/user/example.swf\"\n\n[[bookmark]]\n\n[[bookmark]]\nurl = \"https://ruffle.rs/logo-anim.swf\"\n\n[[bookmark]]\nurl = \"invalid\"\n", |writer| {
+            writer.remove(1);
+        }, "[[bookmark]]\nurl = \"file://home/user/example.swf\"\n\n[[bookmark]]\n\n[[bookmark]]\nurl = \"invalid\"\n")
         }
     }
 }
