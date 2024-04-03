2bd28c9c1caf43751b9a5548e43c253a2114f922
Nathan Adams <dinnerbone@dinnerbone.com>
2024-04-04 01:13:52 +0200

    frontend-utils: Add a DocumentHolder<T> helper type, use it for preferences and bookmarks

--- frontend-utils/src/parse.rs
+++ frontend-utils/src/parse.rs
@@ -1,6 +1,68 @@
 use std::fmt;
+use std::ops::Deref;
 use std::str::FromStr;
 use toml_edit::{ArrayOfTables, DocumentMut, Item, Table, TableLike};
+
+/// A holder over values that may be read and potentially written back to disk.
+///
+/// Two versions of Ruffle may have different preferences, or different values available for each preference.
+/// For this reason, we store both the original toml document *and* the parsed values as we understand them.
+/// Whenever we persist values back to the toml, we only edit the values we changed and leave the remaining
+/// values as they originally were.
+/// This way, switching between different versions will *not* wipe your settings or get Ruffle into an
+/// invalid state.
+pub struct DocumentHolder<T> {
+    /// The original toml document
+    toml_document: DocumentMut,
+
+    /// The actual values stored within the toml document, as this version of Ruffle understands them.
+    inner: T,
+}
+
+impl<T> Deref for DocumentHolder<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T: Default> Default for DocumentHolder<T> {
+    fn default() -> Self {
+        Self {
+            toml_document: Default::default(),
+            inner: Default::default(),
+        }
+    }
+}
+
+impl<T> DocumentHolder<T> {
+    pub fn new(values: T, document: DocumentMut) -> Self {
+        Self {
+            inner: values,
+            toml_document: document,
+        }
+    }
+
+    /// Allows editing of this DocumentHolder.
+    ///
+    /// The provided function is expected to:
+    /// - Only edit the toml **as little as is required** for the given change
+    /// - Ensure that the toml and values are kept in sync, such that reading the toml again provides the same values
+    pub fn edit(&mut self, fun: impl FnOnce(&mut T, &mut DocumentMut)) {
+        fun(&mut self.inner, &mut self.toml_document)
+    }
+
+    /// Takes the saved values from this DocumentHolder, discarding the document.
+    /// Useful when you need the result of parsing, without needing to write to it again later.
+    pub fn take(self) -> T {
+        self.inner
+    }
+
+    pub fn serialize(&self) -> String {
+        self.toml_document.to_string()
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct ParseResult<T: PartialEq + fmt::Debug> {
