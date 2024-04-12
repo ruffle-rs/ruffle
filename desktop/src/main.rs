2f678bfd3938fd6624897adc579a75cfd9ec7c6b
Nathan Adams <dinnerbone@dinnerbone.com>
2024-04-12 23:31:44 +0200

    desktop: Remove --timedemo support. As far as we know, nobody's used it since it was added, and we have better ways of profiling these days.

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -14,7 +14,6 @@ mod gui;
 mod log;
 mod player;
 mod preferences;
-mod time_demo;
 mod util;
 
 use crate::preferences::GlobalPreferences;
@@ -180,11 +179,7 @@ fn main() -> Result<(), Error> {
 
     subscriber.init();
 
-    let result = if opt.timedemo {
-        time_demo::run_timedemo(preferences.cli)
-    } else {
-        App::new(preferences).and_then(|app| app.run())
-    };
+    let result = App::new(preferences).and_then(|app| app.run());
 
     #[cfg(windows)]
     if let Err(error) = &result {
