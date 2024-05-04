60457b328777715d3d5fa10cad19724edf9fcc01
Kamil Jarosz <kjarosh256@gmail.com>
2024-05-05 00:02:43 +0200

    tests: Add missing text control codes

--- tests/input-format/src/format.rs
+++ tests/input-format/src/format.rs
@@ -22,11 +22,22 @@ pub enum MouseButton {
 /// Control inputs to a text field
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum TextControlCode {
-    // TODO: Add control codes for Ctrl+Arrows and Home/End keys
     MoveLeft,
+    MoveLeftWord,
+    MoveLeftLine,
+    MoveLeftDocument,
     MoveRight,
+    MoveRightWord,
+    MoveRightLine,
+    MoveRightDocument,
     SelectLeft,
+    SelectLeftWord,
+    SelectLeftLine,
+    SelectLeftDocument,
     SelectRight,
+    SelectRightWord,
+    SelectRightLine,
+    SelectRightDocument,
     SelectAll,
     Copy,
     Paste,
