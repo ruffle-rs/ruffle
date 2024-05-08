f219d93a042f35c66e662308c034b24e08b36674
Kamil Jarosz <kjarosh256@gmail.com>
2024-05-09 00:40:59 +0200

    debug_ui: Exclude hidden objects properly when searching
    The option "Include Hidden" was not working properly,
    as it did not exclude all hidden objects when disabled.
    An object is hidden also when its ancestor is not visible.

--- core/src/debug_ui/display_object/search.rs
+++ core/src/debug_ui/display_object/search.rs
@@ -110,9 +110,6 @@ impl DisplayObjectSearchWindow {
     }
 
     fn object_matches(&self, object: DisplayObject, cursor: Point<Twips>) -> bool {
-        if !self.include_hidden && !object.visible() {
-            return false;
-        }
         if self.only_mouse_enabled
             && !object
                 .as_interactive()
@@ -131,6 +128,10 @@ impl DisplayObjectSearchWindow {
         object: DisplayObject<'gc>,
         add_to: &mut Vec<DisplayObjectTree>,
     ) {
+        if !self.include_hidden && !object.visible() {
+            return;
+        }
+
         if self.object_matches(object, cursor) {
             let handle = DisplayObjectHandle::new(context, object);
             let color =
