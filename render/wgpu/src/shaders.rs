b5cd4f21209839ce0614d47bb444658cb2ebf5fb
TÖRÖK Attila <torokati44@gmail.com>
2024-05-14 12:50:37 +0200

    render/wgpu: Drop `naga_oil` in favor of manual string manipulation

--- render/wgpu/src/shaders.rs
+++ render/wgpu/src/shaders.rs
@@ -1,10 +1,6 @@
 use crate::blend::ComplexBlend;
 use enum_map::{enum_map, EnumMap};
-use naga_oil::compose::{
-    ComposableModuleDescriptor, Composer, ComposerError, NagaModuleDescriptor, ShaderDefValue,
-};
-use std::borrow::Cow;
-use std::collections::HashMap;
+use ruffle_render::shader_source::SHADER_FILTER_COMMON;
 
 #[derive(Debug)]
 pub struct Shaders {
@@ -30,101 +26,65 @@ pub struct Shaders {
 
 impl Shaders {
     pub fn new(device: &wgpu::Device) -> Self {
-        let mut composer = composer().expect("Couldn't create shader composer");
-        let mut shader_defs = HashMap::new();
-        shader_defs.insert("early_saturate".to_owned(), ShaderDefValue::Bool(true));
-
-        let mut late_saturate_shader_defs = shader_defs.clone();
-        late_saturate_shader_defs.insert("early_saturate".to_owned(), ShaderDefValue::Bool(false));
-
-        let color_shader = make_shader(
-            device,
-            &mut composer,
-            &shader_defs,
-            "color.wgsl",
-            include_str!("../shaders/color.wgsl"),
-        );
+        let color_shader = make_shader(device, "color.wgsl", include_str!("../shaders/color.wgsl"));
+        let bitmap = include_str!("../shaders/bitmap.wgsl");
         let bitmap_shader = make_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "bitmap.wgsl",
-            include_str!("../shaders/bitmap.wgsl"),
+            &bitmap.replace("#EARLY_SATURATE#", "true"),
         );
         let bitmap_late_saturate_shader = make_shader(
             device,
-            &mut composer,
-            &late_saturate_shader_defs,
             "bitmap.wgsl",
-            include_str!("../shaders/bitmap.wgsl"),
+            &bitmap.replace("#EARLY_SATURATE#", "false"),
         );
         let copy_srgb_shader = make_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "copy_srgb.wgsl",
             include_str!("../shaders/copy_srgb.wgsl"),
         );
-        let copy_shader = make_shader(
+        let copy_shader = make_shader(device, "copy.wgsl", include_str!("../shaders/copy.wgsl"));
+        let color_matrix_filter = make_filter_shader(
             device,
-            &mut composer,
-            &shader_defs,
-            "copy.wgsl",
-            include_str!("../shaders/copy.wgsl"),
-        );
-        let color_matrix_filter = make_shader(
-            device,
-            &mut composer,
-            &shader_defs,
             "filter/color_matrix.wgsl",
             include_str!("../shaders/filter/color_matrix.wgsl"),
         );
-        let blur_filter = make_shader(
+        let blur_filter = make_filter_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "filter/blur.wgsl",
             include_str!("../shaders/filter/blur.wgsl"),
         );
-        let glow_filter = make_shader(
+        let glow_filter = make_filter_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "filter/glow.wgsl",
             include_str!("../shaders/filter/glow.wgsl"),
         );
-        let bevel_filter = make_shader(
+        let bevel_filter = make_filter_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "filter/bevel.wgsl",
             include_str!("../shaders/filter/bevel.wgsl"),
         );
-        let displacement_map_filter = make_shader(
+        let displacement_map_filter = make_filter_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "filter/displacement_map.wgsl",
             include_str!("../shaders/filter/displacement_map.wgsl"),
         );
         let gradient_shader = make_shader(
             device,
-            &mut composer,
-            &shader_defs,
             "gradient.wgsl",
             include_str!("../shaders/gradient.wgsl"),
         );
 
         let blend_shaders = enum_map! {
-            ComplexBlend::Multiply => make_shader(device, &mut composer, &shader_defs, "blend/multiply.wgsl", include_str!("../shaders/blend/multiply.wgsl")),
-            ComplexBlend::Lighten => make_shader(device, &mut composer, &shader_defs, "blend/lighten.wgsl", include_str!("../shaders/blend/lighten.wgsl")),
-            ComplexBlend::Darken => make_shader(device, &mut composer, &shader_defs, "blend/darken.wgsl", include_str!("../shaders/blend/darken.wgsl")),
-            ComplexBlend::Difference => make_shader(device, &mut composer, &shader_defs, "blend/difference.wgsl", include_str!("../shaders/blend/difference.wgsl")),
-            ComplexBlend::Invert => make_shader(device, &mut composer, &shader_defs, "blend/invert.wgsl", include_str!("../shaders/blend/invert.wgsl")),
-            ComplexBlend::Alpha => make_shader(device, &mut composer, &shader_defs, "blend/alpha.wgsl", include_str!("../shaders/blend/alpha.wgsl")),
-            ComplexBlend::Erase => make_shader(device, &mut composer, &shader_defs, "blend/erase.wgsl", include_str!("../shaders/blend/erase.wgsl")),
-            ComplexBlend::Overlay => make_shader(device, &mut composer, &shader_defs, "blend/overlay.wgsl", include_str!("../shaders/blend/overlay.wgsl")),
-            ComplexBlend::HardLight => make_shader(device, &mut composer, &shader_defs, "blend/hardlight.wgsl", include_str!("../shaders/blend/hardlight.wgsl")),
+            ComplexBlend::Multiply => make_shader(device, "blend/multiply.wgsl", include_str!("../shaders/blend/multiply.wgsl")),
+            ComplexBlend::Lighten => make_shader(device, "blend/lighten.wgsl", include_str!("../shaders/blend/lighten.wgsl")),
+            ComplexBlend::Darken => make_shader(device, "blend/darken.wgsl", include_str!("../shaders/blend/darken.wgsl")),
+            ComplexBlend::Difference => make_shader(device, "blend/difference.wgsl", include_str!("../shaders/blend/difference.wgsl")),
+            ComplexBlend::Invert => make_shader(device, "blend/invert.wgsl", include_str!("../shaders/blend/invert.wgsl")),
+            ComplexBlend::Alpha => make_shader(device, "blend/alpha.wgsl", include_str!("../shaders/blend/alpha.wgsl")),
+            ComplexBlend::Erase => make_shader(device, "blend/erase.wgsl", include_str!("../shaders/blend/erase.wgsl")),
+            ComplexBlend::Overlay => make_shader(device, "blend/overlay.wgsl", include_str!("../shaders/blend/overlay.wgsl")),
+            ComplexBlend::HardLight => make_shader(device, "blend/hardlight.wgsl", include_str!("../shaders/blend/hardlight.wgsl")),
         };
 
         Self {
@@ -144,48 +104,16 @@ impl Shaders {
     }
 }
 
-fn composer() -> Result<Composer, ComposerError> {
-    let mut composer = Composer::default();
-    // [NA] Hack to get all capabilities since nobody exposes this type easily
-    let capabilities = composer.capabilities;
-    composer = composer.with_capabilities(!capabilities);
-    composer.add_composable_module(ComposableModuleDescriptor {
-        source: include_str!("../shaders/common.wgsl"),
-        file_path: "common.wgsl",
-        ..Default::default()
-    })?;
-    composer.add_composable_module(ComposableModuleDescriptor {
-        source: ruffle_render::shader_source::SHADER_FILTER_COMMON,
-        file_path: "shader_filter_common.wgsl",
-        ..Default::default()
-    })?;
-    Ok(composer)
-}
-
-fn make_shader(
-    device: &wgpu::Device,
-    composer: &mut Composer,
-    shader_defs: &HashMap<String, ShaderDefValue>,
-    name: &str,
-    source: &'static str,
-) -> wgpu::ShaderModule {
+fn make_shader(device: &wgpu::Device, name: &str, source: &str) -> wgpu::ShaderModule {
+    let common = include_str!("../shaders/common.wgsl");
     device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: create_debug_label!("Shader {}", name).as_deref(),
-        source: wgpu::ShaderSource::Naga(Cow::Owned(
-            composer
-                .make_naga_module(NagaModuleDescriptor {
-                    source,
-                    file_path: name,
-                    shader_defs: shader_defs.clone(),
-                    ..Default::default()
-                })
-                .unwrap_or_else(|e| {
-                    panic!(
-                        "{name} failed to compile:\n{}\n{:#?}",
-                        e.emit_to_string(composer),
-                        e
-                    )
-                }),
-        )),
+        source: wgpu::ShaderSource::Wgsl(format!("{}\n{}", common, source).into()),
+    })
+}
+fn make_filter_shader(device: &wgpu::Device, name: &str, source: &str) -> wgpu::ShaderModule {
+    device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: create_debug_label!("Shader {}", name).as_deref(),
+        source: wgpu::ShaderSource::Wgsl(format!("{}\n{}", SHADER_FILTER_COMMON, source).into()),
     })
 }
