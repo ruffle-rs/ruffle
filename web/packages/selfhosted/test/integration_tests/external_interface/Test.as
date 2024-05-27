30aad921f7c677cf77c035ca8bf1467432e5e4b6
Nathan Adams <dinnerbone@dinnerbone.com>
2024-05-28 00:10:00 +0200

    web: Add ExternalInterface tests (some skipped for not working)

---
+++ web/packages/selfhosted/test/integration_tests/external_interface/Test.as
@@ -0,0 +1,128 @@
+﻿package  {
+	import flash.display.MovieClip;
+	import flash.external.ExternalInterface;
+	import flash.utils.getQualifiedClassName;
+	import flash.utils.setTimeout;
+	
+	
+	public class Test extends MovieClip {
+		public function Test() {
+			log("ExternalInterface.available: " + repr(ExternalInterface.available));
+			log("ExternalInterface.objectID: " + repr(ExternalInterface.objectID));
+			
+			try {
+				ExternalInterface.addCallback("callMethodImmediately", function(name: String) {
+					log("callMethodImmediately called with " + arguments.length + " argument" + (arguments.length == 1 ? "" : "s"));
+					log("  " + repr(arguments, "  "));
+					try {
+						log("  call(" + name + ", ...) = " + repr(ExternalInterface.call.apply(null, arguments)));
+					} catch (e) {
+						log("  call(" + name + ", ...) = " + e);
+					}
+				});
+				ExternalInterface.addCallback("callMethodWithDelay", function(name: String) {
+					log("callMethodWithDelay called with " + arguments.length + " argument" + (arguments.length == 1 ? "" : "s"));
+					log("  " + repr(arguments, "  "));
+					var args = arguments;
+					setTimeout(function() {
+						try {
+							log("  call(" + name + ", ...) = " + repr(ExternalInterface.call.apply(null, args)));
+						} catch (e) {
+							log("  call(" + name + ", ...) = " + e);
+						}
+					}, 1);
+				});
+				ExternalInterface.addCallback("log", function() {
+					log("log called with " + arguments.length + " argument" + (arguments.length == 1 ? "" : "s"));
+					log("  " + repr(arguments, "  "));
+				});
+				ExternalInterface.addCallback("returnAValue", function(value: *) {
+					log("returnAValue called with " + repr(value));
+					log("  " + repr(arguments, "  "));
+					return value;
+				});
+				ExternalInterface.addCallback("throwAnException", function() {
+					log("throwAnException called");
+					throw new ArgumentError("Custom Argument Error!", 123);
+				});
+				ExternalInterface.addCallback("setMarshallExceptions", function(value: Boolean) {
+					log("setMarshallExceptions called with " + repr(value));
+					ExternalInterface.marshallExceptions = value;
+				});
+			} catch (e) {
+				log("Error adding callbacks: " + e);
+			}
+		}
+
+		function log(value: *) {
+			trace(value);
+			result.text += value + "\n";
+		}
+		
+		function repr(value: *, indent: String = "  ") {
+			if (value === undefined || value === null || value === true || value === false || value is Number) {
+				return value;
+			} else if (value is String) {
+				return escapeString(value);
+			} else if (value is Array) {
+				if (value.length == 0) {
+					return "[]";
+				} else {
+					var result = "[\n";
+					var nextIndent = indent + "  ";
+					for (var i = 0; i < value.length; i++) {
+						result += nextIndent + repr(value[i], indent + nextIndent) + "\n";
+					}
+					return result + indent + "]";
+				}
+			} else {
+				var keys = [];
+				for (var key in value) {
+					keys.push(key);
+				}
+				keys.sort();
+				
+				var result = getQualifiedClassName(value) + " {";
+				
+				if (keys.length == 0) {
+					return result + "}";
+				} else {
+					result += "\n";
+					var nextIndent = indent + "  ";
+					for (var i = 0; i < keys.length; i++) {
+						result += nextIndent + keys[i] + " = " + repr(value[keys[i]], nextIndent) + "\n";
+					}
+					return result + indent + "}";
+				}
+			}
+		}
+		
+		function escapeString(input: String): String {
+			var output:String = "\"";
+			for (var i:int = 0; i < input.length; i++) {
+				var char:String = input.charAt(i);
+				switch (char) {
+					case "\\":
+						output += "\\\\";
+						break;
+					case "\"":
+						output += "\\\"";
+						break;
+					case "\n":
+						output += "\\n";
+						break;
+					case "\r":
+						output += "\\r";
+						break;
+					case "\t":
+						output += "\\t";
+						break;
+					default:
+						output += char;
+				}
+			}
+			return output + "\"";
+		}
+	}
+	
+}
---
+++ web/packages/selfhosted/test/integration_tests/external_interface/test.ts
@@ -0,0 +1,273 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import {
+    getTraceOutput,
+    injectRuffleAndWait,
+    openTest,
+    playAndMonitor,
+} from "../../utils.js";
+import { expect, use } from "chai";
+import chaiHtml from "chai-html";
+
+use(chaiHtml);
+
+interface RuffleTest {
+    set(...args: any[]): string;
+
+    get(): any[];
+
+    called(): boolean;
+
+    log(...args: any[]): void;
+}
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace WebdriverIO {
+        interface Element {
+            // ExternalInterface makes whatever functions exist "magically" - so let's just declare here for type's sake
+
+            // Logs all arguments to trace
+            log: (...args: unknown[]) => void;
+
+            // Calls `ExternalInterface.call(name, ...args)` after a delay (to avoid reentrancy)
+            callMethodWithDelay: (name: string, ...args: unknown[]) => void;
+
+            // Calls `ExternalInterface.call(name, ...args)` immediately (causing reentrancy)
+            callMethodImmediately: (name: string, ...args: unknown[]) => void;
+
+            // Returns `value`
+            returnAValue: <T>(value: T) => T;
+
+            // Should return an exception...
+            throwAnException: void;
+
+            // Calls `ExternalInterface.marshallExceptions = value`
+            setMarshallExceptions: (value: boolean) => void;
+        }
+    }
+
+    interface Window {
+        RuffleTest: RuffleTest;
+    }
+}
+
+async function getCalledValue(browser: WebdriverIO.Browser) {
+    await browser.waitUntil(
+        async () => await browser.execute(() => window.RuffleTest.called()),
+    );
+    return await browser.execute(() => window.RuffleTest.get());
+}
+
+describe("ExternalInterface", () => {
+    it("loads the test", async () => {
+        await openTest(browser, "integration_tests/external_interface");
+        await injectRuffleAndWait(browser);
+        const player = await browser.$("<ruffle-object>");
+        await playAndMonitor(
+            browser,
+            player,
+            `ExternalInterface.available: true
+ExternalInterface.objectID: "flash_name"
+`,
+        );
+
+        // Set up callbacks
+        await browser.execute(() => {
+            let values: any[] = [];
+            let called = false;
+            window.RuffleTest = {
+                set: function (...args) {
+                    const wasCalled = called;
+                    values = args;
+                    called = true;
+                    return wasCalled ? "already called!" : "success!";
+                },
+                get: function () {
+                    called = false;
+                    return values;
+                },
+                called: function () {
+                    return called;
+                },
+                log: function (...args: any[]) {
+                    (
+                        document.getElementById(
+                            "flash_id",
+                        ) as unknown as WebdriverIO.Element
+                    ).log(...args);
+                },
+            };
+        });
+    });
+
+    it("responds to 'log'", async () => {
+        const player = await browser.$("<ruffle-object>");
+        await browser.execute(
+            (player) =>
+                player.log("Hello world!", {
+                    key: "value",
+                    nested: ["a", "b"],
+                }),
+            player,
+        );
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `log called with 2 arguments
+  [
+    "Hello world!"
+    Object {
+        key = "value"
+        nested = [
+          "a"
+          "b"
+        ]
+      }
+  ]
+`,
+        );
+    });
+
+    it("returns a value", async () => {
+        const player = await browser.$("<ruffle-object>");
+        const returned = await browser.execute(
+            (player) => player.returnAValue(123.4),
+            player,
+        );
+
+        expect(returned).to.eql(123.4);
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `returnAValue called with 123.4
+  [
+    123.4
+  ]
+`,
+        );
+    });
+
+    it("calls a method with delay", async () => {
+        const player = await browser.$("<ruffle-object>");
+        await browser.execute(
+            (player) =>
+                player.callMethodWithDelay("window.RuffleTest.set", true),
+            player,
+        );
+        const actualValue = await getCalledValue(browser);
+        expect(actualValue).to.eql([true]);
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `callMethodWithDelay called with 2 arguments
+  [
+    "window.RuffleTest.set"
+    true
+  ]
+  call(window.RuffleTest.set, ...) = "success!"
+`,
+        );
+    });
+
+    // [NA] Broken on Ruffle at time of writing
+    it.skip("calls a reentrant JS method", async () => {
+        // JS -> Flash -> JS within one call
+        const player = await browser.$("<ruffle-object>");
+        const actualValue = await browser.execute((player) => {
+            player.callMethodImmediately("window.RuffleTest.set", {
+                nested: { object: { complex: true } },
+            });
+            return window.RuffleTest.get();
+        }, player);
+
+        expect(actualValue).to.eql({
+            nested: { object: { complex: true } },
+        });
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `callMethodImmediately called with 2 arguments
+  [
+    "window.RuffleTest.set"
+    Object {
+        nested = Object {
+          object = Object {
+            complex = true
+          }
+        }
+      }
+  ]
+  call(window.RuffleTest.set, ...) = "success!"
+`,
+        );
+    });
+
+    it("calls a reentrant Flash method", async () => {
+        // Flash -> JS -> Flash within one call
+        const player = await browser.$("<ruffle-object>");
+        await browser.execute((player) => {
+            player.callMethodWithDelay("window.RuffleTest.log", "Reentrant!");
+        }, player);
+
+        // [NA] Because of the delay, if we fetch immediately we *may* just get part of the log.
+        await browser.pause(200);
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `callMethodWithDelay called with 2 arguments
+  [
+    "window.RuffleTest.log"
+    "Reentrant!"
+  ]
+log called with 1 argument
+  [
+    "Reentrant!"
+  ]
+  call(window.RuffleTest.log, ...) = undefined
+`,
+        );
+    });
+
+    // [NA] Broken on Ruffle at time of writing
+    it.skip("supports a JS function as name", async () => {
+        const player = await browser.$("<ruffle-object>");
+        await browser.execute((player) => {
+            player.callMethodWithDelay(
+                "function(name){window.RuffleTest.set(name)}",
+                "test",
+            );
+        }, player);
+
+        const actualValue = await getCalledValue(browser);
+        expect(actualValue).to.eql(["test"]);
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `callMethodWithDelay called with 2 arguments
+  [
+    "function(name){window.RuffleTest.set(name)}"
+    "test"
+  ]
+  call(function(name){window.RuffleTest.set(name)}, ...) = "success!"
+`,
+        );
+    });
+
+    it("supports calling a method that doesn't exist", async () => {
+        const player = await browser.$("<ruffle-object>");
+        await browser.execute((player) => {
+            player.callMethodWithDelay("does.not.exist");
+        }, player);
+
+        const actualOutput = await getTraceOutput(browser, player);
+        expect(actualOutput).to.eql(
+            `callMethodWithDelay called with 1 argument
+  [
+    "does.not.exist"
+  ]
+  call(does.not.exist, ...) = undefined
+`,
+        );
+    });
+});
