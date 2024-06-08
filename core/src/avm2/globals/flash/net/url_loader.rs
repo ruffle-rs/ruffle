7a1ee0e109b5265162f86b136b2b012ab96e34b0
Lord-McSweeney <84632019+Lord-McSweeney@users.noreply.github.com>
2024-06-08 14:33:57 -0700

    core: `URLLoader` should check the data format after loading finishes, not when starting a load

--- core/src/avm2/globals/flash/net/url_loader.rs
+++ core/src/avm2/globals/flash/net/url_loader.rs
@@ -2,11 +2,9 @@
 
 use crate::avm2::activation::Activation;
 use crate::avm2::globals::flash::display::loader::request_from_url_request;
-use crate::avm2::object::TObject;
 use crate::avm2::parameters::ParametersExt;
 use crate::avm2::value::Value;
 use crate::avm2::{Error, Object};
-use crate::loader::DataFormat;
 
 /// Native function definition for `URLLoader.load`
 pub fn load<'gc>(
@@ -16,31 +14,13 @@ pub fn load<'gc>(
 ) -> Result<Value<'gc>, Error<'gc>> {
     let request = args.get_object(activation, 0, "request")?;
 
-    let data_format = this
-        .get_public_property("dataFormat", activation)?
-        .coerce_to_string(activation)?;
-
-    // FIXME: Flash checks the data format after load finishes, before interpreting the data-
-    // we check the data format before initializing the load
-    let data_format = if &data_format == b"binary" {
-        DataFormat::Binary
-    } else if &data_format == b"text" {
-        DataFormat::Text
-    } else if &data_format == b"variables" {
-        DataFormat::Variables
-    } else {
-        tracing::warn!("Unknown URLLoaderDataFormat: {}", data_format);
-        DataFormat::Text
-    };
-
-    spawn_fetch(activation, this, request, data_format)
+    spawn_fetch(activation, this, request)
 }
 
 fn spawn_fetch<'gc>(
     activation: &mut Activation<'_, 'gc>,
     loader_object: Object<'gc>,
     url_request: Object<'gc>,
-    data_format: DataFormat,
 ) -> Result<Value<'gc>, Error<'gc>> {
     let request = request_from_url_request(activation, url_request)?;
 
@@ -48,7 +28,6 @@ fn spawn_fetch<'gc>(
         activation.context.player.clone(),
         loader_object,
         request,
-        data_format,
     );
     activation.context.navigator.spawn_future(future);
     Ok(Value::Undefined)
