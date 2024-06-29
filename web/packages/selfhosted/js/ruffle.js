79055442965181f99108796dff41e42fad56cd08
Nathan Adams <dinnerbone@dinnerbone.com>
2024-06-29 16:30:02 +0200

    web: Add onFirstLoad option to installRuffle; use it to configure webpack in selfhosted

--- web/packages/selfhosted/js/ruffle.js
+++ web/packages/selfhosted/js/ruffle.js
@@ -1,3 +1,52 @@
+// eslint-disable-next-line no-unused-vars
+/* global __webpack_public_path__:writable */
+
 import { installRuffle } from "ruffle-core";
 
-installRuffle("local");
+let currentScriptURL = null;
+
+try {
+    if (
+        document.currentScript !== undefined &&
+        document.currentScript !== null &&
+        "src" in document.currentScript &&
+        document.currentScript.src !== ""
+    ) {
+        let src = document.currentScript.src;
+
+        // CDNs allow omitting the filename. If it's omitted, append a slash to
+        // prevent the last component from being dropped.
+        if (!src.endsWith(".js") && !src.endsWith("/")) {
+            src += "/";
+        }
+
+        currentScriptURL = new URL(".", src);
+    }
+} catch (e) {
+    console.warn("Unable to get currentScript URL");
+}
+
+function publicPath(config) {
+    // Default to the directory where this script resides.
+    let path = currentScriptURL?.href ?? "";
+    if (
+        "publicPath" in config &&
+        config.publicPath !== null &&
+        config.publicPath !== undefined
+    ) {
+        path = config.publicPath;
+    }
+
+    // Webpack expects the paths to end with a slash.
+    if (path !== "" && !path.endsWith("/")) {
+        path += "/";
+    }
+
+    return path;
+}
+
+installRuffle("local", {
+    onFirstLoad: () => {
+        __webpack_public_path__ = publicPath(window.RufflePlayer?.config);
+    },
+});
