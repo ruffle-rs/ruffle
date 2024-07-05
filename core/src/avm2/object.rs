525c32fd7bac85f0eb5ac39ce320dc4f5e1d98b9
Lord-McSweeney <Lord-McSweeney@github.com>
2024-07-05 13:06:13 +0300

    avm2: Use `Gc` instead of `GcCell` in `PrimitiveObject`

--- core/src/avm2/object.rs
+++ core/src/avm2/object.rs
@@ -1378,7 +1378,7 @@ impl<'gc> Object<'gc> {
         match self {
             Self::ScriptObject(o) => WeakObject::ScriptObject(ScriptObjectWeak(GcCell::downgrade(o.0))),
             Self::FunctionObject(o) => WeakObject::FunctionObject(FunctionObjectWeak(GcCell::downgrade(o.0))),
-            Self::PrimitiveObject(o) => WeakObject::PrimitiveObject(PrimitiveObjectWeak(GcCell::downgrade(o.0))),
+            Self::PrimitiveObject(o) => WeakObject::PrimitiveObject(PrimitiveObjectWeak(Gc::downgrade(o.0))),
             Self::NamespaceObject(o) => WeakObject::NamespaceObject(NamespaceObjectWeak(Gc::downgrade(o.0))),
             Self::ArrayObject(o) => WeakObject::ArrayObject(ArrayObjectWeak(Gc::downgrade(o.0))),
             Self::StageObject(o) => WeakObject::StageObject(StageObjectWeak(Gc::downgrade(o.0))),
--- core/src/avm2/object/primitive_object.rs
+++ core/src/avm2/object/primitive_object.rs
@@ -9,20 +9,21 @@ use crate::avm2::object::{ClassObject, Object, ObjectPtr, TObject};
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use crate::string::AvmString;
-use gc_arena::{Collect, GcCell, GcWeakCell, Mutation};
+use gc_arena::barrier::unlock;
+use gc_arena::{lock::RefLock, Collect, Gc, GcWeak, Mutation};
 
 /// A class instance allocator that allocates primitive objects.
 pub fn primitive_allocator<'gc>(
     class: ClassObject<'gc>,
     activation: &mut Activation<'_, 'gc>,
 ) -> Result<Object<'gc>, Error<'gc>> {
-    let base = ScriptObjectData::new(class);
+    let base = ScriptObjectData::new(class).into();
 
-    Ok(PrimitiveObject(GcCell::new(
+    Ok(PrimitiveObject(Gc::new(
         activation.context.gc_context,
         PrimitiveObjectData {
             base,
-            primitive: Value::Undefined,
+            primitive: RefLock::new(Value::Undefined),
         },
     ))
     .into())
@@ -31,16 +32,16 @@ pub fn primitive_allocator<'gc>(
 /// An Object which represents a primitive value of some other kind.
 #[derive(Collect, Clone, Copy)]
 #[collect(no_drop)]
-pub struct PrimitiveObject<'gc>(pub GcCell<'gc, PrimitiveObjectData<'gc>>);
+pub struct PrimitiveObject<'gc>(pub Gc<'gc, PrimitiveObjectData<'gc>>);
 
 #[derive(Collect, Clone, Copy, Debug)]
 #[collect(no_drop)]
-pub struct PrimitiveObjectWeak<'gc>(pub GcWeakCell<'gc, PrimitiveObjectData<'gc>>);
+pub struct PrimitiveObjectWeak<'gc>(pub GcWeak<'gc, PrimitiveObjectData<'gc>>);
 
 impl fmt::Debug for PrimitiveObject<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("PrimitiveObject")
-            .field("ptr", &self.0.as_ptr())
+            .field("ptr", &Gc::as_ptr(self.0))
             .finish()
     }
 }
@@ -49,10 +50,10 @@ impl fmt::Debug for PrimitiveObject<'_> {
 #[collect(no_drop)]
 pub struct PrimitiveObjectData<'gc> {
     /// All normal script data.
-    base: ScriptObjectData<'gc>,
+    base: RefLock<ScriptObjectData<'gc>>,
 
     /// The primitive value this object represents.
-    primitive: Value<'gc>,
+    primitive: RefLock<Value<'gc>>,
 }
 
 impl<'gc> PrimitiveObject<'gc> {
@@ -85,10 +86,13 @@ impl<'gc> PrimitiveObject<'gc> {
             _ => unreachable!(),
         };
 
-        let base = ScriptObjectData::new(class);
-        let this: Object<'gc> = PrimitiveObject(GcCell::new(
+        let base = ScriptObjectData::new(class).into();
+        let this: Object<'gc> = PrimitiveObject(Gc::new(
             activation.context.gc_context,
-            PrimitiveObjectData { base, primitive },
+            PrimitiveObjectData {
+                base,
+                primitive: RefLock::new(primitive),
+            },
         ))
         .into();
         this.install_instance_slots(activation.context.gc_context);
@@ -104,22 +108,22 @@ impl<'gc> PrimitiveObject<'gc> {
 
 impl<'gc> TObject<'gc> for PrimitiveObject<'gc> {
     fn base(&self) -> Ref<ScriptObjectData<'gc>> {
-        Ref::map(self.0.read(), |read| &read.base)
+        self.0.base.borrow()
     }
 
     fn base_mut(&self, mc: &Mutation<'gc>) -> RefMut<ScriptObjectData<'gc>> {
-        RefMut::map(self.0.write(mc), |write| &mut write.base)
+        unlock!(Gc::write(mc, self.0), PrimitiveObjectData, base).borrow_mut()
     }
 
     fn as_ptr(&self) -> *const ObjectPtr {
-        self.0.as_ptr() as *const ObjectPtr
+        Gc::as_ptr(self.0) as *const ObjectPtr
     }
 
     fn to_locale_string(
         &self,
         activation: &mut Activation<'_, 'gc>,
     ) -> Result<Value<'gc>, Error<'gc>> {
-        match self.0.read().primitive {
+        match *self.0.primitive.borrow() {
             val @ Value::Integer(_) => Ok(val),
             _ => {
                 let class_name = self.instance_class().name().local_name();
@@ -134,14 +138,14 @@ impl<'gc> TObject<'gc> for PrimitiveObject<'gc> {
     }
 
     fn value_of(&self, _mc: &Mutation<'gc>) -> Result<Value<'gc>, Error<'gc>> {
-        Ok(self.0.read().primitive)
-    }
-
-    fn as_primitive_mut(&self, mc: &Mutation<'gc>) -> Option<RefMut<Value<'gc>>> {
-        Some(RefMut::map(self.0.write(mc), |pod| &mut pod.primitive))
+        Ok(*self.0.primitive.borrow())
     }
 
     fn as_primitive(&self) -> Option<Ref<Value<'gc>>> {
-        Some(Ref::map(self.0.read(), |pod| &pod.primitive))
+        Some(self.0.primitive.borrow())
+    }
+
+    fn as_primitive_mut(&self, mc: &Mutation<'gc>) -> Option<RefMut<Value<'gc>>> {
+        Some(unlock!(Gc::write(mc, self.0), PrimitiveObjectData, primitive).borrow_mut())
     }
 }
