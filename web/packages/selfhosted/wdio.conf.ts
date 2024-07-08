c353a463ab3686b5c16cb17d2792193453249fec
Nathan Adams <dinnerbone@dinnerbone.com>
2024-07-08 14:27:29 +0200

    web: Start splitting up wdio config to enable different browsers or environments

--- web/packages/selfhosted/wdio.conf.ts
+++ web/packages/selfhosted/wdio.conf.ts
@@ -1,4 +1,37 @@
-import type { Options } from "@wdio/types";
+import type { Options, Services } from "@wdio/types";
+
+const capabilities: WebdriverIO.Capabilities[] = [];
+const services: Services.ServiceEntry[] = [];
+
+const headless = process.argv.includes("--headless");
+const chrome = process.argv.includes("--chrome");
+
+if (chrome) {
+    const args = ["--disable-gpu"];
+    if (headless) {
+        args.push("--headless");
+    }
+    capabilities.push({
+        "wdio:maxInstances": 1,
+        browserName: "chrome",
+        "goog:chromeOptions": {
+            args,
+        },
+    });
+    services.push("chromedriver");
+}
+
+services.push([
+    "static-server",
+    {
+        folders: [
+            { mount: "/dist", path: "./dist" },
+            { mount: "/test_assets", path: "./test_assets" },
+            { mount: "/test", path: "./test" },
+        ],
+        port: 4567,
+    },
+]);
 
 export const config: Options.Testrunner = {
     runner: "local",
@@ -8,35 +41,14 @@ export const config: Options.Testrunner = {
         "./test/integration_tests/**/test.ts",
     ],
     maxInstances: 10,
-    capabilities: [
-        {
-            "wdio:maxInstances": 5,
-            browserName: "chrome",
-            "goog:chromeOptions": {
-                args: ["--headless", "--disable-gpu"],
-            },
-        },
-    ],
+    capabilities,
     logLevel: "info",
     bail: 0,
     baseUrl: "http://localhost",
     waitforTimeout: 30000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: [
-        "chromedriver",
-        [
-            "static-server",
-            {
-                folders: [
-                    { mount: "/dist", path: "./dist" },
-                    { mount: "/test_assets", path: "./test_assets" },
-                    { mount: "/test", path: "./test" },
-                ],
-                port: 4567,
-            },
-        ],
-    ],
+    services,
     framework: "mocha",
     reporters: ["spec"],
     mochaOpts: {
