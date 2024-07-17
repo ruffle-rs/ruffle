c0f7ceb9a5c008d422f6cc423ddd2389566aff78
Daniel Jacobs <danielhunterjacobs@gmail.com>
2024-07-17 19:32:27 -0400

    web: Split shadow template into components

---
+++ web/packages/core/src/internal/ui/save-manager.tsx
@@ -0,0 +1,13 @@
+export function SaveManager() {
+    return (
+        <div id="save-manager" class="modal hidden">
+            <div id="modal-area" class="modal-area">
+                <span class="close-modal"></span>
+                    <div class="general-save-options">
+                        <span class="modal-button"></span>
+                    </div>
+                    <table id="local-saves"></table>
+            </div>
+        </div>
+    );
+}
