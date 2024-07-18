a327e469c6eb484fc974eb1950bd220fa9c14174
TÖRÖK Attila <torokati44@gmail.com>
2024-07-18 04:09:07 +0200

    web: Remove now unused `copyToAudioBuffer`

--- web/packages/core/src/ruffle-imports.ts
+++ web/packages/core/src/ruffle-imports.ts
@@ -6,28 +6,6 @@
  */
 
 /**
- * Copies data into the given audio channel.
- * This is necessary because Safari does not support `AudioBuffer.copyToChannel`.
- *
- * @internal
- */
-export function copyToAudioBuffer(
-    audioBuffer: AudioBuffer,
-    leftData: ArrayLike<number>,
-    rightData: ArrayLike<number>,
-): void {
-    if (leftData) {
-        const dstBuffer = audioBuffer.getChannelData(0);
-        dstBuffer.set(leftData);
-    }
-
-    if (rightData) {
-        const dstBuffer = audioBuffer.getChannelData(1);
-        dstBuffer.set(rightData);
-    }
-}
-
-/**
  * Returns the estimated output timestamp for the audio context.
  * This is necessary because web-sys does not export `AudioContext.baseLatency`.
  *
