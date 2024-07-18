e708ec823fd1ba9a0bb77f3476024f24d0adc253
Daniel Jacobs <danielhunterjacobs@gmail.com>
2024-07-18 11:04:00 -0400

    web: Add more type definitions and comment about type checking oddities

--- web/packages/core/src/globals.d.ts
+++ web/packages/core/src/globals.d.ts
@@ -1,6 +1,10 @@
 interface Error {
     avmStack?: string;
 }
+// Just updating this module seems to disable type checking for tsx-dom
+// See https://github.com/Lusito/tsx-dom/issues/22#issuecomment-2236710966
+// Because of that, all the other changes are unneeded, but they're what I think
+// should be used if this worked properly
 module "tsx-dom-types" {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface HTMLAttributes {
@@ -17,16 +21,34 @@ interface SVGSVGElement {
     scale?: string | number;
 }
 interface SVGPathElement {
+    xmlns?: string;
     fill?: string;
     stroke?: string;
     d?: string;
 }
 interface SVGTextElement {
+    xmlns?: string;
     fill?: string;
     stroke?: string;
 }
 interface SVGCircleElement {
+    xmlns?: string;
     fill?: string;
+}
+interface SVGDefsElement {
+    xmlns?: string;
+}
+interface SVGGElement {
+    xmlns?: string;
+}
+interface SVGLinearGradientElement {
+    xmlns?: string;
+}
+interface SVGStopElement {
+    xmlns?: string;
+}
+interface SVGUseElement {
+    xmlns?: string;
 }
 interface HTMLInputElement {
     autocapitalize?: string;
