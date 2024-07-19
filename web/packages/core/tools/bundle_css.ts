1c8e95a061a40ccd4b2fdca30a66eb7cc2fbe12d
Nathan Adams <dinnerbone@dinnerbone.com>
2024-07-19 17:43:10 +0200

    web: Use nesting in css

--- web/packages/core/tools/bundle_css.ts
+++ web/packages/core/tools/bundle_css.ts
@@ -2,12 +2,15 @@ import { replaceInFileSync } from "replace-in-file";
 import fs from "fs";
 import postcss from "postcss";
 import cssnanoPlugin from "cssnano";
+// @ts-expect-error TS7016 We don't care about the types, if it doesn't work that's fine
+import postcssNesting from "postcss-nesting";
 
 const originalCss = fs
     .readFileSync("src/internal/ui/static-styles.css", "utf8")
     .replaceAll("\r", "");
 
 const processor = postcss([
+    postcssNesting,
     cssnanoPlugin({
         preset: ["advanced", { autoprefixer: { add: true } }],
     }),
