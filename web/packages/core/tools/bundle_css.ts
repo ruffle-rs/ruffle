50de5d09d2283306d02eee20e8a91b1babe21a56
Nathan Adams <dinnerbone@dinnerbone.com>
2024-07-19 17:26:44 +0200

    web: Run cssnano over static css

--- web/packages/core/tools/bundle_css.ts
+++ web/packages/core/tools/bundle_css.ts
@@ -1,12 +1,23 @@
 import { replaceInFileSync } from "replace-in-file";
 import fs from "fs";
+import postcss from "postcss";
+import cssnanoPlugin from "cssnano";
 
-const css = fs
+const originalCss = fs
     .readFileSync("src/internal/ui/static-styles.css", "utf8")
     .replaceAll("\r", "");
 
-replaceInFileSync({
-    files: "dist/**",
-    from: [/"\s*\/\*\s*%STATIC_STYLES_CSS%\s*\*\/\s*"/g],
-    to: [JSON.stringify(css)],
-});
+const processor = postcss([
+    cssnanoPlugin({
+        preset: ["advanced", { autoprefixer: { add: true } }],
+    }),
+]);
+processor
+    .process(originalCss, { from: "src/internal/ui/static-styles.css" })
+    .then((result) => {
+        replaceInFileSync({
+            files: "dist/**",
+            from: [/"\s*\/\*\s*%STATIC_STYLES_CSS%\s*\*\/\s*"/g],
+            to: [JSON.stringify(result.css)],
+        });
+    });
