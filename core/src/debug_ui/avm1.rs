d8150e3b08226a1c0e7140b1a469350d60db1e7a
crumblingstatue <radiantstatue@gmail.com>
2024-07-30 21:12:55 +0200

    Add support for editing text values

--- core/src/debug_ui/avm1.rs
+++ core/src/debug_ui/avm1.rs
@@ -4,7 +4,7 @@ use crate::debug_ui::display_object::open_display_object_button;
 use crate::debug_ui::handle::{AVM1ObjectHandle, DisplayObjectHandle};
 use crate::debug_ui::Message;
 use crate::string::AvmString;
-use egui::{Grid, Id, TextEdit, Ui, Window};
+use egui::{Grid, Id, TextBuffer, TextEdit, Ui, Window};
 
 #[derive(Debug, Default)]
 pub struct Avm1ObjectWindow {
@@ -99,7 +99,9 @@ impl Avm1ObjectWindow {
                 return self.num_edit_ui(ui, key, value).map(Value::Number);
             }
             Ok(Value::String(value)) => {
-                TextEdit::singleline(&mut value.to_string()).show(ui);
+                return self
+                    .string_edit_ui(ui, key, value)
+                    .map(|string| Value::String(AvmString::new_utf8(activation.gc(), string)));
             }
             Ok(Value::Object(value)) => {
                 if value.as_executable().is_some() {
@@ -150,18 +152,18 @@ impl Avm1ObjectWindow {
                 match self.value_edit_buf.parse::<f64>() {
                     Ok(num) => {
                         if ui.input(|inp| inp.key_pressed(egui::Key::Enter))
-                            || ui.button("✔").on_hover_text("Set").clicked()
+                            || ui.set_button().clicked()
                         {
                             new_val = Some(num);
                             self.edited_key = None;
                         }
                     }
                     Err(e) => {
-                        ui.add_enabled(false, egui::Button::new("✔"))
+                        ui.add_enabled(false, egui::Button::new(CHECKMARK_ICON))
                             .on_disabled_hover_text(e.to_string());
                     }
                 }
-                if ui.button("🗙").on_hover_text("Cancel").clicked() {
+                if ui.cancel_button().clicked() {
                     self.edited_key = None;
                 }
             });
@@ -169,7 +171,7 @@ impl Avm1ObjectWindow {
             ui.horizontal(|ui| {
                 let num_str = num.to_string();
                 ui.label(&num_str);
-                if ui.button("✏").on_hover_text("Edit").clicked() {
+                if ui.edit_button().clicked() {
                     self.edited_key = Some(key.to_utf8_lossy().into_owned());
                     self.value_edit_buf = num_str;
                     self.focus_text_edit = true;
@@ -177,6 +179,64 @@ impl Avm1ObjectWindow {
             });
         }
         new_val
+    }
+    fn string_edit_ui(
+        &mut self,
+        ui: &mut Ui,
+        key: &AvmString,
+        string: AvmString,
+    ) -> Option<String> {
+        let mut new_val = None;
+        ui.horizontal(|ui| {
+            if self
+                .edited_key
+                .as_ref()
+                .is_some_and(|edit_key| *edit_key == key.to_utf8_lossy())
+            {
+                let re = ui.add(TextEdit::singleline(&mut self.value_edit_buf).desired_width(96.0));
+                if self.focus_text_edit {
+                    re.request_focus();
+                    self.focus_text_edit = false;
+                }
+                if ui.set_button().clicked() {
+                    new_val = Some(self.value_edit_buf.take());
+                    self.edited_key = None;
+                }
+                if ui.cancel_button().clicked() {
+                    self.edited_key = None;
+                }
+            } else {
+                ui.label(string.to_utf8_lossy());
+                if ui.edit_button().clicked() {
+                    self.value_edit_buf = string.to_string();
+                    self.edited_key = Some(key.to_string());
+                    self.focus_text_edit = true;
+                }
+            }
+        });
+        new_val
+    }
+}
+
+const PENCIL_ICON: &str = "✏";
+const CHECKMARK_ICON: &str = "✔";
+const CANCEL_ICON: &str = "🗙";
+
+trait UiExt {
+    fn edit_button(&mut self) -> egui::Response;
+    fn set_button(&mut self) -> egui::Response;
+    fn cancel_button(&mut self) -> egui::Response;
+}
+
+impl UiExt for egui::Ui {
+    fn edit_button(&mut self) -> egui::Response {
+        self.button(PENCIL_ICON).on_hover_text("Edit")
+    }
+    fn set_button(&mut self) -> egui::Response {
+        self.button(CHECKMARK_ICON).on_hover_text("Set")
+    }
+    fn cancel_button(&mut self) -> egui::Response {
+        self.button(CANCEL_ICON).on_hover_text("Cancel")
     }
 }
 
