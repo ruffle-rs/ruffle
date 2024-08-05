ed81f9aded1514f03a4612b3fcb61bf128a9db27
Kornelius Rohrschneider <Korne127@gmx.net>
2024-08-05 16:07:41 +0200

    desktop: Improve desktop packaging structure
    The desktop packaging structure has been improved. All files that are
    only related to packaging the desktop application (e.g. metadata files)
    have been moved from the assets and packages folders into a new
    packaging folder. The folder contains a subfolder for each operating
    system.
    
    All links to any of the moved packaging files in the GitHub workflow,
    build.rs and other packaging files have been updated accordingly.

--- desktop/build.rs
+++ desktop/build.rs
@@ -14,8 +14,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Embed resource file w/ icon on windows
     // To allow for cross-compilation, this must not be behind cfg(windows)!
-    println!("cargo:rerun-if-changed=assets/ruffle_desktop.rc");
-    embed_resource::compile("assets/ruffle_desktop.rc", embed_resource::NONE);
+    println!("cargo:rerun-if-changed=packaging/Windows/ruffle_desktop.rc");
+    embed_resource::compile("packaging/Windows/ruffle_desktop.rc", embed_resource::NONE);
 
     println!("cargo:rerun-if-env-changed=CFG_RELEASE_CHANNEL");
     let channel = channel();
