19956df66e7ea57b5f9d1044d42c07cfe781ccb5
Kornelius Rohrschneider <Korne127@gmx.net>
2024-08-07 15:13:03 +0200

    desktop: Add DMG creation to the macOS build workflow
    The creation of a DMG file has been added to the macOS build workflow.
    It replaces the tar.gz file for macOS and is created and uploaded for
    each release. dmgbuild is used to create it.
    A background image for the DMG file has been added as well.
    
    Additionally, some wording has been improved.

---
+++ desktop/packaging/macOS/settings.py
@@ -0,0 +1,10 @@
+format = "UDZO"
+filesystem = "HFS+"
+files = [("package/Ruffle.app", "Ruffle.app")]
+symlinks = {"Applications": "/Applications"}
+icon_locations = {
+	"Ruffle.app": (160, 300),
+	"Applications": (480, 300)
+}
+background = "desktop/packaging/macOS/DMG Background.tiff"
+window_rect = ((0, 1_000_000), (640, 480))
