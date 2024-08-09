43fe96434a552708519d9251cc5fd5a95a436da7
Nathan Adams <dinnerbone@dinnerbone.com>
2024-08-09 14:46:50 +0200

    desktop: Update tracy and bump profiling

---
+++ desktop/src/tracy.rs
@@ -0,0 +1,20 @@
+use tracing::Metadata;
+use tracing_subscriber::fmt::format::DefaultFields;
+use tracing_tracy::Config;
+
+#[derive(Default)]
+pub struct RuffleTracyConfig(DefaultFields);
+
+impl Config for RuffleTracyConfig {
+    type Formatter = DefaultFields;
+
+    fn formatter(&self) -> &Self::Formatter {
+        &self.0
+    }
+
+    fn stack_depth(&self, _metadata: &Metadata<'_>) -> u16 {
+        // How much, if any, of the stack trace to capture for each event
+        // Obviously, this adds overhead
+        0
+    }
+}
