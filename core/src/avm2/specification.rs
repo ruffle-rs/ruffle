571bb2b2be8b0c80d06164ffea506e648281a195
TÖRÖK Attila <torokati44@gmail.com>
2024-09-06 12:41:27 +0200

    core: fix: Revert an accidental condition negation in AVM2 stub reporting

--- core/src/avm2/specification.rs
+++ core/src/avm2/specification.rs
@@ -370,7 +370,7 @@ impl Definition {
     ) {
         for class_trait in traits {
             if !class_trait.name().namespace().is_public()
-                && class_trait
+                && !class_trait
                     .name()
                     .namespace()
                     .exact_version_match(avm2.as3_namespace)
