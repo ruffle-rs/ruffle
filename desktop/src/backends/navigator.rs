a958591a613ca935b40fd70e033d2449cee06625
Kamil Jarosz <kjarosh256@gmail.com>
2024-09-11 23:36:03 +0200

    desktop: Implement asking for permission before accessing files
    This implements the filesystem access mode setting and its three options.
    When set to Ask, the filesystem access dialog will be shown to the user.

--- desktop/src/backends/navigator.rs
+++ desktop/src/backends/navigator.rs
@@ -3,13 +3,17 @@ use ruffle_frontend_utils::backends::navigator::NavigatorInterface;
 use std::fs::File;
 use std::io;
 use std::io::ErrorKind;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use tokio::sync::oneshot;
 use url::Url;
 use winit::event_loop::EventLoopProxy;
 
+use crate::cli::FilesystemAccessMode;
 use crate::custom_event::RuffleEvent;
+use crate::gui::dialogs::filesystem_access_dialog::{
+    FilesystemAccessDialogConfiguration, FilesystemAccessDialogResult,
+};
 use crate::gui::dialogs::network_access_dialog::{
     NetworkAccessDialogConfiguration, NetworkAccessDialogResult,
 };
@@ -20,13 +24,45 @@ use crate::util::open_url;
 pub struct DesktopNavigatorInterface {
     // Arc + Mutex due to macOS
     event_loop: Arc<Mutex<EventLoopProxy<RuffleEvent>>>,
+
+    filesystem_access_mode: FilesystemAccessMode,
+
+    // TODO Make this more generic, maybe a manager?
+    allowed_paths: Arc<Mutex<Vec<PathBuf>>>,
 }
 
 impl DesktopNavigatorInterface {
-    pub fn new(event_loop: EventLoopProxy<RuffleEvent>) -> Self {
+    pub fn new(
+        event_loop: EventLoopProxy<RuffleEvent>,
+        movie_path: Option<PathBuf>,
+        filesystem_access_mode: FilesystemAccessMode,
+    ) -> Self {
         Self {
             event_loop: Arc::new(Mutex::new(event_loop)),
+            allowed_paths: Arc::new(Mutex::new(if let Some(movie_path) = movie_path {
+                vec![movie_path]
+            } else {
+                Vec::new()
+            })),
+            filesystem_access_mode,
         }
+    }
+
+    async fn ask_for_filesystem_access(&self, path: &Path) -> bool {
+        let (notifier, receiver) = oneshot::channel();
+        let _ = self
+            .event_loop
+            .lock()
+            .expect("Non-poisoned event loop")
+            .send_event(RuffleEvent::OpenDialog(DialogDescriptor::FilesystemAccess(
+                FilesystemAccessDialogConfiguration::new(
+                    notifier,
+                    self.allowed_paths.clone(),
+                    path.to_path_buf(),
+                ),
+            )));
+
+        receiver.await == Ok(FilesystemAccessDialogResult::Allow)
     }
 }
 
@@ -45,6 +81,18 @@ impl NavigatorInterface for DesktopNavigatorInterface {
     }
 
     async fn open_file(&self, path: &Path) -> io::Result<File> {
+        let path = &path.canonicalize()?;
+
+        let allow = match self.filesystem_access_mode {
+            FilesystemAccessMode::Allow => true,
+            FilesystemAccessMode::Deny => false,
+            FilesystemAccessMode::Ask => self.ask_for_filesystem_access(path).await,
+        };
+
+        if !allow {
+            return Err(ErrorKind::PermissionDenied.into());
+        }
+
         File::open(path).or_else(|e| {
             if cfg!(feature = "sandbox") {
                 use rfd::FileDialog;
