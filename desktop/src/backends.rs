1111f8cac3444380f6ade4916b1d6036cb3f04e5
Mads Marquart <mads@marquart.dk>
2024-09-14 05:21:18 +0200

    desktop: Move cpal audio backend to frontend-utils

--- desktop/src/backends.rs
+++ desktop/src/backends.rs
@@ -1,10 +1,8 @@
-mod audio;
 mod external_interface;
 mod fscommand;
 mod navigator;
 mod ui;
 
-pub use audio::CpalAudioBackend;
 pub use external_interface::DesktopExternalInterfaceProvider;
 pub use fscommand::DesktopFSCommandProvider;
 pub use navigator::DesktopNavigatorInterface;
