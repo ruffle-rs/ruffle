7c76f2144f998b70a0d9bc8c899750456102255e
Nathan Adams <dinnerbone@dinnerbone.com>
2024-10-03 01:20:45 +0200

    web: Document the modules

--- web/packages/core/src/index.ts
+++ web/packages/core/src/index.ts
@@ -1,10 +1,7 @@
-/**
- * This is the public API of the web version of Ruffle.
- *
- * Types should only be exported here if they are intended to be part of that public API, not internal.
- */
-
 export * as Setup from "./public/setup";
+
 export * as Config from "./public/config";
+
 export * as Player from "./public/player";
+
 export * from "./build-info";
--- web/packages/core/src/public/player/player-element.ts
+++ web/packages/core/src/public/player/player-element.ts
@@ -4,7 +4,7 @@ import { FlashAPI } from "./flash";
 /**
  * A Ruffle player's HTML element.
  *
- * This is either created through `window.RufflePlayer.createPlayer()`, or polyfilled from a `<embed>`/`<object>` tag.
+ * This is either created through `window.RufflePlayer.latest().createPlayer()`, or polyfilled from a `<embed>`/`<object>` tag.
  *
  * In addition to usual HTML attributes, this player contains methods and properties that belong to both
  * the **Flash JS API** and **legacy Ruffle API**s.
