4dd996b5ac063c74e7170ff0f6bf6f51a2491388
Nathan Adams <dinnerbone@dinnerbone.com>
2024-10-02 17:50:06 +0200

    web: Don't export polyfills, utils or version APIs, they're internal

--- web/packages/core/src/index.ts
+++ web/packages/core/src/index.ts
@@ -4,14 +4,10 @@
  * Types should only be exported here if they are intended to be part of that public API, not internal.
  */
 
-export * from "./polyfills";
 export * from "./public-api";
-export * from "./version";
-export * from "./version-range";
 export * from "./config";
 export * from "./load-options";
 export * from "./build-info";
-export * from "./swf-utils";
 export * from "./movie-metadata";
 export * from "./install";
 export * from "./public/flash";
