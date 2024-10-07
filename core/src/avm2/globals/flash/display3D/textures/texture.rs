92079e622f1024b60898773c9849b7f3ae537db0
Lord-McSweeney <Lord-McSweeney@github.com>
2024-10-06 21:51:31 -0700

    avm2: Remove three more uses of `coerce_to_object`, used in Stage3D texture handling

--- core/src/avm2/globals/flash/display3D/textures/texture.rs
+++ core/src/avm2/globals/flash/display3D/textures/texture.rs
@@ -117,7 +117,9 @@ pub fn upload_from_bitmap_data<'gc>(
     args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
     if let Some(texture) = this.as_texture() {
-        if let Some(source) = args[0].coerce_to_object(activation)?.as_bitmap_data() {
+        let source_obj = args.get_object(activation, 0, "source")?;
+
+        if let Some(source) = source_obj.as_bitmap_data() {
             let mip_level = args[1].coerce_to_u32(activation)?;
             if mip_level == 0 {
                 texture.context3d().copy_bitmapdata_to_texture(
