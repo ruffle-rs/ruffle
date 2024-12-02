858fcebc81c780e1b3ab9900eb54a62e76000b4b
kcz <1732074+cookie-s@users.noreply.github.com>
2024-12-01 19:07:43 -0500

    avm2: Implement Matrix3D with 2D support only
    Replace stubs for flash.geom.Transform.matrix3D getter/setter with an
    actual implementation of Matrix3D with limited support.
    
    This implementation is just a proxy to the existing 2D matrix
    implementation. Therefore transformations beyond 2D transformation work
    differently from the expected result.

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -8,6 +8,7 @@ pub mod error;
 pub mod filters;
 pub mod lines;
 pub mod matrix;
+pub mod matrix3d;
 pub mod pixel_bender;
 // The `renderdoc` crate doesn't compile on apple platforms
 #[cfg(all(feature = "renderdoc", not(target_vendor = "apple")))]
