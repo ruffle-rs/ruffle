75c6e72f453bd927b9561bdb673a5a721a1e7408
Kamil Jarosz <kjarosh256@gmail.com>
2024-12-04 14:21:35 +0100

    avm2: Make getCharBoundaries() return null for 0-width characters

--- core/src/avm2/globals/flash/text/text_field.rs
+++ core/src/avm2/globals/flash/text/text_field.rs
@@ -1790,6 +1790,10 @@ pub fn get_char_boundaries<'gc>(
         return Ok(Value::Null);
     };
 
+    if bounds.width() == swf::Twips::ZERO {
+        return Ok(Value::Null);
+    }
+
     let rect = activation
         .avm2()
         .classes()
