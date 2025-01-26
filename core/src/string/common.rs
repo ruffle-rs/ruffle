b98f27257131f025d256f8ff1411626794db9d64
Lord-McSweeney <Lord-McSweeney@github.com>
2025-01-26 11:09:51 -0800

    avm2: Use interned strings in Value code

--- core/src/string/common.rs
+++ core/src/string/common.rs
@@ -1,28 +1,48 @@
-use super::{AvmAtom, AvmStringInterner, AvmStringRepr, WStr};
+use super::{AvmString, AvmStringInterner, AvmStringRepr, WStr};
 use gc_arena::{Gc, Mutation};
 
+#[allow(non_snake_case)]
 pub struct CommonStrings<'gc> {
-    pub str_x: AvmAtom<'gc>,
-    pub str_y: AvmAtom<'gc>,
-    pub str_width: AvmAtom<'gc>,
-    pub str_height: AvmAtom<'gc>,
+    pub str_false: AvmString<'gc>,
+    pub str_height: AvmString<'gc>,
+    pub str_Infinity: AvmString<'gc>,
+    pub str_NaN: AvmString<'gc>,
+    pub str_null: AvmString<'gc>,
+    pub str_toString: AvmString<'gc>,
+    pub str_true: AvmString<'gc>,
+    pub str_undefined: AvmString<'gc>,
+    pub str_valueOf: AvmString<'gc>,
+    pub str_width: AvmString<'gc>,
+    pub str_x: AvmString<'gc>,
+    pub str_y: AvmString<'gc>,
 }
 
 impl<'gc> CommonStrings<'gc> {
     pub fn new(mc: &Mutation<'gc>, interner: &mut AvmStringInterner<'gc>) -> Self {
         let mut intern_from_static = |s: &'static [u8]| {
             let wstr = WStr::from_units(s);
-            interner.intern_inner(mc, wstr, |wstr| {
+
+            let atom = interner.intern_inner(mc, wstr, |wstr| {
                 let repr = AvmStringRepr::from_raw_static(wstr, true);
                 Gc::new(mc, repr)
-            })
+            });
+
+            atom.into()
         };
 
         Self {
+            str_false: intern_from_static(b"false"),
+            str_height: intern_from_static(b"height"),
+            str_Infinity: intern_from_static(b"Infinity"),
+            str_NaN: intern_from_static(b"NaN"),
+            str_null: intern_from_static(b"null"),
+            str_toString: intern_from_static(b"toString"),
+            str_true: intern_from_static(b"true"),
+            str_undefined: intern_from_static(b"undefined"),
+            str_valueOf: intern_from_static(b"valueOf"),
+            str_width: intern_from_static(b"width"),
             str_x: intern_from_static(b"x"),
             str_y: intern_from_static(b"y"),
-            str_width: intern_from_static(b"width"),
-            str_height: intern_from_static(b"height"),
         }
     }
 }
