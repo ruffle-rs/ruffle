17575113019a82c5b07559421a60d42a4884266a
Kamil Jarosz <kjarosh256@gmail.com>
2025-01-31 21:12:41 +0100

    core: Move parse_font_list from avm2 to core
    This function is useful both in AVM1 and AVM2.

--- core/src/avm2/globals/flash/text/style_sheet.rs
+++ core/src/avm2/globals/flash/text/style_sheet.rs
@@ -3,7 +3,7 @@ use crate::avm2::parameters::ParametersExt;
 use crate::avm2::{Activation, Error, Value};
 use crate::html::{transform_dashes_to_camel_case, CssStream};
 use crate::string::AvmString;
-use ruffle_wstr::{WStr, WString};
+use ruffle_wstr::WStr;
 
 pub use crate::avm2::object::style_sheet_allocator;
 
@@ -62,46 +62,11 @@ pub fn inner_parse_font_family<'gc>(
     args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
     let input = args.get_string(activation, 0)?;
-    let mut result = WString::new();
-
-    let mut pos = 0;
-    while pos < input.len() {
-        // Skip whitespace
-        while input.get(pos) == Some(' ' as u16) {
-            pos += 1;
-        }
-
-        // Find the whole value
-        let start = pos;
-        while input.get(pos) != Some(',' as u16) && pos < input.len() {
-            pos += 1;
-        }
-
-        let mut value = &input[start..pos];
-
-        if pos < input.len() {
-            pos += 1; // move past the comma
-        }
-
-        // Transform some names
-        if value == b"mono" {
-            value = WStr::from_units(b"_typewriter");
-        } else if value == b"sans-serif" {
-            value = WStr::from_units(b"_sans");
-        } else if value == b"serif" {
-            value = WStr::from_units(b"_serif");
-        }
-
-        // Add it to the result (without any extra space)
-        if !value.is_empty() {
-            if !result.is_empty() {
-                result.push_char(',');
-            }
-            result.push_str(value);
-        }
-    }
-
-    Ok(Value::String(AvmString::new(activation.gc(), result)))
+    let parsed_font_list = crate::html::parse_font_list(input.as_wstr());
+    Ok(Value::String(AvmString::new(
+        activation.gc(),
+        parsed_font_list,
+    )))
 }
 
 pub fn clear_internal<'gc>(
