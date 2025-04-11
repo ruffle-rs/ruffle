614069187d74a68bfa19c9027c564ddd97426a90
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2025-04-11 22:08:39 +0200

    avm2: Rename BasicBlock->Block

--- core/src/avm2/optimizer/blocks.rs
+++ core/src/avm2/optimizer/blocks.rs
@@ -3,19 +3,22 @@ use crate::avm2::op::Op;
 use std::cell::Cell;
 use std::collections::{HashMap, HashSet};
 
+// Represents non-overlapping slices of ops with
+// a single entry point and many exit points.
+// (as opposed to basic blocks, which have 1 entry and exit point)
 #[derive(Debug)]
-pub struct BasicBlock<'a, 'gc> {
+pub struct Block<'a, 'gc> {
     // The ops making up this block.
     pub ops: &'a [Cell<Op<'gc>>],
 
-    // The index of the first op making up this BasicBlock.
+    // The index of the first op making up this Block.
     pub start_index: usize,
 }
 
 pub fn assemble_blocks<'a, 'gc>(
     code: &'a [Cell<Op<'gc>>],
     jump_targets: &HashSet<usize>,
-) -> (Vec<BasicBlock<'a, 'gc>>, HashMap<usize, usize>) {
+) -> (Vec<Block<'a, 'gc>>, HashMap<usize, usize>) {
     let mut block_list = Vec::with_capacity(2);
     let mut current_block_start = 0;
 
@@ -31,7 +34,7 @@ pub fn assemble_blocks<'a, 'gc>(
         ) || jump_targets.contains(&(i + 1))
         // The next op is a jump target
         {
-            let block = BasicBlock {
+            let block = Block {
                 start_index: current_block_start,
                 ops: &code[current_block_start..i + 1],
             };
