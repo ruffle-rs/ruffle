ruffle-rs/ruffle

commit d9b881289d0c7ba03216b4e6238e014fb2b5ae68

avm1: Migrate StageObject to a native object

core/src/avm1/object/stage_object.rs
@@ -4,16 +4,15 @@ use crate::avm1::activation::Activation;
 use crate::avm1::clamp::Clamp;
 use crate::avm1::error::Error;
 use crate::avm1::property_map::PropertyMap;
-use crate::avm1::{Object, ObjectPtr, ScriptObject, TObject, Value};
+use crate::avm1::Value;
 use crate::avm_warn;
 use crate::display_object::{
     DisplayObject, MovieClip, TDisplayObject, TDisplayObjectContainer, TInteractiveObject,
 };
 use crate::string::{AvmString, StringContext, WStr};
 use crate::types::Percent;
-use gc_arena::{Collect, Gc, GcWeak};
+use gc_arena::Collect;
 use ruffle_macros::istr;
-use std::fmt;
 use swf::Twips;
 
 pub fn get_property<'gc>(
@@ -66,11 +65,11 @@ pub fn get_property<'gc>(
 /// Notify any bound text fields of a property change.
 ///
 /// This should be called every time a property is set on a AVM1 object.
-pub fn notify_property_change<'a, 'gc>(
+pub fn notify_property_change<'gc>(
     dobj: DisplayObject<'gc>,
     property_name: AvmString<'gc>,
     value: Value<'gc>,
-    activation: &mut Activation<'a, 'gc>,
+    activation: &mut Activation<'_, 'gc>,
 ) -> Result<(), Error<'gc>> {
     // Check if a text field is bound to this property and update the text if so.
     let case_sensitive = activation.is_case_sensitive();
@@ -90,13 +89,11 @@ pub fn notify_property_change<'a, 'gc>(
     Ok(())
 }
 
-pub fn has_own_property<'gc>(
+pub fn has_display_object_property<'gc>(
     dobj: DisplayObject<'gc>,
     activation: &mut Activation<'_, 'gc>,
     name: AvmString<'gc>,
 ) -> bool {
-    // Note that `hasOwnProperty` does NOT return true for child display objects.
-
     let magic_property = name.starts_with(b'_');
     if magic_property
         && activation
@@ -194,142 +191,6 @@ fn parse_level_id(digits: &WStr) -> i32 {
         level_id = level_id.wrapping_neg();
     }
     level_id
-}
-
-
-/// A ScriptObject that is inherently tied to a display node.
-#[derive(Clone, Copy, Collect)]
-#[collect(no_drop)]
-pub struct StageObject<'gc>(Gc<'gc, StageObjectData<'gc>>);
-
-#[derive(Collect)]
-#[collect(no_drop)]
-pub struct StageObjectData<'gc> {
-    /// The underlying script object.
-    ///
-    /// This is used to handle "expando properties" on AVM1 display nodes, as
-    /// well as the underlying prototype chain.
-    base: ScriptObject<'gc>,
-
-    /// The display node this stage object
-    pub display_object: DisplayObject<'gc>,
-}
-
-impl<'gc> StageObject<'gc> {
-    /// Create a weak reference to the underlying data of this `StageObject`
-    pub fn as_weak(self) -> GcWeak<'gc, StageObjectData<'gc>> {
-        Gc::downgrade(self.0)
-    }
-
-    /// Create a stage object for a given display node.
-    pub fn for_display_object(
-        context: &StringContext<'gc>,
-        display_object: DisplayObject<'gc>,
-        proto: Object<'gc>,
-    ) -> Self {
-        Self(Gc::new(
-            context.gc(),
-            StageObjectData {
-                base: ScriptObject::new(context, Some(proto)),
-                display_object,
-            },
-        ))
-    }
-}
-
-impl fmt::Debug for StageObject<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("StageObject")
-            .field("ptr", &Gc::as_ptr(self.0))
-            .field("display_object", &self.0.display_object)
-            .finish()
-    }
-}
-
-impl<'gc> TObject<'gc> for StageObject<'gc> {
-    fn raw_script_object(&self) -> ScriptObject<'gc> {
-        self.0.base
-    }
-
-    fn get_local_stored(
-        &self,
-        name: impl Into<AvmString<'gc>>,
-        activation: &mut Activation<'_, 'gc>,
-        is_slash_path: bool,
-    ) -> Option<Value<'gc>> {
-        let name = name.into();
-
-        if let Some(value) = self
-            .0
-            .base
-            .get_local_stored(name, activation, is_slash_path)
-        {
-            return Some(value);
-        }
-
-        let dobj = self.0.display_object;
-        get_property(dobj, name, activation, is_slash_path)
-    }
-
-    fn set_local(
-        &self,
-        name: AvmString<'gc>,
-        value: Value<'gc>,
-        activation: &mut Activation<'_, 'gc>,
-        this: Object<'gc>,
-    ) -> Result<(), Error<'gc>> {
-        let base = self.0.base;
-        let dobj = self.0.display_object;
-
-        notify_property_change(dobj, name, value, activation)?;
-
-        if base.has_own_property(activation, name) {
-            // 1) Actual properties on the underlying object
-            base.set_local(name, value, activation, this)
-        } else if let Some(property) = activation
-            .context
-            .avm1
-            .display_properties()
-            .get_by_name(name)
-        {
-            // 2) Display object properties such as _x, _y
-            property.set(activation, dobj, value)
-        } else {
-            base.set_local(name, value, activation, this)
-        }
-    }
-
-    fn has_property(&self, activation: &mut Activation<'_, 'gc>, name: AvmString<'gc>) -> bool {
-        let dobj = self.0.display_object;
-        if !dobj.avm1_removed() && self.0.base.has_property(activation, name) {
-            return true;
-        }
-
-        has_own_property(dobj, activation, name)
-    }
-
-    fn get_keys(
-        &self,
-        activation: &mut Activation<'_, 'gc>,
-        include_hidden: bool,
-    ) -> Vec<AvmString<'gc>> {
-        let mut keys = self.0.base.get_keys(activation, include_hidden);
-        enumerate_keys(self.0.display_object, &mut keys);
-        keys
-    }
-
-    /// Get the underlying stage object, if it exists.
-    fn as_stage_object(&self) -> Option<StageObject<'gc>> {
-        Some(*self)
-    }
-
-    fn as_display_object(&self) -> Option<DisplayObject<'gc>> {
-        Some(self.0.display_object)
-    }
-
-    fn as_ptr(&self) -> *const ObjectPtr {
-        self.0.base.as_ptr()
-    }
 }
 
 /// Properties shared by display objects in AVM1, such as _x and _y.

core/src/avm1.rs
@@ -32,7 +32,6 @@ pub use globals::array::ArrayBuilder;
 pub use globals::context_menu::make_context_menu_state;
 pub use globals::sound::start as start_sound;
 pub use object::script_object::ScriptObject;
-pub use object::stage_object::StageObject;
 pub use object::{NativeObject, Object, ObjectPtr, TObject};
 pub use property::Attribute;
 pub use property_map::PropertyMap;

core/src/avm1/activation.rs
@@ -582,7 +582,7 @@ impl<'a, 'gc> Activation<'a, 'gc> {
     }
 
     fn stack_push(&mut self, mut value: Value<'gc>) {
-        if let Value::Object(Object::StageObject(s)) = value {
+        if let Value::Object(obj) = value {
             // Note that there currently exists a subtle issue with this logic:
             // If the cached `Object` in a `MovieClipReference` becomes invalidated, causing it to switch back to path-based object resolution,
             // it should *never* switch back to cache-based resolution
@@ -591,7 +591,7 @@ impl<'a, 'gc> Activation<'a, 'gc> {
             // Fixing this will require a thorough refactor of AVM1 to store `Either<MovieClipReference, Object>
             // can refer to a MovieClip
             // There is a ignored test for this issue of "reference laundering" at "avm1/string_paths_reference_launder"
-            if let Some(mcr) = MovieClipReference::try_from_stage_object(self, s) {
+            if let Some(mcr) = MovieClipReference::try_from_stage_object(self, obj) {
                 value = Value::MovieClip(mcr);
             }
         }

core/src/avm1/globals/transform.rs
@@ -24,10 +24,7 @@ impl<'gc> TransformObject<'gc> {
         let clip = match args {
             // `Transform` constructor accepts exactly 1 argument.
             [Value::MovieClip(clip)] => Some(*clip),
-            [Value::Object(clip)] => {
-                let stage_object = clip.as_stage_object()?;
-                MovieClipReference::try_from_stage_object(activation, stage_object)
-            }
+            [Value::Object(clip)] => MovieClipReference::try_from_stage_object(activation, *clip),
             _ => return None,
         };
         Some(Self { clip })

core/src/avm1/object.rs
@@ -21,10 +21,11 @@ use crate::avm1::globals::transform::TransformObject;
 use crate::avm1::globals::xml::Xml;
 use crate::avm1::globals::xml_socket::XmlSocket;
 use crate::avm1::object::super_object::SuperObject;
-use crate::avm1::{Activation, Attribute, Error, ScriptObject, StageObject, Value};
+use crate::avm1::{Activation, Attribute, Error, ScriptObject, Value};
 use crate::bitmap::bitmap_data::BitmapDataWrapper;
-use crate::display_object::DisplayObject;
-use crate::display_object::TDisplayObject;
+use crate::display_object::{
+    Avm1Button, DisplayObject, EditText, MovieClip, TDisplayObject as _, Video,
+};
 use crate::html::TextFormat;
 use crate::streams::NetStream;
 use crate::string::AvmString;
@@ -58,6 +59,11 @@ pub enum NativeObject<'gc> {
     /// 'desynchronize' the "property view" and the "array view" (used by, e.g., `toString()`).
     Array(()),
     Function(Gc<'gc, FunctionObject<'gc>>),
+
+    MovieClip(MovieClip<'gc>),
+    Button(Avm1Button<'gc>),
+    EditText(EditText<'gc>),
+    Video(Video<'gc>),
 
     Date(Gc<'gc, Cell<Date>>),
     BlurFilter(BlurFilter<'gc>),
@@ -119,6 +125,18 @@ impl<'gc> BoxedF64<'gc> {
     }
 }
 
+impl<'gc> NativeObject<'gc> {
+    pub fn as_display_object(self) -> Option<DisplayObject<'gc>> {
+        match self {
+            Self::MovieClip(dobj) => Some(DisplayObject::MovieClip(dobj)),
+            Self::Button(dobj) => Some(DisplayObject::Avm1Button(dobj)),
+            Self::EditText(dobj) => Some(DisplayObject::EditText(dobj)),
+            Self::Video(dobj) => Some(DisplayObject::Video(dobj)),
+            _ => None,
+        }
+    }
+}
+
 /// Represents an object that can be directly interacted with by the AVM
 /// runtime.
 #[enum_trait_object(
@@ -127,7 +145,6 @@ impl<'gc> BoxedF64<'gc> {
     #[collect(no_drop)]
     pub enum Object<'gc> {
         ScriptObject(ScriptObject<'gc>),
-        StageObject(StageObject<'gc>),
         SuperObject(SuperObject<'gc>),
     }
 )]
@@ -323,10 +340,8 @@ pub trait TObject<'gc>: 'gc + Collect<'gc> + Into<Object<'gc>> + Clone + Copy {
     ) -> Result<Value<'gc>, Error<'gc>> {
         let this = (*self).into();
 
-        if let Some(s) = this.as_stage_object() {
-            let d_o = s.as_display_object().unwrap();
-
-            if d_o.avm1_removed() {
+        if let Some(dobj) = this.as_display_object() {
+            if dobj.avm1_removed() {
                 return Ok(Value::Undefined);
             }
         }
@@ -608,11 +623,6 @@ pub trait TObject<'gc>: 'gc + Collect<'gc> + Into<Object<'gc>> + Clone + Copy {
 
     fn set_native(&self, _gc_context: &Mutation<'gc>, _native: NativeObject<'gc>) {}
 
-    /// Get the underlying stage object, if it exists.
-    fn as_stage_object(&self) -> Option<StageObject<'gc>> {
-        None
-    }
-
     /// Get the underlying super object, if it exists.
     fn as_super_object(&self) -> Option<SuperObject<'gc>> {
         None
@@ -620,6 +630,11 @@ pub trait TObject<'gc>: 'gc + Collect<'gc> + Into<Object<'gc>> + Clone + Copy {
 
     /// Get the underlying display node for this object, if it exists.
     fn as_display_object(&self) -> Option<DisplayObject<'gc>> {
+        None
+    }
+
+    /// Get the underlying stage object, if it exists, but doesn't follow `super` objects.
+    fn as_display_object_no_super(&self) -> Option<DisplayObject<'gc>> {
         None
     }
 

core/src/avm1/object/script_object.rs
@@ -1,14 +1,15 @@
 use crate::avm1::activation::Activation;
 use crate::avm1::error::Error;
 use crate::avm1::function::{Executable, ExecutionName, ExecutionReason};
-use crate::avm1::object::NativeObject;
+use crate::avm1::object::{stage_object, NativeObject};
 use crate::avm1::property::{Attribute, Property};
 use crate::avm1::property_map::{Entry, PropertyMap};
 use crate::avm1::{Object, ObjectPtr, TObject, Value};
+use crate::display_object::{DisplayObject, TDisplayObject as _};
 use crate::ecma_conversions::f64_to_wrapping_i32;
 use crate::string::{AvmString, StringContext};
 use core::fmt;
-use gc_arena::{Collect, GcCell, Mutation};
+use gc_arena::{Collect, GcCell, GcWeakCell, Mutation};
 use ruffle_macros::istr;
 
 #[derive(Clone, Collect)]
@@ -52,9 +53,27 @@ impl<'gc> Watcher<'gc> {
 #[collect(no_drop)]
 pub struct ScriptObject<'gc>(GcCell<'gc, ScriptObjectData<'gc>>);
 
+#[derive(Copy, Clone, Collect)]
+#[collect(no_drop)]
+pub struct ScriptObjectWeak<'gc>(GcWeakCell<'gc, ScriptObjectData<'gc>>);
+
+impl<'gc> ScriptObjectWeak<'gc> {
+    pub fn upgrade(self, mc: &Mutation<'gc>) -> Option<ScriptObject<'gc>> {
+        self.0.upgrade(mc).map(ScriptObject)
+    }
+}
+
+impl fmt::Debug for ScriptObjectWeak<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ScriptObjectWeak")
+            .field("ptr", &self.0.as_ptr())
+            .finish()
+    }
+}
+
 #[derive(Collect)]
 #[collect(no_drop)]
-pub struct ScriptObjectData<'gc> {
+struct ScriptObjectData<'gc> {
     native: NativeObject<'gc>,
     properties: PropertyMap<'gc, Property<'gc>>,
     interfaces: Vec<Object<'gc>>,
@@ -70,6 +89,10 @@ impl fmt::Debug for ScriptObject<'_> {
 }
 
 impl<'gc> ScriptObject<'gc> {
+    pub fn as_weak(self) -> ScriptObjectWeak<'gc> {
+        ScriptObjectWeak(GcCell::downgrade(self.0))
+    }
+
     pub fn new(context: &StringContext<'gc>, proto: Option<Object<'gc>>) -> Self {
         let object = Self(GcCell::new(
             context.gc(),
@@ -89,6 +112,16 @@ impl<'gc> ScriptObject<'gc> {
             );
         }
         object
+    }
+
+    pub fn new_with_native(
+        context: &StringContext<'gc>,
+        proto: Option<Object<'gc>>,
+        native: NativeObject<'gc>,
+    ) -> Self {
+        let obj = Self::new(context, proto);
+        obj.set_native(context.gc(), native);
+        obj
     }
 
     // Creates a ScriptObject, without assigning any __proto__ property.
@@ -170,14 +203,20 @@ impl<'gc> TObject<'gc> for ScriptObject<'gc> {
         &self,
         name: impl Into<AvmString<'gc>>,
         activation: &mut Activation<'_, 'gc>,
-        _is_slash_path: bool,
+        is_slash_path: bool,
     ) -> Option<Value<'gc>> {
-        self.0
-            .read()
-            .properties
-            .get(name.into(), activation.is_case_sensitive())
+        let name = name.into();
+        let read = self.0.read();
+
+        read.properties
+            .get(name, activation.is_case_sensitive())
             .filter(|property| property.allow_swf_version(activation.swf_version()))
             .map(|property| property.data())
+            .or_else(|| {
+                read.native.as_display_object().and_then(|dobj| {
+                    stage_object::get_property(dobj, name, activation, is_slash_path)
+                })
+            })
     }
 
     /// Set a named property on the object.
@@ -188,7 +227,8 @@ impl<'gc> TObject<'gc> for ScriptObject<'gc> {
         activation: &mut Activation<'_, 'gc>,
         this: Object<'gc>,
     ) -> Result<(), Error<'gc>> {
-        if let NativeObject::Array(_) = self.native() {
+        let native = self.native();
+        if let NativeObject::Array(_) = native {
             if name == istr!("length") {
                 let new_length = value.coerce_to_i32(activation)?;
                 let old_length = self.get_data(istr!("length"), activation);
@@ -201,6 +241,20 @@ impl<'gc> TObject<'gc> for ScriptObject<'gc> {
                 let length = self.length(activation)?;
                 if index >= length {
                     self.set_length(activation, index.wrapping_add(1))?;
+                }
+            }
+        } else if let Some(dobj) = native.as_display_object() {
+            stage_object::notify_property_change(dobj, name, value, activation)?;
+            // 'magic' display object properties (such as _x, _y, etc) take
+            // priority over properties in prototypes.
+            if !self.has_own_property(activation, name) {
+                if let Some(property) = activation
+                    .context
+                    .avm1
+                    .display_properties()
+                    .get_by_name(name)
+                {
+                    return property.set(activation, dobj, value);
                 }
             }
         }
@@ -247,6 +301,14 @@ impl<'gc> TObject<'gc> for ScriptObject<'gc> {
         } else {
             None
         }
+    }
+
+    fn as_display_object_no_super(&self) -> Option<DisplayObject<'gc>> {
+        self.0.read().native.as_display_object()
+    }
+
+    fn as_display_object(&self) -> Option<DisplayObject<'gc>> {
+        self.0.read().native.as_display_object()
     }
 
     /// Call the underlying object.
@@ -465,17 +527,27 @@ impl<'gc> TObject<'gc> for ScriptObject<'gc> {
 
     /// Checks if the object has a given named property.
     fn has_property(&self, activation: &mut Activation<'_, 'gc>, name: AvmString<'gc>) -> bool {
-        self.has_own_property(activation, name)
-            || if let Value::Object(proto) = self.proto(activation) {
-                proto.has_property(activation, name)
-            } else {
-                false
+        let dobj = self.as_display_object_no_super();
+
+        // Normal property checks
+        if dobj.is_none_or(|o| !o.avm1_removed()) {
+            if self.has_own_property(activation, name) {
+                return true;
+            } else if let Value::Object(proto) = self.proto(activation) {
+                if proto.has_property(activation, name) {
+                    return true;
+                }
             }
+        }
+
+        // Fallback: display object properties
+        dobj.is_some_and(|o| stage_object::has_display_object_property(o, activation, name))
     }
 
     /// Checks if the object has a given named property on itself (and not,
     /// say, the object's prototype or superclass)
     fn has_own_property(&self, activation: &mut Activation<'_, 'gc>, name: AvmString<'gc>) -> bool {
+        // Note that `hasOwnProperty` does NOT return true for display object properties.
         self.0
             .read()
             .properties
@@ -526,13 +598,19 @@ impl<'gc> TObject<'gc> for ScriptObject<'gc> {
         );
 
         // Then our own keys.
-        out_keys.extend(self.0.read().properties.iter().filter_map(move |(k, p)| {
+        let read = self.0.read();
+        out_keys.extend(read.properties.iter().filter_map(move |(k, p)| {
             if include_hidden || p.is_enumerable() {
                 Some(k)
             } else {
                 None
             }
         }));
+
+        // Then display object keys.
+        if let Some(dobj) = read.native.as_display_object() {
+            stage_object::enumerate_keys(dobj, &mut out_keys);
+        }
 
         out_keys
     }

core/src/avm1/object_reference.rs
@@ -1,9 +1,9 @@
-use super::{object::stage_object::StageObjectData, Activation, Object, StageObject, TObject};
+use super::{object::script_object::ScriptObjectWeak, Activation, Object, TObject, Value};
 use crate::{
     display_object::{DisplayObject, TDisplayObject, TDisplayObjectContainer},
     string::{AvmString, WStr, WString},
 };
-use gc_arena::{lock::Lock, GcWeak};
+use gc_arena::lock::Lock;
 use gc_arena::{Collect, Gc, Mutation};
 use ruffle_macros::istr;
 
@@ -63,32 +63,33 @@ struct MovieClipReferenceData<'gc> {
     /// A weak reference to the target stage object that `path` points to
     /// This is used for fast-path resvoling when possible, as well as for re-generating `path` (in the case the target object is renamed)
     /// If this is `None` then we have previously missed the cache, due to the target object being removed and re-created, causing us to fallback to the slow path resolution
-    cached_stage_object: Lock<Option<GcWeak<'gc, StageObjectData<'gc>>>>,
+    cached_object: Lock<Option<ScriptObjectWeak<'gc>>>,
 }
 
 impl<'gc> MovieClipReference<'gc> {
     pub fn try_from_stage_object(
         activation: &mut Activation<'_, 'gc>,
-        stage_object: StageObject<'gc>,
+        object: Object<'gc>,
     ) -> Option<Self> {
-        // We can't use as_display_object + as_movie_clip here as we explicitly don't want to convert `SuperObjects`
-        let display_object = stage_object.as_display_object().unwrap();
+        // We can't use as_display_object here as we explicitly don't want to convert `SuperObjects`
+        let display_object = object.as_display_object_no_super()?;
         let (path, cached) = if let DisplayObject::MovieClip(mc) = display_object {
-            (mc.path(), stage_object)
+            (mc.path(), display_object.object())
         } else if activation.swf_version() <= 5 {
             let display_object = Self::process_swf5_references(activation, display_object)?;
-            let stage_object = display_object
-                .object()
-                .coerce_to_object(activation)
-                .as_stage_object()?;
-            (display_object.path(), stage_object)
+
+            (display_object.path(), display_object.object())
         } else {
+            return None;
+        };
+
+        let Value::Object(Object::ScriptObject(cached)) = cached else {
             return None;
         };
 
         let mc_ref = MovieClipReferenceData {
             path: MovieClipPath::new_from_path(activation.gc(), path),
-            cached_stage_object: Some(cached.as_weak()).into(),
+            cached_object: Some(cached.as_weak()).into(),
         };
         Some(Self(Gc::new(activation.gc(), mc_ref)))
     }
@@ -119,13 +120,16 @@ impl<'gc> MovieClipReference<'gc> {
         self,
         activation: &mut Activation<'_, 'gc>,
     ) -> Option<(bool, Object<'gc>, DisplayObject<'gc>)> {
-        // Check if we have a cache we can use
-        if let Some(cache) = self.0.cached_stage_object.get() {
-            // Check if we can re-use the cached `DisplayObject`, if we can then take this fast path
-            if let Some(mc) = cache.upgrade(activation.gc()) {
+        // Check if we can re-use the cached `DisplayObject`, if we can then take this fast path
+        if let Some(cache) = self
+            .0
+            .cached_object
+            .get()
+            .and_then(|c| c.upgrade(activation.gc()))
+        {
+            if let Some(display_object) = cache.as_display_object_no_super() {
                 // We have to fallback to manual path-walking if the object is removed
-                if !mc.display_object.avm1_removed() {
-                    let display_object = mc.display_object;
+                if !display_object.avm1_removed() {
                     let display_object = Self::process_swf5_references(activation, display_object)?;
 
                     // Note that there is a bug here but this *is* how it works in Flash:
@@ -133,17 +137,13 @@ impl<'gc> MovieClipReference<'gc> {
                     // However, if we remove and re-create the clip, the stored path (the original path) will differ from the path of the cached object (the modified path)
                     // Essentially, changes to `_name` are reverted after the clip is re-created
 
-                    return Some((
-                        true,
-                        display_object.object().coerce_to_object(activation),
-                        display_object,
-                    ));
+                    return Some((true, cache.into(), display_object));
                 }
             }
         }
 
         // We missed the cache, switch to always use the slow-path
-        self.0.cached_stage_object.take();
+        self.0.cached_object.take();
 
         // Either the GcWeak ref is gone, or the clip can't be used (not on stage etc)
         // Here we manually parse the path, in order to find the target display object

core/src/avm1/scope.rs
@@ -148,16 +148,15 @@ impl<'gc> Scope<'gc> {
 
             // If we failed to find the value in the scope chain, but it *would* resolve on `self.locals()` if it wasn't
             // a removed clip, then try resolving on root instead
-            if let (CallableValue::UnCallable(Value::Undefined), Object::StageObject(s)) =
-                (&res, self.locals())
+            if top_level
+                && matches!(res, CallableValue::UnCallable(Value::Undefined))
+                && self.locals().has_own_property(activation, name)
             {
-                if top_level && s.raw_script_object().has_property(activation, name) {
-                    return activation
-                        .root_object()
-                        .coerce_to_object(activation)
-                        .get_non_slash_path(name, activation)
-                        .map(|v| CallableValue::Callable(self.locals_cell(), v));
-                }
+                return activation
+                    .root_object()
+                    .coerce_to_object(activation)
+                    .get_non_slash_path(name, activation)
+                    .map(|v| CallableValue::Callable(self.locals_cell(), v));
             }
 
             Ok(res)
@@ -178,11 +177,11 @@ impl<'gc> Scope<'gc> {
         value: Value<'gc>,
         activation: &mut Activation<'_, 'gc>,
     ) -> Result<(), Error<'gc>> {
-        let removed = if let Some(s) = self.values.as_stage_object() {
-            s.as_display_object().unwrap().avm1_removed()
-        } else {
-            false
-        };
+        let removed = self
+            .values
+            .as_display_object()
+            .map(|o| o.avm1_removed())
+            .unwrap_or_default();
 
         if !removed
             && (self.class == ScopeClass::Target || self.locals().has_property(activation, name))

commit fba7bbde8bdff5bdab10089d63213d61b790fd42

avm1: avoid a double-borrow error in `stage_object::notify_property_change`
Fixes #20080, #20104, etc... (regression introduced by #20027)

core/src/avm1/object/stage_object.rs
@@ -13,6 +13,7 @@ use crate::string::{AvmString, StringContext, WStr};
 use crate::types::Percent;
 use gc_arena::Collect;
 use ruffle_macros::istr;
+use smallvec::SmallVec;
 use swf::Twips;
 
 pub fn get_property<'gc>(
@@ -71,6 +72,9 @@ pub fn notify_property_change<'gc>(
     value: Value<'gc>,
     activation: &mut Activation<'_, 'gc>,
 ) -> Result<(), Error<'gc>> {
+    // Temporary vec to avoid double-borrow errors.
+    let mut text_fields = SmallVec::<[_; 1]>::new();
+
     // Check if a text field is bound to this property and update the text if so.
     let case_sensitive = activation.is_case_sensitive();
     if let Some(bindings) = dobj.avm1_text_field_bindings() {
@@ -81,11 +85,14 @@ pub fn notify_property_change<'gc>(
                 binding.variable_name.eq_ignore_case(&property_name)
             }
         }) {
-            binding
-                .text_field
-                .set_html_text(&value.coerce_to_string(activation)?, activation.context);
+            text_fields.push(binding.text_field);
         }
     }
+
+    for tf in text_fields {
+        tf.set_html_text(&value.coerce_to_string(activation)?, activation.context);
+    }
+
     Ok(())
 }
 