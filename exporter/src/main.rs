390b22eff6ea48e16d72d3a8dbf4dc412783fec8
AeroXuk <AeroXuk@users.noreply.github.com>
2025-05-30 17:14:28 +0100

    exporter: Add --force-play option
    Force play forces the main movie clip to play and bypasses "Click to play" buttons, etc.

--- exporter/src/main.rs
+++ exporter/src/main.rs
@@ -3,8 +3,10 @@ use clap::Parser;
 use image::RgbaImage;
 use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
+use ruffle_core::focus_tracker::TDisplayObject;
 use ruffle_core::limits::ExecutionLimit;
 use ruffle_core::tag_utils::SwfMovie;
+use ruffle_core::Player;
 use ruffle_core::PlayerBuilder;
 use ruffle_render_wgpu::backend::{request_adapter_and_device, WgpuRenderBackend};
 use ruffle_render_wgpu::clap::{GraphicsBackend, PowerPreference};
@@ -17,6 +19,7 @@ use std::io::{self, Write};
 use std::panic::catch_unwind;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
+use std::sync::Mutex;
 use walkdir::{DirEntry, WalkDir};
 
 #[derive(Parser, Debug, Copy, Clone)]
@@ -64,6 +67,12 @@ struct Opt {
     #[clap(flatten)]
     size: SizeOpt,
 
+    /// Force the main timeline to play, bypassing "Click to Play" buttons and similar restrictions.
+    /// This can help automate playback in some SWFs, but may break or alter content that expects user interaction.
+    /// Use with caution: enabling this may cause some movies to behave incorrectly.
+    #[clap(long)]
+    force_play: bool,
+
     /// Type of graphics backend to use. Not all options may be supported by your current system.
     /// Default will attempt to pick the most supported graphics backend.
     #[clap(long, short, default_value = "default")]
@@ -87,6 +96,7 @@ fn take_screenshot(
     skipframes: u32,
     progress: &Option<ProgressBar>,
     size: SizeOpt,
+    force_play: bool,
 ) -> Result<Vec<RgbaImage>> {
     let movie = SwfMovie::from_path(swf_path, None).map_err(|e| anyhow!(e.to_string()))?;
 
@@ -124,6 +134,10 @@ fn take_screenshot(
             ));
         }
 
+        if force_play {
+            force_root_clip_play(&player);
+        }
+
         player.lock().unwrap().preload(&mut ExecutionLimit::none());
 
         player.lock().unwrap().run_frame();
@@ -156,6 +170,26 @@ fn take_screenshot(
         }
     }
     Ok(result)
+}
+
+fn force_root_clip_play(player: &Arc<Mutex<Player>>) {
+    let mut player_guard = player.lock().unwrap();
+
+    // Check and resume if suspended
+    if !player_guard.is_playing() {
+        player_guard.set_is_playing(true);
+    }
+
+    // Also resume the root MovieClip if stopped
+    player_guard.mutate_with_update_context(|ctx| {
+        if let Some(root_clip) = ctx.stage.root_clip() {
+            if let Some(movie_clip) = root_clip.as_movie_clip() {
+                if !movie_clip.playing() {
+                    movie_clip.play();
+                }
+            }
+        }
+    });
 }
 
 fn find_files(root: &Path, with_progress: bool) -> Vec<DirEntry> {
@@ -223,6 +257,7 @@ fn capture_single_swf(descriptors: Arc<Descriptors>, opt: &Opt) -> Result<()> {
         opt.skipframes,
         &progress,
         opt.size,
+        opt.force_play,
     )?;
 
     if let Some(progress) = &progress {
@@ -316,6 +351,7 @@ fn capture_multiple_swfs(descriptors: Arc<Descriptors>, opt: &Opt) -> Result<()>
             opt.skipframes,
             &progress,
             opt.size,
+            opt.force_play,
         ) {
             let mut relative_path = file
                 .path()
