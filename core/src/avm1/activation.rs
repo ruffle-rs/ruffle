f8262ae9010bf519e101e9a7ca8d185613cd10cd
CUB3D <callumthom11@gmail.com>
2025-06-26 01:48:56 +0100

    AVM1: Fix MovieClipReference laundering issue

--- core/src/avm1/activation.rs
+++ core/src/avm1/activation.rs
@@ -545,14 +545,6 @@ impl<'a, 'gc> Activation<'a, 'gc> {
 
     fn stack_push(&mut self, mut value: Value<'gc>) {
         if let Value::Object(obj) = value {
-            // Note that there currently exists a subtle issue with this logic:
-            // If the cached `Object` in a `MovieClipReference` becomes invalidated, causing it to switch back to path-based object resolution,
-            // it should *never* switch back to cache-based resolution
-            // However, currently if a `MovieClipReference` in this invalidated-cache state is converted back to an `Object`, such as when passed as an argument to a function,
-            // if it pushed back onto the stack then it will be converted into a new `MovieClipReference`, causing it to switch back to cache-based resolution
-            // Fixing this will require a thorough refactor of AVM1 to store `Either<MovieClipReference, Object>
-            // can refer to a MovieClip
-            // There is a ignored test for this issue of "reference laundering" at "avm1/string_paths_reference_launder"
             if let Some(mcr) = MovieClipReference::try_from_stage_object(self, obj) {
                 value = Value::MovieClip(mcr);
             }
