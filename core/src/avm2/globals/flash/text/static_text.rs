0e87a5fbd263e8c67a9a988e0412755012681ba6
Kamil Jarosz <kjarosh256@gmail.com>
2025-07-03 23:02:15 +0200

    avm2: Make StaticText class abstract

--- core/src/avm2/globals/flash/text/static_text.rs
+++ core/src/avm2/globals/flash/text/static_text.rs
@@ -1,18 +1,6 @@
-use crate::avm2::error::argument_error;
-use crate::avm2::{Activation, ClassObject, Error, Object, TObject, Value};
+use crate::avm2::{Activation, Error, TObject, Value};
 use crate::prelude::TDisplayObject;
 use crate::string::AvmString;
-
-pub fn static_text_allocator<'gc>(
-    _class: ClassObject<'gc>,
-    activation: &mut Activation<'_, 'gc>,
-) -> Result<Object<'gc>, Error<'gc>> {
-    return Err(Error::avm_error(argument_error(
-        activation,
-        "Error #2012: StaticText$ class cannot be instantiated.",
-        2012,
-    )?));
-}
 
 /// Implements `StaticText.text`
 pub fn get_text<'gc>(
