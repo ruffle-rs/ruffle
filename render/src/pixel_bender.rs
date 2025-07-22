71fa1a1a5bf593c95df4a4e737ed6d824d1a1858
Kamil Jarosz <kjarosh256@gmail.com>
2025-07-22 22:38:12 +0200

    pixelbender: Handle missing out param properly
    Flash Player throws error 2004

--- render/src/pixel_bender.rs
+++ render/src/pixel_bender.rs
@@ -43,6 +43,9 @@ pub enum PixelBenderParsingError {
     #[error("Incompatible register kinds")]
     IncompatibleRegisterKinds,
 
+    #[error("Missing output parameter")]
+    MissingOutputParameter,
+
     #[error("IO error: {0}")]
     IoError(#[from] std::io::Error),
 
@@ -412,6 +415,11 @@ pub fn parse_shader(mut data: &[u8], validate: bool) -> Result<PixelBenderShader
     }
     // Any metadata left in the vec is associated with our final parameter.
     apply_metadata(&mut shader, &mut metadata);
+
+    if validate && shader.output_channels().is_none() {
+        return Err(PixelBenderParsingError::MissingOutputParameter);
+    }
+
     Ok(shader)
 }
 
