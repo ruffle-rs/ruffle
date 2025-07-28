ea8bfbe2aea23d55ad4b5edccb7b514d5521a861
Lord-McSweeney <Lord-McSweeney@github.com>
2025-07-27 21:35:28 -0700

    avm2: Use ParametersExt in SharedObject and Proxy code

--- core/src/avm2/globals/flash/utils/proxy.rs
+++ core/src/avm2/globals/flash/utils/proxy.rs
@@ -1,5 +1,6 @@
 use crate::avm2::activation::Activation;
 use crate::avm2::object::Object;
+use crate::avm2::parameters::ParametersExt;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 
@@ -10,7 +11,7 @@ pub fn is_attribute<'gc>(
     _this: Value<'gc>,
     args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
-    if let Some(Value::Object(Object::QNameObject(qname_object))) = args.get(0) {
+    if let Value::Object(Object::QNameObject(qname_object)) = args.get_value(0) {
         return Ok(qname_object.name().is_attribute().into());
     }
     Ok(false.into())
