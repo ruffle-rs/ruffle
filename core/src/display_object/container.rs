ruffle-rs/ruffle

commit 4cc9ba2c00790c597376bc062d021daaeb791f59

core: don't store the `SwfMovie` in `ChildContainer`
We only need the movie to know whether the container is in AVM1 or AVM2 mode, so
just store a boolean instead.

core/src/display_object/container.rs
@@ -23,7 +23,6 @@ use std::collections::BTreeMap;
 use std::fmt::Debug;
 use std::ops::{Bound, RangeBounds};
 use std::rc::Rc;
-use std::sync::Arc;
 
 /// Dispatch the `removedFromStage` event on a child and all of it's
 /// grandchildren, recursively.
@@ -213,7 +212,7 @@ pub trait TDisplayObjectContainer<'gc>:
         child.set_depth(depth);
 
         if let Some(removed_child) = removed_child {
-            if !self.raw_container().movie().is_action_script_3() {
+            if !self.raw_container().is_action_script_3() {
                 removed_child.avm1_unload(context);
             }
             removed_child.set_parent(context, None);
@@ -281,7 +280,7 @@ pub trait TDisplayObjectContainer<'gc>:
 
         child.set_place_frame(0);
         child.set_parent(context, Some(this));
-        if !self.raw_container().movie().is_action_script_3() {
+        if !self.raw_container().is_action_script_3() {
             child.set_avm1_removed(false);
         }
 
@@ -318,7 +317,7 @@ pub trait TDisplayObjectContainer<'gc>:
         ));
 
         // Check if this child should have delayed removal (AVM1 only)
-        if !self.raw_container().movie().is_action_script_3() {
+        if !self.raw_container().is_action_script_3() {
             let should_delay_removal = {
                 let mut activation = Activation::from_nothing(
                     context,
@@ -366,7 +365,7 @@ pub trait TDisplayObjectContainer<'gc>:
             ChildContainer::remove_child_from_render_list(this, child, context);
 
         if removed_from_render_list {
-            if !self.raw_container().movie.is_action_script_3() {
+            if !self.raw_container().is_action_script_3() {
                 child.avm1_unload(context);
             } else if !matches!(child.object2(), Avm2Value::Null) {
                 //TODO: This is an awful, *awful* hack to deal with the fact
@@ -445,7 +444,7 @@ pub trait TDisplayObjectContainer<'gc>:
             let this: DisplayObjectContainer<'gc> = (*self).into();
             ChildContainer::remove_child_from_render_list(this, removed, context);
 
-            if !self.raw_container().movie.is_action_script_3() {
+            if !self.raw_container().is_action_script_3() {
                 removed.avm1_unload(context);
             } else if !matches!(removed.object2(), Avm2Value::Null) {
                 removed.set_parent(context, None);
@@ -490,9 +489,9 @@ pub trait TDisplayObjectContainer<'gc>:
     /// According to the AS2 documentation, it should affect only automatic tab ordering.
     /// However, that does not seem to be the case, as it also affects custom ordering.
     fn is_tab_children(&self, context: &mut UpdateContext<'gc>) -> bool {
-        let this: DisplayObject<'_> = (*self).into();
-        if this.movie().is_action_script_3() {
-            self.raw_container().tab_children
+        let container = self.raw_container();
+        if container.is_action_script_3() {
+            container.tab_children
         } else {
             self.is_tab_children_avm1(context)
         }
@@ -500,7 +499,7 @@ pub trait TDisplayObjectContainer<'gc>:
 
     fn set_tab_children(&self, context: &mut UpdateContext<'gc>, value: bool) {
         let this: DisplayObject<'_> = (*self).into();
-        if this.movie().is_action_script_3() {
+        if self.raw_container().is_action_script_3() {
             self.raw_container_mut(context.gc()).tab_children = value;
         } else {
             this.set_avm1_property(istr!(context, "tabChildren"), value.into(), context);
@@ -653,21 +652,21 @@ pub struct ChildContainer<'gc> {
 
     mouse_children: bool,
 
-    /// The movie this ChildContainer belongs to.
-    movie: Arc<SwfMovie>,
+    /// Specifies whether this container uses AVM1 or AVM2 semantics.
+    is_action_script_3: bool,
 
     /// Specifies whether children are present in the tab ordering.
     tab_children: bool,
 }
 
 impl<'gc> ChildContainer<'gc> {
-    pub fn new(movie: Arc<SwfMovie>) -> Self {
+    pub fn new(movie: &SwfMovie) -> Self {
         Self {
             render_list: Rc::new(Vec::new()),
             depth_list: BTreeMap::new(),
             has_pending_removals: false,
             mouse_children: true,
-            movie,
+            is_action_script_3: movie.is_action_script_3(),
             tab_children: true,
         }
     }
@@ -928,12 +927,8 @@ impl<'gc> ChildContainer<'gc> {
         self.mouse_children = mouse_children;
     }
 
-    pub fn movie(&self) -> Arc<SwfMovie> {
-        self.movie.clone()
-    }
-
-    pub fn set_movie(&mut self, movie: Arc<SwfMovie>) {
-        self.movie = movie;
+    pub fn is_action_script_3(&self) -> bool {
+        self.is_action_script_3
     }
 
     /// Insert a child at a given render list position.

core/src/display_object/stage.rs
@@ -152,7 +152,7 @@ impl<'gc> Stage<'gc> {
             gc_context,
             StageData {
                 base: Default::default(),
-                child: RefLock::new(ChildContainer::new(movie.clone())),
+                child: RefLock::new(ChildContainer::new(&movie)),
                 background_color: Cell::new(None),
                 letterbox: Cell::new(Letterbox::Fullscreen),
                 // This is updated when we set the root movie
@@ -229,12 +229,9 @@ impl<'gc> Stage<'gc> {
     }
 
     pub fn set_movie(self, gc_context: &Mutation<'gc>, movie: Arc<SwfMovie>) {
-        self.0.movie.replace(movie.clone());
-
         // Stage is the only DO that has a fake movie set and then gets the real movie set.
-        unlock!(Gc::write(gc_context, self.0), StageData, child)
-            .borrow_mut()
-            .set_movie(movie);
+        *self.raw_container_mut(gc_context) = ChildContainer::new(&movie);
+        self.0.movie.replace(movie.clone());
     }
 
     pub fn set_loader_info(self, gc_context: &Mutation<'gc>, loader_info: Avm2Object<'gc>) {

core/src/context.rs
@@ -338,8 +338,8 @@ impl<'gc> UpdateContext<'gc> {
 
     /// Change the root movie.
     ///
-    /// This should only be called once, as it makes no attempt at removing
-    /// previous stage contents. If you need to load a new root movie, you
+    /// This should only be called once, as it makes no attempt at proper clean-up
+    /// of previous stage contents. If you need to load a new root movie, you
     /// should use `replace_root_movie`.
     pub fn set_root_movie(&mut self, movie: SwfMovie) {
         if !self.forced_frame_rate {

core/src/display_object/avm1_button.rs
@@ -75,7 +75,7 @@ impl<'gc> Avm1Button<'gc> {
             Avm1ButtonData {
                 cell: RefLock::new(Avm1ButtonDataMut {
                     base: Default::default(),
-                    container: ChildContainer::new(source_movie.movie.clone()),
+                    container: ChildContainer::new(&source_movie.movie),
                     hit_area: BTreeMap::new(),
                     hit_bounds: Default::default(),
                     text_field_bindings: Vec::new(),

core/src/display_object/loader_display.rs
@@ -47,7 +47,7 @@ impl<'gc> LoaderDisplay<'gc> {
             activation.gc(),
             LoaderDisplayData {
                 base: RefLock::new(Default::default()),
-                container: RefLock::new(ChildContainer::new(movie.clone())),
+                container: RefLock::new(ChildContainer::new(&movie)),
                 avm2_object: Lock::new(None),
                 movie,
             },

core/src/display_object/movie_clip.rs
@@ -205,7 +205,7 @@ impl<'gc> MovieClipData<'gc> {
             tag_stream_pos: Cell::new(0),
             current_frame: Cell::new(0),
             audio_stream: Cell::new(None),
-            container: ChildContainer::new(movie),
+            container: ChildContainer::new(&movie),
             object: None,
             clip_event_handlers: Vec::new(),
             clip_event_flags: Cell::new(ClipEventFlag::empty()),
@@ -346,11 +346,12 @@ impl<'gc> MovieClip<'gc> {
         );
 
         mc.base.base.reset_for_movie_load();
+        mc.container = ChildContainer::new(&movie);
         mc.shared = Gc::new(
             context.gc(),
             MovieClipShared::with_data(
                 0,
-                movie.clone().into(),
+                movie.into(),
                 total_frames,
                 loader_info.map(|l| l.into()),
             ),
@@ -360,7 +361,6 @@ impl<'gc> MovieClip<'gc> {
         mc.base.base.set_is_root(is_root);
         mc.current_frame.set(0);
         mc.audio_stream.set(None);
-        mc.container = ChildContainer::new(movie);
         drop(mc);
     }
 

commit 2961612397c3011a1b516b03685201bd7ed88ee6

core: Fix resetting stage state on set_movie
This patch fixes a regression introduced in
4cc9ba2c00790c597376bc062d021daaeb791f59 that reset stage's state on
set_movie and broke reloading the root movie in AVM1.

core/src/display_object/container.rs
@@ -931,6 +931,10 @@ impl<'gc> ChildContainer<'gc> {
         self.is_action_script_3
     }
 
+    pub fn set_is_action_script_3(&mut self, is_action_script_3: bool) {
+        self.is_action_script_3 = is_action_script_3;
+    }
+
     /// Insert a child at a given render list position.
     ///
     /// If the child is already a child of another container, you must remove

core/src/display_object/stage.rs
@@ -230,8 +230,13 @@ impl<'gc> Stage<'gc> {
 
     pub fn set_movie(self, gc_context: &Mutation<'gc>, movie: Arc<SwfMovie>) {
         // Stage is the only DO that has a fake movie set and then gets the real movie set.
-        *self.raw_container_mut(gc_context) = ChildContainer::new(&movie);
-        self.0.movie.replace(movie.clone());
+        // NOTE: Make sure to NOT reset any state here, AVM1 depends on it.
+
+        let is_action_script_3 = movie.is_action_script_3();
+        self.0.movie.replace(movie);
+        unlock!(Gc::write(gc_context, self.0), StageData, child)
+            .borrow_mut()
+            .set_is_action_script_3(is_action_script_3);
     }
 
     pub fn set_loader_info(self, gc_context: &Mutation<'gc>, loader_info: Avm2Object<'gc>) {