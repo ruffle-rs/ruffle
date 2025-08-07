f4c062f4ba7bb2c7a478f9a96233c071baa18661
Lord-McSweeney <Lord-McSweeney@github.com>
2025-08-06 18:38:20 -0700

    chore: Improve ParametersExt documentation

--- core/src/avm2/parameters.rs
+++ core/src/avm2/parameters.rs
@@ -13,15 +13,15 @@ use ruffle_macros::istr;
 /// The rules for ActionScript type coercion may be surprising. Here is a table mapping
 /// ParametersExt functions to the corresponding ActionScript types:
 ///
-/// `get_value`: All parameter types work
-/// `get_f64`: `Number`, `int`, or `uint` type
-/// `get_i32`: `Number`, `int`, or `uint` type
-/// `get_u32`: `Number`, `int`, or `uint` type
-/// `get_bool`: `Boolean` type only
-/// `get_string` and family: `String` type only
-/// `get_object` and family: Any non-primitive type; i.e. any type *except* the following:
+/// `ParametersExt::get_value`: All parameter types work
+/// `ParametersExt::get_f64`: `Number`, `int`, or `uint` type
+/// `ParametersExt::get_i32`: `Number`, `int`, or `uint` type
+/// `ParametersExt::get_u32`: `Number`, `int`, or `uint` type
+/// `ParametersExt::get_bool`: `Boolean` type only
+/// `ParametersExt::get_string` and family: `String` type only
+/// `ParametersExt::get_object` and family: Any non-primitive type; i.e. any type *except* the following:
 ///   - `*` (aka "any") type
-///   - `Object` type
+///   - `Object` type (as `Object` can represent any primitive value except `undefined`)
 ///   - `Boolean` type
 ///   - `int` type
 ///   - `uint` type
@@ -60,25 +60,26 @@ pub trait ParametersExt<'gc> {
         }
     }
 
-    /// Gets the value at the given index and coerces it to an f64.
-    ///
-    /// If the value is null or is undefined, 0.0 is returned.
-    /// If the object cannot be coerced to an f64, a TypeError 1050 is raised.
+    /// Gets the Number-typed value at the given index. It is expected that the
+    /// value is numerical.
     fn get_f64(&self, index: usize) -> f64 {
         self.get_value(index).as_f64()
     }
 
-    /// Gets the uint-typed value at the given index.
+    /// Gets the uint-typed value at the given index. It is expected that the
+    /// value is numerical.
     fn get_u32(&self, index: usize) -> u32 {
         self.get_value(index).as_u32()
     }
 
-    /// Gets the int-typed value at the given index.
+    /// Gets the int-typed value at the given index. It is expected that the
+    /// value is numerical.
     fn get_i32(&self, index: usize) -> i32 {
         self.get_value(index).as_i32()
     }
 
-    /// Gets the Boolean-typed value at the given index.
+    /// Gets the Boolean-typed value at the given index. It is expected that the
+    /// value is of the Boolean type.
     fn get_bool(&self, index: usize) -> bool {
         match self.get_value(index) {
             Value::Bool(b) => b,
@@ -86,7 +87,8 @@ pub trait ParametersExt<'gc> {
         }
     }
 
-    /// Gets the String-typed value at the given index.
+    /// Gets the String-typed value at the given index. It is expected that the
+    /// value is either String or Null.
     ///
     /// If the value is null, None is returned.
     fn try_get_string(&self, index: usize) -> Option<AvmString<'gc>> {
