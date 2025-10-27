180b9c0bd92c75ff40b631bf456a3c736552d3cd
Kamil Jarosz <kjarosh256@gmail.com>
2025-10-27 18:17:50 +0100

    avm1: Add missing globals
    Added stubs of missing globals from the documentation.

---
+++ core/src/avm1/globals/text_renderer.rs
@@ -0,0 +1,25 @@
+//! TextRenderer object
+
+use crate::avm1::activation::Activation;
+use crate::avm1::error::Error;
+use crate::avm1::property_decl::{DeclContext, Declaration, SystemClass};
+use crate::avm1::{Object, Value};
+
+const PROTO_DECLS: &[Declaration] = declare_properties! {};
+
+pub fn create_class<'gc>(
+    context: &mut DeclContext<'_, 'gc>,
+    super_proto: Object<'gc>,
+) -> SystemClass<'gc> {
+    let class = context.class(constructor, super_proto);
+    context.define_properties_on(class.proto, PROTO_DECLS);
+    class
+}
+
+fn constructor<'gc>(
+    _activation: &mut Activation<'_, 'gc>,
+    _this: Object<'gc>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error<'gc>> {
+    Ok(Value::Undefined)
+}
