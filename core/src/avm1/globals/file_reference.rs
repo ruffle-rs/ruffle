10776660f36b2f470f69afba4a01da46efdbfac5
Kamil Jarosz <kjarosh256@gmail.com>
2025-10-31 23:54:01 +0100

    avm1: Fix crashes when calling FileReference.browse
    Caught by `argstest`, this patch prevents it from panicking.

--- core/src/avm1/globals/file_reference.rs
+++ core/src/avm1/globals/file_reference.rs
@@ -233,6 +233,10 @@ pub fn browse<'gc>(
     this: Object<'gc>,
     args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
+    if !matches!(this.native(), NativeObject::FileReference(_)) {
+        return Ok(Value::Undefined);
+    }
+
     let file_filters = match args.get(0) {
         Some(Value::Object(array)) => {
             // Array of filter objects.
