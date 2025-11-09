bb79b6abe746653276a3a43a5a5f3b8149bb1866
Lord-McSweeney <batuharmanci@gmail.com>
2025-11-08 21:09:21 -0800

    core: Remove some unnecessary Activation creations

--- core/src/socket.rs
+++ core/src/socket.rs
@@ -379,15 +379,12 @@ impl<'gc> Sockets<'gc> {
 
                     match target {
                         SocketKind::Avm2(target) => {
-                            let activation = Avm2Activation::from_nothing(context);
-
                             // Clear the buffers if the connection was closed.
                             target.read_buffer().clear();
                             target.write_buffer().clear();
 
-                            let close_evt =
-                                EventObject::bare_default_event(activation.context, "close");
-                            Avm2::dispatch_event(activation.context, close_evt, target.into());
+                            let close_evt = EventObject::bare_default_event(context, "close");
+                            Avm2::dispatch_event(context, close_evt, target.into());
                         }
                         SocketKind::Avm1(target) => {
                             let mut activation = Avm1Activation::from_stub(
