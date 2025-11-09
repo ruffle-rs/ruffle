c61125f3130c1188359dd0179fb3d2db416098b1
Lord-McSweeney <batuharmanci@gmail.com>
2025-11-08 21:09:21 -0800

    core: Remove some unnecessary Activation creations

--- core/src/loader.rs
+++ core/src/loader.rs
@@ -2266,27 +2266,13 @@ fn select_file_dialog<'gc>(
                             if !dialog_result.is_cancelled() {
                                 target_object.init_from_dialog_result(dialog_result);
 
-                                let activation = Avm2Activation::from_nothing(uc);
-                                let select_event = Avm2EventObject::bare_default_event(
-                                    activation.context,
-                                    "select",
-                                );
-                                Avm2::dispatch_event(
-                                    activation.context,
-                                    select_event,
-                                    target_object.into(),
-                                );
+                                let select_event =
+                                    Avm2EventObject::bare_default_event(uc, "select");
+                                Avm2::dispatch_event(uc, select_event, target_object.into());
                             } else {
-                                let activation = Avm2Activation::from_nothing(uc);
-                                let cancel_event = Avm2EventObject::bare_default_event(
-                                    activation.context,
-                                    "cancel",
-                                );
-                                Avm2::dispatch_event(
-                                    activation.context,
-                                    cancel_event,
-                                    target_object.into(),
-                                );
+                                let cancel_event =
+                                    Avm2EventObject::bare_default_event(uc, "cancel");
+                                Avm2::dispatch_event(uc, cancel_event, target_object.into());
                             }
                         }
                         Err(err) => {
@@ -2361,14 +2347,8 @@ pub fn save_file_dialog<'gc>(
                             target_object.into(),
                         );
                     } else {
-                        let activation = Avm2Activation::from_nothing(uc);
-                        let cancel_event =
-                            Avm2EventObject::bare_default_event(activation.context, "cancel");
-                        Avm2::dispatch_event(
-                            activation.context,
-                            cancel_event,
-                            target_object.into(),
-                        );
+                        let cancel_event = Avm2EventObject::bare_default_event(uc, "cancel");
+                        Avm2::dispatch_event(uc, cancel_event, target_object.into());
                     }
                 }
                 Err(err) => {
