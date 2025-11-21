2183fbaa9f49c762793f2ee82925e51ae9cdb487
Lord-McSweeney <batuharmanci@gmail.com>
2025-11-21 12:12:08 -0800

    avm2: Add `make_error_1020` and use it

--- core/src/avm2/verify.rs
+++ core/src/avm2/verify.rs
@@ -1,7 +1,8 @@
 use crate::avm2::class::Class;
 use crate::avm2::error::{
-    make_error_1014, make_error_1019, make_error_1021, make_error_1025, make_error_1026,
-    make_error_1032, make_error_1054, make_error_1107, verify_error, Error1014Type,
+    make_error_1014, make_error_1019, make_error_1020, make_error_1021, make_error_1025,
+    make_error_1026, make_error_1032, make_error_1054, make_error_1107, verify_error,
+    Error1014Type,
 };
 use crate::avm2::method::Method;
 use crate::avm2::multiname::Multiname;
@@ -129,11 +130,7 @@ pub fn verify_method<'gc>(
                     )?));
                 }
                 Err(AbcReadError::IoError(_)) => {
-                    return Err(Error::avm_error(verify_error(
-                        activation,
-                        "Error #1020: Code cannot fall off the end of a method.",
-                        1020,
-                    )?));
+                    return Err(make_error_1020(activation));
                 }
                 Err(_) => unreachable!(),
             };
