f50890d72681c19721b7430804a9d9f6852687a5
Moulins <arthur.heuillard@orange.fr>
2025-11-27 16:35:45 +0100

    tests: distinguish between panicky and non-panicky known_failure tests
    Tests that are expected to panic are now required to explicitely say so
    in their `test.toml`, by using `known_failure.panic = "expected message"`.
    
    Additionally, only panics raised during player ticking are caught, and not those
    happenning elsewhere (e.g. in rendering, or in test framework code).

---
+++ tests/framework/src/options/known_failure.rs
@@ -0,0 +1,53 @@
+use std::fmt;
+
+use serde::{
+    Deserialize, Deserializer,
+    de::{self, value::MapAccessDeserializer},
+};
+
+#[derive(Clone, Debug, Default)]
+pub enum KnownFailure {
+    #[default]
+    None,
+    AnyCheck,
+    Panic {
+        message: String,
+    },
+}
+
+impl<'de> Deserialize<'de> for KnownFailure {
+    fn deserialize<D: Deserializer<'de>>(deser: D) -> Result<Self, D::Error> {
+        deser.deserialize_any(KnownFailureVisitor)
+    }
+}
+
+struct KnownFailureVisitor;
+
+impl<'de> de::Visitor<'de> for KnownFailureVisitor {
+    type Value = KnownFailure;
+
+    fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("a boolean, or `.panic = 'message'`")
+    }
+
+    fn visit_bool<E: de::Error>(self, v: bool) -> Result<Self::Value, E> {
+        if v {
+            Ok(KnownFailure::AnyCheck)
+        } else {
+            Ok(KnownFailure::None)
+        }
+    }
+
+    fn visit_map<A: de::MapAccess<'de>>(self, map: A) -> Result<Self::Value, A::Error> {
+        #[derive(Deserialize)]
+        #[serde(deny_unknown_fields)]
+        enum Raw {
+            #[serde(rename = "panic")]
+            Panic(String),
+        }
+
+        match Raw::deserialize(MapAccessDeserializer::new(map))? {
+            Raw::Panic(message) => Ok(KnownFailure::Panic { message }),
+        }
+    }
+}
