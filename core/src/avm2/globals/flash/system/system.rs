0b3d48af77f453b56c8cdd62cb7880c2816e77eb
Lord-McSweeney <batuharmanci@gmail.com>
2025-12-02 22:28:27 -0800

    avm2: Add `make_error_2176` and use it

--- core/src/avm2/globals/flash/system/system.rs
+++ core/src/avm2/globals/flash/system/system.rs
@@ -15,11 +15,7 @@ pub fn set_clipboard<'gc>(
     // TODO: Check the type of event that triggered the function call.
     #[cfg(target_family = "wasm")]
     if false {
-        return Err(Error::avm_error(crate::avm2::error::error(
-            activation,
-            "Error #2176: Certain actions, such as those that display a pop-up window, may only be invoked upon user interaction, for example by a mouse click or button press.",
-            2176,
-        )?));
+        return Err(crate::avm2::error::make_error_2176(activation));
     }
 
     let new_content = args.get_string_non_null(activation, 0, "text")?;
