55a1ecad462f18f2376c0ae9237d252c0a9d30bd
zjk261 <98996990+zjk261@users.noreply.github.com>
2025-12-06 17:49:06 +0800

    avm1: constructor of function objects should exist

--- core/src/avm1/function.rs
+++ core/src/avm1/function.rs
@@ -487,6 +487,15 @@ impl<'gc> FunctionObject<'gc> {
             Attribute::DONT_ENUM | Attribute::DONT_DELETE | Attribute::VERSION_6,
         );
 
+        // This should be correct, because f.hasOwnProperty("constructor") returns true in flashplayer.
+        let constr_val = fn_proto.get_data_raw(istr!(context, "constructor"));
+        obj.define_value(
+            context.gc(),
+            istr!(context, "constructor"),
+            constr_val,
+            Attribute::DONT_ENUM | Attribute::DONT_DELETE,
+        );
+
         if let Some(prototype) = prototype {
             prototype.define_value(
                 context.gc(),
