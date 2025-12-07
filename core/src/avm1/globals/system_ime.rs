6dbf7d508fdfea6cc6e64a1eecc377db4ae7769d
Kamil Jarosz <kjarosh256@gmail.com>
2025-12-07 13:28:20 +0100

    avm1: Reorder System.IME properties to match Flash
    This makes the argstest v8 FP32 diff smaller by 1.6%.

--- core/src/avm1/globals/system_ime.rs
+++ core/src/avm1/globals/system_ime.rs
@@ -6,21 +6,22 @@ use crate::avm1::{Object, Value};
 use crate::string::AvmString;
 
 const OBJECT_DECLS: &[Declaration] = declare_properties! {
-    "ALPHANUMERIC_FULL" => string("ALPHANUMERIC_FULL"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "ALPHANUMERIC_HALF" => string("ALPHANUMERIC_HALF"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "CHINESE" => string("CHINESE"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "JAPANESE_HIRAGANA" => string("JAPANESE_HIRAGANA"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "JAPANESE_KATAKANA_FULL" => string("JAPANESE_KATAKANA_FULL"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "JAPANESE_KATAKANA_HALF" => string("JAPANESE_KATAKANA_HALF"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "KOREAN" => string("KOREAN"; DONT_ENUM | DONT_DELETE | READ_ONLY);
     "UNKNOWN" => string("UNKNOWN"; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "onIMEComposition" => method(on_ime_composition; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "doConversion" => method(do_conversion; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "getConversionMode" => method(get_conversion_mode; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "KOREAN" => string("KOREAN"; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "JAPANESE_KATAKANA_HALF" => string("JAPANESE_KATAKANA_HALF"; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "JAPANESE_KATAKANA_FULL" => string("JAPANESE_KATAKANA_FULL"; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "JAPANESE_HIRAGANA" => string("JAPANESE_HIRAGANA"; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "CHINESE" => string("CHINESE"; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "ALPHANUMERIC_HALF" => string("ALPHANUMERIC_HALF"; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "ALPHANUMERIC_FULL" => string("ALPHANUMERIC_FULL"; DONT_ENUM | DONT_DELETE | READ_ONLY);
     "getEnabled" => method(get_enabled; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "setCompositionString" => method(set_composition_string; DONT_ENUM | DONT_DELETE | READ_ONLY);
-    "setConversionMode" => method(set_conversion_mode; DONT_ENUM | DONT_DELETE | READ_ONLY);
     "setEnabled" => method(set_enabled; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "getConversionMode" => method(get_conversion_mode; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "setConversionMode" => method(set_conversion_mode; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "setCompositionString" => method(set_composition_string; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    "doConversion" => method(do_conversion; DONT_ENUM | DONT_DELETE | READ_ONLY);
+    // TODO: onIMEComposition doesn't look like it's a built-in property.
+    "onIMEComposition" => method(on_ime_composition; DONT_ENUM | DONT_DELETE | READ_ONLY);
 };
 
 pub fn create<'gc>(
