fe004a0d5241db45e653ef4189f0f0a57651df1b
Moulins <arthur.heuillard@orange.fr>
2026-02-13 17:52:44 +0100

    avm1: Migrate `flash.automation.StageCapture` to ASnative-style declarations.

--- core/src/avm1/globals/automation_stage_capture.rs
+++ core/src/avm1/globals/automation_stage_capture.rs
@@ -5,115 +5,66 @@ use crate::avm1::{Object, Value};
 use crate::avm1_stub;
 
 const PROTO_DECLS: StaticDeclarations = declare_static_properties! {
-    "capture" => method(capture; DONT_ENUM | DONT_DELETE);
-    "cancel" => method(cancel; DONT_ENUM | DONT_DELETE);
-    "getFileNameBase" => method(get_file_name_base; DONT_ENUM | DONT_DELETE);
-    "setFileNameBase" => method(set_file_name_base; DONT_ENUM | DONT_DELETE);
-    "getClipRect" => method(get_clip_rect; DONT_ENUM | DONT_DELETE);
-    "setClipRect" => method(set_clip_rect; DONT_ENUM | DONT_DELETE);
-    "listenForStageCapture" => method(listen_for_stage_capture; DONT_ENUM | DONT_DELETE);
-    "valueOf" => method(value_of; DONT_DELETE);
-    "toString" => method(to_string; DONT_DELETE);
+    use fn method;
+    "capture" => method(CAPTURE; DONT_ENUM | DONT_DELETE);
+    "cancel" => method(CANCEL; DONT_ENUM | DONT_DELETE);
+    "getFileNameBase" => method(GET_FILE_NAME_BASE; DONT_ENUM | DONT_DELETE);
+    "setFileNameBase" => method(SET_FILE_NAME_BASE; DONT_ENUM | DONT_DELETE);
+    "getClipRect" => method(GET_CLIP_RECT; DONT_ENUM | DONT_DELETE);
+    "setClipRect" => method(SET_CLIP_RECT; DONT_ENUM | DONT_DELETE);
+    "listenForStageCapture" => method(LISTEN_FOR_STAGE_CAPTURE; DONT_ENUM | DONT_DELETE);
+    "valueOf" => method(VALUE_OF; DONT_DELETE);
+    "toString" => method(TO_STRING; DONT_DELETE);
 };
 
 pub fn create_class<'gc>(
     context: &mut DeclContext<'_, 'gc>,
     super_proto: Object<'gc>,
 ) -> SystemClass<'gc> {
-    let class = context.empty_class(super_proto);
+    let class = context.native_class(table_constructor!(method), None, super_proto);
     context.define_properties_on(class.proto, PROTO_DECLS(context));
     class
 }
 
-fn capture<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(activation, "flash.automation.StageCapture", "capture");
-    Ok(Value::Undefined)
+pub mod method {
+    pub const CAPTURE: u16 = 0;
+    pub const CANCEL: u16 = 1;
+    pub const GET_FILE_NAME_BASE: u16 = 2;
+    pub const SET_FILE_NAME_BASE: u16 = 3;
+    pub const GET_CLIP_RECT: u16 = 4;
+    pub const SET_CLIP_RECT: u16 = 5;
+    pub const LISTEN_FOR_STAGE_CAPTURE: u16 = 6;
+    pub const VALUE_OF: u16 = 7;
+    pub const TO_STRING: u16 = 8;
+    pub const CONSTRUCTOR: u16 = 100;
 }
 
-fn cancel<'gc>(
+pub fn method<'gc>(
     activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
+    this: Object<'gc>,
     _args: &[Value<'gc>],
+    index: u16,
 ) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(activation, "flash.automation.StageCapture", "cancel");
-    Ok(Value::Undefined)
-}
+    use method::*;
+    const CNAME: &str = "flash.automation.StageCapture";
 
-fn get_file_name_base<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(
-        activation,
-        "flash.automation.StageCapture",
-        "getFileNameBase"
-    );
-    Ok(Value::Undefined)
-}
+    if index == CONSTRUCTOR {
+        avm1_stub!(activation, CNAME);
+        return Ok(this.into());
+    }
 
-fn set_file_name_base<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(
-        activation,
-        "flash.automation.StageCapture",
-        "setFileNameBase"
-    );
-    Ok(Value::Undefined)
-}
+    match index {
+        CAPTURE => avm1_stub!(activation, CNAME, "capture"),
+        CANCEL => avm1_stub!(activation, CNAME, "cancel"),
+        GET_FILE_NAME_BASE => avm1_stub!(activation, CNAME, "getFileNameBase"),
+        SET_FILE_NAME_BASE => avm1_stub!(activation, CNAME, "setFileNameBase"),
+        GET_CLIP_RECT => avm1_stub!(activation, CNAME, "getClipRect"),
+        SET_CLIP_RECT => avm1_stub!(activation, CNAME, "setClipRect"),
+        LISTEN_FOR_STAGE_CAPTURE => avm1_stub!(activation, CNAME, "listenForStageCapture"),
+        VALUE_OF => avm1_stub!(activation, CNAME, "valueOf"),
+        TO_STRING => avm1_stub!(activation, CNAME, "toString"),
+        _ => (),
+    }
 
-fn get_clip_rect<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(activation, "flash.automation.StageCapture", "getClipRect");
-    Ok(Value::Undefined)
-}
-
-fn set_clip_rect<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(activation, "flash.automation.StageCapture", "setClipRect");
-    Ok(Value::Undefined)
-}
-
-fn listen_for_stage_capture<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(
-        activation,
-        "flash.automation.StageCapture",
-        "listenForStageCapture"
-    );
-    Ok(Value::Undefined)
-}
-
-fn value_of<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(activation, "flash.automation.StageCapture", "valueOf");
-    Ok(Value::Undefined)
-}
-
-fn to_string<'gc>(
-    activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
-    _args: &[Value<'gc>],
-) -> Result<Value<'gc>, Error<'gc>> {
-    avm1_stub!(activation, "flash.automation.StageCapture", "toString");
     Ok(Value::Undefined)
 }
