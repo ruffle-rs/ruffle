54f4bb787235eef52ef3aef896b87b77e21f4149
ChrisCPI <92183829+ChrisCPI@users.noreply.github.com>
2026-02-24 15:09:38 -0500

    render: Simplify and improve gradient logic

--- render/wgpu/src/mesh.rs
+++ render/wgpu/src/mesh.rs
@@ -299,7 +299,6 @@ impl CommonGradient {
             let mut colors = [0; GRADIENT_SIZE * 4];
             let mut last = 0;
             let mut next;
-            let mut threshold = 1;
 
             let convert = if gradient.interpolation == GradientInterpolation::LinearRgb {
                 |c| srgb_to_linear(c / 255.0) * 255.0
@@ -308,27 +307,23 @@ impl CommonGradient {
             };
 
             for t in 0..GRADIENT_SIZE {
-                if last + 1 < gradient.records.len() {
-                    let next_ratio = gradient.records[last + 1].ratio as usize;
-
-                    if t > next_ratio {
-                        last += 1;
-                    }
+                if last + 1 < gradient.records.len()
+                    && t > gradient.records[last + 1].ratio as usize
+                {
+                    last += 1;
                 }
                 next = (last + 1).min(gradient.records.len() - 1);
 
                 assert!(last == next || last + 1 == next);
 
-                let prev_record = &gradient.records[last.saturating_sub(1)];
                 let last_record = &gradient.records[last];
                 let next_record = &gradient.records[next];
 
-                let ratio_streak = last_record.ratio == prev_record.ratio
-                    && last_record.ratio == next_record.ratio
-                    && t == (last_record.ratio as usize) + threshold;
-
-                let a = if t <= last_record.ratio as usize || ratio_streak {
-                    // We are before the first gradient record
+                let a = if t <= last_record.ratio as usize || last_record.ratio == next_record.ratio
+                {
+                    // We are before the first gradient record,
+                    // or this record's ratio is equal to the next one,
+                    // meaning we need to do a full stop of this color for 1 pixel.
                     0.0
                 } else if t > next_record.ratio as usize {
                     // We are after the last record
@@ -336,14 +331,6 @@ impl CommonGradient {
                 } else {
                     (t as f32 - last_record.ratio as f32)
                         / (next_record.ratio as f32 - last_record.ratio as f32)
-                };
-
-                // Increment the threshold by 1, to correctly detect
-                // if the next ratio will also be the same.
-                if ratio_streak {
-                    threshold += 1;
-                } else {
-                    threshold = 1;
                 };
 
                 colors[t * 4] = lerp(
