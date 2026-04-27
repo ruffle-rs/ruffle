098e1df88de05376ffe594ca8053db95a4922c3b
Nathan Adams <dinnerbone@dinnerbone.com>
2026-04-27 21:36:00 +0200

    tests: Add [filter], --skip and --exact to compile CLI args

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -2,7 +2,8 @@ mod compile;
 
 use crate::compile::{CompileOptions, main_compile};
 use anyhow::Context;
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use ruffle_fs_tests_runner::FsTestsRunner;
 use ruffle_test_framework::options::TestOptions;
 use ruffle_test_framework::test::Test;
 use ruffle_test_framework::vfs::VfsPath;
@@ -14,6 +15,42 @@ use std::path::PathBuf;
 struct Opt {
     #[command(subcommand)]
     command: Commands,
+}
+
+// Copied out of libtest_mimic to be similar enough that you can hop between actual tests & this util CLI.
+#[derive(Args)]
+struct TestFilterOptions {
+    /// If set, filters are matched exactly rather than by substring.
+    #[arg(
+        long = "exact",
+        help = "Exactly match filters rather than by substring."
+    )]
+    pub exact: bool,
+
+    /// A list of filters. Tests whose names contain parts of any of these
+    /// filters are skipped.
+    #[arg(
+        long = "skip",
+        value_name = "FILTER",
+        help = "Skip tests whose names contain FILTER (this flag can be used multiple times)."
+    )]
+    pub skip: Vec<String>,
+
+    /// Filter string. Only tests which contain this string are run.
+    #[arg(
+        value_name = "FILTER",
+        help = "The FILTER string is tested against the name of all tests, and only those tests \
+                 whose names contain the filter are run."
+    )]
+    pub filter: Option<String>,
+}
+
+impl TestFilterOptions {
+    pub fn apply<T>(&self, runner: &mut FsTestsRunner<T>) {
+        runner.with_filter(self.filter.clone());
+        runner.with_skip(self.skip.clone());
+        runner.with_exact(self.exact);
+    }
 }
 
 #[derive(Subcommand)]
