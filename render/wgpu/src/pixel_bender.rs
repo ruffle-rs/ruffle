c1293b5848ec0cf88834d7dbe0da6ecbf83aee6b
TÖRÖK Attila <torokati44@gmail.com>
2026-07-16 15:30:14 +0200

    create_pipeline_layout bind_group_layouts API change

--- render/wgpu/src/pixel_bender.rs
+++ render/wgpu/src/pixel_bender.rs
@@ -194,7 +194,7 @@ impl PixelBenderWgpuShader {
                 .device
                 .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                     label: pipeline_layout_label.as_deref(),
-                    bind_group_layouts: &[&bind_group_layout],
+                    bind_group_layouts: &[Some(&bind_group_layout)],
                     immediate_size: 0,
                 });
 
