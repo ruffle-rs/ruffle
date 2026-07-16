fb7f7643759894df2ae000d2d45e114ae5279c3b
Lord-McSweeney <batuharmanci@gmail.com>
2026-07-16 05:11:47 -0700

    avm2: Optimize out `pushscope` op better
    This removes `simple_scope_structure`, which only worked if the getlocal0 and
    pushscope ops were in a specific place and format. Instead, we now do some
    work across the type-aware optimizer and the peephole optimizer to remove the
    op.

--- core/src/avm2/optimizer/peephole.rs
+++ core/src/avm2/optimizer/peephole.rs
@@ -21,16 +21,42 @@ pub fn preprocess_peephole(ops: &[Cell<Op<'_>>]) {
 
 /// A peephole optimizer to run after type-aware optimizations. This should be
 /// called once on the entire code slice.
-pub fn postprocess_peephole<'a>(
-    ops: &'a [Cell<Op<'_>>],
-    jump_targets: &HashSet<usize>,
-    has_exceptions: bool,
-) {
-    // Gather some information...
-    let simple_scope_op_positions =
-        simple_scope_structure(ops, jump_targets).filter(|_| !has_exceptions);
+pub fn postprocess_peephole<'a>(ops: &'a [Cell<Op<'_>>], jump_targets: &HashSet<usize>) {
+    // Determine if this method ever depends on the state of the scope stack.
+    let mut uses_scope_ops = false;
 
-    // Now actually run the peephole optimizer.
+    for op in ops {
+        match op.get() {
+            Op::GetScopeObject { .. }
+            | Op::SetGlobalSlot { .. }
+            | Op::FindProperty { .. }
+            | Op::FindPropStrict { .. }
+            | Op::NewFunction { .. }
+            | Op::NewClass { .. }
+            | Op::PopScope => {
+                uses_scope_ops = true;
+                break;
+            }
+
+            _ => {}
+        }
+    }
+
+    // If the scope stack is never read from, only written to, we can replace
+    // `PushScope` ops with `Pop`. This allows the peephole optimizer to emit
+    // slightly better code.
+    for current_op in ops {
+        match current_op.get() {
+            Op::PushScope {
+                input_not_null: true,
+            } if !uses_scope_ops => {
+                current_op.set(Op::Pop);
+            }
+            _ => {}
+        }
+    }
+
+    // Now actually run the main peephole optimizer.
     let mut last_op: Option<&'a Cell<Op<'_>>> = None;
 
     for (i, current_op) in ops.iter().enumerate() {
@@ -140,104 +166,4 @@ pub fn postprocess_peephole<'a>(
             last_op = Some(current_op);
         }
     }
-
-    // Gather some more information...
-    let mut uses_scope_ops = false;
-
-    for op in ops {
-        match op.get() {
-            Op::GetScopeObject { .. }
-            | Op::SetGlobalSlot { .. }
-            | Op::FindProperty { .. }
-            | Op::FindPropStrict { .. }
-            | Op::NewFunction { .. }
-            | Op::NewClass { .. } => {
-                uses_scope_ops = true;
-                break;
-            }
-
-            _ => {}
-        }
-    }
-
-    // Eliminate the `getlocal0` and `pushscope` ops at the beginning of the
-    // method, if possible.
-    if let Some((getlocal0_pos, pushscope_pos)) = simple_scope_op_positions
-        && !uses_scope_ops
-    {
-        ops[getlocal0_pos].set(Op::Nop);
-        ops[pushscope_pos].set(Op::Nop);
-    }
-}
-
-/// Checks if the method fits the following pattern:
-///
-/// ```text
-/// [Debug/DebugFile/DebugLine] zero or more times
-/// GetLocal { index: 0 }
-/// [Debug/DebugFile/DebugLine] zero or more times
-/// PushScope
-/// ...
-/// ```
-///
-/// along with the following conditions:
-/// * No jumps to that initial PushScope opcode, or anything before it
-/// * No additional scope-related opcodes (PushScope, PushWith, PopScope)
-///
-/// If all these conditions are met, the method will return the positions of
-/// the GetLocal { 0 } and PushScope ops. Otherwise, it will return None.
-fn simple_scope_structure(
-    ops: &[Cell<Op<'_>>],
-    jump_targets: &HashSet<usize>,
-) -> Option<(usize, usize)> {
-    let mut getlocal0_pos = None;
-    for (i, op) in ops.iter().enumerate() {
-        match op.get() {
-            // Ignore any initial debug opcodes
-            Op::Debug { .. } | Op::DebugFile { .. } | Op::DebugLine { .. } => {}
-            // Look for an initial getlocal0
-            Op::GetLocal { index: 0 } => {
-                getlocal0_pos = Some(i);
-                break;
-            }
-            // Anything else doesn't fit the pattern, so give up
-            _ => return None,
-        }
-    }
-    // Give up if we didn't find it
-    let getlocal0_pos = getlocal0_pos?;
-
-    let mut pushscope_pos = None;
-    for (i, op) in ops.iter().enumerate().skip(getlocal0_pos + 1) {
-        match op.get() {
-            // Ignore any debug opcodes
-            Op::Debug { .. } | Op::DebugFile { .. } | Op::DebugLine { .. } => {}
-            // Look for a pushscope
-            Op::PushScope => {
-                pushscope_pos = Some(i);
-                break;
-            }
-            // Anything else doesn't fit the pattern, so give up
-            _ => return None,
-        }
-    }
-    // Give up if we didn't find it
-    let pushscope_pos = pushscope_pos?;
-
-    for i in 0..=pushscope_pos {
-        if jump_targets.contains(&i) {
-            return None;
-        }
-    }
-
-    for op in &ops[pushscope_pos + 1..] {
-        match op.get() {
-            Op::PushScope | Op::PushWith | Op::PopScope => {
-                return None;
-            }
-            _ => {}
-        }
-    }
-
-    Some((getlocal0_pos, pushscope_pos))
 }
