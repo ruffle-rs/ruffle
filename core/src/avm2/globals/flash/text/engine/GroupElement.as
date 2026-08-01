94b2d10db16e24fd3635bc9df4d61a04879909e0
Lord-McSweeney <batuharmanci@gmail.com>
2026-07-31 17:59:01 -0700

    avm2: Make GroupElement store its data in `ElementData`
    This makes it much easier to access it in the future

--- core/src/avm2/globals/flash/text/engine/GroupElement.as
+++ core/src/avm2/globals/flash/text/engine/GroupElement.as
@@ -3,8 +3,6 @@ package flash.text.engine {
 
     [API("662")]
     public final class GroupElement extends ContentElement {
-        internal var _elements = null;
-
         public function GroupElement(
             elements:Vector.<ContentElement> = null,
             elementFormat:ElementFormat = null,
@@ -12,85 +10,56 @@ package flash.text.engine {
             textRotation:String = "rotate0"
         ) {
             super(elementFormat, eventMirror, textRotation);
+
             this.init();
             this.setElements(elements);
         }
 
         private native function init():void;
 
-        public function get elementCount():int {
-            return this._elements.length;
-        }
+        public native function get elementCount():int;
 
-        public function getElementAt(index:int):ContentElement {
-            if (index < 0 || index >= this._elements.length) {
-                throw new RangeError("Error #2006: The supplied index is out of bounds.", 2006);
+        public native function getElementAt(index:int):ContentElement;
+
+        public function getElementIndex(testElement:ContentElement):int {
+            for (var i:int = 0; i < this.elementCount; i ++) {
+                var element:ContentElement = this.getElementAt(i);
+                if (testElement == element) {
+                    return i;
+                }
             }
-            return this._elements[index];
+
+            return -1;
         }
 
-        public function getElementIndex(element:ContentElement):int {
-            return this._elements.indexOf(element);
-        }
+        public native function setElements(elements:Vector.<ContentElement>):void;
 
-        public function setElements(elements:Vector.<ContentElement>):void {
-            if (elements == null) {
-                this._elements = new Vector.<ContentElement>();
-            } else {
-                this._elements = elements.AS3::concat();
-            }
-        }
-
-        public function replaceElements(
+        public native function replaceElements(
             beginIndex:int,
             endIndex:int,
             newElements:Vector.<ContentElement>
-        ):Vector.<ContentElement> {
-            // This some sort of special case that doesn't throw.
-            if (beginIndex == endIndex && newElements == null) {
-                return null;
-            }
+        ):Vector.<ContentElement>;
 
-            if (beginIndex < 0 || beginIndex > this._elements.length ||
-                endIndex < 0 || endIndex > this._elements.length) {
-                throw new RangeError("Error #2006: The supplied index is out of bounds.", 2006);
-            }
-
-            var old = this._elements.AS3::splice(beginIndex, endIndex - beginIndex);
-            if (newElements) {
-                for (var i = 0; i < newElements.length; i++) {
-                    this._elements.AS3::insertAt(beginIndex + i, newElements[i]);
-                }
-            }
-            return old;
-        }
-
+        // TODO: This method should probably be implemented in native code
         public function splitTextElement(elementIndex:int, splitIndex:int):TextElement {
-            var element = getElementAt(elementIndex);
+            var element:ContentElement = this.getElementAt(elementIndex);
             if (!(element instanceof TextElement)) {
                 throw new ArgumentError("Error #2004: One of the parameters is invalid.", 2004);
             }
 
-            var text = element.text;
+            var text:String = element.text;
             if (splitIndex < 0 || splitIndex >= text.length) {
                 throw new RangeError("Error #2006: The supplied index is out of bounds.", 2006);
             }
-
             element.text = text.slice(0, splitIndex);
-            var newTextElement = new TextElement(text.slice(splitIndex));
-            this._elements.AS3::insertAt(elementIndex + 1, newTextElement);
+
+            var newTextElement:ContentElement = new TextElement(text.slice(splitIndex));
+            this.replaceElements(
+                elementIndex + 1,
+                elementIndex + 1,
+                Vector.<ContentElement>([newTextElement])
+            );
             return newTextElement;
-        }
-
-        // FIXME: This is wrong, FP doesn't do an override of `get text` in GroupElement
-        override public function get text():String {
-            var resultingText:String = "";
-
-            for (var i = 0; i < this._elements.length; i ++) {
-                resultingText += this._elements[i].text;
-            }
-
-            return resultingText;
         }
     }
 }
--- core/src/avm2/globals/flash/text/engine/group_element.rs
+++ core/src/avm2/globals/flash/text/engine/group_element.rs
@@ -1,7 +1,9 @@
 use crate::avm2::activation::Activation;
-use crate::avm2::error::Error;
-use crate::avm2::object::ElementData;
+use crate::avm2::error::{Error, Error2004Type, Error2006Type, make_error_2004, make_error_2006};
+use crate::avm2::object::{ContentElementObject, ElementData, Object, VectorObject};
+use crate::avm2::parameters::ParametersExt;
 use crate::avm2::value::Value;
+use crate::avm2::vector::VectorStorage;
 
 pub fn init<'gc>(
     activation: &mut Activation<'_, 'gc>,
@@ -20,4 +22,182 @@ pub fn init<'gc>(
     };
 
     Ok(Value::Undefined)
+}
+
+pub fn get_element_count<'gc>(
+    _activation: &mut Activation<'_, 'gc>,
+    this: Value<'gc>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error<'gc>> {
+    let this = this
+        .as_object()
+        .unwrap()
+        .as_content_element_object()
+        .unwrap();
+
+    let data = this.element_data();
+    let ElementData::Group { elements } = &*data else {
+        unreachable!("Data can only have been set to Group");
+    };
+
+    Ok(Value::from_usize_lossy(elements.len()))
+}
+
+pub fn get_element_at<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    this: Value<'gc>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error<'gc>> {
+    let this = this
+        .as_object()
+        .unwrap()
+        .as_content_element_object()
+        .unwrap();
+
+    let data = this.element_data();
+    let ElementData::Group { elements } = &*data else {
+        unreachable!("Data can only have been set to Group");
+    };
+
+    let index = args.get_i32(0);
+
+    let index = usize::try_from(index)
+        .map_err(|_| make_error_2006(activation, Error2006Type::RangeError))?;
+
+    let element = elements
+        .get(index)
+        .copied()
+        .ok_or_else(|| make_error_2006(activation, Error2006Type::RangeError))?;
+
+    Ok(element.into())
+}
+
+pub fn set_elements<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    this: Value<'gc>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error<'gc>> {
+    let this = this
+        .as_object()
+        .unwrap()
+        .as_content_element_object()
+        .unwrap();
+
+    let data = this.element_data();
+    let ElementData::Group { elements } = &*data else {
+        unreachable!("Data can only have been set to Group");
+    };
+
+    let element_count = elements.len();
+
+    drop(data);
+
+    let new_elements = args
+        .try_get_object(0)
+        .map(|o| o.as_vector_object().unwrap());
+
+    replace_elements_impl(activation, this, 0, element_count, new_elements)?;
+
+    Ok(Value::Undefined)
+}
+
+pub fn replace_elements<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    this: Value<'gc>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error<'gc>> {
+    let this = this
+        .as_object()
+        .unwrap()
+        .as_content_element_object()
+        .unwrap();
+
+    let begin_index = args.get_i32(0);
+    let end_index = args.get_i32(1);
+
+    let new_elements = args
+        .try_get_object(2)
+        .map(|o| o.as_vector_object().unwrap());
+
+    let begin_index = usize::try_from(begin_index)
+        .map_err(|_| make_error_2006(activation, Error2006Type::RangeError))?;
+
+    let end_index = usize::try_from(end_index)
+        .map_err(|_| make_error_2006(activation, Error2006Type::RangeError))?;
+
+    // This is some sort of special case that doesn't throw.
+    if begin_index == end_index && new_elements.is_none() {
+        Ok(Value::Null)
+    } else {
+        let result = replace_elements_impl(activation, this, begin_index, end_index, new_elements)?;
+
+        Ok(result.into())
+    }
+}
+
+fn replace_elements_impl<'gc>(
+    activation: &mut Activation<'_, 'gc>,
+    this: ContentElementObject<'gc>,
+    begin_index: usize,
+    end_index: usize,
+    new_elements: Option<VectorObject<'gc>>,
+) -> Result<VectorObject<'gc>, Error<'gc>> {
+    let mut data = this.element_data_mut(activation.gc());
+    let ElementData::Group { elements } = &mut *data else {
+        unreachable!("Data can only have been set to Group");
+    };
+
+    if begin_index > elements.len() || end_index > elements.len() {
+        return Err(make_error_2006(activation, Error2006Type::RangeError));
+    }
+
+    let new_elements = new_elements.map(|o| o.storage());
+
+    let new_elements = if let Some(new_elements) = new_elements {
+        // TODO perform more validation:
+        // - Can't insert the same element twice
+        // - Can't create a loop
+        new_elements
+            .iter()
+            .map(|e| {
+                let content_element = e.as_object().and_then(|o| o.as_content_element_object());
+
+                let Some(content_element) = content_element else {
+                    // There was a `null` value in the vector
+                    return Err(make_error_2004(activation, Error2004Type::ArgumentError));
+                };
+
+                if Object::ptr_eq(content_element, this) {
+                    // Can't insert a group element into itself
+                    return Err(make_error_2004(activation, Error2004Type::ArgumentError));
+                }
+
+                let data = content_element.element_data();
+                if matches!(&*data, ElementData::Invalid) {
+                    // Can't insert an instance of a user-defined subclass of
+                    // `ContentElement` into a group element
+                    return Err(make_error_2004(activation, Error2004Type::ArgumentError));
+                }
+
+                Ok(content_element)
+            })
+            .collect::<Result<Vec<_>, _>>()?
+    } else {
+        // Passing `null` results in no elements
+        Vec::new()
+    };
+
+    let removed_elements = elements
+        .splice(begin_index..end_index, new_elements)
+        .map(Value::from)
+        .collect::<Vec<_>>();
+
+    // Return the elements that were removed
+    let new_vs = VectorStorage::from_values(
+        removed_elements,
+        false,
+        Some(activation.avm2().class_defs().contentelement),
+    );
+
+    Ok(VectorObject::from_vector(new_vs, activation))
 }
