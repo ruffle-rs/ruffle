a5d3aa2a1b676589751b56a7250f17de3ddae5d0
Lord-McSweeney <batuharmanci@gmail.com>
2026-08-13 20:22:29 -0700

    debug_ui/avm2: Improve list of Domains
    Display Classes directly instead of ClassObjects, and remove the
    now-unnecessary dropdown for each Class.

--- core/src/debug_ui/domain.rs
+++ core/src/debug_ui/domain.rs
@@ -1,11 +1,10 @@
-use egui::{CollapsingHeader, TextEdit, Ui, Window, collapsing_header::CollapsingState};
+use crate::avm2::Domain;
+use crate::context::UpdateContext;
+use crate::debug_ui::Message;
+use crate::debug_ui::avm2_class::class_name;
+use crate::debug_ui::handle::{ClassHandle, DomainHandle};
 
-use crate::{avm2::Domain, context::UpdateContext};
-
-use super::{
-    Message,
-    handle::{AVM2ObjectHandle, DomainHandle},
-};
+use egui::{TextEdit, Ui, Window, collapsing_header::CollapsingState};
 
 #[derive(Debug, Default)]
 pub struct DomainListWindow {
@@ -64,35 +63,18 @@ impl DomainListWindow {
                     classes.sort_by_key(|(name, _, _)| *name);
 
                     for (class_index, (_, _, class)) in classes.iter().enumerate() {
-                        let class_name = class.name().to_qualified_name(context.gc());
-                        if !class_name.to_string().to_ascii_lowercase().contains(search) {
+                        let name = class_name(context.gc(), **class);
+                        if !name.to_string().to_ascii_lowercase().contains(search) {
                             continue;
                         }
 
-                        let class_id =
-                            format!("class_{}_{}_{:p}", depth, class_index, class.as_ptr());
-
-                        ui.push_id(class_id, |ui| {
-                            CollapsingHeader::new(format!("Class {class_name}")).show(ui, |ui| {
-                                for (obj_index, class_obj) in
-                                    class.class_objects().iter().enumerate()
-                                {
-                                    ui.push_id(
-                                        format!("class_obj_{}_{}", class_index, obj_index),
-                                        |ui| {
-                                            let button = ui.button(format!("{class_obj:?}"));
-                                            if button.clicked() {
-                                                messages.push(Message::TrackAVM2Object(
-                                                    AVM2ObjectHandle::new(
-                                                        context,
-                                                        (*class_obj).into(),
-                                                    ),
-                                                ));
-                                            }
-                                        },
-                                    );
-                                }
-                            });
+                        ui.push_id(format!("class_obj_{class_index}"), |ui| {
+                            let button = ui.button(name);
+                            if button.clicked() {
+                                messages.push(Message::TrackAVM2Class(ClassHandle::new(
+                                    context, **class,
+                                )));
+                            }
                         });
                     }
 
