c4b4a8877869f09c6e347d95c944f8add6a105ad
Kamil Jarosz <kjarosh256@gmail.com>
2026-08-15 23:40:28 +0200

    desktop: Gate FreeType behind a feature
    FreeType remains a Linux-specific dependency but now is put behind the
    freetype feature, so that Ruffle can be built without it on Linux.

--- frontend-utils/src/backends/ui.rs
+++ frontend-utils/src/backends/ui.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "freetype")]
+#[cfg(all(target_os = "linux", feature = "freetype"))]
 mod freetype_font_renderer;
 
-#[cfg(feature = "freetype")]
+#[cfg(all(target_os = "linux", feature = "freetype"))]
 pub use freetype_font_renderer::FreetypeFontRenderer;
