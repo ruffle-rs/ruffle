02c2ec6e2f56f8ef9f26c42780fb1c9be6924701
Nathan Adams <dinnerbone@dinnerbone.com>
2026-09-10 01:59:26 +0200

    wgpu: Increase dynamic vertex buffer size to *4 as we always have 4 vertices per object, not 1

--- render/wgpu/src/dynamic_transforms.rs
+++ render/wgpu/src/dynamic_transforms.rs
@@ -25,7 +25,7 @@ impl DynamicTransforms {
         });
         let vertex_buffer = descriptors.device.create_buffer(&wgpu::BufferDescriptor {
             label: None,
-            size: (mem::size_of::<PosUvVertex>() as u64 * ESTIMATED_OBJECTS_PER_CHUNK)
+            size: (mem::size_of::<PosUvVertex>() as u64 * 4 * ESTIMATED_OBJECTS_PER_CHUNK)
                 .min(descriptors.limits.max_buffer_size),
             usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
             mapped_at_creation: false,
