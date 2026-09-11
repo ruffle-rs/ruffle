f55eba0b75d80fa2d006fbbc849e548e82c46b21
Daniel Jacobs <danielhunterjacobs@gmail.com>
2026-09-11 14:41:11 -0400

    web: Make necessary config changes for TS 6 upgrade

--- web/packages/core/src/internal/player/inner.tsx
+++ web/packages/core/src/internal/player/inner.tsx
@@ -57,19 +57,6 @@ declare global {
     interface AudioSession {
         type?: string;
     }
-    // See https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1615
-    type OrientationLockType =
-        | "any"
-        | "landscape"
-        | "landscape-primary"
-        | "landscape-secondary"
-        | "natural"
-        | "portrait"
-        | "portrait-primary"
-        | "portrait-secondary";
-    interface ScreenOrientation extends EventTarget {
-        lock(orientation: OrientationLockType): Promise<void>;
-    }
 }
 
 /**
